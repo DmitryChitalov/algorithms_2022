a159957b3e90c1c2f47e8eb4cc1a561b62de4050
galina <galina-olefirova@mail.ru>
2021-12-13 22:36:14 +0300

    completed the task 2

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -20,9 +20,9 @@
 
 def min_val_1(list_1):
     min_num_1 = list_1[0]                                           # O(1)
-    for i in list_1                                                 # O(N)
-        for k in range(list_1.index(i) + 1, len(list_1) - 1, 1)     # O(N)
-            if min_num_1 > list_1[k]:                               # O(len(k))
+    for i in list_1:                                                # O(N)
+        for k in range(list_1.index(i) + 1, len(list_1) - 1, 1):    # O(N)
+            if min_num_1 > list_1[k]:                               # O(1)
                 min_num_1 = list_1[k]                               # O(1)
         return min_num_1                                            # O(1)
 
@@ -36,7 +36,7 @@ print(min_val_1(lst))
 def min_val_2(list_2):
     min_num_2 = list_2[0]                # O(1)
     for y in list_2:                     # O(N)
-        if y < min_num_2:                # O(len(y))
+        if y < min_num_2:                # O(1)
             min_num_2 = y                # O(1)
     return min_num_2                     # O(1)
 
