ffda7cac12fdc54f7bfdf1a846da381e78bd7706
Nick Milkin <nickita.milckin@yandex.ru>
2021-12-15 17:02:36 +0100

    ДЗ к уроку 1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,47 @@
 в массив стопок (lst = [[], [], [], [],....]) либо созданием объекта
 класса-стек в самом же классе.
 """
+
+
+class StackClass:
+    def __init__(self, limit):
+        self.elems = [[]]
+        self.limit = limit
+
+    def is_empty(self):
+        return self.elems == []
+
+    def push_in(self, el):
+        """Предполагаем, что верхний элемент стека находится в конце списка"""
+        for i in range(len(self.elems)):
+            if len(self.elems[i]) < self.limit:
+                self.elems[i].append(el)
+                if len(self.elems[i]) == self.limit:
+                    self.elems.append([])
+                break
+
+    def pop_out(self):
+        return self.elems.pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        return len(self.elems)
+
+    def print_stack(self):
+        print(self.elems)
+
+
+stack1 = StackClass(2)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+stack1.push_in(23)
+
+stack1.print_stack()
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,64 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+    def print_queue(self):
+        print(self.elems)
+
+
+class Board:
+    def __init__(self):
+        self.types = {'Базовая очередь': QueueClass(), 'Решенные задачи': QueueClass(), 'На доработку':  QueueClass()}
+
+    def add_task(self, task):
+        self.types['Базовая очередь'].to_queue(task)
+
+    def move(self, from_q, to_q):
+        self.types[to_q].to_queue(self.types[from_q].from_queue())
+
+    def solve(self):
+        self.move('Базовая очередь', 'Решенные задачи')
+
+    def think(self):
+        self.move('Базовая очередь', 'На доработку')
+
+    def finish(self):
+        self.move('На доработку', 'Решенные задачи')
+
+    def print_tasks(self):
+        for i in self.types.keys():
+            print(i)
+            self.types[i].print_queue()
+
+
+tasks = Board()
+tasks.add_task('task_1')
+tasks.add_task('task_2')
+tasks.add_task('task_3')
+tasks.add_task('task_4')
+tasks.add_task('task_5')
+tasks.add_task('task_6')
+tasks.solve()
+tasks.think()
+tasks.finish()
+tasks.think()
+tasks.think()
+
+tasks.print_tasks()
