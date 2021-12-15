c1be14a1ba6f9020f7a10252baf6c146728ffce0
galina <galina-olefirova@mail.ru>
2021-12-15 22:04:16 +0300

    completed the task

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -14,3 +14,43 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --код с нуля писать не нужно, требуется доработать пример с урока
 """
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+def pal_checker(string):
+    deque = DequeClass()
+    for char in string.replace(' ', ''):
+     deque.add_to_front(char)
+
+    keepGoing = True
+    while deque.size() > 1 and keepGoing:
+        first = deque.remove_from_front()
+        last = deque.remove_from_rear()
+        if first != last:
+            keepGoing = False
+
+    return keepGoing
+
+
+print(pal_checker("молоко делили ледоколом"))
