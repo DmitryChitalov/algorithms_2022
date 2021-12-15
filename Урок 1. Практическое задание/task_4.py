20bd02b9e924b655c6855ba91197a785c58e75e5
KTo <kto@lovetou.ru>
2021-12-15 10:04:34 +0700

    Домашнее задание урок 1

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -19,4 +19,116 @@
 который вы придумаете, например, реализовать словарь.
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
+
+
+ВЫВОДЫ:
+По критеирю "удобство использования" на мой взгляд самое эффективное первое решение,
+т.к. оно усточиво в неверному вводу
+
 """
+
+
+# O(n)
+from pip._internal.network.session import user_agent
+from setuptools.package_index import user_agent
+
+
+def grant_access1(user_name, vault):
+    """
+    :param user_name: user name
+    :param vault: password storage
+    :return: true if granted, otherwise false
+    """
+    user_info = vault.get(user_name)
+    if not user_info:
+        print('Пользователь не найден.')
+        return False
+
+    password = input('Введите пароль:')
+    if password != user_info['password']:
+        print('Пароль неверный')
+        return False
+
+    if not user_info['active']:
+        answer_yes = ['yes', 'y', 'yep', 'д', 'да']
+        answer_no = ['no', 'n', 'not', 'н', 'нет']
+        while True:
+            action = input('Пользователь не активирован, активировать сейчас? (д/н)')
+            if action in answer_no:
+                print('Доступ запрещен, активация обязательна')
+                return False
+            if action in answer_yes:
+                user_info['active'] = True
+                print('Активация успешна')
+                break
+
+    return True
+
+
+# O(1)
+def grant_access2(user_name, vault):
+    """
+    :param user_name: user name
+    :param vault: password storage
+    :return: true if granted, otherwise false
+    """
+    user_info = vault.get(user_name)
+    if not user_info:
+        print('Пользователь не найден.')
+        return False
+
+    password = input('Введите пароль:')
+    if password != user_info['password']:
+        print('Пароль неверный')
+        return False
+
+    if not user_info['active']:
+        answer_yes = ['yes', 'y', 'yep', 'д', 'да']
+        answer_no = ['no', 'n', 'not', 'н', 'нет']
+        action = input('Пользователь не активирован, активировать сейчас? (д/н)')
+        if action in answer_no:
+            print('Доступ запрещен, активация обязательна')
+            return False
+        if action in answer_yes:
+            user_info['active'] = True
+            print('Активация успешна')
+            return True
+
+        print('Ответ не распознан, доступ запрещен')
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+
+    vault = {
+        'user_name1': {'password': '1', 'active': True},
+        'user_name2': {'password': '2', 'active': False}
+    }
+
+    # Пример использования
+
+    print('Вариант 1', end='\n\n')
+
+    user_name = "user_name1"
+    print('Пользователь:', user_name)
+    print(grant_access1(user_name, vault))
+    user_name = "user_name2"
+    print('Пользователь:', user_name)
+    print(grant_access1(user_name, vault))
+    user_name = "user_name3"
+    print('Пользователь:', user_name)
+    print(grant_access1(user_name, vault))
+
+    print()
+    print('Вариант 2', end='\n\n')
+    user_name = "user_name1"
+    print('Пользователь:', user_name)
+    print(grant_access2(user_name, vault))
+    user_name = "user_name2"
+    print('Пользователь:', user_name)
+    print(grant_access2(user_name, vault))
+    user_name = "user_name3"
+    print('Пользователь:', user_name)
+    print(grant_access2(user_name, vault))
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,143 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        if self.is_empty():
+            raise ValueError('Queue is empty')
+
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+    def __str__(self):
+        return ', '.join(map(str, self.elems))
+
+
+class TaskBoard:
+
+    def __init__(self):
+        self.boards = {
+            'Поступившие': QueueClass(),
+            'В работе': QueueClass(),
+            'На тестировании': QueueClass(),
+            'На доработку': QueueClass(),
+            'Выполненные': QueueClass()
+        }
+
+    def q2q(self, name_from, name_to):
+        self.boards[name_to].to_queue(self.boards[name_from].from_queue())
+
+    def new_task(self, description):
+        self.boards['Поступившие'].to_queue(description)
+
+    def to_work(self):
+        if not self.boards['На доработку'].is_empty():
+            self.q2q('На доработку', 'В работе')
+        else:
+            self.q2q('Поступившие', 'В работе')
+
+    def to_test(self):
+        self.q2q('В работе', 'На тестировании')
+
+    def to_rework(self):
+        self.q2q('На тестировании', 'На доработку')
+
+    def rework_to_work(self):
+        self.q2q('На доработку', 'В работе')
+
+    def task_completed(self):
+        self.q2q('На тестировании', 'Выполненные')
+
+    def show_boards(self):
+        print('Текущее состояние дел:')
+        for k, v in self.boards.items():
+            print('Доска:', k)
+            print(' Задачи:', v)
+        print()
+
+
+# Пример использования
+if __name__ == '__main__':
+
+    # Цель: спасти семью от жажды
+    # Гипотеза: молоко должно помочь
+    # Задача: купить молоко
+
+    # Декомпозиция
+    task_board = TaskBoard()
+    task_board.new_task('Выйти из дома')
+    task_board.new_task('Дойти до магазина')
+    task_board.new_task('Купить молока')
+    task_board.new_task('Вернуться целым и не вредимым')
+
+    task_board.show_boards()
+
+    # Запускаем на выполнение
+    task_board.to_work()
+    task_board.to_work()
+    task_board.to_work()
+    task_board.to_work()
+
+    # Лишняя д.б. эксцепшн
+    try:
+        task_board.to_work()
+    except ValueError as e:
+        print('Что-то пошло не так по причине:', e)
+        print('А нет, все норм! Так и должно быть')
+        print()
+
+    task_board.show_boards()
+
+    # Передаем на тестирование
+    task_board.to_test()
+    task_board.to_test()
+    task_board.to_test()
+    task_board.to_test()
+
+    task_board.show_boards()
+
+    # Не то купил) е мае переделывай
+    task_board.to_rework()
+    task_board.to_rework()
+    task_board.to_rework()
+    task_board.to_rework()
+
+    task_board.show_boards()
+
+    # Снова в бой
+    task_board.to_work()
+    task_board.to_work()
+    task_board.to_work()
+    task_board.to_work()
+
+    task_board.show_boards()
+
+    # Чо сразу не сказала какое надо?! Вторая попытка
+    task_board.to_test()
+    task_board.to_test()
+    task_board.to_test()
+    task_board.to_test()
+
+    task_board.show_boards()
+
+    # Все норм!
+    task_board.task_completed()
+    task_board.task_completed()
+    task_board.task_completed()
+    task_board.task_completed()
+
+    task_board.show_boards()
+    # Задача выполнена! Гипотеза подтвердилась, цель достигнута - семья спасена.
