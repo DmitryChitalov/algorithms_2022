cf4811fe2e9dae5b5dae66101095563a7d96d50c
Dmitry Chitalov <35995629+DmitryChitalov@users.noreply.github.com>
2021-12-17 14:32:22 +0500

    Update task_3.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -13,3 +13,27 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 -- нельзя использовать встроенные функции min() и sort()
 """
+
+
+# Сложность линейная O(n)
+def minimal_number1(lst):
+    min_num = lst[0]
+    for i in num_list:
+        if i < min_num:
+            min_num = i
+    return min_num
+
+
+# Сложность квадратичная O(n**2)
+def minimal_number2(lst):
+    min_num = 0
+    for n in num_list:
+        for i in range(len(num_list)):
+            if n > num_list[i]:
+                min_num = num_list[i]
+    return min_num
+
+
+num_list = [750, 378, 430, 545, 359, 725, 943, 373, 143, 483, 215, 652, 194, 30, 841, 978, 902, 702, 824, 622, 289, 105]
+print(minimal_number1(num_list))
+print(minimal_number2(num_list))
