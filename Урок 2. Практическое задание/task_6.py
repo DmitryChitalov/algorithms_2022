a1ef6b937a8f0b4b72787df532a37f7b19fdce81
Aleksey Nikolskii <aleksey.nikolskiy4@rsce.ru>
2021-12-17 15:57:57 +0300

    Сделано задание 6

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,35 @@
 
 Решите через рекурсию. Решение через цикл не принимается.
 """
+
+import random
+
+
+def game(minimum, maximum, attempts, answer=''):
+    if answer == '':
+        print('Угадайте число')
+        answer = random.randint(minimum, maximum)
+    user_answ = int(input("Осталось попыток: %s. Число находится в диапазоне от %s до %s , введите ответ " % (
+        attempts, minimum, maximum)))
+    attempts -= 1
+    if user_answ > answer:
+        print("Неправильно, ваш ответ больше загаданного")
+        if attempts == 0:
+            print('Попытки закончились, ответ был %s' % answer)
+            exit()
+        game(minimum, user_answ, attempts, answer)
+    elif user_answ < answer:
+        print("Неправильно, ваш ответ меньше загаданного")
+        if attempts == 0:
+            print('Попытки закончились, ответ был %s' % answer)
+            exit()
+        game(user_answ, maximum, attempts, answer)
+    elif user_answ == answer:
+        print('ПРАВИЛЬНО!')
+        exit()
+
+
+mins = 1
+maxs = 100
+atte = 2
+game(mins, maxs, atte)
