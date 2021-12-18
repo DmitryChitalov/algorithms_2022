1b173a1de34c03ef2a4085dace9a6425d1a77ff4
HP <nikolajkostukovic7@gmail.com>
2021-12-18 17:04:36 +0300

    algorimik.py

--- Урок 1. Практическое задание/algorimik.py
+++ Урок 1. Практическое задание/algorimik.py
@@ -135,5 +135,189 @@ while len(max_profit_2) < 3:                                                # O(
 
 print(max_profit_2)
 
-# Вывод: Первый способ выполняется быстрее так как самым "тяжелым" алгоритмом,
+# Вывод: Первый вариант выполняется быстрее так как самым "тяжелым" алгоритмом,
 # является функция SORT() = O (n log n)
+
+
+#V
+
+"""
+Задание 5.
+Задание на закрепление навыков работы со стеком
+Реализуйте структуру "стопка тарелок".
+Мы можем складывать тарелки в стопку и при превышении некоторого значения
+нужно начать складывать тарелки в новую стопку.
+Структура должна предусматривать наличие нескольких стеков.
+Создание нового стека происходит при достижении предыдущим стеком порогового значения.
+Реализуйте по аналогии с примером, рассмотренным на уроке, необходимые методы,
+для реализации это структуры, добавьте новые методы (не рассмотренные в примере с урока)
+для реализации этой задачи.
+После реализации структуры, проверьте ее работу на различных сценариях
+Подсказка:
+Отдельне стопки можно реализовать через:
+1) созд-е экземпляров стека (если стопка - класс)
+или
+2) lst = [[], [], [], [],....]
+Примечание: в этом задании вспомните ваши знания по работе с ООП
+и опирайтесь на пример урока
+Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
+Задание творческое. Здесь нет жестких требований к выполнению.
+"""
+
+class StackClass:
+    def __init__(self):
+        self.elems = [[],[],[],[],[]]
+
+    def push_in(self, el):
+        """ верхний элемент стека находится в конце списка"""
+        for i in range(0, len(self.elems) - 1, 1):
+            if len(self.elems[i]) < 5:
+                self.elems[i].append(el)
+                break
+
+    def pop_out(self):
+        return self.elems.pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        return len(self.elems)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.max = None
+
+    def push(self, item):
+        self.stack.append(item)
+        if len(self.stack) == 1 or item > self.max:
+            self.max = item
+
+if __name__ == '__main__':
+    stack_1 = StackClass()
+    i = 0
+    while i < 18:
+        stack_1.push_in(1+i)
+        i += 1
+
+    print(stack_1.elems)
+
+#vi
+"""
+Задание 6.
+Задание на закрепление навыков работы с очередью
+Реализуйте структуру "доска задач".
+Структура должна предусматривать наличие несольких очередей задач, например,
+1) базовой, откуда задачи берутся, решаются и отправляются в список решенных
+2) очередь на доработку, когда нерешенные задачи из первой очереди отправляются
+на корректировку решения
+После реализации структуры, проверьте ее работу на различных сценариях
+Примечание: в этом задании вспомните ваши знания по работе с ООП
+и опирайтесь на пример урока
+Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
+Задание творческое. Здесь нет жестких требований к выполнению.
+"""
+
+class QueueClass:
+    def __init__(self):
+        self.base = []
+        self.solved = []
+        self.revision = []
+
+
+    def is_empty(self):
+        return self.base == []
+
+    def to_queue(self, item):
+        self.base.insert(0, item)
+
+    def from_queue(self):
+        return self.base.pop()
+
+    def to_solved(self):
+        self.solved.insert(0, self.base.pop())
+
+    def to_revision(self):
+        self.revision.insert(0, self.base.pop())
+
+    def return_from_revision_to_base(self):
+        self.base.insert(0, self.revision.pop())
+
+    def size(self):
+        return len(self.base)
+
+
+if __name__ == '__main__':
+    qc_obj = QueueClass()
+
+    qc_obj.to_queue('my_obj')
+    qc_obj.to_queue(4)
+    qc_obj.to_queue(True)
+
+    print(qc_obj.base)
+    qc_obj.to_solved()
+    print(qc_obj.solved)
+    qc_obj.to_revision()
+    print(qc_obj.base)
+    print(qc_obj.revision)
+    qc_obj.return_from_revision_to_base()
+    print(qc_obj.revision)
+    print(qc_obj.base)
+
+# vii
+
+"""
+Задание 7.
+Задание на закрепление навыков работы с деком
+В рассмотренном на уроке листинге есть один недостаток
+Приведенный код способен "обработать" только строку без пробелов, например, 'топот'
+Но могут быть и такие палиндромы, как 'молоко делили ледоколом'
+Вам нужно доработать программу так, чтобы она могла выполнить проверку на палиндром
+и в таких строках (включающих пробелы)
+Примечание:
+Вам не нужно писать код с нуля. Вам нужно доработать пример с урока.
+"""
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+def pal_checker(string):
+    string = string.replace(" ", "")
+    dc_obj = DequeClass()
+
+    for el in string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+if __name__ == '__main__':
+    print(pal_checker("молоко делили ледоколом"))
