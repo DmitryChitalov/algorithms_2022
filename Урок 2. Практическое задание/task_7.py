b9817f24a27d7dfc24db8cba863e257ae09117d9
Arthur <Arthur.tsoktoev@gmail.com>
2021-12-18 13:55:21 +0300

    Выполнено

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -12,3 +12,21 @@
 
 Решите через рекурсию. Решение через цикл не принимается.
 """
+
+def check_left(num):
+    if num == 1:
+        return num
+    else:
+        return num + check_left(num-1)
+
+def check_right(num):
+    return num * (num + 1) / 2
+
+try:
+    num = int(input('Введите число: '))
+    if check_left(num) == check_right(num):
+        print(f'Числа {check_left(num)} и {int(check_right(num))} равны')
+    else:
+        print(f'Числа {check_left(num)} и {int(check_right(num))} не равны')
+except ValueError:
+    print('Вы вместо числа ввели строку (((. Исправьтесь')
