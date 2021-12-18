1381b07560ab1c016666e511f53ba75e5c38063e
Nikita <anthrowolf@ya.ru>
2021-12-18 18:14:57 +0300

    task 1

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -27,3 +27,27 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+
+def calculator():
+    operand_1 = input('Input the first operand: ')
+    operand_2 = input('Input the second operand: ')
+    operation = input('Input operator (+, -, *, / or 0 to exit): ')
+    if operation not in '/+-*0':
+        print('You inputted a wrong operator!')
+        return calculator()
+    if not operand_1.isdigit() or not operand_1.isdigit():
+        print('You inputted not a number(s)')
+        return calculator()
+    if operation == '/' and operand_2 == '0':
+        print('You cannot divide on zero')
+        return calculator()
+    result = eval(f'{int(operand_1)}{operation}{int(operand_2)}')
+    if operation == '0':
+        return result
+    print(f'Result: {result}')
+    return calculator()
+
+
+if __name__ == '__main__':
+    calculator()
