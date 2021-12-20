7b34b2b9f47f666ac1fcc10ed92cdf15d72c0be6
Leonid <simplesession@gmail.com>
2021-12-20 21:05:22 +0500

    dz_3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,23 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+from hashlib import pbkdf2_hmac
+from binascii import hexlify
+
+url_base = {}
+
+
+def url_check(url):
+    if url in url_base:
+        print(f'{url} always in base! hash is {url_base[url]}')
+    else:
+        url_base['url'] = hexlify(pbkdf2_hmac(hash_name='sha256',
+                                              password=url.encode('utf-8'),
+                                              salt='url'.encode('utf-8'),
+                                              iterations=10000))
+        print(f'{url} added to base!')
+
+
+url_check('url')
+url_check('url')
+url_check('url2')
