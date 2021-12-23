cf5ee67e65120a5fc2ea794d96361c4fb423812c
SerdyukovaNS <96206906+SerdyukovaNS@users.noreply.github.com>
2021-12-23 17:25:44 +0300

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,21 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+dict_url = {}
+salt = '1452yfdh789'
+
+
+def cache_url(url):
+    if url in dict_url.keys():
+        print(f'хеш {url} - {dict_url[url]}')
+    else:
+        dict_url[url] = hashlib.sha512(salt.encode() + url.encode()).hexdigest()
+        print(dict_url)
+
+
+cache_url('https://netflix.com')
+cache_url('https://netflix.com')
+cache_url('https://www.kinopoisk.ru')
+cache_url('https://www.kinopoisk.ru')
