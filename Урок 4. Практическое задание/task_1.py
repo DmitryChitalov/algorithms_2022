3979b55ab00472f2c5ad28ddef0708a462ab5647
mrfuu04 <mrfuu04@gmail.com>
2021-12-23 20:56:55 +0300

    task_1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,27 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [x for x in range(len(nums)) if x % 2 == 0]
+
+
+setup = 'from __main__ import '
+my_list = [x for x in range(10000)]
+
+print(f'Функция {func_1.__name__} выполнена за ', timeit('func_1(my_list)',
+                                                         setup=setup + 'func_1, my_list', number=10000))
+
+print(f'Функция {func_2.__name__} выполнена за ', timeit('func_2(my_list)',
+                                                         setup=setup + 'func_2,' + ' my_list', number=10000))
+
+
+"""   Итог:
+            Функция func_1 выполнена за  26.5885274
+            Функция func_2 выполнена за  15.337786299999998
+            Практически в два раза уменьшили время выполнения алгоритма по заполнению списка индексами
+            путем использования list comprehensions вместо append
+            Не смотря на то, что оба алгоритма имеют сложноть O(n) - lc с одинаковыми входными данными
+            выполняется быстрее.
+"""
