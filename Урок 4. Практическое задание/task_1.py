132577cb38db48694fa4ce6be6cc3811e6a1274c
NikBulatov <nikita_bulatov@icloud.com>
2021-12-25 20:48:16 +0300

    task 1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,13 +6,15 @@
 
 Сделайте замеры времени выполнения кода с помощью модуля timeit
 
-Попробуйте оптимизировать код, чтобы снизить время выполнения
+Попробуйте оптимизировать код, чтобы снизить время выполнения.
 Проведите повторные замеры
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
 from timeit import timeit
+
+TEST_LIST = list(range(100))
 
 
 def func_1(nums):
@@ -21,3 +23,16 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def optimize_func(array):
+    return [array[i] for i in range(len(array)) if not i % 2]
+
+
+"""
+Для оптимизации функции использовал List Comprehensions, что увеличило скорость выполнения функции почти в 2 раза.
+"""
+
+print(timeit("func_1(TEST_LIST)", globals=globals()))
+print(timeit("optimize_func(TEST_LIST)", globals=globals()))
+# print(timeit("memorize_func(TEST_LIST)", globals=globals()))
