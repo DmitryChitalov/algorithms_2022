83eeaaed1d45841d8141ebc64aca1a9c8608db2c
NikBulatov <nikita_bulatov@icloud.com>
2021-12-25 21:45:57 +0300

    task 4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,6 +9,8 @@
 Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
+from timeit import timeit
+from collections import Counter
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -21,8 +23,7 @@ def func_1():
         if count > m:
             m = count
             num = i
-    return f'Чаще всего встречается число {num}, ' \
-           f'оно появилось в массиве {m} раз(а)'
+    return f'Чаще всего встречается число {num}, оно появилось в массиве {m} раз(а)'
 
 
 def func_2():
@@ -33,9 +34,26 @@ def func_2():
 
     max_2 = max(new_array)
     elem = array[new_array.index(max_2)]
-    return f'Чаще всего встречается число {elem}, ' \
-           f'оно появилось в массиве {max_2} раз(а)'
+    return f'Чаще всего встречается число {elem}, оно появилось в массиве {max_2} раз(а)'
 
 
+def my_func_counter():
+    result = Counter(array).most_common(1)[0]
+    return f'Чаще всего встречается число {result[0]}, ' \
+           f'оно появилось в массиве {result[1]} раз(а)'
+
+
+# def my_func():
+#     return f'Чаще всего встречается число {}, ' \
+#            f'оно появилось в массиве {} раз(а)'
+
+"""
+В своей функции использую класс Counter из встроенного модуля collections. 
+Но данный метод замедляет скорость выполнения почти в 2 раза
+"""
 print(func_1())
 print(func_2())
+print(my_func_counter())
+print(timeit("func_1()", globals=globals()))
+print(timeit("func_2()", globals=globals()))
+print(timeit("my_func_counter()", globals=globals()))
