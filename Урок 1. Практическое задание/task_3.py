cab6862d27a20013eea7b30378d05b5e725b9bf2
Alexander Son <alexson2302@gmail.com>
2021-12-26 00:59:34 +0300

    lesson-1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,34 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+companies = {'Apple': 1000000, 'Microsoft': 12381173, 'Disney': 12378389, 'Sony': 384593, '20th century fox': 35934723}  #O(1)
+temp_companies = companies.copy()  #O(n)
+top_companies = {}  #O(1)
+for i in range(3):  #O(n)
+    max_income = max(temp_companies.values()) #O(n)
+    # max(temp_companies.values())
+    top_companies[list(temp_companies)[list(temp_companies.values()).index(max_income)]] = max_income  #O(1)
+    del temp_companies[list(temp_companies)[list(temp_companies.values()).index(max_income)]]  #O(n)
+
+# print(max(companies.values()), list(companies)[list(companies.values()).index(35934723)], list(companies.values()).index(35934723), type(companies))
+
+print(top_companies)
+"""
+end
+"""
+
+""" 
+Второй чуть получше 
+O(n log n)
+"""
+
+companies = {'Apple': 1000000, 'Microsoft': 12381173, 'Disney': 12378389, 'Sony': 384593, '20th century fox': 35934723} #O(1)
+sort_companies = sorted(companies.values(), reverse=True)  #O(n log n)
+print(sort_companies)
+n = 0  #O(1)
+for i in sort_companies:  #O(n)
+    n += 1 #O(1)
+    name = list(companies)[list(companies.values()).index(i)] #O(1)
+    print(name, companies[name])
+    if n == 3: #O(1)
+        break
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -20,3 +20,60 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+print('------------------------ 1-е решение ------------------------')
+
+service_db = [{'login': 'Human3000', 'password': 'asd12dasd', 'auth': True},
+              {'login': 'Guy123', 'password': 'asdad2123asd', 'auth': False},
+              {'login': 'Humblealx', 'password': 'asd83fj291d', 'auth': True}]  #O(1)
+
+'''
+#O(n)
+'''
+def auth_in_service(login, password):
+    message = f'\nДобро пожаловать! \n{login}' #O(1)
+
+    for user in service_db: #O(n)
+        if user['login'] == login and user['password'] == password: #O(1)
+            auth = user['auth'] #O(1)
+            break
+        else:
+            auth = None #O(1)
+
+    # print(auth)
+
+    if auth is None: #O(1)
+        message = '\nНеправльны логин или пароль' #O(1)
+    elif not auth: #O(1)
+        message = '\nДля допуска к ресурсу, вам нужно активировать свою учётную запись!' #O(1)
+
+    return message #O(1)
+
+
+print(auth_in_service('Human3000', 'asd12dasd'))
+print(auth_in_service('Guy123', 'asdad2123asd'))
+print(auth_in_service('Guy123', 'asdad2123asdфыв'))
+
+
+print('------------------------ 2-е решение ------------------------')
+
+service_db = ['Human3000', 'asd12dasd', True,
+              'Guy123', 'asdad2123asd', False,
+              'Humblealx', 'asd83fj291d', True]
+
+'''
+#O(n)
+'''
+def auth_in_service_2(login, password):
+    message = f'\nДобро пожаловать! \n{login}' #O(1)
+
+    if not login in service_db or not password in service_db: #O(n)
+        message = '\nНеправльны логин или пароль' #O(1)
+    elif login in service_db and service_db.index(login)+1 == service_db.index(password) and service_db[service_db.index(password)+1] != True: #O(n)
+        message = '\nДля допуска к ресурсу, вам нужно активировать свою учётную запись!' #O(1)
+
+    return message
+
+
+print(auth_in_service_2('Human3000', 'asd12dasd'))
+print(auth_in_service_2('Guy123', 'asdad2123asd'))
+print(auth_in_service_2('Guy123', 'asdad2123asdф'))
