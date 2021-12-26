7e58de7595aa6ed4a1406ea31ad175c3264be1c1
NikBulatov <nikita_bulatov@icloud.com>
2021-12-26 12:04:22 +0300

    task 2

---
+++ Урок 4. Практическое задание/task_2.py
@@ -0,0 +1,85 @@
+"""
+Задание 2.
+
+Приведен код, который формирует из введенного числа
+обратное по порядку входящих в него цифр.
+Задача решена через рекурсию
+Выполнена попытка оптимизировать решение через мемоизацию.
+Сделаны замеры обеих реализаций.
+
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!
+
+П.С. задание не такое простое, как кажется
+"""
+
+from timeit import timeit
+from random import randint
+
+
+def recursive_reverse(number):
+    return str(number % 10) if number == 0 else f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+num_100 = randint(10000, 1000000)
+num_1000 = randint(1000000, 10000000)
+num_10000 = randint(100000000, 10000000000000)
+
+print('Не оптимизированная функция recursive_reverse')
+print(
+    timeit(
+        "recursive_reverse(num_100)",
+        setup='from __main__ import recursive_reverse, num_100',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_1000)",
+        setup='from __main__ import recursive_reverse, num_1000',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_10000)",
+        setup='from __main__ import recursive_reverse, num_10000',
+        number=10000))
+
+
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+
+    return decorate
+
+
+@memoize
+def recursive_reverse_mem(number):
+    return '' if number == 0 else f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
+
+
+print('Оптимизированная функция recursive_reverse_mem')
+print(
+    timeit(
+        'recursive_reverse_mem(num_100)',
+        setup='from __main__ import recursive_reverse_mem, num_100',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_1000)',
+        setup='from __main__ import recursive_reverse_mem, num_1000',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_10000)',
+        setup='from __main__ import recursive_reverse_mem, num_10000',
+        number=10000))
+
+"""
+На мой взгляд мемоизация не нужна, так как функция str() имеет сложность O(1), которая является наименьшей.
+Мемоизация здесь незначительно ускоряет алгоритм/
+Мемоизация необходима, если функцию будем вызывать несколько раз с одинаковым аргументом 
+"""
