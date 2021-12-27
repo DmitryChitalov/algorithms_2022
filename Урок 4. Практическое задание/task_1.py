26d4bba533e4a3a0cfc07ca5c240621353c55fe0
Vladimir Ivanov <vivanov89@gmail.com>
2021-12-27 22:23:50 +0300

    lesson_4 Ivanov V.

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,24 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def lc(nums):
+    new_arr = [x for x in range(len(nums)) if x % 2 == 0]
+
+
+setup = 'from __main__ import '
+test_list = [x for x in range(10000)]
+
+print(f'Функция func_1 - ',
+      timeit('func_1(test_list)',
+             setup=setup + 'func_1, test_list', number=10000))
+
+print(f'Функция lc -',
+      timeit('lc(test_list)',
+             setup=setup + 'lc,' + ' test_list', number=10000))
+
+
+# func_1 выполняется за 15.413346971
+# lc выполняется за 9.000430900999998
+# list comprehensions выполняется в 2 раза быстрее с одинаоквыми вводными
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,8 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
 """
+from random import randint
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
@@ -35,3 +37,20 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_nums(enter_num):
+    return str(enter_num % 10)[::-1]
+
+
+nums = randint(10000, 1000000)
+# print(num_100, reverse_lc(num_100))
+print(timeit("revers(nums)", setup='from __main__ import revers, nums', globals=globals()))
+print(timeit("revers_2(nums)", setup='from __main__ import revers_2, nums', globals=globals()))
+print(timeit("revers_3(nums)", setup='from __main__ import revers_3, nums', globals=globals()))
+print(timeit("revers_nums(nums)", setup='from __main__ import revers_nums, nums', globals=globals()))
+
+# 1.677488771 - рекурсия самая медленная
+# 1.0657876149999999 - цикл выполняется быстрее рекурсии
+# 0.388942718 - срез выполняется быстрее циела и рекурсии
+# 0.35413986899999994 - а в моем случае код выполняется чуть быстрее среза из-за отсутствия переменных
