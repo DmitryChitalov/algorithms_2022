58abe01be018b54540662e1ea53edf329ddc5d86
SerdyukovaNS <96206906+SerdyukovaNS@users.noreply.github.com>
2021-12-27 10:55:45 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,20 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+
+print(timeit("func_1(nums)", globals=globals(), number=1000))
+print(timeit("func_2(nums)", globals=globals(), number=1000))
+
+
+# при использовании цикла, время выполнения состовило 5.9501149
+# при использовании list comprehension время выполнения уменьшилось до 4.7168768000000005
+
+
