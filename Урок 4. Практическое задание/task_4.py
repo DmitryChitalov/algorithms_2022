3bd679aead6131bd7815ec3760e303b65a993804
SerdyukovaNS <96206906+SerdyukovaNS@users.noreply.github.com>
2021-12-27 11:04:25 +0300

    Update task_4.py

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -37,5 +37,39 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    elem = max(array, key=array.count)
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {array.count(elem)} раз(а)'
+
+
+
+def func_4():
+    dict_1 = {}
+    count = itm = 0
+    for item in reversed(array):
+        dict_1[item] = dict_1.get(item, 0) + 1
+        if dict_1[item] >= count:
+            count, itm = dict_1[item], item
+    return f'Чаще всего встречается число {itm}, ' \
+           f'оно появилось в массиве {count} раз(а)'
+
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+
+print(timeit("func_1()", globals=globals(), number=1000))
+print(timeit("func_2()", globals=globals(), number=1000))
+print(timeit("func_3()", globals=globals(), number=1000))
+print(timeit("func_4()", globals=globals(), number=1000))
+
+# время выполнения func_1  - 0.0761689
+# время выполнения func_2 - 0.07905859999999999
+# время выполнения func_3 - 0.07725869999999999
+# время выполнения func_4 - 0.009335799999999977
+# наиболее эффективный алгоритм func_4, с использованием словаря
+
