07c02641765b5cc36ec63bf6a2dd99d119038005
SerdyukovaNS <96206906+SerdyukovaNS@users.noreply.github.com>
2021-12-27 11:05:40 +0300

    Update task_5.py

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -34,5 +34,46 @@ def simple(i):
     return n
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+print(simple(10))
+print(simple(100))
+print(simple(1000))
+
+print(timeit("simple(10)", globals=globals(), number=100))
+print(timeit("simple(100)", globals=globals(), number=100))
+print(timeit("simple(1000)", globals=globals(), number=100))
+# 29
+# 0.010534500000000335
+# 541
+# 1.0681459000000002
+# 7919
+# 18.0117374
+
+
+
+def eratosfen(n):
+    sieve = list(range(100000))
+    sieve[1] = 0
+    for i in sieve:
+        if i > 1:
+            for j in range(i + i, len(sieve), i):
+                sieve[j] = 0
+    sieve1 = [x for x in sieve if x != 0]
+    return sieve1[n-1]
+
+print(eratosfen(10))
+print(eratosfen(100))
+print(eratosfen(1000))
+
+print(timeit("eratosfen(10)", globals=globals(), number=100))
+print(timeit("eratosfen(100)", globals=globals(), number=100))
+print(timeit("eratosfen(1000)", globals=globals(), number=100))
+
+# 29
+# 1.192325499999999
+# 541
+# 1.214452399999999
+# 7919
+# 1.2510748000000014
+
+# Чем больше порядковый номер простого числа, тем эффективнее «Решето Эратосфена»,
+# первый алгоритм эффективнее использовать с порядковыми номерами простых чисел до 100.
