4b1358686c888f803e637c22c527ef0e16c28e3f
Semushkin_Anton <rkzton@yandex.ru>
2021-12-29 19:51:57 +0300

    Lesson_4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,9 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
 """
+from timeit import timeit
+
+nums = 123456789
 
 
 def revers(enter_num, revers_num=0):
@@ -35,3 +38,66 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+
+# Вариант 4 мемоизация
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorate
+
+
+@memoize
+def revers_4(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{revers_4(number // 10)}'
+
+
+# Вариант 5 Встроиная функция join
+def revers_5(enter_num):
+    return reversed(str(enter_num))
+
+
+# Вариант 6. Сокращенная версия от варианта 3
+def revers_6(enter_num):
+    return str(enter_num)[::-1]
+
+
+print('Время выполнения. Вариант 1: ', end='')
+print(timeit('revers(nums)', setup='from __main__ import revers, nums', number=1000))
+print('Время выполнения. Вариант 2: ', end='')
+print(timeit('revers_2(nums)', setup='from __main__ import revers_2, nums', number=1000))
+print('Время выполнения. Вариант 3: ', end='')
+print(timeit('revers_3(nums)', setup='from __main__ import revers_3, nums', number=1000))
+print('Время выполнения. Вариант 4: ', end='')
+print(timeit('revers_4(nums)', setup='from __main__ import revers_4, nums', number=1000))
+print('Время выполнения. Вариант 5: ', end='')
+print(timeit('revers_5(nums)', setup='from __main__ import revers_5, nums', number=1000))
+print('Время выполнения. Вариант 6: ', end='')
+print(timeit('revers_6(nums)', setup='from __main__ import revers_6, nums', number=1000))
+print('---------------------------Однократное выполнение --------------------')
+
+'''
+Вариант 1. Рекурсивный метод, долгий метод, из-за задействованого стека вызовов и сохранения результатов вызова 
+        в оперативной памяти.
+Вариант 2. Такой же как и первый, но без рекурсии. Использован цикл с переназначением уже созданной переменной.
+        Выполняется быстрее первого из-за отсутсвия необходимости сохранения результата каждого вызова.
+Вариант 3. Быстрый способ. Скорость за счет использованя встроенного в Python среза, оптемизированного.
+Вариант 4. Мемоизация. При однократном запуске скрипта самы долгий. Будет самым быстрым, только при многократном
+        прогоне скрипта, при обработке большого массива данных.
+Вариант 5. Самый быстрый способ, за счет испоьзования встроенной функции reversed. В отличае от варианта 4, 
+        результат вычисления сразу передается в return, без создания промежуточных переменных.
+Вариант 6. Тоже самый быстрый способ, за счет использования среза, т.е. встроенной функции. Упращенный вариант 4. 
+
+Вывод: Самые быстрые варианты 5 и 6, за счет использования оптемизированных встроенных функций, без создания 
+        промежуточных переменных, и передачей результата вычислений сразу в return.  
+'''
+
