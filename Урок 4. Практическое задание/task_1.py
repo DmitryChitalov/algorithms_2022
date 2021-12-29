53f935a161734b01dcb5f4773c52a0480772cd3b
IgorKlm1969 <shakli@yandex.ru>
2021-12-29 18:25:07 +0300

    д4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -14,6 +14,7 @@
 
 from timeit import timeit
 
+nums = [num for num in range(10)]
 
 def func_1(nums):
     new_arr = []
@@ -21,3 +22,60 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+print(f'func_1 результат замера: '
+      f'{timeit("func_1(nums)", "from __main__ import func_1", globals=globals(), number=1000000)} '
+      f'сек')
+print('*'*60)
+
+nums = [num for num in range(10)]
+new_arr = [i for i, num in enumerate(nums) if num % 2 == 0]
+
+# new_arr = [i if j % 2 == 0 else None for i, j in enumerate(nums)] вариант плохо работает
+print(f'generator результат замера: '
+      f'{timeit("new_arr = [i for i, num in enumerate(nums) if num % 2 == 0]", globals=globals(), number=1000000)}')
+print('*'*60)
+
+
+nums = [num for num in range(100)]
+
+
+def func_2(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+
+
+print(f'func_2 результат замера: '
+      f'{timeit("func_2(nums)", "from __main__ import func_1", globals=globals(), number=1000000)} сек')
+print('*'*60)
+
+
+new_arr = [i for i, num in enumerate(nums) if num % 2 == 0]
+
+print(f'generator результат замера: '
+      f'{timeit("new_arr = [i for i, num in enumerate(nums) if num % 2 == 0]", globals=globals(), number=1000000)}')
+'''
+Результаты замеров
+
+func_1 результат замера: 0.8944282 сек
+************************************************************
+generator результат замера: 0.8811345
+************************************************************
+func_2 результат замера: 6.9086071 сек
+************************************************************
+generator результат замера: 5.583961799999999
+
+здесь видно, что генератор списка работает быстрее
+ чем встроенная функция "enumerate"
+
+
+
+
+
+'''
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -12,15 +12,18 @@
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
 """
 
+from cProfile import run
+from timeit import timeit
 
-def revers(enter_num, revers_num=0):
+
+def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -35,3 +38,60 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+
+num = 123123131
+
+
+run('revers_1(num)')
+run('revers_2(num)')
+run('revers_3(num)')
+
+print('1 -', timeit(f'revers_1({num})', globals=globals(), number=1000000))
+print('2 -', timeit(f'revers_2({num})', globals=globals(), number=1000000))
+print('3 -', timeit(f'revers_3({num})', globals=globals(), number=1000000))
+
+"""
+Первая  (рекурсия) самая медленная
+Вторая функция (цикл) немного быстрее первой
+Третья функция (срез) -- самая быстрая, т.к. написана на основе встроенных функций
+
+13 function calls (4 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     10/1    0.000    0.000    0.000    0.000 task_3.py:19(revers_1) ## Первая  (рекурсия)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:29(revers_2)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:37(revers_3)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+1 - 1.8428851
+2 - 1.2257759
+3 - 0.2619794999999998
+
+
+"""
