c1e9b336799eb8ef8ac733ba6a9c75c0396b3339
mrfuu04 <mrfuu04@gmail.com>
2021-12-30 17:04:52 +0300

    'task_1_4'

--- Урок 6. Практическое задание/task_1_4.py
+++ Урок 6. Практическое задание/task_1_4.py
@@ -30,3 +30,57 @@
 
 Это файл для четвертого скрипта
 """
+
+from collections import namedtuple
+from recordclass import recordclass
+from memory_profiler import memory_usage, profile
+
+"""
+Изначальный вариант взял из текущего курса.
+Задача: перевернуть число.
+Урок 2, task 3
+"""
+
+
+def dec(func):
+    def wrapper(*args):
+        start = memory_usage()
+        res = func(*args)
+        return f'Заняло пямяти = {memory_usage()[0] - start[0]}'
+
+    return wrapper
+
+
+@dec
+def get_rev(n):
+    return func(n)
+
+
+@dec
+def func(num, reversed=''):
+    if num == 0:
+        return reversed
+    reversed += str(num % 10)
+    return func(num // 10, reversed)
+
+
+@dec
+def revers_while(enter_num, revers_num=''):
+    while enter_num != 0:
+        revers_num = revers_num + str(enter_num % 10)
+        enter_num //= 10
+    return revers_num
+
+
+print(get_rev(123456789059823704558235236346547456645294689234465330))
+print(revers_while(123456789059823704558235236346547456645294689234465330))
+
+"""
+Вывод:
+        Рекурсия:
+                Заняло пямяти = 0.1171875
+        Цикл:
+                Заняло пямяти = 0.0
+        Повторные вызовы функции в рекурсивном методе существенно влияют на память т. к. результаты
+        каждого вызова функции сохраняются в памяти. цикл и быстрее рекурсии и занимеет меньше памяти
+"""
