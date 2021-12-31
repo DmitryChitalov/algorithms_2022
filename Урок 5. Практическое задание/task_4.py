bb4844ac360b32db8e313f3185d3f95effdd8ea1
Vladimir Ivanov <vivanov89@gmail.com>
2021-12-31 11:33:16 +0300

    lesson_5 Ivanov V.

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,30 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+my_dict = {}
+ordered_dict = OrderedDict()
+
+
+def func_my_dict():
+    for i in range(1, 10000):
+        my_dict[i] = i + 1
+    return my_dict
+
+
+def func_ordered_dict():
+    for i in range(1, 10000):
+        ordered_dict[i] = i + 1
+    return ordered_dict
+
+
+print(
+    timeit("func_my_dict()", globals=globals(), number=1000)) # 1.151253929
+print(timeit("func_ordered_dict()", globals=globals(), number=1000)) # 1.3533036180000002
+
+# обычный словарь заполняется быстрее но при использовании Ordereddict код проясняет,
+# что порядок элементов в словаре важен, например, если необходимо сравнить словари,
+# то Ordereddict правильный выбор.
