057ff122297ba3528700d269e6c78f4361079166
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-01-03 18:09:44 +0300

    task_1_3

--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -29,4 +29,78 @@
 генераторы, numpy, использование слотов, применение del, сериализация и т.д.
 
 Это файл для третьего скрипта
+Урок 5 курс алгоритмов. Задание 2.
 """
+from memory_profiler import profile
+from pympler.asizeof import asizeof
+
+
+# Original
+class Hex:
+
+    def __init__(self, number: str):
+        self.number = list(number)
+
+    def __add__(self, other):
+        if isinstance(other, Hex):
+            return Hex('%X' % (int(''.join(self.number), 16) + int(''.join(other.number), 16)))
+        elif isinstance(other, str):
+            return Hex('%X' % (int(''.join(self.number), 16) + int(''.join(other), 16)))
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        if isinstance(other, Hex):
+            return Hex('%X' % (int(''.join(self.number), 16) * int(''.join(other.number), 16)))
+        elif isinstance(other, str):
+            return Hex('%X' % (int(''.join(self.number), 16) * int(other, 16)))
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __str__(self):
+        return f'{self.number}'
+
+    def __repr__(self):
+        return 'Hexadecimal'
+
+
+# Optimize
+class Hex2:
+    __slots__ = 'number',
+
+    def __init__(self, number: str):
+        self.number = list(number)
+
+    def __add__(self, other):
+        if isinstance(other, Hex2):
+            return Hex2('%X' % (int(''.join(self.number), 16) + int(''.join(other.number), 16)))
+        elif isinstance(other, str):
+            return Hex2('%X' % (int(''.join(self.number), 16) + int(''.join(other), 16)))
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        if isinstance(other, Hex2):
+            return Hex2('%X' % (int(''.join(self.number), 16) * int(''.join(other.number), 16)))
+        elif isinstance(other, str):
+            return Hex2('%X' % (int(''.join(self.number), 16) * int(other, 16)))
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __str__(self):
+        return f'{self.number}'
+
+    def __repr__(self):
+        return 'Hexadecimal'
+
+
+if __name__ == '__main__':
+    print(asizeof(Hex('F3B')))
+    print(asizeof(Hex2('F3B')))
+    print("""Используя __slots__ (слоты) в создании класса, мы ограничиваем объект в создании дополнительных атрибутов, 
+что позволяет сэкономить память 
+    """)
