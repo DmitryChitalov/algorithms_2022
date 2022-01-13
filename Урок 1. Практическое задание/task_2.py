aaa812eb1ffaeccbc2fa0a7499db3b9c91767e50
Vladislav <vladislav.kapralov@gmail.com>
2022-01-13 22:45:38 +0300

    Task 1 remaid + task 2 half made

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -13,3 +13,28 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 -- нельзя использовать встроенные функции min() и sort()
 """
+import numpy as np
+
+list = np.random.randint(-10, 10, 10)
+
+print("Таков список: ",list)
+
+def search_min_quadratic(list):
+    min = list[0]  # O(1)
+    for i in list:  # O(n)
+        if i < min:  # O(1)
+            min = i  # O(1)
+
+    return min
+
+
+
+def search_min_linear(list):
+    min = list[0]       #O(1)
+    for i in list:      #O(n)
+        if i < min:     #O(1)
+            min = i     #O(1)
+    return min
+
+print("Минимальный элемент: ", search_min_quadratic(list))
+print("Минимальный элемент: ", search_min_linear(list))
