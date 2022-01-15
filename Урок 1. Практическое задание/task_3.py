6b1a398fd61a414d3755ac9e9ae6a15415d6cd69
Evgeny <evgeny.hyvarinen@gmail.com>
2022-01-15 13:41:23 +0200

    task 3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,56 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+info_store = {
+    "poor company": -345.414,
+    "negative solutions": -31412515141412,
+    "Google vilage": 1324155.3130,
+    "Abb": 43431211,
+    "Siemens": 116002334.44,
+    "Reaktor base": 2030304,
+    "Kumara": 5922323.4214,
+    "Drama company": 323330314,
+    "Komedy Film": 203204124.31,
+    "Some other": 12034
+}
+
+
+def get_three_profitable_comp(info_storage:  dict) -> dict:
+    """
+    Первый вариант.
+    :arg = dict possible also (tuple, list)
+    :return dict
+
+    Сложность: Т(n) = 2n + 5 => O(n)
+    """
+    tmp_storage = info_storage.copy()                                           # O(n)
+    max_annual_profit_companies = {}                                            # O(1)
+    for x in range(3):                                                          # O(1)
+        company, value = max(tmp_storage.items(), key=lambda x: x[1])           # O(n)
+        del tmp_storage[company]                                                # O(1)
+        max_annual_profit_companies.update({company: value})                    # O(1)
+    return max_annual_profit_companies                                          # O(1)
+
+
+print(f"Вариант 1: {get_three_profitable_comp(info_store)}")
+
+def get_three_profitable_comp_2(info_storage:  dict) -> dict:
+    """
+    второй вариант.
+    :arg = dict possible also (tuple, list)
+    :return dict
+
+    Сложность: T(n) = n^2 + n log n + 2 = O(n^2)
+    """
+    tmp_storage = [(company, profit) for company, profit in info_storage.copy().items()]     # O(n^2)
+    tmp_storage.sort(key=lambda x: x[1])                                                     # O(nlog n)
+    max_annual_profit_companies = {company: profit for company, profit in tmp_storage[-3:]}  # O(1)
+    return max_annual_profit_companies                                                       # O(1)
+
+print(f"Вариант 2: {get_three_profitable_comp_2(info_store)}")
+
+
+"""
+Решение 1 более еффективно, так как сложность O(n)
+Решение 2 более компактно но сложность квадратичная
+"""
