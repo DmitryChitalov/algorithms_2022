8244fdc7b2d2fe67a563a61c385e53ca9e8d923f
alexandr50 <dmitrymaslov2016@mail.ru>
2022-01-16 16:39:05 +0300

    Homework for lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,72 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+from random import randint
+from timeit import timeit
+
+def bubble_sort(list_obj):
+    n = 1
+    while n < len(list_obj):
+        for i in range(len(list_obj) - 1):
+            if list_obj[i] < list_obj[i - 1]:
+                list_obj[i], list_obj[i - 1] = list_obj[i - 1], list_obj[i]
+        n += 1
+    return list_obj
+
+def bubble_sort_2(list_obj):
+    count = 0
+    flag = True
+    for i in range(len(list_obj) - 1):
+        if flag is False:
+            return count, list_obj
+        flag = False
+        for j in range(len(list_obj) - 1 - i):
+            if list_obj[j + 1] > list_obj[j]:
+                list_obj[j], list_obj[j + 1] = list_obj[j + 1], list_obj[j]
+                flag = True
+            count += 1
+    return count, list_obj
+
+list_1 = [randint(-100, 100) for x in range(10)]
+
+print('Пузырек 10 элементов = ',
+      timeit('bubble_sort(list_1[:])', globals=globals(), number=1000))
+print('Пузырек_2 10 элементов = ',
+      timeit('bubble_sort_2(list_1[:])', globals=globals(), number=1000))
+# Пузырек 10 элементов =  0.042360099999999984
+# Пузырек_2 10 элементов =  0.032833500000000015
+
+list_1 = [randint(-100, 100) for x in range(100)]
+print('Пузырек 100 элементов = ',
+      timeit('bubble_sort(list_1[:])', globals=globals(), number=1000))
+print('Пузырек_2 100 элементов = ',
+      timeit('bubble_sort_2(list_1[:])', globals=globals(), number=1000))
+
+# Пузырек 100 элементов =  3.5059751
+# Пузырек_2 100 элементов =  3.1685647000000006
+
+list_1 = [randint(-100, 100) for x in range(500)]
+print('Пузырек 500 элементов = ',
+      timeit('bubble_sort(list_1[:])', globals=globals(), number=1000))
+print('Пузырек_2 500 элементов = ',
+      timeit('bubble_sort_2(list_1[:])', globals=globals(), number=1000))
+# Пузырек 500 элементов = 111.17879389999999
+# Пузырек_2 500 элементов = 91.0835901
+# Доработка пузырькового метода не оказалась эффективной
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -13,3 +13,23 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from timeit import timeit
+from random import randint
+from statistics import median
+
+m = 10
+my_list_10 = [randint(0, 100) for x in range(2*m+1)]
+print(timeit("median(my_list_10[:])", globals=globals(), number=1000))
+m = 100
+my_list_100 = [randint(0, 100) for x in range(2*m+1)]
+print(timeit("median(my_list_100[:])", globals=globals(), number=1000))
+m = 1000
+my_list_1000 = [randint(0, 100) for x in range(2*m+1)]
+print(timeit("median(my_list_1000[:])", globals=globals(), number=1000))
+
+
+# Результаты:
+# m 10 - 0.004026799999999997
+# m 100 - 0.040113099999999985
+# m 1000 - 0.6270933000000001
+# Самый лучший способ решения
