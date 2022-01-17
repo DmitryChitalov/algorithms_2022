191e63f311db5ab0d87dac11f194827f56a1e99d
Andrey <zhdanov.an81@yandex.ru>
2022-01-17 20:03:40 +0300

    Выполнение задачи 6 урока 2.

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,21 @@
 
 Решите через рекурсию. Решение через цикл не принимается.
 """
+import random
+
+def recur_method(count, numb):
+
+    print(f"Попытка №{count}")
+    answer = int(input("Введите число от 0 до 100: "))
+    if count == 10 or answer == numb:
+        if answer == numb:
+            print("Верно!")
+        print(f"Загаданное число: {numb}")
+    else:
+        if answer > numb:
+            print(f"Загаданное число меньше чем {numb}")
+        else:
+            print(f"Загаданное число больше чем {numb}")
+        recur_method(count + 1, numb)
+
+recur_method(1, random.randint(0, 100))
