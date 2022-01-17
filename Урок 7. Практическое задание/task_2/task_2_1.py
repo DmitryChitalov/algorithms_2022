6fc2844e6b2b216a58f953e8cbefa1f37cbbd89b
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-01-17 18:52:26 +0300

    task 2_1 reduce mistakes

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -16,9 +16,6 @@
 """
 from timeit import timeit
 from random import randint
-from statistics import median
-# Есть хороший пакет с готовыми алгоритмами и другими полезными модулями
-from algorithms.sort.heap_sort import max_heap_sort
 
 m = 10
 ARRAY_10 = [randint(-100, 100) for i in range(2 * m + 1)]
@@ -27,9 +24,35 @@ ARRAY_100 = [randint(-100, 100) for j in range(2 * m + 1)]
 m = 1000
 ARRAY_1000 = [randint(-100, 100) for k in range(2 * m + 1)]
 
-print(f'Median of array with m = 10: {median(ARRAY_10)}',
-      timeit('max_heap_sort(ARRAY_10)', globals=globals(), number=100))
-print(f'Median of array with m = 100: {median(ARRAY_100)}',
-      timeit('max_heap_sort(ARRAY_100)', globals=globals(), number=100))
-print(f'Median of array with m = 1000: {median(ARRAY_1000)}',
-      timeit('max_heap_sort(ARRAY_1000)', globals=globals(), number=100))
+
+def heap_sort(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        max_heapify(arr, i)
+    return arr[len(arr) // 2]
+
+
+def max_heapify(arr, end):
+    last_parent = (end - 1) // 2
+
+    for parent in range(last_parent, -1, -1):
+        current_parent = parent
+
+        while current_parent <= last_parent:
+            child = 2 * current_parent + 1
+            if child + 1 <= end and arr[child] < arr[child + 1]:
+                child = child + 1
+            if arr[child] > arr[current_parent]:
+                arr[current_parent], arr[child] = arr[child], arr[current_parent]
+                current_parent = child
+            else:
+                break
+    arr[0], arr[end] = arr[end], arr[0]
+
+
+if __name__ == '__main__':
+    print(f'Median of array with m = 10: {heap_sort(ARRAY_10[:])}',
+          timeit('heap_sort(ARRAY_10[:])', globals=globals(), number=100))
+    print(f'Median of array with m = 100: {heap_sort(ARRAY_100[:])}',
+          timeit('heap_sort(ARRAY_100[:])', globals=globals(), number=100))
+    print(f'Median of array with m = 1000: {heap_sort(ARRAY_1000[:])}',
+          timeit('heap_sort(ARRAY_1000[:])', globals=globals(), number=100))
