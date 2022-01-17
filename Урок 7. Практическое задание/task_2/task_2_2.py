254608757c3c97d146c9fd883d10aecf304f7a24
SerdyukovaNS <96206906+SerdyukovaNS@users.noreply.github.com>
2022-01-17 18:53:56 +0300

    Update task_2_2.py

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -13,3 +13,40 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def find_median(lst):
+    if len(lst) % 2 == 0:
+        center = []
+        for i in lst:
+            b = 0
+            for j in lst:
+                if i < j:
+                    b += 1
+            if len(lst) == b * 2 + 2 or len(lst) == b * 2:
+                center.append(i)
+        return sum(center) / 2
+    else:
+        for i in lst:
+            num = i
+            b = 0
+            for j in lst:
+                if i < j:
+                    b += 1
+            if len(lst) == 2 * b + 1:
+                return num
+
+
+m = 10
+array = [randint(0, 100) for x in range(2 * m + 1)]
+print(timeit("find_median(array[:])", globals=globals(), number=1000))  # 0.0032130000000000023
+
+m = 100
+array = [randint(0, 100) for x in range(2 * m + 1)]
+print(timeit("find_median(array[:])", globals=globals(), number=1000))  #  0.8905265
+
+m = 1000
+array = [randint(0, 100) for x in range(2 * m + 1)]
+print(timeit("find_median(array[:])", globals=globals(), number=1000))  #  101.7426828
