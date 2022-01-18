aeb0839f8aa8e93c3f167e35112832d8a9a82a49
Andrey <zhdanov.an81@yandex.ru>
2022-01-18 17:33:27 +0300

    Выполнение задачи 2 урока 1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -13,3 +13,25 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 -- нельзя использовать встроенные функции min() и sort()
 """
+# сложность O(n)
+def learn_min_meaning(lst):
+    min_meaning = lst[0] if lst else None
+    for i in range(1, len(lst)):
+        if lst[i] < min_meaning:
+            min_meaning = lst[i]
+    return min_meaning
+
+# Сложность O(n**2)
+def learn_min_meaning2(lst):
+    min_meaning2 = lst[0] if lst else None
+    for i in lst:
+        for j in range(1, len(lst)):
+            if min_meaning2 > lst[j]:
+                min_meaning2 = lst[j]
+        return min_meaning2
+
+
+training_list = [15, 59, 99, 11, 57, 7, 102, 1]
+
+print(learn_min_meaning(training_list))
+print(learn_min_meaning2(training_list))
