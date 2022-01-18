3482260e3fcb863f66e66a5ecdddc9eb48eff112
Vladimir Ivanov <vivanov89@gmail.com>
2022-01-18 11:33:59 +0300

    lesson 7 Ivanov

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -13,3 +13,28 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from timeit import timeit
+from random import randint
+
+
+def median(list, x):
+    for _ in range(x):
+        list.remove(max(list))
+    return max(list)
+
+
+x = 10
+list1 = [randint(0, 100) for i in range(2 * x + 1)]
+print(timeit("median(list1[:], x)", globals=globals(), number=100))
+
+x = 100
+list2 = [randint(0, 100) for i in range(2 * x + 1)]
+print(timeit("median(list2[:], x)", globals=globals(), number=100))
+
+x = 1000
+list3 = [randint(0, 100) for i in range(2 * x + 1)]
+print(timeit("median(list3[:], x)", globals=globals(), number=100))
+# 0.0008408209999999985
+# 0.059101734999999996
+# 3.7655562220000003
