6417b673e09da4acf79bcf3e76db1e8e735ad44b
dobr_bobr <obrosovd@gmail.com>
2022-01-19 14:19:23 +0300

    1_HW

---
+++ 1_HW_Algoritms/1.5_HW_algoritm.py
@@ -0,0 +1,54 @@
+"""
+Задание 5. На закрепление навыков работы со стеком
+Реализуйте структуру "стопка тарелок".
+Мы можем складывать тарелки в стопку и при превышении некоторого значения
+нужно начать складывать тарелки в новую стопку.
+Структура должна предусматривать наличие нескольких стеков.
+Создание нового стека происходит при достижении предыдущим
+стеком порогового значения.
+После реализации структуры, проверьте ее работу на различных сценариях.
+Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
+--реализуйте по аналогии с примером, рассмотренным на уроке
+--создание нового стека можно реализовать добавлением новой пустой стопки
+в массив стопок (lst = [[], [], [], [],....]) либо созданием объекта
+класса-стек в самом же классе.
+"""
+
+class StackClass:
+    def __init__(self):
+        self.elems = []
+
+    def push_in(self, el):
+        """Предполагаем, что верхний элемент стека находится в конце списка"""
+        for i in range(0, len(self.elems) - 1):
+            if len(self.elems[i]) < 5:
+                self.elems[i].append(el)
+                break
+
+    def pop_out(self):
+        return self.elems.pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        return len(self.elems)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.max = None
+
+    def push(self, item):
+        self.stack.append(item)
+        if len(self.stack) == 1 or item > self.max:
+            self.max = item
+
+if __name__ == '__main__':
+    stack_1 = StackClass()
+    i = 0
+    while i < 18:
+        stack_1.push_in(1+i)
+        i += 1
+
+    print(stack_1.elems)
