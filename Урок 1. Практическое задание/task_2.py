b02e29d0428b567a7e1cb85242f69a620950f10c
Svet <utug2004@gmail.com>
2022-01-19 17:49:22 +0300

    p

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -13,3 +13,32 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 -- нельзя использовать встроенные функции min() и sort()
 """
+
+# o(n^2)
+
+
+def min_value_1(value):
+    for i in value:
+        min_v = True
+        for m in value:
+            if m < i:
+                min_v = False
+        if min_v:
+            return i
+
+
+# o(n)
+
+
+def min_value_2(value):
+    min_v = value.pop()
+    for number in value:
+        if number < min_v:
+            min_v = number
+    return min_v
+
+
+value = [20, 10, 8, 16, 18]
+
+print(min_value_1(value))
+print(min_value_2(value))
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -20,3 +20,52 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+users = [
+    {"Login": "1", "Password": "111111", "is_active": True},
+    {"Login": "2", "Password": "222222", "is_active": False},
+    {"Login": "3", "Password": "333333", "is_active": False},
+    {"Login": "4", "Password": "444444", "is_active": True},
+    {"Login": "5", "Password": "555555", "is_active": True}
+]
+
+
+# 1 O(n)
+def authentication(login, password):
+    for i in users:
+        if i.get("Login") == login:
+            if i.get("Password") == password:
+                if i.get("is_active"):
+                    return f'Access allowed'
+                else:
+                    return f'Please activate your account'
+            else:
+                return f'Wrong password'
+
+
+print(authentication("1", "222222"))
+print(authentication("2", "222222"))
+print(authentication("1", "111111"))
+
+# 2 O(1)
+users2 = {"1": {"Password": "111111", "is_active": True},
+          "2": {"Password": "222222", "is_active": False},
+          "3": {"Password": "333333", "is_active": False},
+          "4": {"Password": "444444", "is_active": True},
+          "5": {"Password": "555555", "is_active": True}}
+
+
+def authentication2(user_list, login, password):
+    if user_list.get(login):
+        if user_list[login]["Password"] == password and user_list[login]["is_active"]:
+            return f'Access allowed'
+        elif user_list[login]["Password"] == password and not user_list[login]["is_active"]:
+            return f'Please activate your account'
+        elif user_list[login]["Password"] != password:
+            return f'Wrong password'
+    else:
+        return f'Please create account'
+
+
+print(authentication2(users2, "1", "222222"))
+print(authentication2(users2, "2", "222222"))
+print(authentication2(users2, "1", "111111"))
