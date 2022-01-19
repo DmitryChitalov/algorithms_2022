2ede483fe2ac8a86d657538a830b082a9c452dbf
Aleksey Nikolskii <aleksey.nikolskiy4@rsce.ru>
2022-01-19 14:37:05 +0300

    Доработано 2 и 3 задания

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,233 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах
 """
+
+from timeit import timeit
+from collections import deque
+from random import randint
+
+
+def check_list_append(my_lst):
+    for i in range(0, 10000):
+        my_lst.append(i)
+
+
+def check_deque_append(my_deque):
+    for i in range(0, 10000):
+        my_deque.append(i)
+
+
+def check_list_pop(my_lst):
+    for i in range(0, 10000):
+        my_lst.pop()
+
+
+def check_deque_pop(my_deque):
+    for i in range(0, 10000):
+        my_deque.pop()
+
+
+def check_list_extend(my_lst):
+    add_lst = list(randint(0, 10000) for i in range(10))
+    for i in range(0, 1000):
+        my_lst.extend(add_lst)
+
+
+def check_deque_extend(my_deque):
+    add_deque = list(randint(0, 10000) for i in range(10))
+    for i in range(0, 1000):
+        my_deque.extend(add_deque)
+
+
+def check_list_insert(my_lst):
+    for i in range(0, 1000):
+        my_lst.insert(0, i)
+
+
+def check_deque_appendleft(my_deque):
+    for i in range(0, 1000):
+        my_deque.appendleft(i)
+
+
+def check_list_popleft(my_lst):
+    for i in range(1, 10):
+        my_lst.pop(i)
+
+
+def check_deque_popleft(my_deque):
+    for i in range(1, 10):
+        my_deque.popleft()
+
+
+def check_list_extendleft(my_lst):
+    add_lst = list(randint(0, 10000) for i in range(10))
+    for i in range(0, 1000):
+        my_lst.insert(0, add_lst)
+
+
+def check_deque_extendleft(my_deque):
+    add_deque = deque(randint(0, 10000) for i in range(10))
+    for i in range(0, 1000):
+        my_deque.extendleft(add_deque)
+
+
+def list_get(my_lst, idx):
+    return my_lst[idx]
+
+
+def deque_get(my_deque, idx):
+    return my_deque[idx]
+
+
+def check_list_get(my_lst):
+    for i in range(0, 10000):
+        list_get(my_lst, randint(1,10))
+
+
+def check_deque_get(my_deque):
+    for i in range(0, 10000):
+        deque_get(my_deque, randint(1,10))
+
+
+new_lst = list(randint(0, 10000) for i in range(1000))
+new_deque = deque(randint(0, 10000) for j in range(1000))
+print("list append", timeit("check_list_append(new_lst)", number=2000, globals=globals()), "seconds")
+print("deque append", timeit("check_deque_append(new_deque)", number=2000, globals=globals()), "seconds")
+
+# В результате 4х проходов, операция list append выполняется незначительно медленнее чем deque append
+# list append 2.115750741 seconds
+# deque append 1.7006729270000003 seconds
+
+# list append 2.1505018799999998 seconds
+# deque append 1.6311090680000002 seconds
+
+# list append 2.115640122 seconds
+# deque append 1.591039914 seconds
+
+# list append 2.173480568 seconds
+# deque append 1.601812796 seconds
+
+print("-------------------------------------------------------------------------------------")
+print("list pop", timeit("check_list_pop(new_lst)", number=2000, globals=globals()), "seconds")
+print("deque pop", timeit("check_deque_pop(new_deque)", number=2000, globals=globals()), "seconds")
+
+# В результате 4х проходов, операция list pop выполняется незначительно медленнее чем deque pop
+# list pop 2.2226102789999995 seconds
+# deque pop 1.8161810469999997 seconds
+
+# list pop 2.3396283009999994 seconds
+# deque pop 1.8594533270000007 seconds
+
+# list pop 2.2224737139999995 seconds
+# deque pop 1.864917031 seconds
+
+# list pop 2.2311733309999995 seconds
+# deque pop 1.8315574779999997 seconds
+
+print("-------------------------------------------------------------------------------------")
+print("list extend", timeit("check_list_extend(new_lst)", number=2000, globals=globals()), "seconds")
+print("deque extend", timeit("check_deque_extend(new_deque)", number=2000, globals=globals()), "seconds")
+
+# За четыре прохода, операция list extend выполняется в среднем в два раза медленне чем deque extend.
+# Сделать большее количество повтрений ( как и увеличть массивы для добавления не представляется возможным, возникает Memory Error)
+
+# list extend 0.736774881 seconds
+# deque extend 0.42801091700000005 seconds
+
+# list extend 0.7688722859999999 seconds
+# deque extend 0.40176951699999996 seconds
+
+# list extend 0.740372874 seconds
+# deque extend 0.40924832499999997 seconds
+
+# list extend 0.7437162700000001 seconds
+# deque extend 0.4190350370000001 seconds
+
+new_lst = list(randint(0, 10000) for i in range(1000))
+new_deque = deque(randint(0, 10000) for j in range(1000))
+print("-------------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------")
+print("list insert", timeit("check_list_insert(new_lst)", number=200, globals=globals()), "seconds")
+print("deque appendleft", timeit("check_deque_appendleft(new_deque)", number=200, globals=globals()), "seconds")
+
+# За 4 прохода операция list insert выполняется гораздо медленнее, чем deque appendleft
+# list insert 7.410647322 seconds
+# deque appendleft 0.017128385000000357 seconds
+
+# list insert 7.646397117999999 seconds
+# deque appendleft 0.016565308000000556 seconds
+
+# list insert 7.737764756 seconds
+# deque appendleft 0.01643644199999983 seconds
+
+# list insert 7.34050385 seconds
+# deque appendleft 0.015951482000000183 seconds
+
+print("-------------------------------------------------------------------------------------")
+print("list popleft", timeit("check_list_popleft(new_lst)", number=20000, globals=globals()), "seconds")
+print("deque popleft", timeit("check_deque_popleft(new_deque)", number=20000, globals=globals()), "seconds")
+# За четыре прохода операция list popleft выполняется гораздо медленнее,  чем deque popleft
+# list popleft 3.971813061 seconds
+# deque popleft 0.00023720599999776937 seconds
+
+# list popleft 4.391337903 seconds
+# deque popleft 0.000226371000000114 seconds
+
+# list popleft 3.498286392 seconds
+# deque popleft 0.00022352099999878305 seconds
+
+# list popleft 3.836609493 seconds
+# deque popleft 0.00024005600000265304 seconds
+
+
+print("-------------------------------------------------------------------------------------")
+print("list extendleft", timeit("check_list_extendleft(new_lst)", number=200, globals=globals()), "seconds")
+print("deque extendleft", timeit("check_deque_extendleft(new_deque)", number=200, globals=globals()), "seconds")
+
+# За четыре прохода операция  list extendleft выполняется гораздо медленнее, чем deque extendleft
+# list extendleft 7.503493503 seconds
+# deque extendleft 0.03978234100000044 seconds
+#
+# list extendleft 7.3521961870000005 seconds
+# deque extendleft 0.04031662299999983 seconds
+
+# list extendleft 7.416697766 seconds
+# deque extendleft 0.0418715719999998 seconds
+
+# list extendleft 8.291434297 seconds
+# deque extendleft 0.04488082899999846 seconds
+
+print("-------------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------")
+print("list get", timeit("check_list_get(new_lst)", number=200, globals=globals()), "seconds")
+print("deque get", timeit("check_deque_get(new_deque)", number=200, globals=globals()), "seconds")
+
+# За 4 прохода время обращения по индексу у list get  и у deque get примерно одинаковое
+# list get 2.787052984 seconds
+# deque get 2.9590267850000003 seconds
+
+# list get 2.834062958 seconds
+# deque get 2.7729954369999996 seconds
+
+# list get 2.806539452 seconds
+# deque get 2.7502907340000005 seconds
+
+# list get 2.8312156400000004 seconds
+# deque get 2.951112911 seconds
+
+
+# Итоговые выводы
+#
+#  1) в сравнении  операций append, pop, extend списка и дека ,
+#  операции append и pop для списка выполняются немного медленнее чем для дека.
+# операция extend - сильно быстрее для дека, чем для списка
+
+# 2) в сравнении операций appendleft, popleft, extendleft дека и соответствующих им операций списка - все операции
+# для дека выполняются намного быстрее , чем для списка.
+
+# 3) в сравнении операции получения элемента списка и дека по индексу - время выполнения для списка и дека примерно
+# одинаковое .
+
+# Таким образом, если необходимо использовать операции appendleft, popleft, extendleft - то оптимально будет
+# пользоваься деком .
+
