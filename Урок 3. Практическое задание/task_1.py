a70c32c85b6fa1b30407736133833a8bb9fc8ac8
Demidov Evgeniy <Demidov.evg.v@gmail.com>
2022-01-20 20:57:21 +0300

    materials

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -23,3 +23,58 @@ b) выполните со списком и словарем операции: 
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+import hashlib
+from binascii import hexlify
+
+print(10)
+object = hashlib.pbkdf2_hmac(hash_name='sha256', 
+   password=b'123',
+   salt=b'salt',
+   iterations=1000000)
+
+print(hexlify(object))
+
+# Продвинутая хэш функция:
+hashlib.pbkdf2_hmac(hash_name='sha256', 
+   password=b'123',
+   salt=b'salt',
+   iterations=1000000)
+# Преобразовать в строку для базы данных:
+from binascii import hexlify
+print(hexlify(object))
+# генерация случайных байтов:
+os.urandom()
+# генерация случайного числа:
+uuid4.uuid4()
+salt = uuid4().hex
+
+res = hashlib.sha256(salt.encode() + passwd.encode()).hexdigest()
+
+
+def memorize(func):
+    def g(n, memory={}):
+        r = memory.get(n)
+        if r is None:
+            r = func(n)
+            memory[n] = r
+        return r
+    return g
+
+
+@memorize
+def f(n):
+    if n < 2:
+        return n
+    return f(n - 1) + f(n - 2)
+
+from sys import getsizeof
+from pympler.asizeof import asizeof
+
+
+d = {1: '1', 2: '2', 3: '3'}
+print(getsizeof(d))  # -> 240
+print(asizeof(d))  # -> 504
+
+t = (1, 2, 3)
+print(getsizeof(t))  # -> 72
+print(asizeof(t))  # -> 168
