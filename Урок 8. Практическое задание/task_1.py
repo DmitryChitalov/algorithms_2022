f69f8bad2a5d983b272631ca9972cf15dc19ced2
KTo <kto@lovetou.ru>
2022-01-20 23:07:53 +0700

    Домашнее задание урок 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -1,6 +1,5 @@
 """
 Задание 1.
-
 Реализуйте кодирование строки по алгоритму Хаффмана.
 У вас два пути:
 1) тема идет тяжело? тогда вы можете, опираясь на примеры с урока,
@@ -8,8 +7,74 @@
 Разрешается и приветствуется изменение имен переменных,
 выбор других коллекций, различные изменения
 и оптимизации.
-
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например,
 через ООП или предложить иной подход к решению.
 """
+
+"""
+Ну, вроде получилось, получается, что чтобы потом этот код разобрать, нужно вместе с текстом передавать частоты и в 
+приемнике строить такое же дерево.
+"""
+
+from collections import Counter, deque
+
+
+class BinaryTree:
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+
+class HaffmanTree:
+    def __init__(self):
+        self._code = []
+
+    def encode(self, string):
+        self._walk(self.build(string))
+
+    def build(self, string):
+        def __build(seq):
+            if len(seq) == 1:
+                return seq[0]
+
+            el1, el2 = seq.popleft(), seq.popleft()
+            weight = el1[1] + el2[1]
+            new_el = BinaryTree(left=el1[0], right=el2[0])
+
+            if len(seq) <= 0:
+                seq.insert(0, new_el)
+                return seq[0]
+            else:
+                for i in range(len(seq)):
+                    if seq[i][1] >= weight:
+                        seq.insert(i, (new_el, weight))
+                        break
+                    if i == len(seq) - 1:
+                        seq.append((new_el, weight))
+                __build(seq)
+            return seq[0]
+
+        return __build(self._initial(string))
+
+    def _walk(self, node, left=None):
+        if left is not None:
+            self._code.append('0') if left else self._code.append('1')
+        if isinstance(node, str):
+            # print(node, self._code)
+            print(''.join(self._code), end='')
+            self._code.pop()
+            return
+        self._walk(node.left, True)
+        self._walk(node.right, False)
+        if len(self._code) > 0:
+            self._code.pop()
+
+    def _initial(self, string):
+        return deque(sorted(dict(Counter(string)).items(), key=lambda i: i[1]))
+
+
+if __name__ == '__main__':
+    s = 'beep boop beer!'
+    ht = HaffmanTree()
+    ht.encode(s)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -12,17 +12,37 @@
 протестируйте на реальных данных - на клиентском коде.
 """
 
+"""
+Предлагаемое решение декоратором, вроде как для расширения функционала, не меняя исходного решения, правда я завязался 
+на имя функции, что на мой взгляд не очень хорошо, но зато получается один декоратор для всех функций.  
+"""
+
+
+def before_insert(func):
+    def wrapper(node, newnode):
+        if func.__name__ == 'insert_left' and node.root < newnode:
+            raise ValueError(f'Значение должно быть меньше {node.root}')
+        elif func.__name__ == 'insert_right' and node.root > newnode:
+            raise ValueError(f'Значение должно быть больше {node.root}')
+        else:
+            pass
+
+        return func(node, newnode)
+    return wrapper
+
 
 class BinaryTree:
-    def __init__(self, root_obj):
+    def __init__(self, root_obj, value=0):
         # корень
         self.root = root_obj
         # левый потомок
         self.left_child = None
         # правый потомок
         self.right_child = None
+        self.value = 0
 
     # добавить левого потомка
+    @before_insert
     def insert_left(self, new_node):
         # если у узла нет левого потомка
         if self.left_child == None:
@@ -36,8 +56,10 @@ class BinaryTree:
             # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.left_child = self.left_child
             self.left_child = tree_obj
+        return self.left_child
 
     # добавить правого потомка
+    @before_insert
     def insert_right(self, new_node):
         # если у узла нет правого потомка
         if self.right_child == None:
@@ -51,6 +73,7 @@ class BinaryTree:
             # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.right_child = self.right_child
             self.right_child = tree_obj
+        return self.right_child
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -69,14 +92,20 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+if __name__ == '__main__':
+    r = BinaryTree(8)
+    # print(r.get_root_val())
+    # print(r.get_left_child())
+    r.insert_left(4)
+    b1 = r.insert_left(2)
+    b1.insert_right(3)
+    r.insert_left(4)
+    # print(r.get_left_child())
+    # print(r.get_left_child().get_root_val())
+    b2 = r.insert_right(12)
+    b2.insert_left(4)
+    # print(r.get_right_child())
+    # print(r.get_right_child().get_root_val())
+    # r.get_right_child().set_root_val(16)
+    # print(r.get_right_child().get_root_val())
+    print(r)
