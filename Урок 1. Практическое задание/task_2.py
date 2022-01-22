17fd8259bbbb8efa5ded8ca0bd51b59a522799ad
aleksandrhmelev <ahmelev@gmail.com>
2022-01-22 15:26:32 +0700

    start

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -4,12 +4,41 @@
 Реализуйте два алгоритма.
 
 Оба должны обеспечивать поиск минимального значения для списка.
+"""
 
+lst = [5, 8, 10, 3, 18, 4, 75]
+
+"""
 Сложность первого алгоритма должна быть O(n^2) - квадратичная.
+"""
 
+
+def min_val_v1(list):
+    first_element = list[0]
+    for i in list[1:]:
+        if i < first_element:
+            first_element = i
+    return first_element
+
+
+print(min_val_v1(lst))
+
+"""
 Сложность второго алгоритма должна быть O(n) - линейная.
+"""
 
 
+def min_val_v2(list):
+    first_element = list.pop()
+    for i in list:
+        if i < first_element:
+            first_element = i
+    return first_element
+
+
+print(min_val_v2(lst))
+
+"""
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 -- нельзя использовать встроенные функции min() и sort()
 """
