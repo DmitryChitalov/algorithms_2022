d39a59374be00777d1e0091239643fb11a04b13c
admin <ostap_to@mail.ru>
2022-01-25 00:21:19 +0800

    Revert "Lesson 1, task 4 complited."
    This reverts commit 97a2fe4d

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -1,41 +1,16 @@
-class DequeClass:
-    def __init__(self):
-        self.elems = []
+"""
+Задание 7. На закрепление навыков работы с деком
 
-    def is_empty(self):
-        return self.elems == []
+В рассмотренном на уроке листинге есть один недостаток
+Приведенный код способен "обработать" только строку без пробелов,
+например, 'топот'
 
-    def add_to_front(self, elem):
-        self.elems.append(elem)
+Но могут быть и такие палиндромы, как 'молоко делили ледоколом'
 
-    def add_to_rear(self, elem):
-        self.elems.insert(0, elem)
+Вам нужно доработать программу так, чтобы она могла выполнить
+проверку на палиндром
+и в таких строках (включающих пробелы)
 
-    def remove_from_front(self):
-        return self.elems.pop()
-
-    def remove_from_rear(self):
-        return self.elems.pop(0)
-
-    def size(self):
-        return len(self.elems)
-
-
-def pal_checker(string):
-    dc_obj = DequeClass()
-    new_str = string.replace(' ', '')
-    for el in new_str:
-        dc_obj.add_to_rear(el)
-
-    still_equal = True
-
-    while dc_obj.size() > 1 and still_equal:
-        first = dc_obj.remove_from_front()
-        last = dc_obj.remove_from_rear()
-        if first != last:
-            still_equal = False
-
-    return still_equal
-
-
-print(pal_checker("молоко делили ледоколом"))
+Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
+--код с нуля писать не нужно, требуется доработать пример с урока
+"""
