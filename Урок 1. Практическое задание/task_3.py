9e0d9e7c0bd5e2e559fb091b5bc8549c71ec3906
Damir <damir.urmanov2003@mail.ru>
2022-01-24 21:59:27 +0500

    lesson_1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -16,4 +16,28 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+from itertools import islice
 
+companies_salaries = {'Amazon': 10000, 'Audi': 5000, 'Tesla': 30000000000000, 'Coca-Cola': 2000, 'KFC': 9202,
+                      'SpaceX': 3440, 'Facebook': 49395, 'Google': 354645400000, 'Apple': 7757345000000}
+key_val = list(companies_salaries.items())
+
+
+def sorted_ver():  # Итог: O(n log n)
+    key_val_sorted = sorted(key_val, key=lambda i: i[1], reverse=True)  # O(n log n)
+    for v in islice(key_val_sorted, 3):  # O(n)
+        print('sorted: ', v)  # O(1)
+
+
+sorted_ver()
+
+
+def max_ver():  # Итог: O(n)
+    for v in islice(key_val, 3):  # O(n)
+        print('max: ', max(key_val, key=lambda i: i[1]))  # O(n)
+        key_val.remove(max(key_val, key=lambda i: i[1]))  # O(n)
+
+
+max_ver()
+
+# Вывод: решение с max_ver() эффективнее, т.к O(n) потребляет меньше времени чем O(n log n)
