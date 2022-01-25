3e88b52b7ab75654ac53d17564049d78b073bd28
Timur <timur.pnz@mail.ru>
2022-01-25 09:53:19 +0300

    task_3,4 done

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -23,3 +23,72 @@ b) выполните со списком и словарем операции: 
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+import random
+from functools import wraps
+from time import time
+
+
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args):
+        ts = time()
+        result = f(*args)
+        te = time()
+        print(f.__name__, te-ts)
+        return result
+    return wrap
+
+test_list = []
+test_dict = {}
+#Сложность O(n)
+@timing
+def go_list():
+    for i in range(10000):
+        test_list.append(i+1)
+
+
+go_list()
+#Врем заполнения списска через append = 0.0010061264038085938
+
+#Сложность O(n)
+@timing
+def go_dict():
+    for i in range(10000):
+        test_dict.setdefault(i+1, chr(97+i))
+
+
+go_dict()
+#Врем заполнения словаря через setdefault = 0.003990650177001953
+
+"""
+Заполнения списска через append в несколько раз быстрее
+чам заполнения словаря через setdefault. Это связано с тем, 
+что не нужно хэшировать данны, на что тратится время.
+"""
+n = random.randint(0, 9)
+
+@timing
+def plus_list():
+    test_list[n] = n + n
+
+@timing
+def plus_dict():
+    test_dict[n] = n + n
+
+plus_list()
+plus_dict()
+
+
+
+@timing
+def pop_list():
+    test_list.pop(n)
+
+@timing
+def pop_dict():
+    test_dict.pop(n)
+
+pop_list()
+pop_dict()
+
