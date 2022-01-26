eb4c8185d9c997ba8ab3512c94159efa485fb1c6
Alexey <zombeaver@gmail.com>
2022-01-26 21:43:21 +0500

    done tasks_1-2

--- Урок 2. Практическое задание/Svatov_Alexey_dz_2/task_1_re_v3.py
+++
@@ -1,5 +0,0 @@
-def mathematics(operator):
-	(return print(f'Выход из программы'), [return 'Ваш результат: {0}'.format(eval(input('Введите первое число: ')+operator+input('Введите второе число: '))), mathematics(input('Введите операцию: '))])[operator == '0']
-			
-mathematics(input('Введите операцию: '))
-			
