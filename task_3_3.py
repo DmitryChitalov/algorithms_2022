c1687f39f34cbce8b75ed2b57281a7cee8fbaad5
Mary <pereverzeva.mary@gmail.com>
2022-01-26 11:20:01 +0300

    task_3_3 added

---
+++ task_3_3.py
@@ -0,0 +1,34 @@
+from collections import deque
+from timeit import timeit
+
+lst_test = [5, 60, 900, 8000, 15000, 350000, 700000]
+deq_test = deque('307981456820')
+
+num = 5
+
+""" 3. операции получения элемента списка и дека """
+# Список
+
+
+def get_el_from_lst(lst_test):
+    for i in range(num):
+        j = lst_test[i]
+
+
+# Дек
+
+
+def get_el_from_deq(deq_test):
+    for i in range(num):
+        j = deq_test[i]
+
+
+# Замеры времени
+print(timeit("get_el_from_lst(lst_test)", globals=globals()))
+print(timeit("get_el_from_deq(deq_test)", globals=globals()))
+
+"""
+1.5871612
+1.4988796
+Элементы из дека можно получить быстрее
+"""
