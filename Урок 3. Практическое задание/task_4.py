83b5b380a5a037eb3078a27f995364974e6fd036
admin <ostap_to@mail.ru>
2022-01-28 12:52:50 +0800

    Lesson_3, task_4 complited.

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,24 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+from hashlib import pbkdf2_hmac
+
+
+def hash_url(input_url):
+    input_url_hash = pbkdf2_hmac('sha256',
+                                 password=(input_url.encode('UTF-8')),
+                                 salt=b'bubu',
+                                 iterations=10000)
+    print(dict_url)
+    if input_url_hash == dict_url.get(input_url):
+        print(f'хеш-url: {input_url_hash}')
+    else:
+        dict_url.update({input_url: input_url_hash})
+        print(dict_url)
+        return hash_url(input('Введите URL: '))
+
+
+
+dict_url = {}
+hash_url(input('Введите URL: '))
+
