064839404d9e2231a593aac3b5f2e8ad65ce2309
GDIfox <87175965+GDIfox@users.noreply.github.com>
2022-01-28 15:00:34 +0300

    Update task_5.py

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,49 @@
 в массив стопок (lst = [[], [], [], [],....]) либо созданием объекта
 класса-стек в самом же классе.
 """
+
+
+class PlateStackClass:
+    def __init__(self, max_plate=5):
+        self.elems = []
+        self.max_plate = max_plate
+
+    def is_empty(self):
+        return self.elems == []
+
+    def push_in(self, el):
+        """Предполагаем, что верхний элемент стека находится в конце списка"""
+        if len(self.elems) == 0:
+            new_plate_stack = [el]
+            self.elems.append(new_plate_stack)
+
+        elif len(self.elems[len(self.elems)-1]) == self.max_plate:
+            new_plate_stack = [el]
+            self.elems.append(new_plate_stack)
+        else:
+            self.elems[len(self.elems)-1].append(el)
+
+    def pop_out(self):
+        return self.elems.pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        return len(self.elems)
+
+
+plate_ob1 = PlateStackClass(5)
+plate_ob1.push_in(1)
+plate_ob1.push_in(2)
+plate_ob1.push_in(3)
+plate_ob1.push_in(4)
+plate_ob1.push_in(5)
+plate_ob1.push_in(6)
+plate_ob1.push_in(7)
+plate_ob1.push_in(8)
+plate_ob1.push_in(9)
+plate_ob1.push_in(10)
+plate_ob1.push_in(11)
+
+print(plate_ob1.elems)
