f85381e2c935cfbc40b9339eb74f0e2d1a805e4e
GDIfox <87175965+GDIfox@users.noreply.github.com>
2022-01-29 13:10:30 +0300

    Update task_1.py

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -23,3 +23,41 @@ b) выполните со списком и словарем операции: 
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+
+
+from datetime import datetime
+
+def timer(time_func):
+    def test_time():
+        start = datetime.now()
+        result = time_func()
+        print(datetime.now() - start)
+        return result
+    return test_time
+
+@timer
+def spisok():
+    l = []
+    for i in range(1, 10000):
+        if i**2 % 2 == 0:
+            l.append(i)
+    return l
+
+
+@timer
+def dic():
+    d = {}
+    for i in range(1, 10000):
+        if i % 2 == 0:
+            d[i] = i**2
+    return d
+
+
+
+l = spisok()      # 0:00:00.003000
+d = dic()         # 0:00:00.003001
+
+# пример может быть не удачный получился, но по факту
+# словарь обрабатывается медленее за счет того что когда добавляется
+# информация, необходимо посчитать хеш, а всписок просто добавляется
+# например в конец
