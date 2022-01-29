9a52372bd04178f57b5620d1bbb39f996a26c058
Evgeny <evgeny.hyvarinen@gmail.com>
2022-01-29 19:24:47 +0200

    task 1 lesson 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,7 +13,7 @@
 """
 
 from timeit import timeit
-
+numbers = list(range(50000))
 
 def func_1(nums):
     new_arr = []
@@ -21,3 +21,47 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+print("func_1:", timeit("func_1(numbers)", "from __main__ import func_1, numbers", number=1000))
+
+
+def func_2(nums: list) -> list:
+    """
+    функция оптимизирована с помощью list comprehension работает быстрей чем обычный цикл примерно на 20% если верить
+    замерам времени
+    :param nums: list
+    :return: list
+    """
+    new_arr = [x for x in range(len(nums)) if nums[x] % 2 == 0]
+    return new_arr
+
+
+print("func_2:", timeit("func_2(numbers)", "from __main__ import func_2, numbers", number=1000))
+
+
+def func_3(nums: list):
+    """
+    Оптимизирована функция возвращает генератор по которому надо еще обойти либо сделать сгенерировать список
+    :param nums: list
+    :return: generator
+    """
+    new_arr = (x for x in range(len(nums)) if nums[x] % 2 == 0)
+    return new_arr
+
+print("func_3:", timeit("func_3(numbers)", globals=globals(), number=1000))
+
+
+def func_4(nums: list):
+    """
+    filter не хранит значения в памяти, следственно работает быстрее, но функция возвращает итератор
+    который в последствии нужно обойти либо сгенерировать список что потребует ресурсов и имеет свою сложность
+    выполнения, однако на мой взгляд самая оптимизированая функция
+    :param nums: list
+    :return: iterator
+    """
+    new_arr = filter(lambda x: x % 2 == 0, nums)
+    return new_arr
+
+
+print("func_4:", timeit("func_4(numbers)", globals=globals(), number=1000))
