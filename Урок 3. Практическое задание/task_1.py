f4a6e7333028efaccd53ff0722b95f86270cdec1
Alexandra <scribbo@mail.ru>
2022-01-30 12:59:56 +0300

    HW_3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -23,3 +23,79 @@ b) выполните со списком и словарем операции: 
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+
+
+def time_of_func(func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        t = time.perf_counter()
+        func(*args, **kwargs)
+        print(f'Время выполнения функции {func.__name__} равно {time.perf_counter() - t}')
+    return wrapper
+
+
+@time_of_func                               #> 0.0009686
+def fill_list(n, new_list):
+    for i in range(n):
+        new_list.append(i)                  #O(1)
+    return new_list
+
+
+my_list = []
+fill_list(10000, my_list)
+
+
+@time_of_func                               #> 0.0013688999999999993
+def fill_dict(n, new_dict):
+    for i in range(n):
+        new_dict[i] = i+1                   #O(1)
+    return new_dict
+
+
+my_dict = {}
+fill_dict(10000, my_dict)
+
+# заполнение списка быстрее, т.к. при заполнении словоря тратится дополнительное время на хэширование
+
+
+@time_of_func                               #> 0.006703600000000004
+def change_list(n, new_list):
+    for i in range(n):
+        new_list.insert(i, i + 1)           #O(n)
+    return new_list
+
+
+print(change_list(1000, my_list))
+
+
+@time_of_func                               #> 0.00011830000000000174
+def change_dict(n, new_dict):
+    for i in range(n):
+        new_dict[i] = i + 1                 #O(1)
+    return new_dict
+
+
+print(change_dict(1000, my_dict))
+
+
+@time_of_func                               #> 0.0017938999999999941
+def remove_list_el(n, new_list):
+    for i in range(n):
+        new_list.pop(i)                     #O(n)
+    return new_list
+
+
+print(remove_list_el(1000, my_list))
+
+
+@time_of_func                                #> 0.0001212999999999978
+def remove_dict_el(n, new_dict):
+    for i in range(n):
+        new_dict.pop(i)                      #O(1)
+    return new_dict
+
+
+print(remove_dict_el(1000, my_dict))
+
+# Изменение и удаление элементов словаря быстрее, чем изменение и удаление элементов списка.
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,34 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+
+class Cache:
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.entries = {}
+
+    def password_hash(self, p):
+        from hashlib import pbkdf2_hmac
+        from binascii import hexlify
+
+        obj = pbkdf2_hmac(hash_name=self.protocol,
+                          password=p.encode('utf-8'),
+                          salt=b'whybother',
+                          iterations=1)
+        return hexlify(obj)
+
+    def add(self, url):
+        urlhash = self.password_hash(url)
+        if urlhash in self.entries.values():
+            return urlhash
+        else:
+            self.entries.update({url: urlhash})
+
+
+my_url_cache = Cache('sha512')
+my_url_cache.add('https://docs.python.org/3/library/stdtypes.html#typesmapping')
+print(my_url_cache.add('https://wikipedia.org/'))
+print(my_url_cache.add('https://docs.python.org/3/library/stdtypes.html#typesmapping'))
+print(f'Cache contains {len(my_url_cache.entries)} records')
+
