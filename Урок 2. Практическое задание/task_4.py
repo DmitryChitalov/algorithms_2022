228f1466dab1d5359041d4fbe1061e340972ebae
geekbrains <info@geekbrains.ru>
2022-01-31 12:38:45 +0300

    dz_2

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -10,3 +10,29 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Нужно обойтисть без создания массива!
 """
+from sys import getrecursionlimit, setrecursionlimit
+
+
+def recursive_number_series_sum(count, sum_=1.0):  # Решение через рекурсию.
+    if count <= 1:
+        return sum_
+    sum_ += recursive_number_series_sum(count - 1, -sum_ / 2)
+    return sum_
+
+
+def lh_number_series_sum(num):  # ДЛЯ ПРОВЕРКИ !!! Решение через цикл
+    return sum([1 / (2 ** n) * (1, -1)[n % 2 or 0] for n in range(num)])
+
+
+print(f'{       lh_number_series_sum(3)=}')
+print(f'{recursive_number_series_sum(3)=}')
+print(f'{       lh_number_series_sum(20)=}')
+print(f'{recursive_number_series_sum(20)=}')
+# c num = 999 ошибка: RecursionError: maximum recursion depth exceeded in comparison
+print(f'{getrecursionlimit()=}')
+setrecursionlimit(2002)
+print(f'{getrecursionlimit()=}')
+print(f'{       lh_number_series_sum(2000)=}')
+print(f'{recursive_number_series_sum(2000)=}')
+
+
