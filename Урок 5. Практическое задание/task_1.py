5c3464286a7299c39f1ee65a1852260677a0c628
Evgeny <evgeny.hyvarinen@gmail.com>
2022-02-01 00:15:09 +0200

    task 1 lesson 5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -28,3 +28,44 @@
 Предприятия, с прибылью выше среднего значения: Рога
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+import collections
+import re
+import statistics
+
+
+def base_questions() -> tuple:
+    company_name = input("Введите название предприятия\n").title()
+    company_profit = re.findall(r"\d+", input("Прибыль предприятия \33[32m%s\33[0m за каждый квартал "
+                                              "(через пробел, всего 4 квартала) \n" % company_name))
+    return company_name, company_profit
+
+
+def company_stat(storage) -> str:
+    if storage:
+        average = statistics.mean((x.profit for x in storage))
+        top_companies = ",".join([x.name for x in filter(lambda x: x.profit >= average, storage)])
+        floor_companies = ",".join([x.name for x in filter(lambda x: x.profit < average, storage)])
+
+        return "Средняя годовая прибыль всех предприятий:\33[34m %.2f \33[0m \nПредприятия, с прибылью выше среднего " \
+               "значения:\33[32m %s \33[0m \nПредприятия, с прибылью ниже среднего значения:\33[31m %s \33[0m\n" % (
+                   average, top_companies, floor_companies)
+
+
+""" Вариант 1 namedtuple """
+def fill_namedTuple(num: int) -> list:
+    COMPANY = collections.namedtuple("company", "name profit q1 q2 q3 q4", defaults=["unknown", 0, 0, 0, 0, 0])
+    lst = []
+    for _ in range(0, num):
+        company_name, company_profit = base_questions()
+        try:
+            lst.append(COMPANY(company_name, sum(map(float, company_profit)), *map(float, company_profit)))
+        except TypeError:
+            print("\33[31m Прибыль предприятия через пробел, всего 4 квартала\33[0m ")
+            continue
+    return lst
+
+
+if __name__ == "__main__":
+    number = int(input("Введите количество предприятий для расчета прибыли\n"))
+    storage = fill_namedTuple(number)
+    print(company_stat(storage))
