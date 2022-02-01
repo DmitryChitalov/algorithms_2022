11e3e47c99c6f5e7a5cd278fa31381461477bae6
AntonLev <www.Garu33@mail.ru>
2022-02-01 21:00:23 +0300

    HW_3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,27 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+from hashlib import sha512
+from uuid import uuid4
+
+
+class cash_web:
+    def __init__(self):
+        self.urls = {}
+
+    def hashing(self, url):
+        salt = uuid4().hex
+        url_hash = sha512(salt.encode() + url.encode()).hexdigest()
+        return url_hash
+
+    def cash_save(self, url):
+        url_hash = self.hashing(url)
+        self.urls.setdefault(url, url_hash)
+        return print(f'Запись {url} добавлена!')
+
+
+url_1 = 'https://habr.com/ru/company/ruvds/blog/'
+search_url = cash_web()
+search_url.cash_save(url_1)
+print(search_url.urls)
