9447b547d526dd5b6f5e54e86db310f92127c93e
Anastasia <Ambrella915@gmail.com>
2022-02-01 16:03:26 +0300

    Lesson_3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,29 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+"""library = {'gogo': '2b3993b869bf9fe5fbf66c62cd3219771d028e1a03787e52d8c8f482cf2e03d7',  # 123
+           'wow': '7bde3a244ee3d33225d6df473aba2f539e3f84460bfb78bd6a3e77d31b259218',  # 456
+           'qwerty': 'fab6d43655bbca6e71240739f3aad3f9050b5a051924ab3a3bd17a9678525a74'  # 789
+           }
+
+
+def authentication():
+    login = input('Введите логин: ')
+    password = input('Введите пароль: ')
+    password_two = input('Введите пароль еще раз: ')
+    hash_password = sha256(login.encode() + password.encode()).hexdigest()
+    return login, hash_password
+
+
+authentication()"""
+import sqlite3
+from hashlib import sha256
+
+def database_connection():
+    conn = sqlite3.connect('Chinook_Sqlite.sqlite')
+    cursor = conn.cursor()
+    conn.close()
+
+
+
+
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,21 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+from uuid import uuid4
+import hashlib
+
+salt = uuid4().hex
+cache_url = {}
+
+
+def url_address(url_add):
+    if cache_url.get(url_add):
+        print(f'{url_add} есть в кэше.')
+    else:
+        result = hashlib.sha256(salt.encode() + url_add.encode()).hexdigest()
+        cache_url[url_add] = result
+        print(f'{url_add}: {result}')
+
+
+url_address('https://translate.google.com/')
+url_address('https://translate.google.com/')
