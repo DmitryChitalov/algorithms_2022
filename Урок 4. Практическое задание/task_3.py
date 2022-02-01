966084754ea652c49e23fc7c431ab9f55d5338c9
LeonidPolizharov <polizharov@mail.ru>
2022-02-01 15:49:48 +0300

    HW-4_v2

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -5,22 +5,23 @@
 обратное по порядку входящих в него
 цифр и вывести на экран.
 
-Сделайте профилировку каждого алгоритма через cProfile и через timeit
+Сделайте профилировку каждого алгоритма через timeit
 
 Обязательно предложите еще свой вариант решения!
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
 """
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return int(revers_num)  # добавил вывод int(revers_num), иначе не работало
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers(enter_num, revers_num)  # добавил return, иначе не работало
 
 
 def revers_2(enter_num, revers_num=0):
@@ -28,10 +29,40 @@ def revers_2(enter_num, revers_num=0):
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-    return revers_num
+    return int(revers_num)
 
 
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    enter_num = list(str(enter_num))
+    enter_num.reverse()
+    revers_num = ''.join(enter_num)
+    return revers_num
+
+
+numb = 1233576457567
+print(timeit("revers(numb)", setup="from __main__ import revers, numb",
+             number=1000))
+# Самый долгий алгоритм т.к. использована рекурсия,сложность экспоненциальная
+# 0.003428500000000001
+
+print(timeit("revers_2(numb)", setup="from __main__ import revers_2, numb",
+             number=1000))
+# Алгоритм через цикл, несколько быстрее предыдущего, сложность O(N)
+# 0.002561599999999997
+
+print(timeit("revers_3(numb)", setup="from __main__ import revers_3, numb",
+             number=1000))
+# Реверс числа с помощью среза получается самым эффективным, судя по времени исполнения, сложность O(N)
+# 0.0003438000000000052
+
+print(timeit("revers_4(numb)", setup="from __main__ import revers_4, numb",
+             number=1000))
+# Алгоритм с использованием встроенной функции reverse() и джойном тоже довольно эффективен, хотя и дольше среза, 
+# сложность O(N) 
+# 0.0007293999999999981 
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,11 +9,10 @@
 Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
+from timeit import timeit
 
-array = [1, 3, 1, 3, 4, 5, 1]
 
-
-def func_1():
+def func_1(array):
     m = 0
     num = 0
     for i in array:
@@ -25,7 +24,7 @@ def func_1():
            f'оно появилось в массиве {m} раз(а)'
 
 
-def func_2():
+def func_2(array):
     new_array = []
     for el in array:
         count2 = array.count(el)
@@ -37,5 +36,50 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3(array):
+    my_dict = {}
+    for i in array:
+        my_dict.setdefault(i, 0)
+        my_dict[i] += 1
+    max_val = max(list(my_dict.items()), key=lambda j: j[1])
+    return f'Чаще всего встречается число {max_val[0]}, ' \
+           f'оно появилось в массиве {max_val[1]} раз(а)'
+
+
+def func_4(array):
+    max_val1 = max(list(dict((x, array.count(x)) for x in set(array)).items()), key=lambda j: j[1])
+    return f'Чаще всего встречается число {max_val1[0]}, оно появилось в массиве {max_val1[1]} раз(а)'
+
+
+array = [1, 3, 1, 3, 4, 5, 1]
+array2 = [1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1]
+
+
+print(func_1(array))
+print(timeit("func_1(array)", setup="from __main__ import func_1, array", number=1000))
+# 0.0018184000000000047
+print(func_2(array))
+print(timeit("func_2(array)", setup="from __main__ import func_2, array", number=1000))
+# 0.0018588999999999967
+print(func_3(array))
+print(timeit("func_3(array)", setup="from __main__ import func_3, array", number=1000))
+# 0.002740599999999996
+print(func_4(array))
+print(timeit("func_4(array)", setup="from __main__ import func_4, array", number=1000))
+# 0.002879699999999999
+# При небольшом входном массиве изначальные алгоритмы работают быстрее.
+
+
+print(func_1(array2))
+print(timeit("func_1(array2)", setup="from __main__ import func_1, array2", number=1000))
+# 0.009771599999999998
+print(func_2(array2))
+print(timeit("func_2(array2)", setup="from __main__ import func_2, array2", number=1000))
+# 0.012465899999999995
+print(func_3(array2))
+print(timeit("func_3(array2)", setup="from __main__ import func_3, array2", number=1000))
+# 0.005186999999999997
+print(func_4(array2))
+print(timeit("func_4(array2)", setup="from __main__ import func_4, array2", number=1000))
+# 0.004168900000000003
+# На массиве побольше ситуация поменялась, исходные алгоритмы работают медленнее.
