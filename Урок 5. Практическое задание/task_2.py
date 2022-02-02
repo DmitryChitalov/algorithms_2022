cb79677f7908b681033fc01bb7189d60f5b05de2
GDIfox <87175965+GDIfox@users.noreply.github.com>
2022-02-02 16:15:15 +0300

    Update task_2.py

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,42 @@ reduce
 __mul__
 __add__
 """
+
+
+from collections import defaultdict
+from collections import deque
+
+
+def my_func_1(string):
+    func_1 = 0
+    num = deque(string)
+    num.reverse()
+    for i in range(len(num)):
+        func_1 += tab[num[i]] * 16 ** i
+    return func_1
+
+
+def my_func_2(numb):
+    num = deque()
+    while numb > 0:
+        d = numb % 16
+        for i in tab:
+            if tab[i] == d:
+                num.append(i)
+        numb //= 16
+    num.reverse()
+    return list(num)
+
+
+znak = '0123456789ABCDEF'
+tab = defaultdict(int)
+counter = 0
+for key in znak:
+    tab[key] += counter
+    counter += 1
+
+num_1 = my_func_1(input('Введите первое число в шестнадцатиричном формате:\n ').upper())
+num_2 = my_func_1(input('Введите второе число в шестнадцатиричном формате:\n ').upper())
+
+print(f'Сумма чисел: {my_func_2(num_1 + num_2)}')
+print(f'Произведение чисел: {my_func_2(num_1 * num_2)}')
