25d79a6ed19c3831f2b0b1742d5687fd380321fe
KosarevDV <KosarevDV@users.noreply.github.com>
2022-02-02 17:25:10 +0300

    Косарев ДВ., группа GU_python_1738, курс Алгоритмы от 12.01. ДЗ к уроку 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,7 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
 """
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
@@ -35,3 +36,28 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return int("".join(reversed(str(enter_num))))
+
+
+def revers_5(enter_num):
+    revers_num = 0
+    while enter_num > 0:
+        enter_num, r = divmod(enter_num, 10)
+        revers_num = revers_num * 10 + r
+    return revers_num
+
+
+n = 1234567
+
+print('Время рекурсии: ', timeit('revers(n)', number=1000, globals=globals()))
+print('Время цикла: ', timeit('revers_2(n)', number=1000, globals=globals()))
+print('Время среза: ', timeit('revers_3(n)', number=1000, globals=globals()))
+print('Время метода списка reversed: ', timeit('revers_4(n)', number=1000, globals=globals()))
+print('Время функции divmod: ', timeit('revers_5(n)', number=10000, globals=globals()))
+
+
+# Самый быстрый вариант - срез. Возможно потому что срез не выполняет вычисления
+# а посто берет элементы по индексам в обратном порядке
