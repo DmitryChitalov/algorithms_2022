b644fb73e36c0894c8e58f4602e5c9bbb13f7ac5
Vetrov Andrey <78134527+andrvet92@users.noreply.github.com>
2022-02-02 15:26:23 +0300

    Первое задание сделано!

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -15,3 +15,35 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class ToDoClass:
+    def __init__(self):
+        self.elements = []
+
+    def add_task(self, item):
+        self.elements.insert(0, item)
+
+    def size(self):
+        return len(self.elements)
+
+    def from_queue(self):
+        return self.elements.pop()
+
+    def relocate(self, other, item):
+        other.elements.insert(0, item.from_queue())
+
+
+if __name__ == '__main__':
+    main_tasks = ToDoClass()
+    imp_tasks = ToDoClass()
+
+    main_tasks.add_task('Убрать квартиру')
+    main_tasks.add_task('Приготовить ужин на вечер')
+    main_tasks.add_task('Купить пылесос')
+    main_tasks.add_task('Тренировка')
+
+    print(main_tasks.size())  # Смотрим размер основного списка задач
+    main_tasks.relocate(imp_tasks, main_tasks)  # Переносим задачу из основного списка в список доработок
+    print(imp_tasks.size())  # Смотрим размер списка на доработки
+    print(imp_tasks.from_queue())  # Смотрим какая задача в списке на доработки
