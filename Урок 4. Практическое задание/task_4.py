c34a55d8cff5424e019f295f11228d70737a18f2
ahmelev <ahmelev@gmail.com>
2022-02-03 22:35:56 +0700

    Задача 1, 3, 4 - заменил global на "from __main__ import" так как именно на 4 задаче выяснилось что global притормаживает

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,6 +9,8 @@
 Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
 """
+from collections import Counter
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -37,5 +39,22 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    number = Counter(array).most_common(1)
+    return f'Чаще всего встречается число {number[0][0]},' \
+           f'оно появилось в массиве {number[0][1]} раз(а)'
+
+
+def func_4():
+    return f'Чаще всего встречающееся число и количество вхождений в массив: {str(Counter(array).most_common(1)).strip("[()]")}'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+print(f'Замер времени функции func_1: {timeit("func_1", "from __main__ import func_1", number=10000)}')
+print(f'Замер времени функции func_2: {timeit("func_2", "from __main__ import func_2", number=10000)}')
+print(f'Замер времени функции func_3: {timeit("func_3", "from __main__ import func_3", number=10000)}')
+print(f'Замер времени функции func_3: {timeit("func_4", "from __main__ import func_4", number=10000)}')
