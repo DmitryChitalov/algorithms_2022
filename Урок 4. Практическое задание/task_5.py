12df2500da0f93efde8e4232146d30ea6d4a72ed
Damir <damir.urmanov2003@mail.ru>
2022-02-04 02:09:09 +0500

    lesson_4

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -12,6 +12,7 @@
 10, 100, 1000
 Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
 """
+from timeit import timeit
 
 
 def simple(i):
@@ -34,5 +35,38 @@ def simple(i):
     return n
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+def eratosfen(i):
+    try:
+        n = 2
+        v = i**2
+        sieve = [x for x in range(v)]
+        sieve[1] = 0
+        while n < v:
+            if sieve[n] != 0:
+                m = n * 2
+                while m < v:
+                    sieve[m] = 0
+                    m += n
+            n += 1
+        return [p for p in sieve if p != 0][i - 1]
+    except IndexError:
+        return 2
+
+
+idx = int(input('Введите порядковый номер искомого простого числа: '))
+print(simple(idx))
+print(eratosfen(idx))
+
+print(f"Наивный алгоритм 10: {timeit('simple(10)', globals=globals(), number=10)}")
+print(f"Решето Эратосфена 10: {timeit('eratosfen(10)', globals=globals(), number=10)}\n")
+
+print(f"Наивный алгоритм 100: {timeit('simple(100)', globals=globals(), number=10)}")
+print(f"Решето Эратосфена 100: {timeit('eratosfen(100)', globals=globals(), number=10)}\n")
+
+print(f"Наивный алгоритм 1000: {timeit('simple(1000)', globals=globals(), number=10)}")
+print(f"Решето Эратосфена 1000: {timeit('eratosfen(1000)', globals=globals(), number=10)}")
+
+print('\nВывод: я не смог найти Решето Эратосфена в интернете и в материалах, поэтому взял его у вас из примера. \n'
+      'По подсчетам, "наивный" алгоритм срабатывает быстрее чем Решето Эратосфена. Плюс к этому, \n'
+      'у вас в примере если ввести аргумент = 1, то в Решето будет ошибка, поэтому я добавил обработку исключений \n'
+      'к функции (проверял и без нее, Решето быстрее не становится)')
