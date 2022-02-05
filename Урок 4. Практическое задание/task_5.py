ceef49056251560be116523018c4496cbf4f7c5f
Damir <damir.urmanov2003@mail.ru>
2022-02-06 03:12:03 +0500

    lesson_4_corrected_eratosthenes

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -12,9 +12,24 @@
 10, 100, 1000
 Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
 """
-from timeit import timeit
+from timeit import timeit, default_timer
 
 
+# def time_it(n):
+#     def deco(func):
+#         def wrapper(*args):
+#             t_sum = 0
+#             for el in range(n):
+#                 start_time = default_timer()
+#                 func(*args)
+#                 delta = default_timer() - start_time
+#                 t_sum += delta
+#             return t_sum
+#         return wrapper
+#     return deco
+
+
+# @time_it(100)
 def simple(i):
     """Без использования «Решета Эратосфена»"""
     count = 1
@@ -35,38 +50,35 @@ def simple(i):
     return n
 
 
-def eratosfen(i):
-    try:
-        n = 2
-        v = i**2
-        sieve = [x for x in range(v)]
-        sieve[1] = 0
-        while n < v:
-            if sieve[n] != 0:
-                m = n * 2
-                while m < v:
-                    sieve[m] = 0
-                    m += n
-            n += 1
-        return [p for p in sieve if p != 0][i - 1]
-    except IndexError:
-        return 2
+# @time_it(100)
+def eratosthenes(i):
+    # O(n log(log n))
+    """Используя алгоритм «Решето Эратосфена»"""
+    n = 2
+    l = 10000
+    sieve = [x for x in range(l)]
+    sieve[1] = 0
+    while n < l:
+        if sieve[n] != 0:
+            m = n*2
+            while m < l:
+                sieve[m] = 0
+                m += n
+        n += 1
+    return [p for p in sieve if p != 0][i-1]
 
 
 idx = int(input('Введите порядковый номер искомого простого числа: '))
 print(simple(idx))
 print(eratosfen(idx))
 
-print(f"Наивный алгоритм 10: {timeit('simple(10)', globals=globals(), number=10)}")
-print(f"Решето Эратосфена 10: {timeit('eratosfen(10)', globals=globals(), number=10)}\n")
-
+print(f"Наивный алгоритм 10: {timeit('simple(10)', globals=globals(), number=100)}")
+print(f"Решето Эратосфена 10: {timeit('eratosfen(10)', globals=globals(), number=100)}\n")
+#
 print(f"Наивный алгоритм 100: {timeit('simple(100)', globals=globals(), number=10)}")
 print(f"Решето Эратосфена 100: {timeit('eratosfen(100)', globals=globals(), number=10)}\n")
 
 print(f"Наивный алгоритм 1000: {timeit('simple(1000)', globals=globals(), number=10)}")
 print(f"Решето Эратосфена 1000: {timeit('eratosfen(1000)', globals=globals(), number=10)}")
 
-print('\nВывод: я не смог найти Решето Эратосфена в интернете и в материалах, поэтому взял его у вас из примера. \n'
-      'По подсчетам, "наивный" алгоритм срабатывает быстрее чем Решето Эратосфена. Плюс к этому, \n'
-      'у вас в примере если ввести аргумент = 1, то в Решето будет ошибка, поэтому я добавил обработку исключений \n'
-      'к функции (проверял и без нее, Решето быстрее не становится)')
+print('\nВывод: я не смог найти Решето в интернете и материалах, взял у вас из примера. Решето работает быстрее')
