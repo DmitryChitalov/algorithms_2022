9d11bfdc190ff35d9578cede7539303a6cabab8b
Timur <timur.pnz@mail.ru>
2022-02-05 16:10:35 +0300

    task_5 done

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -9,3 +9,21 @@
 Опищите эту проблему и найдите простой путь ее решения.
 Опишите этот путь и покажите его применение.
 """
+from memory_profiler import profile
+
+@profile
+def decor(func):
+    def wrapper(*argv):
+        return func(*argv)
+    return wrapper
+
+@decor
+def get_rev_number(number, rev_number = ''):                        # 0
+    if number == 0:                                                 # 0
+        return rev_number                                           # 0
+    else:
+        last_number = number % 10                                   # 0
+        rev_number += str(last_number)                              # 0
+        return get_rev_number(number // 10, rev_number)             # 0.2
+
+print(get_rev_number(1234))
