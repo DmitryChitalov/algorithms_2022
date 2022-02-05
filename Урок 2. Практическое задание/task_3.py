01aa810e6424e81ccf4aac272be7a5545592b6b0
Nikolay <green.kolayn@yandex.ru>
2022-02-05 14:33:46 +0300

    commit

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -22,3 +22,16 @@
 number = input('Введите число: ')
 
 print(f'Чило {number} в обратном порядке: {number[::-1]}')
+
+
+def check(num, a = ''):
+    if num == 0:
+        return a
+    else:
+        a += f'{num % 10}'
+        num = num // 10
+        return check(num, a)
+
+
+num = int(input('Введите число: '))
+print(f'Чило {num} в обратном порядке: {check(num)}')
