3ff2b28aca7d9595c27b626321c34db8e005f239
GDIfox <87175965+GDIfox@users.noreply.github.com>
2022-02-05 12:47:05 +0300

    Update task_2.py

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -9,3 +9,36 @@
 Опищите эту проблему и найдите простой путь ее решения.
 Опишите этот путь и покажите его применение.
 """
+
+from random import randint
+from memory_profiler import profile
+
+
+@profile
+def make_list():
+    # Cписком
+    result = [elem for idx, elem in enumerate(src) if elem > src[idx-1] and idx > 0]
+    print(result)
+
+
+@profile
+def make_generator():
+    # Оптимизация по памяти, генератором
+    result = (elem for idx, elem in enumerate(src) if elem > src[idx-1] and idx > 0)
+    print(result)
+
+
+if __name__ == "__main__":
+    src = [randint(0, 100000) for i in range(1000000)]
+
+    make_list()
+    make_generator()
+
+"""
+Оптимизация используя генератор. 
+попытаемся получить результат по памяти...
+В первом варианте lc, во втором генератор
+До:    62.2 MiB      0.0 MiB           1       print(result)
+После: 58.4 MiB      0.0 MiB           1       print(result)
+    
+"""
