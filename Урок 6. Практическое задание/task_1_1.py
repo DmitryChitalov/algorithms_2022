88dc2851d138af3747202fc94a029c54d36b265a
Evgeny <evgeny.hyvarinen@gmail.com>
2022-02-06 17:26:32 +0200

    task 1, 2 lesson 6

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -30,3 +30,120 @@
 
 Это файл для первого скрипта
 """
+from pympler import asizeof
+import collections
+import json
+import memory_profiler
+from timeit import timeit
+
+""" исходное """
+info_store = {
+    "poor company": -345.414,
+    "negative solutions": -31412515141412,
+    "Google vilage": 1324155.3130,
+    "Abb": 43431211,
+    "Siemens": 116002334.44,
+    "Reaktor base": 2030304,
+    "Kumara": 5922323.4214,
+    "Drama company": 323330314,
+    "Komedy Film": 203204124.31,
+    "Some other": 12034
+}
+
+#@memory_profiler.profile
+def get_three_profitable_comp(info_storage) -> dict:
+    """
+    Задание 3 из урока 1 курса Алгоритмы и структуры данных.
+    Задача:
+    Имеется хранилище с информацией о компаниях: название и годовая прибыль.
+    Для реализации хранилища можно применить любой подход,
+    который вы придумаете, например, реализовать словарь.
+    Реализуйте поиск трех компаний с наибольшей годовой прибылью.
+    Выведите результат.
+    Сложность: Т(n) = 2n + 5 => O(n)
+    """
+    tmp_storage = info_storage.copy()
+    max_annual_profit_companies = {}
+    for x in range(3):
+        company, value = max(tmp_storage.items(), key=lambda x: x[1])
+        del tmp_storage[company]
+        max_annual_profit_companies.update({company: value})
+    return max_annual_profit_companies
+
+
+""" оптимизированное """
+info_store_opt = json.dumps({
+    "poor company": -345.414,
+    "negative solutions": -31412515141412,
+    "Google vilage": 1324155.3130,
+    "Abb": 43431211,
+    "Siemens": 116002334.44,
+    "Reaktor base": 2030304,
+    "Kumara": 5922323.4214,
+    "Drama company": 323330314,
+    "Komedy Film": 203204124.31,
+    "Some other": 12034
+})
+
+#@memory_profiler.profile
+def get_three_profitable_comp_opt(storage: json) -> list:
+    """
+    Оптимизированая функция, Использование Counter вместо написания дополнительной логики.
+    Десиреализация обьекта.
+    :param storage: json string
+    :return: list
+    """
+    values = json.loads(storage)
+    counter = collections.Counter(values)
+    return counter.most_common(3)
+
+
+if __name__ == "__main__":
+    print(f"dict (info_store) size: {asizeof.asizeof(info_store)}")
+    print(f'function get_three_profitable_comp executing takes: '
+          f'{timeit("get_three_profitable_comp(info_store)", globals=globals(), number=100000)} s')
+    #print(get_three_profitable_comp(info_store))
+
+    print(f"serialized object (info_store_opt) size: {asizeof.asizeof(info_store_opt)}")
+    print(f'function get_three_profitable_comp_opt executing takes: '
+          f'{timeit("get_three_profitable_comp_opt(info_store_opt)", globals=globals(), number=100000)} s')
+    #print(get_three_profitable_comp_opt(info_store_opt))
+
+
+"""
+Результаты: 
+    dict (info_store) size: 1272
+    function get_three_profitable_comp executing takes: 0.5589341 s
+    serialized object (info_store_opt) size: 320
+    function get_three_profitable_comp_opt executing takes: 1.494539 s
+Вывод: 
+    Видна разница оптимиризования словаря, против сериализации обьекта в json -> потребление памяти уменьшилось в 4 раза.
+    Однако время затраченое на исполнения функции увеличилось почти в 3 раза.
+    Затраты памяти не столь велики поэтому профилирование памяти не показало особых результатов.
+    
+Профилирование памяти:
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    53     19.7 MiB     19.7 MiB           1   @memory_profiler.profile
+    54                                         def get_three_profitable_comp(info_storage) -> dict:                                   
+    65     19.7 MiB      0.0 MiB           1       tmp_storage = info_storage.copy()
+    66     19.7 MiB      0.0 MiB           1       max_annual_profit_companies = {}
+    67     19.7 MiB      0.0 MiB           4       for x in range(3):
+    68     19.7 MiB      0.0 MiB          57           company, value = max(tmp_storage.items(), key=lambda x: x[1])
+    69     19.7 MiB      0.0 MiB           3           del tmp_storage[company]
+    70     19.7 MiB      0.0 MiB           3           max_annual_profit_companies.update({company: value})
+    71     19.7 MiB      0.0 MiB           1       return max_annual_profit_companies
+
+
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    88     19.7 MiB     19.7 MiB           1   @memory_profiler.profile
+    89                                         def get_three_profitable_comp_opt(storage: json) -> list:
+    95     19.7 MiB      0.0 MiB           1       values = json.loads(storage)
+    96     19.7 MiB      0.0 MiB           1       counter = collections.Counter(values)
+    97     19.7 MiB      0.0 MiB           1       return counter.most_common(3)
+
+"""
+
--- Урок 6. Практическое задание/task_1_2.py
+++ Урок 6. Практическое задание/task_1_2.py
@@ -30,3 +30,91 @@
 
 Это файл для второго скрипта
 """
+import hashlib
+import string
+import random
+from uuid import uuid4
+
+import memory_profiler
+from pympler import asizeof
+from memory_profiler import profile
+
+""" исходное """
+
+
+class webCaching:
+    """
+    Задание 4 из урока 3 курса Алгоритмы и структуры данных.
+    Задача:
+    Реализуйте скрипт "Кэширование веб-страниц"
+    Функция должна принимать url-адрес и проверять есть ли в кэше соответствующая страница или нет.
+    """
+    def __init__(self):
+        self._cache = {}
+
+    #@memory_profiler.profile
+    def set_cache(self, url: str):
+        if url and url not in self._cache:
+            salt = uuid4().hex.encode("utf-8")
+            self._cache.update({url: hashlib.sha512(salt + url.encode("utf-8")).hexdigest()})
+        elif url in self._cache.keys():
+            print("already exists: %s" % self.show_cache[url])
+
+    @property
+    def show_cache(self):
+        return self._cache
+
+
+""" оптимизированное """
+
+class webCaching_opt:
+
+    __slots__ = "_cache"
+
+    def __init__(self):
+        self._cache = {}
+
+    #@memory_profiler.profile
+    def set_cache(self, url: str):
+        if url and url not in self._cache:
+            salt = uuid4().hex.encode("utf-8")
+            self._cache.update({url: hashlib.sha512(salt + url.encode("utf-8")).hexdigest()})
+        elif url in self._cache.keys():
+            print("already exists: %s" % self.show_cache[url])
+
+    @property
+    def show_cache(self):
+        return self._cache
+
+
+if __name__ == "__main__":
+    obj = webCaching()
+    obj.set_cache("https://vk.com")
+    obj.set_cache("https://vk.ru")
+    obj_opt = webCaching_opt()
+    obj_opt.set_cache("https://vk.com")
+    obj_opt.set_cache("https://vk.ru")
+
+    print(f"Class webCaching instance size: {asizeof.asizeof(obj)}")
+    print(f"Class webCaching_opt instance size: {asizeof.asizeof(obj_opt)}")
+
+"""
+Результаты: 
+    Class webCaching instance size: 960
+    Class webCaching_opt instance size: 48
+Вывод: 
+    Использование слотов существенно сокращет потребление памяти, с ростом количества записей в словаре, 
+    потребление памяти не увеличивалось в оптимизированном Классе.
+
+профилирование памяти метода set_cache(self):
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    75     19.8 MiB     19.8 MiB           1       @memory_profiler.profile
+    76                                             def set_cache(self, url: str):
+    77     19.8 MiB      0.0 MiB           1           if url and url not in self._cache:
+    78     19.8 MiB      0.0 MiB           1               salt = uuid4().hex.encode("utf-8")
+    79     19.8 MiB      0.0 MiB           1               self._cache.update({url: hashlib.sha512(salt + url.encode("utf-8")).hexdigest()})
+    80                                                 elif url in self._cache.keys():
+    81                                                     print("already exists: %s" % self.show_cache[url])
+"""
