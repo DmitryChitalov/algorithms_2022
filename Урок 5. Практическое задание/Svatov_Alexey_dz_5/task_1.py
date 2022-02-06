e374f2ac8f0b6500a01d213cb24af48cb9c9de58
Alexey <zombeaver@gmail.com>
2022-02-06 13:33:04 +0500

    done task_1

--- Урок 5. Практическое задание/Svatov_Alexey_dz_5/task_1.py
+++ Урок 5. Практическое задание/Svatov_Alexey_dz_5/task_1.py
@@ -28,5 +28,30 @@
 Предприятия, с прибылью выше среднего значения: Рога
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+import itertools
+from collections import namedtuple
 
 
+def main_info():
+    count = int(input('Введите количество предприятий для расчета прибыли: '))
+    company_list = []
+    Company_tuple = namedtuple('Company_tuple', ['name', 'first', 'second', 'third', 'fourth', 'annual_profit'])
+    for i in range(count):
+        company = input('Введите название предприятия: ')
+        cash = list(map(int, input('Через пробел введите прибыль данного предприятия за каждый квартал '
+                                   '(Всего 4 квартала): ').split()))
+        _ = Company_tuple(company, *cash, round(sum(cash)))
+        company_list.append(_)
+    return company_list
+
+
+def analize_info(input_info):
+    mean_annual_profit = sum(_.annual_profit for _ in input_info) / len(input_info)
+    company_upper_mean = str([_.name for _ in input_info if _.annual_profit >= mean_annual_profit]).strip('[]')
+    company_lower_mean = str([_.name for _ in input_info if _.annual_profit < mean_annual_profit]).strip('[]')
+    return f'Средняя годовая прибыль всех предприятий: {mean_annual_profit}\n' \
+           f'Предприятия, с прибылью выше среднего значения: {company_upper_mean}\n' \
+           f'Предприятия, с прибылью ниже среднего значения: {company_lower_mean}'
+
+
+print(analize_info(main_info()))
