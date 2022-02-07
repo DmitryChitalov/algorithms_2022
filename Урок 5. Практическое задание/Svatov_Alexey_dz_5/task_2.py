c3d7f54f1f39b79ce0d5d341e5aa2876e26417f0
alexeysvatov <zombeaver@gmail.com>
2022-02-07 11:04:51 +0500

    done task_2_first variant

--- Урок 5. Практическое задание/Svatov_Alexey_dz_5/task_2.py
+++ Урок 5. Практическое задание/Svatov_Alexey_dz_5/task_2.py
@@ -24,3 +24,31 @@ reduce
 __mul__
 __add__
 """
+
+# __________________________________________________________1___________________________________________________________
+
+from collections import defaultdict
+from functools import reduce
+
+
+def input_num():
+    ddict_num = defaultdict(list)
+    raw_num = input('Введите шестнадцатиричное число: ')
+    dec_num = int(raw_num, 16)
+    for el in raw_num:
+        ddict_num[dec_num].append(el)
+    return ddict_num
+
+
+def sum_hex(first_num, second_num):
+    ddict_result = defaultdict(list)
+    dec_sum_result = first_num.popitem()[0] + second_num.popitem()[0]
+    hex_sum_result = hex(dec_sum_result)[2:]
+    for el in hex_sum_result:
+        ddict_result[dec_sum_result].append(el)
+    return f'Сумма чисел из примера: {ddict_result[dec_sum_result]}'
+
+
+first = input_num()
+second = input_num()
+print(sum_hex(first, second))
