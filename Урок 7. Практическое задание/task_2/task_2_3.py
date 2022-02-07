89fc8dfeadfe341237e47b05e6c7f0074560824d
Demidov <demidov.evg.v@gmail.com>
2022-02-07 14:29:02 +0300

    demidov_hw8_v2

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -15,21 +15,3 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
-import random
-import numpy as np
-from timeit import repeat
-
-
-def get_median(arr):
-    return np.median(arr)
-
-m=10
-array_1 = np.array([i for i in random.choices(range(-100, 100), k=2*m + 1)])
-m=100
-array_2 = np.array([i for i in random.choices(range(-100, 100), k=2*m + 1)])
-m=1000
-array_3 = np.array([i for i in random.choices(range(-100, 100), k=2*m + 1)])
-
-for array_, m in [('array_1', 10), ('array_2', 100), ('array_3', 1000)]:
-    time_ = repeat(stmt=f'get_median({array_})', repeat=5, globals=globals(), number=1)
-    print(f'For {m = } execution time = {time_}')
