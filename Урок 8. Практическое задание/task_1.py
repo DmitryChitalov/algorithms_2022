cd5ca09159648fcf383f92f75607ee5c8f83b890
Demidov <demidov.evg.v@gmail.com>
2022-02-07 14:26:50 +0300

    demidov_hw_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -13,3 +13,154 @@
 Вы можете реализовать задачу, например,
 через ООП или предложить иной подход к решению.
 """
+# ====== Итоговый вывод ========
+# message = 'beep boop beer!'
+# message_code = ['00', '11', '11', '011', '101', '00', '010', '010', '011', '101', '00', '11', '11', '1001', '1000']
+# ==============================
+# code_table:
+# {' ': '101',
+#  '!': '1000',
+#  'b': '00',
+#  'e': '11',
+#  'o': '010',
+#  'p': '011',
+#  'r': '1001'}
+# ==============================
+# Sending message...
+# Receiving message...
+# Get message: beep boop beer!
+
+from collections import Counter
+from dataclasses import dataclass
+from pprint import pprint
+import shelve
+
+
+class HuffmanCoding:
+    @dataclass
+    class Element:
+        name: str
+        weight: int
+        tree: dict()
+
+    @classmethod
+    def get_huffman_code(cls, message: str):
+        elements_frequency = cls.get_elements_frequency(message)
+        tree = cls.get_huffman_tree(elements_frequency)
+        code_table = cls.get_huffman_table(tree)
+        code = []
+        for letter in message:
+            code.append(code_table[letter])
+        return code, code_table
+
+    @classmethod
+    def get_elements_frequency(cls,message: str):
+        element_frequency = sorted(Counter(message).items(), 
+                                    key=lambda el: (el[1], el[0]), 
+                                    reverse=True)
+
+        element_frequency = [cls.Element(name=element, weight=weight, tree=element) 
+                            for element, weight in element_frequency]
+        return element_frequency
+
+    @classmethod
+    def get_huffman_tree(cls, element_frequency: list[Element]):
+        if len(element_frequency) == 0:
+            return {}
+        elif len(element_frequency) == 1:
+            return {0: element_frequency.pop().name}
+            
+        while len(element_frequency) >= 2:
+            first_element_frequency = element_frequency.pop()
+            second_element_frequency = element_frequency.pop()
+
+            new_name = (f'{first_element_frequency.name}'
+                        f'{second_element_frequency.name}')
+
+            new_weight = (first_element_frequency.weight + 
+                                                    second_element_frequency.weight)
+            tree = {0: first_element_frequency.tree, 
+                    1: second_element_frequency.tree}
+            new_element_frequency = cls.Element(new_name, new_weight, tree)
+            if not element_frequency:
+                break
+                
+            if new_weight <= element_frequency[-1].weight:
+                element_frequency.append(new_element_frequency)
+            elif new_weight > element_frequency[0].weight:
+                element_frequency.insert(0, new_element_frequency)
+            else: 
+                for i in range(-1, -len(element_frequency)-1, -1):
+                    cur_element = element_frequency[i]
+                    if new_weight <= cur_element.weight:
+                        element_frequency.insert(i+1, new_element_frequency)
+                        break
+        
+        return new_element_frequency.tree  
+
+    @classmethod
+    def get_huffman_table(cls, tree: dict):
+        code_table = {}
+        def fill_code_table(tree, path=''):
+            if not isinstance(tree, dict):
+                code_table[tree] = path
+            elif len(tree) == 1:
+                fill_code_table(tree[0], path=f'{path}0')
+            else:
+                fill_code_table(tree[0], path=f'{path}0')
+                fill_code_table(tree[1], path=f'{path}1')
+
+        fill_code_table(tree, path='')
+
+        return code_table
+      
+
+class Sender:
+    @staticmethod
+    def send_message(message_code, *, setter, **kwargs):
+        with shelve.open('resourse') as db:
+            db['message_code'] = message_code
+            db['code_table'] = kwargs['code_table']
+
+
+class Recipient:
+    @classmethod
+    def get_message(cls, *, getter, resourse):
+        with shelve.open('resourse') as db:
+            message_code = db['message_code']
+            code_table = db['code_table']
+            return cls.decode_huffman_message(message_code, code_table)
+    
+    @classmethod
+    def decode_huffman_message(cls, message_code: list, code_table: dict):
+        message = []
+        for cur_code in message_code:
+            for letter, code in code_table.items():
+                if cur_code == code:
+                    message.append(letter)
+                    break          
+        return ''.join(message)
+
+
+        
+
+message = 'beep boop beer!'
+message_code, code_table = HuffmanCoding.get_huffman_code(message)
+print(f'{message_code = }')
+print('='*30)
+print('code_table: ')
+pprint(code_table)
+print('='*30)
+print('Sending message...')
+Sender.send_message(message_code, 
+                    setter=shelve.open, 
+                    resourse='resourse', 
+                    code_table=code_table)
+print('Receiving message...')
+message = Recipient.get_message(getter=shelve.open, resourse='resourse')
+print(f'Get message: {message}')
+
+
+
+
+
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -11,72 +11,152 @@
 Поработайте с оптимизированной структурой,
 протестируйте на реальных данных - на клиентском коде.
 """
+# Добавлен вывод дерева на печать
+# Например:
+#   _10__ 
+#  /     \
+#  8     12
+# / \   /
+# 7 9  11
+# Добавлена проверка для релазации бинарного дерева поиска, 
+# таким образом если встявляется неподходящее значение в ветку, 
+# то будет пользовательское исключение.
 
+class OrderError(ValueError):
+    pass
 
-class BinaryTree:
+ValueError
+class BinaryTreeSearch:
     def __init__(self, root_obj):
-        # корень
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
+        if new_node >= self.root:
+            raise OrderError(f'New node({new_node}) bigger or equal then parent({self.root})')
+
+        new_tree_obj = self.__class__(new_node)
         if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+            self.left_child = new_tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            if new_node > self.left_child.root:
+                new_tree_obj.left_child = self.left_child
+            else:
+                new_tree_obj.right_child = self.left_child
+            self.left_child = new_tree_obj
 
-    # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        if new_node < self.root:
+            raise OrderError(f'New node({new_node = }) less then parent({self.root})')
 
-    # метод доступа к правому потомку
+        new_tree_obj = self.__class__(new_node)
+        if self.right_child == None:
+            self.right_child = new_tree_obj
+        else:
+            if new_node <= self.right_child.root:
+                new_tree_obj.right_child = self.right_child
+            else:
+                new_tree_obj.left_child = self.right_child
+            self.right_child = new_tree_obj
+
     def get_right_child(self):
         return self.right_child
 
-    # метод доступа к левому потомку
     def get_left_child(self):
         return self.left_child
 
-    # метод установки корня
     def set_root_val(self, obj):
         self.root = obj
 
-    # метод доступа к корню
     def get_root_val(self):
         return self.root
 
+    def __str__(self):
+        def recursive_print(node=self):
+            sub_line = str(node.root)
+            sub_width = len(sub_line)
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+            if node.right_child is None and node.left_child is None:
+                height = 1
+                middle = sub_width // 2 
+                return [sub_line], sub_width, height, middle
+
+            if node.right_child is None:
+                _ = recursive_print(node.left_child)
+                lines, width_left_branch, height_left_branch, middle_left_branch = _
+
+                first_line = ((middle_left_branch + 1) * ' ' + 
+                              (width_left_branch - middle_left_branch - 1) * '_' + 
+                               sub_line)
+
+                second_line = (middle_left_branch * ' ' + '/' + 
+                              (width_left_branch - middle_left_branch - 1 + sub_width) * ' ')
+
+                shifted_lines = [line + sub_width * ' ' for line in lines]
+
+                return ([first_line, second_line] + shifted_lines, 
+                        middle_left_branch + sub_width, 
+                        height_left_branch + 2, 
+                        middle_left_branch + sub_width // 2)
+            
+            if node.left_child is None:
+                _ = recursive_print(node.right_child)
+                lines, width_right_branch, height_right_branch, middle_right_branch = _
+
+                first_line = (sub_line + middle_right_branch * '_' + 
+                             (width_right_branch - middle_right_branch) * ' ')
+
+                second_line = ((sub_width + middle_right_branch) * ' ' + '\\' + 
+                               (width_right_branch - middle_right_branch - 1) * ' ')
+
+                shifted_lines = [sub_width * ' ' + line for line in lines]
+
+                return ([first_line, second_line] + shifted_lines, 
+                        width_right_branch + sub_width, 
+                        height_right_branch + 2, 
+                        sub_width // 2)
+
+            _ = recursive_print(node.left_child)
+            left, width_left_branch, height_left_branch, middle_left_branch = _
+
+            _ = recursive_print(node.right_child)
+            right, width_right_branch, height_right_branch, middle_right_branch= _
+
+            first_line = ((middle_left_branch + 1) * ' ' + 
+                          (width_left_branch - middle_left_branch - 1) * '_' + 
+                          sub_line + middle_right_branch * '_' + 
+                          (width_right_branch - middle_right_branch) * ' ')
+
+            second_line = (middle_left_branch * ' ' + '/' + 
+                          (width_left_branch - middle_left_branch - 1 + 
+                            sub_width + middle_right_branch) * ' ' + 
+                          '\\' + (width_right_branch - middle_right_branch - 1) * ' ')
+
+            if height_left_branch < height_right_branch:
+                left += [width_left_branch * ' '] * (height_right_branch - height_left_branch)
+            elif height_right_branch < height_left_branch:
+                right += [width_right_branch * ' '] * (height_left_branch - height_right_branch)
+
+            zipped_lines = zip(left, right)
+            lines = [first_line, second_line] + [a + sub_width * ' ' + b for a, b in zipped_lines]
+
+            return (lines, width_left_branch + width_right_branch + sub_width, 
+                    max(height_left_branch, height_right_branch) + 2, 
+                    width_left_branch + sub_width // 2)
+       
+        lines, *_ = recursive_print(self)
+    
+        return '\n'.join(lines)
+    
+
+
+r = BinaryTreeSearch(10)
+r.insert_left(9)
+r.insert_left(8)
+r.get_left_child().insert_left(7)
+
 r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+r.get_right_child().insert_left(11)
+print(r)
+
