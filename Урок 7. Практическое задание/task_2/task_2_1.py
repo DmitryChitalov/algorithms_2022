1f94e08b047118fefa3ebd40cfbc80838951d424
KosarevDV <KosarevDV@users.noreply.github.com>
2022-02-08 00:09:54 +0300

    Косарев ДВ, гр. GU_python_1738, курс Алгоритмы от 12.01. ДЗ к уроку 7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -14,3 +14,32 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def find_median_shell(arr):
+    gap = len(arr) // 2
+    while gap:
+        for i in range(gap, len(arr)):
+            val = arr[i]
+            pos = i
+            while pos >= gap and arr[pos - gap] > val:
+                arr[pos] = arr[pos - gap]
+                pos -= gap
+                arr[pos] = val
+        gap = gap // 2
+    return arr[len(arr)//2]
+
+
+data_11 = [randint(-100, 100) for _ in range(2 * 5 + 1)]
+data_101 = [randint(-100, 100) for _ in range(2 * 50 + 1)]
+data_1001 = [randint(-100, 100) for _ in range(2 * 500 + 1)]
+
+print(data_11)
+print(sorted(data_11))
+print('Значение медианы: ', find_median_shell(data_11[:]))
+
+print('Время выполнения сортировки Шелла на массиве из 11 элементов: ', timeit('find_median_shell(data_11[:])', number=1000, globals=globals()))
+print('Время выполнения сортировки Шелла на массиве из 101 элементов: ', timeit('find_median_shell(data_101[:])', number=1000, globals=globals()))
+print('Время выполнения сортировки Шелла на массиве из 1001 элементов: ', timeit('find_median_shell(data_1001[:])', number=1000, globals=globals()))
