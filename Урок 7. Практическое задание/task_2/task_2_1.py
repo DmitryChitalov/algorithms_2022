f4dd93d744d421363ad5e4db72e249025eed5220
GDIfox <87175965+GDIfox@users.noreply.github.com>
2022-02-08 11:01:43 +0300

    Update task_2_1.py

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -14,3 +14,37 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+
+from random import randint
+from timeit import timeit
+
+def shella(massiv):
+    inc = len(massiv) // 2
+    while inc:
+        for i, elem in enumerate(massiv):
+            while i >= inc and massiv[i - inc] > elem:
+                massiv[i] = massiv[i - inc]
+                i -= inc
+            massiv[i] = elem
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+  
+data_1 = [randint (0,2000) for i in range(0, 10+1)]
+shella(data_1)
+print(f'{timeit("(data_1[:])", globals=globals(), number=1000)}', data_1)
+
+data_2 = [randint (0,2000) for z in range(0, 100+1)]
+shella(data_2)
+print(f'{timeit("(data_2[:])", globals=globals(), number=1000)}', data_2)
+
+data_3 = [randint (0,2000) for q in range(0, 1000+1)]
+shella(data_3)
+print(f'{timeit("(data_3[:])", globals=globals(), number=1000)}', data_3)
+
+"""
+сортировка Шелла:
+при 10    - 0.00014670006930828094
+при 100   - 0.0005620999727398157
+при 1000  - 0.002624700078740716
+"""
+
