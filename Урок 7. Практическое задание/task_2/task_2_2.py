aafab0850e60ca91fd6dc9fe40a6ede861ad8f7a
GDIfox <87175965+GDIfox@users.noreply.github.com>
2022-02-08 11:02:35 +0300

    Update task_2_2.py

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -13,3 +13,46 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+
+import random
+from timeit import timeit
+
+massive = []
+def mediana(argument):
+    for i in range(len(argument)):
+        less = 0
+        more = 0
+        for x in range(len(argument)):
+            if argument[i] < argument[x]:
+                less += 1
+            elif argument[i] > argument[x]:
+                more += 1
+            elif argument[i] == argument[x]:
+                less += 1
+                more += 1
+        if more == less:
+            median = argument[i]
+    return median
+
+
+
+for i in range(1000+1):
+    massive.append(random.randint(0, 2000))
+
+print(f'массив: {massive}')
+print(f'Медиана в массиве: {mediana(massive)}')
+print(f'{timeit("(mediana(massive))", globals=globals(), number=1000)}')
+
+"""
+при 10 - 
+массив: [1832, 1333, 385, 723, 785, 171, 1638, 1721, 375, 473, 1183]
+Медиана в массиве: 785
+0.024466499919071794
+
+при 100 - 
+1.3140128999948502
+
+при 1000 -
+106.94403600005899
+"""
