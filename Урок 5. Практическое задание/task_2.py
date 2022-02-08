8aae51a91d916318186a4558df1ea04da05e60b3
ahmelev <ahmelev@gmail.com>
2022-02-08 22:58:40 +0700

    Задача 2

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,48 @@ reduce
 __mul__
 __add__
 """
+
+from collections import defaultdict
+from functools import reduce
+
+
+def hexadecimal_multiplication(first, second):
+    num_first_list = list(first)
+    num_second_list = list(second)
+    numbers = defaultdict(list, a=num_first_list, b=num_second_list)
+    summa = reduce(lambda j, k: j + k, [int(''.join(i), 16) for i in numbers.values()])
+    total_sum = list(f"{summa:X}")
+    print(f"Сумма чисел из примера: {total_sum}")
+    work_numbers = reduce(lambda l, m: l * m, [int(''.join(i), 16) for i in numbers.values()])
+    work = list(f"{work_numbers:X}")
+    print(f"произведение - {work}")
+    return numbers.values()
+
+
+num_first = input('Введите первое шестнадцатеричное число: ')
+num_second = input('Введите второе шестнадцатеричное число: ')
+print('1) через collections')
+
+hexadecimal_multiplication(num_first, num_second)
+
+
+class Hexadecimals:
+    def __init__(self):
+        self.number_1 = num_first
+        self.number_2 = num_second
+        self.number_1_1 = int(self.number_1, 16)
+        self.number_2_2 = int(self.number_2, 16)
+
+    def __mul__(self, other):
+        return list(f"{self.number_1_1 * other.number_2_2:X}")
+
+    def __add__(self, other):
+        return list(f"{self.number_1_1 + other.number_2_2:X}")
+
+
+number_one = Hexadecimals()
+number_two = Hexadecimals()
+
+print('2) через ООП')
+print(f'Сумма чисел из примера: {number_one + number_two}')
+print(f'произведение - {number_one * number_two}')
