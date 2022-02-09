490bd0c847f2fba9614f8549528279a59d13ea30
mikechirkov44 <90888317+mikechirkov44@users.noreply.github.com>
2022-02-09 19:59:12 +0300

    ДЗ к уроку 1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,27 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+from random import sample
+
+rnd_list = sample(range(-100, 100), 20)
+print(rnd_list)
+
+# 1: O(n)
+min_num = rnd_list[0]
+for el in rnd_list:
+    if el < min_num:
+        min_num = el
+
+print(f'Минимальное число O(n): {min_num}')
+
+# 2: O(n^2)
+min_num = rnd_list[0]
+for i in rnd_list:
+    for j in range(rnd_list.index(i), len(rnd_list), 1):
+        if rnd_list[j] < min_num:
+            min_num = rnd_list[j]
+print(f'Минимальное число O(n^2): {min_num}')
+
+
+
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -6,7 +6,7 @@
 2) оцените сложность каждого выражения в этих решениях в нотации О-большое
 3) оцените итоговую сложность каждого решения в нотации О-большое
 3) сделайте вывод, какое решение эффективнее и почему
-
+werwer
 Сама задача:
 Имеется хранилище с информацией о компаниях: название и годовая прибыль.
 Для реализации хранилища можно применить любой подход,
@@ -17,3 +17,36 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+COMPANY_INCOME = {
+    'Company_1': 100000,
+    'Company_2': 120000,
+    'Company_3': 25000,
+    'Company_4': 250000,
+    'Company_5': 750000,
+    'Company_6': 450000,
+    'Company_7': 30000
+}
+
+# Решение 1 O(NlogN)
+def get_profit_1(dictionary: dict):
+    res = []  # O(1)
+    for i in dictionary.items():  # O(n)
+        res.append(i)  # O(1)
+    res.sort(key=lambda x: (x[1], x[0]), reverse=True)  # O(NlogN)
+    return res[0:3]
+
+print(get_profit_1(COMPANY_INCOME))
+
+
+# Решение 2   O(N^2)
+def get_profit_2(dictionary: dict):
+    max_values = {}  # O(1)
+    for i in dictionary.keys():  # O(N)
+        max_ = dictionary.get(i)  # O(1)
+        for j in dictionary.keys():  # O(N)
+            if max_ < dictionary.get(j):  # O(1)
+
+                max_ = dictionary.get(i)  # O(1)
+            max_values[i] = max_  # O(1)
+    return list(sorted(max_values.items(), key=lambda v: v[1], reverse=True))[:3]  # O(1)
+print(get_profit_2(COMPANY_INCOME))
