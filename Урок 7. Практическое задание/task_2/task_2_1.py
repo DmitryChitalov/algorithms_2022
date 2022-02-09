255974574d4b2ce60be4bc92eec8cf20e3191a8a
LeonidPolizharov <polizharov@mail.ru>
2022-02-10 00:58:53 +0300

    HW-7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -1,16 +1,56 @@
+from random import randint
+from timeit import timeit
+
+
+def shell_sort(data):
+    last_index = len(data) - 1
+    step = len(data)//2
+    while step > 0:
+        for i in range(step, last_index + 1, 1):
+            j = i
+            delta = j - step
+            while delta >= 0 and data[delta] > data[j]:
+                data[delta], data[j] = data[j], data[delta]
+                j = delta
+                delta = j - step
+        step //= 2
+    return data
+
+
+m = 26
+my_array = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(my_array)
+print(f'Медиана ряда: {shell_sort(my_array[:])[m]}')
+
+
+# # замеры 10
+print(
+    timeit(
+        "shell_sort(my_array[:])[m]",
+        globals=globals(),
+        number=1000))
+
+my_array = [randint(-100, 100) for _ in range(100 * 2 + 1)]
+
+# замеры 100
+print(
+    timeit(
+        "shell_sort(my_array[:])[m]",
+        globals=globals(),
+        number=1000))
+
+my_array = [randint(-100, 100) for _ in range(1000 * 2 + 1)]
+
+# замеры 1000
+print(
+    timeit(
+        "shell_sort(my_array[:])[m]",
+        globals=globals(),
+        number=1000))
+
+
 """
-Задание 2. Массив размером 2m + 1, где m – натуральное число,
-заполнен случайным образом. Найдите в массиве медиану.
-Медианой называется элемент ряда, делящий его на
-две равные по длине части:
-в одной находятся элементы,
-которые не меньше медианы,
-в другой – не больше медианы.
-
-Решите задачу тремя способами:
-
-1) с помощью сортировки, которую мы не рассматривали на уроке (Гномья, Шелла,
-Кучей)
-
-сделайте замеры на массивах длиной 10, 100, 1000 элементов
+0.0451825
+0.2540887
+3.7879927
 """
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -1,15 +1,44 @@
+from random import randint
+from timeit import timeit
+
+
+def medi_search(lst):
+    for i in range(len(lst) // 2):
+        lst.pop(lst.index(max(lst)))
+    return max(lst)
+
+
+m = 26
+my_array = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(my_array)
+print(f'Медиана ряда: {medi_search(my_array)}')
+
+# замеры 10
+print(
+    timeit(
+        "medi_search(my_array)",
+        globals=globals(),
+        number=1000))
+
+# замеры 100
+m = 126
+my_array = [randint(-100, 100) for _ in range(2 * 100 + 1)]
+print(
+    timeit(
+        "medi_search(my_array)",
+        globals=globals(),
+        number=1000))
+
+# замеры 1000
+my_array = [randint(-100, 100) for _ in range(2 * 1000 + 1)]
+print(
+    timeit(
+        "medi_search(my_array)",
+        globals=globals(),
+        number=1000))
+
 """
-Задание 2. Массив размером 2m + 1, где m – натуральное число,
-заполнен случайным образом. Найдите в массиве медиану.
-Медианой называется элемент ряда, делящий его на
-две равные по длине части:
-в одной находятся элементы,
-которые не меньше медианы,
-в другой – не больше медианы.
-
-Решите задачу тремя способами:
-
-2) без сортировки
-
-сделайте замеры на массивах длиной 10, 100, 1000 элементов
+0.00040699999999999764
+0.0006743000000000027
+0.0430266
 """
