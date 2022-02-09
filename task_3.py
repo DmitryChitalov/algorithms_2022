defbd6562d795a3bf70c60cb5b71ad0af319f8d2
Mary <pereverzeva.mary@gmail.com>
2022-02-09 15:07:13 +0300

    task_3 adited

--- task_3.py
+++ task_3.py
@@ -1,14 +1,16 @@
-# Выходит не совсем верный ответ... Во множество попадает и слово целиком, и пустая подстрока (откуда?)
-
 from hashlib import sha256
 
 word = 'papa'
 hash_set = set()
+str_set = set()
 
 for i in range(len(word)):
-    for j in range(i, len(word)+1):
+    for j in range(i+1, len(word)+1):
         # print(word[i:j])
-        hashed_element = sha256(word[i:j].encode()).hexdigest()
-        hash_set.add(hashed_element)
+        if word[i:j] != word:
+            hashed_element = sha256(word[i:j].encode()).hexdigest()
+            hash_set.add(hashed_element)
+            # str_set.add(word[i:j])
 
 print(f'Количество уникальных подстрок в слове {word}: {len(hash_set)}')
+# print(str_set)
