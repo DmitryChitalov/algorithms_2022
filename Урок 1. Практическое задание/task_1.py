0ba2ac25b9d420d1954968cb626210c1377dbbd0
mikechirkov44 <90888317+mikechirkov44@users.noreply.github.com>
2022-02-10 19:11:23 +0300

    Create task_1.py

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -36,10 +36,10 @@ def check_2(lst_obj):
     что такой элемент отстутствует
     в оставшихся справа элементах
 
-    Сложность: O(n).
+    Сложность: O(n^2).
     """
     for j in range(len(lst_obj)):          # O(n)
-        if lst_obj[j] in lst_obj[j+1:]:    # O(1)
+        if lst_obj[j] in lst_obj[j+1:]:    # O(n)
             return False                   # O(1)
     return True                            # O(1)
 
