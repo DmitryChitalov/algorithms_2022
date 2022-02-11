14266c179d121abec15d023f84747d5c70fe7945
Vladimir <89384911+Alekseev1990@users.noreply.github.com>
2022-02-11 16:08:59 +0300

    task_6_1_2

--- Урок 6. Практическое задание/task_1_2.py
+++ Урок 6. Практическое задание/task_1_2.py
@@ -30,3 +30,40 @@
 
 Это файл для второго скрипта
 """
+
+# Алгоритмы и структуры данных на Python. Базовый курс Урок 4, Задание 3.
+
+from memory_profiler import memory_usage
+
+
+def memory(func):
+    def wrapper(*args):
+        m1 = memory_usage()
+        res = func(*args)
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        print(f"Выполнение заняло {mem_diff} Mib")
+        return res
+
+    return wrapper
+
+
+@memory  # Выполнение заняло 0.77734375 Mib
+def revers_4(list_of_nums):
+    return ''.join(reversed(str(list_of_nums)))
+
+
+@memory  # Выполнение заняло 0.0 Mib
+def revers_4_1(list_of_nums):
+    yield reversed(str(list_of_nums))
+
+
+list_of_nums = ('123456789' * 10000)
+# for i in list_of_nums:
+#     print(list_of_nums)
+
+revers_4(list_of_nums)
+revers_4_1(list_of_nums)
+
+#  При оптимизации в этой задаче я использовал оператор yield, тем самым исключил возможность сохранения промежуточных
+# результатов и всей последовательности в памяти. Как результат, оптимизиция прошла успешно.
