bac0c74e32420857f96ea3984d74b74328537416
Alexandra <scribbo@mail.ru>
2022-02-12 20:57:49 +0300

    HW_6

--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -29,4 +29,112 @@
 генераторы, numpy, использование слотов, применение del, сериализация и т.д.
 
 Это файл для третьего скрипта
+
+3. Написать функцию thesaurus(), принимающую в качестве аргументов имена сотрудников и возвращающую словарь,
+в котором ключи — первые буквы имён, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы. Например:
+>>>  thesaurus("Иван", "Мария", "Петр", "Илья")
+{
+    "И": ["Иван", "Илья"], 
+    "М": ["Мария"], "П": ["Петр"]
+}
 """
+from memory_profiler import memory_usage, profile
+from pympler import asizeof
+from random import random
+
+
+def memory(func):
+    def wrapper(*args, **kwargs):
+        m1 = memory_usage()
+        res = func(*args)
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        print(f"Выполнение заняло {mem_diff} Mib")
+        return res
+
+    return wrapper
+
+
+def thesaurus(*args):
+    my_dict = {}
+
+    for i in range(len(args)):
+        if my_dict.get(args[i][0]) is not None:
+            list = my_dict[args[i][0]]
+            list.append(args[i])
+            my_dict.update({args[i][0]: list})
+        else:
+            my_dict.update({args[i][0]: [args[i]]})
+
+    return my_dict
+
+
+def sort_dict(dict_init):
+    dict_sorted = {}
+    list_keys = list(dict_init.keys())
+    list_keys.sort()
+    for i in list_keys:
+        dict_sorted.update({i: dict_init[i]})
+    print(dict_sorted)
+
+
+def thesaurus2(*args):
+    my_dict = {}
+
+    for ar in args:
+        if my_dict.get(ar[0]) is not None:
+            words = my_dict[ar[0]]
+            words.append(ar)
+            my_dict.update({ar[0]: words})
+        else:
+            my_dict.update({ar[0]: [ar]})
+
+    return my_dict
+
+
+def sort_dict2(dict_init):
+    dict_sorted = {}
+    list_keys = list(dict_init.keys())
+    list_keys.sort()
+    for key in list_keys:
+        dict_sorted.update({key: dict_init[key]})
+    return dict_sorted
+
+
+
+
+
+@memory
+def func_call1():
+    sort_dict(thesaurus(*gen()))
+    print(asizeof.asizeof(thesaurus(*gen()))) # -> 649800
+
+
+@memory
+def func_call2():
+    mydict = thesaurus2(*gen())
+    print(sort_dict2(mydict))
+    print(asizeof.asizeof(mydict)) # -> 648968
+
+
+def gen():
+    array = (chr(65+int(random()*25)) + chr(60+int(random()*25)) for i in range(10000))
+    return array
+
+func_call2()
+# func_call1()
+# func_call2()
+
+
+#
+# снижение размера памяти произошло в результате перехода от перебора по индексу к перебору самих элементов,
+# а также использования одного и того же объекта для сортированного и несортированного словаря
+
+# оригинал
+# 649352 * 2
+# Выполнение заняло 1.97265625 Mib
+
+# оптимизация
+# 648904
+# Выполнение заняло 0.77734375 Mib
+
