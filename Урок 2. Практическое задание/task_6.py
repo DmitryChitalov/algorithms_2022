2200768c29f7ed4a1b1f297fd6c9f7301f6be45a
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2022-02-13 00:50:39 +0300

    add hw for lesson_2

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,25 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+import random
+
+
+def task_6(a=random.randint(0, 100), n=10):
+    b = int(input("Угадайте число от 0 до 100: "))
+    if b == a:
+        return print("Вы угадали")
+    elif n == 1:
+        return print("Закончились попытки")
+    elif b > 100 or b < 0:
+        return print("Введите корректное число, попытка не засчитана"), task_6(a, n)
+    elif b > a:
+        n -= 1
+        return print(f"Загаданное число меньше, количество попыток {n}"), task_6(a, n)
+    elif b < a:
+        n -= 1
+        return print(f"Загаданное число больше, количество попыток {n}"), task_6(a, n)
+    return print(a)
+
+
+if __name__ == "__main__":
+    task_6()
