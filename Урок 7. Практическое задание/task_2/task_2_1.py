b329c4519632a5429396ad1ee3fa5d21fc251b7a
Anastasia <Ambrella915@gmail.com>
2022-02-12 22:46:20 +0300

    Lesson_7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -14,3 +14,37 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from timeit import timeit
+from random import randint
+
+
+def sort_gnome(el):
+    n, i = len(el), 0
+    while True:
+        if i + 1 == n:
+            break
+        if el[i + 1] >= el[i]:
+            i += 1
+        else:
+            el[i], el[i + 1] = el[i + 1], el[i]
+            if i > 0:
+                i -= 1
+            else:
+                i += 1
+    return el
+
+
+print('замеры 10')
+m = 5
+orig_list_1 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit("sort_gnome(orig_list_1[:])", globals=globals(), number=1000))
+
+print('замеры 100')
+m = 50
+orig_list_2 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit("sort_gnome(orig_list_2[:])", globals=globals(), number=1000))
+
+print('замеры 1000')
+m = 500
+orig_list_3 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit("sort_gnome(orig_list_3[:])", globals=globals(), number=1000))
--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -15,3 +15,28 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+from random import randint
+from timeit import timeit
+from statistics import median
+
+
+def median_elem(el):
+    return median(el)
+
+
+print('замеры 10')
+m = 5
+orig_list_1 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit("median_elem(orig_list_1[:])", globals=globals(), number=1000))
+
+print('замеры 100')
+m = 50
+orig_list_2 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit("median_elem(orig_list_2[:])", globals=globals(), number=1000))
+
+print('замеры 1000')
+m = 500
+orig_list_3 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit("median_elem(orig_list_3[:])", globals=globals(), number=1000))
+
+"""Поиск медианы быстрее всего функцией median из библиотеки statistics"""
