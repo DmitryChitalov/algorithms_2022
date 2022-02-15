cd8f914768b6bcaaadf30880eeaf8dfef55bc5f4
Vladimir <89384911+Alekseev1990@users.noreply.github.com>
2022-02-15 14:13:09 +0300

    task 7_2_1

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -14,3 +14,50 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+# гномья сортировка
+
+from random import randint
+from timeit import timeit
+
+
+def lst_sort(lst):
+    i, j, size = 1, 2, len(lists_task)
+    i = 1
+    while i < size:
+        if (lst[i - 1] <= lst[i]):
+            i += 1
+        else:
+            tmp = lst[i]
+            lst[i] = lst[i - 1]
+            lst[i - 1] = tmp
+            i -= 1
+            if (i == 0):
+                i = 1
+    return lst
+
+
+m = 5
+lists_task = [randint(0, 100) for _ in range(2 * m + 1)]
+print(lst_sort(lists_task))
+lst = lst_sort(lists_task)
+print(lst[m])
+print(timeit("lst_sort(lists_task[:])", globals=globals(), number=100))  # 0.000298037000000001
+
+
+m = 50
+lists_task = [randint(0, 200) for _ in range(2 * m + 1)]
+print(lst_sort(lists_task))
+lst = lst_sort(lists_task)
+print(lst[m])
+print(timeit("lst_sort(lists_task[:])", globals=globals(), number=100))  # 0.002546452999999997
+
+
+m = 500
+lists_task = [randint(0, 2000) for _ in range(2 * m + 1)]
+print(lst_sort(lists_task))
+lst = lst_sort(lists_task)
+print(lst[m])
+print(timeit("lst_sort(lists_task[:])", globals=globals(), number=100))  # 0.030413348000000007
+
+# медиана - число с индексом m, то есть вводимое число будет индексом медианы.
