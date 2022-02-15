0ad93db4fb9a9b56ad9286b991b519b641643f35
alexeysvatov <zombeaver@gmail.com>
2022-02-15 16:10:54 +0500

    done task_2

--- Урок 7. Практическое задание/Svatov_Alexey_dz_7/task_2/task_2_2.py
+++ Урок 7. Практическое задание/Svatov_Alexey_dz_7/task_2/task_2_2.py
@@ -13,3 +13,31 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+
+def array_gen(m):
+    return [randint(-100, 100) for _ in range(2 * m + 1)]
+
+
+def median_del_max_elem(lst):
+    for i in range(len(lst) // 2):
+        lst.remove(max(lst))
+    return f'Медиана списка = {max(lst)}'
+
+
+test_list_11 = array_gen(5)
+test_list_101 = array_gen(50)
+test_list_1001 = array_gen(500)
+
+print(timeit("median_del_max_elem(test_list_11[:])", globals=globals(), number=1000))
+print(timeit("median_del_max_elem(test_list_101[:])", globals=globals(), number=1000))
+print(timeit("median_del_max_elem(test_list_1001[:])", globals=globals(), number=1000))
+
+"""
+0.0014527310013363604
+0.051363287999265594
+5.1892462040013925
+"""
