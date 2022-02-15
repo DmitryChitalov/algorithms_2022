02594ed2902ee3e9c96c1d4acb731853c2bb42ef
KosarevDV <KosarevDV@users.noreply.github.com>
2022-02-16 00:25:43 +0300

    Косарев ДВ, гр. GU_python_1738, курс Алгоритмы от 12.01. ДЗ к уроку 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -13,3 +13,55 @@
 Вы можете реализовать задачу, например,
 через ООП или предложить иной подход к решению.
 """
+
+from collections import Counter, deque
+
+s = "beep boop beer!"
+final_code = dict()
+
+
+class BinaryTree:
+    def __init__(self, left, right):
+        # левый потомок
+        self.left = left
+        # правый потомок
+        self.right = right
+
+
+def haffman(s):
+    count_str = Counter(s)          # частотный анализ
+    sorted_str = deque(sorted(count_str.items(), key=lambda item: item[1]))     # сортируем дек по значеним
+    #print(sorted_str)
+    while len(sorted_str) > 1:          # пока в деке больше 1 элемена
+        value = sorted_str[0][1] + sorted_str[1][1]             # соединяем значения двух наименьших элементов
+        node = BinaryTree(left=sorted_str.popleft()[0], right=sorted_str.popleft()[0])    # создаем узел
+        for i, elem in enumerate(sorted_str):                                             # и ищем куда его вставить
+            if value > elem[1]:                 # если значение двух минимальных элементов больше третьего
+                continue                         # идем дальше
+            else:                                # пока не найдем элемент с бОльшим знчением, вставим узел перед ним
+                sorted_str.insert(i, (node, value))
+                break
+        else:
+            sorted_str.append((node, value))         # если сумма двух минимальных больше значения всех остальных то
+                                                     # вставляем узел в конец дека
+    print(sorted_str)
+    print(sorted_str[0][0])
+    return sorted_str[0][0]                     # Почему возвращаем значение нулевого элемента???
+
+
+#print(haffman(s))
+
+def haffman_code(tree, path=''):                            # Собираем результат
+    if not isinstance(tree, BinaryTree):                    # Если элемент не имеет потомков(не является binary tree)
+        final_code[tree] = path                             # то элемент - лист дерева, возвращем собранный до него путь
+    else:                                                   # если имеет то рекурсивно продолжаем обход по поддеревьям
+        haffman_code(tree.left, path=f'{path}0')            # Если идем налево то добавляем к пути 0
+        haffman_code(tree.right, path=f'{path}1')           # Если идем направо то добавляем к пути 1
+
+
+haffman_code(haffman(s))
+
+print(final_code)
+
+for i in s:
+    print(final_code[i], end=' ')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -13,7 +13,13 @@
 """
 
 
+class BTError(Exception):
+    pass
+
+
 class BinaryTree:
+    __slots__ = ['root', 'left_child', 'right_child']         # сработает ли это и есть ли смысл?
+
     def __init__(self, root_obj):
         # корень
         self.root = root_obj
@@ -24,41 +30,54 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
+        # если значние нового узла больше или равно корню, он не может быть левым потомком
+        if new_node >= self.root:
+            raise BTError('Элемент не может быть левым потомком.')
+        else:
+            # если у узла нет левого потомка
+            if self.left_child == None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
+                self.left_child = BinaryTree(new_node)
         # если у узла есть левый потомок
-        else:
+            else:
             # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
+                tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
+        # если значние нового узла меньше корня, он не может быть правым потомком
+        if new_node < self.root:
+            raise BTError('Элемент не может быть правым потомком.')
+        else:
+            # если у узла нет правого потомка
+             if self.right_child == None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
+                self.right_child = BinaryTree(new_node)
         # если у узла есть правый потомок
-        else:
+             else:
             # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
+                tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
 
     # метод доступа к правому потомку
     def get_right_child(self):
+        if self.right_child is None:
+            raise BTError('У узла нет правого потомка')
         return self.right_child
 
     # метод доступа к левому потомку
     def get_left_child(self):
+        if self.left_child is None:
+            raise BTError('У узла нет левого потомка')
         return self.left_child
+
 
     # метод установки корня
     def set_root_val(self, obj):
@@ -66,17 +85,25 @@ class BinaryTree:
 
     # метод доступа к корню
     def get_root_val(self):
-        return self.root
+        if self.root is None:
+            raise BTError('элемент отсутствует')
+        else:
+            return self.root
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+try:
+    r = BinaryTree(8)
+    print(r.get_root_val())          # 8
+#   print(r.get_left_child())        # нет левого потомка
+#   r.insert_left(40)                # Элемент не может быть левым потомком
+    r.insert_right(40)               # добавили справа узел 40
+#    print(r.get_left_child())       # У узла нет левого потомка
+#    print(r.get_left_child().get_root_val())   # У узла нет левого потомка
+#   r.insert_right(6)                # Элемент не может быть правым потомком
+    r.insert_left(6)                 # добавили слева узел 40
+    print(r.get_right_child())          #<__main__.BinaryTree object at 0x000001E7D3906740>
+    print(r.get_right_child().get_root_val())       # 40
+    r.get_right_child().set_root_val(16)            # установили узлу 40 значение 16
+    print(r.get_right_child().get_root_val())       # 16
+except BTError as e:
+    print('Произошла ошибка: ', e)
