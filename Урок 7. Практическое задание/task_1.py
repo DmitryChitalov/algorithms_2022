545dd0dccdec3b3af161993a3a7bfed121666528
Vladimir <89384911+Alekseev1990@users.noreply.github.com>
2022-02-15 14:10:54 +0300

    task_7_1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,43 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+
+import random
+from timeit import timeit
+
+numbers = [random.randint(-100, 100) for _ in range(1000)]
+
+
+def bubble_insert(nums):
+    for j in range(len(nums) - 1):
+        for i in range(len(nums) - 1 - j):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+    return nums
+
+
+print(bubble_insert(numbers))
+print(timeit("bubble_insert(numbers)", globals=globals(), number=100))  # 0.0031117389999999953, 18.020798568
+
+
+def bubble_insert_2(nums):
+    n = 1
+    count = 0
+    flag = True
+    for j in range(len(nums) - n):
+        if flag is False:
+            return nums
+        flag = False
+        for i in range(len(nums) - 1 - j):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                flag = True
+            count += 1
+    return nums
+
+
+print(bubble_insert_2(numbers))  # 0.002179038000000001, 15.368825631000004
+print(timeit("bubble_insert(numbers)", globals=globals(), number=100))
+
+# Доработка дает положительный результат, если в сформированном случайным образом списке в большом количестве проходов
+# не совершается сортировка, так как элементы уже на своих местах.
