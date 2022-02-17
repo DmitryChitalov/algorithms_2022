4ba193828b8e235f88b2eb187b5c6db0897cced0
Kulikov I.K <ilyatrg@gmail.com>
2022-02-17 20:47:39 +0500

    lesson4 - Timeit

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,9 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 
+from timeit import timeit
+from collections import Counter
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +40,22 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    elem = Counter(array).most_common()[0]
+    return f'Чаще всего встречается число {elem[0]}, ' \
+           f'оно появилось в массиве {elem[1]} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+
+print(timeit("func_1()", globals=globals(), number=10000))
+print(timeit("func_2()", globals=globals(), number=10000))
+print(timeit("func_3()", globals=globals(), number=10000))
+
+"""
+    Из всех функций самая быстрая func_1, так как использует стандартные
+методы списка и цикл. func_2 медленнее, так как тратит время на формирование 
+нового словаря, а func_3 хоть и самая медленная, но написана за 60 секунд :)
+"""
