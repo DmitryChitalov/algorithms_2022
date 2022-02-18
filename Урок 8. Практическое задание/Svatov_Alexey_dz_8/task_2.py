fa653da612d2b103dc8ab6c392e3c3e88348fa66
alexeysvatov <zombeaver@gmail.com>
2022-02-18 12:06:50 +0500

    done task_2

--- Урок 8. Практическое задание/Svatov_Alexey_dz_8/task_2.py
+++ Урок 8. Практическое задание/Svatov_Alexey_dz_8/task_2.py
@@ -24,33 +24,47 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            # если у узла нет левого потомка
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # проверка на соответствие ветке
+            elif new_node > self.get_left_child().get_root_val():
+                raise ErrorValue
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+        except ErrorValue:
+            self.insert_left(int(input('Введённое значение не соответствует ветке\n'
+                                       'Введите меньшее значение: ')))
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            # если у узла нет правого потомка
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # проверка на соответствие ветке
+            elif new_node < self.get_right_child().get_root_val():
+                raise ErrorValue
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+        except ErrorValue:
+            self.insert_right(int(input('Введённое значение не соответствует ветке\n'
+                                        'Введите большее значение: ')))
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -69,6 +83,11 @@ class BinaryTree:
         return self.root
 
 
+# класс исключения
+class ErrorValue(Exception):
+    pass
+
+
 r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
@@ -80,3 +99,8 @@ print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
 print(r.get_right_child().get_root_val())
+print(r.get_left_child().get_root_val())
+
+# Проверка введённого значения
+r.insert_left(50)
+print(r.get_left_child().get_root_val())
