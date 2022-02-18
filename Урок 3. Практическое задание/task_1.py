4b788bf4108ce0a25997b1e71e4a69e4147d5f2f
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2022-02-18 08:33:43 +0300

    add hw3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -28,3 +28,77 @@ b) получение элемента списка, оцените сложно
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+
+import time
+
+# декоратор времени
+
+
+def timeline(func):
+
+    def time_line_2(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        print(f'время выполнения {func.__name__} составило {end - start} с')
+        return res
+    return time_line_2
+
+
+num_list = []
+num_dict = {}
+
+# a)
+
+
+@timeline
+def list_append_a_1(num):
+    for i in range(num):
+        num_list.append(i)  # O(1)
+
+
+@timeline
+def dict_add_a_2(num):
+    for i in range(num):
+        num_dict[i] = i  # O(1)
+
+
+# b)
+
+
+@timeline
+def list_get_value_b_1(num):
+    for i in range(num):
+        return num_list[i]   # O(1)
+
+
+@timeline
+def dict_get_value_b_2(num):
+    for i in range(num):
+        return num_dict[i]   # O(1)
+
+
+# c)
+
+
+@timeline
+def list_pop_1(num):
+    for i in range(num):
+        return num_list.pop(i)   # O(n)
+
+
+@timeline
+def dict_pop_c_2(num):
+    for i in range(num):
+        return num_dict.pop(i)   # O(1)
+
+
+if __name__ == "__main__":
+    list_append_a_1(1000000)
+    dict_add_a_2(1000000)
+    print("_" * 100)
+    list_get_value_b_1(1000000)
+    dict_get_value_b_2(1000000)
+    print("_" * 100)
+    list_pop_1(100000)
+    dict_pop_c_2(100000)
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,23 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+import hashlib
+from uuid import uuid4
+
+salt = uuid4().hex
+
+cashe_obj = {}
+
+
+def task_4(url):
+    if cashe_obj.get(url):
+        print(f"Такой {url} есть")
+    else:
+        res = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        cashe_obj[url] = res
+        print(cashe_obj)
+
+
+if __name__ == "__main__":
+    task_4('https:geekbrains.ru')
+    task_4('https:geekbrains.ru')
