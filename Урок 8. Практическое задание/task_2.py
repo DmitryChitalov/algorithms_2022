0557534253d119f8d6e7eb7b5d344035d4793235
geekbrains <info@geekbrains.ru>
2022-02-20 19:27:35 +0300

    dz_8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -11,72 +11,77 @@
 Поработайте с оптимизированной структурой,
 протестируйте на реальных данных - на клиентском коде.
 """
+from random import randint
 
 
+# Валидацию объединил с автоматизацией вставки. В правую ветку - значения больше корневого, в левую - меньше.
+# При попытке вставить уже существующие узлы дерева вызывается исключение (заменил на сообщение об ошибке).
+# Класс еще можно развить функционально (удаление, замена узла, перестройка дерева до сбалансированного...), но такого
+# задания вроде не было.
 class BinaryTree:
     def __init__(self, root_obj):
-        # корень
-        self.root = root_obj
-        # левый потомок
-        self.left_child = None
-        # правый потомок
-        self.right_child = None
+        self.root = root_obj  # корень
+        self.left_child = None  # левый потомок
+        self.right_child = None  # правый потомок
 
-    # добавить левого потомка
-    def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+    def insert_node(self, value):  # вставляем узел, рекурсивно обходя дерево, пока не найдем нужное место
+        def _add(node, val):
+            if val < node.root:
+                if node.left_child is not None:
+                    _add(node.left_child, val)
+                else:
+                    node.left_child = BinaryTree(val)
+            elif val > node.root:
+                if node.right_child is not None:
+                    _add(node.right_child, val)
+                else:
+                    node.right_child = BinaryTree(val)
+            else:  # val == node.root
+                # raise Exception(f"Узел '{val}' уже существует в дереве")
+                print(f"Узел '{val}' уже существует в дереве")
 
-    # добавить правого потомка
-    def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        _add(self, value)
 
-    # метод доступа к правому потомку
-    def get_right_child(self):
-        return self.right_child
+    def sorted_list(self):  # обходим дерево и возвращаем сортированный список значений
+        def _sorted_list(node, lst):
+            if node is not None:
+                _sorted_list(node.left_child, lst)
+                lst.append(node.root)
+                _sorted_list(node.right_child, lst)
 
-    # метод доступа к левому потомку
-    def get_left_child(self):
-        return self.left_child
+        if self.root is not None:
+            _sorted_list(self, list_ := [])
+            return list_
 
-    # метод установки корня
-    def set_root_val(self, obj):
-        self.root = obj
+    def print_tree(self):  # печать дерева: корень крайний слева.
+        def _print_tree(node, level=0):
+            if node is not None:
+                _print_tree(node.right_child, level + 1)
+                print(f"{' ' * 4 * level}--| {node.root}")
+                _print_tree(node.left_child, level + 1)
 
-    # метод доступа к корню
-    def get_root_val(self):
-        return self.root
+        if self.root is not None:
+            print('---- Tree: -------------')
+            _print_tree(self)
+            print('------------------------')
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+# проверяем на тестовом массиве, печатаем дерево, получаем упорядоченный массив, обходя дерево
+bt = BinaryTree(8)
+[bt.insert_node(i) for i in [2, 5, 3, 6, 12, 9, 10, 15]]
+bt.print_tree()
+print(bt.sorted_list())
+
+# Проверяем на случайных значениях.
+# Сортируем уникальные значения входного массива и сравниваем со значениями, полученными обходом дерева
+list_len = 100
+test_len = 50
+for i in range(test_len):
+    li = [randint(-list_len, list_len) for _ in range(list_len)]
+    li.append(0)  # добавляем 0, если не выпал случайно
+    bt = BinaryTree(0)
+    [bt.insert_node(i) for i in li]
+    print(sorted(set(li)))
+    print(bt.sorted_list())
+    assert bt.sorted_list() == sorted(set(li))
+print(f"{test_len} тестов на {list_len} значениях завершились успешно!")
