4f55412a692e78c000d88c31494e5e7aef220f56
gas53 <45990653+GAS53@users.noreply.github.com>
2022-02-22 10:23:34 +0300

    Add files via upload

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,101 @@ reduce
 __mul__
 __add__
 """
+# 1 вариант решение через collections
+from collections import UserString
+
+
+in_1 = 'A2'  # input('введите 1-ое 16-и ричное число например A2 ')
+in_2 = 'C4F'  # input('введите 2-ое 16-и ричное число например C4F ')
+in_1 = in_1.upper()
+in_2 = in_2.upper()
+
+
+z = [str(x) for x in range(1, 10)]+['A', 'B', 'C', 'D', 'E', 'F']
+convert_di = {x:num for num, x in enumerate(z, start=1)}
+unconvert_di = {v:k for k, v in convert_di.items()}
+
+
+def converter(num):
+    res = 0
+    for en, item in enumerate(num[::-1]):
+        res += convert_di[item]*(16**en)
+    return res
+
+
+def unconverter(num):
+    res2 = UserString('')
+    while num > 16:
+        res = num % 16
+        num = num // 16
+        
+        if res < 16:
+            res2 += unconvert_di[res]
+        if num < 16:
+            res2 += unconvert_di[num]
+    res2 = res2[::-1]
+    return res2
+
+
+res = unconverter(converter(in_1) + converter(in_2))
+print(f'сумма чисел {res}')
+
+res = unconverter(converter(in_1) * converter(in_2))
+print(f'произведение чисел {res}')
+
+
+# 2 вариант решение через ООП
+
+class Digit():
+    def __init__(self, hex_num, convert=True):
+        self.convert_di = {x:num for num, x in enumerate(z, start=1)}
+        self.unconvert_di = {v:k for k, v in convert_di.items()}
+        if convert:
+            self.decimal_num = self.converter(hex_num.upper())
+        else:
+            self.decimal_num = hex_num
+
+    def converter(self, num):
+        res = 0
+        for en, item in enumerate(num[::-1]):
+            res += self.convert_di[item]*(16**en)
+        return res
+
+    def unconverter(self, num):
+        res2 = ''
+        while num > 16:
+            res = num % 16
+            num = num // 16
+            
+            if res < 16:
+                res2 += self.unconvert_di[res]
+            if num < 16:
+                res2 += self.unconvert_di[num]
+        res2 = res2[::-1]
+        return res2
+
+    def __mul__(self, cls):
+        return Digit((self.decimal_num * cls.decimal_num), False)
+
+    def __add__(self, cls):
+        return Digit((self.decimal_num + cls.decimal_num), False)
+
+    def __sub__(self, cls):
+        return Digit((self.decimal_num - cls.decimal_num), False)
+
+    def __truediv__(self, cls):
+        return Digit(int(self.decimal_num / cls.decimal_num), False)
+
+    def __str__(self) -> str:
+        '''return hex digit'''
+        return str(self.unconverter(self.decimal_num))
+        
+hex_1 = Digit('ca2')
+hex_2 = Digit('4f')
+hex_summ = hex_1 + hex_2
+hex_multiply = hex_1 * hex_2
+hex_div = hex_1 - hex_2
+print(f'сумма {hex_summ}')
+print(f'произведение {hex_multiply}')
+print(f'разность {hex_div}')
+print(f'остаток от деления {hex_multiply}')
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,122 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+from collections import deque
+from timeit import timeit
+from random import randint
+
+main_li = [i for i in range(100)]
+li = main_li.copy()
+de = deque(li.copy())
+test_li = [randint(1,99) for _ in range(5)]
+
+
+def li_append():
+    for i in test_li:
+        li.append(i)
+
+def de_append():
+    for i in test_li:
+        de.append(i)
+
+def li_pop():
+    for _ in range(100):
+        li.pop()
+
+def de_pop():
+    for _ in range(100):
+        de.pop()
+
+def li_extend():
+    li.extend(test_li)
+
+def de_extend():
+    de.extend(test_li)
+
+# l_app = timeit(li_append,globals = globals(), number=10000)
+# d_app = timeit(de_append,globals = globals(), number=10000)
+# l_pop = timeit(li_pop,globals = globals(), number=1)
+# d_pop = timeit(de_pop,globals = globals(), number=1)
+# l_extend = timeit(li_extend,globals = globals(), number=10000)
+# d_extend = timeit(de_extend,globals = globals(), number=10000)
+# print(l_pop, d_pop, 'test')
+# print(f'сравнение списка с деком насколько % дек быстрее списка:\n при расширении {d_app*100/l_app} \n \
+# при вырезании {d_pop*100/l_pop},\n при обьеденении {d_extend*100/l_extend}')
+'''
+сравнение deque со списком насколько % дек быстрее списка:
+ при добавлении 84.1635816718657 
+ при вырезании 90.10068381491539,
+ при обьеденении 165.1381857370785
+
+Выводы при append и pop быстрее у дека(<100%) а extend быстрее у списка (>100%)
+
+'''
+# 2 
+def de_appendleft():
+    for i in test_li:
+        de.appendleft(i)
+
+def li_appendleft():
+    for i in test_li:
+        li.insert(0,i)
+
+def de_popleft():
+    for _ in test_li:
+        de.popleft()
+
+def li_popleft():
+    for _ in test_li:
+        li.pop(0)
+
+def de_extendleft():
+    de.extendleft(test_li)
+
+def li_extendleft():
+    new_li = []
+    new_li.extend(test_li)
+    new_li.extend(li)
+
+
+d_appendleft = timeit(de_appendleft,globals = globals(), number=1000)
+l_appendleft = timeit(li_appendleft,globals = globals(), number=1000)
+d_popleft = timeit(de_popleft,globals = globals(), number=1)
+l_popleft = timeit(li_popleft,globals = globals(), number=1)
+d_extendleft = timeit(de_extendleft,globals = globals(), number=1000)
+l_extendleft = timeit(li_extendleft,globals = globals(), number=1000)
+print(l_appendleft,d_appendleft)
+print(f'сравнение списка с деком насколько % дек быстрее списка:\n при appendleft {d_appendleft*100/l_appendleft}\n \
+при popleft {d_popleft*100/l_popleft}\n \
+при extendleft {d_extendleft*100/l_extendleft}')
+
+'''
+сравнение списка с деком насколько % дек быстрее списка:
+ при appendleft 5.037506524249985
+ при popleft 30.807461696721415
+ при extendleft 1.7679838084417203
+ 
+ выводы все значения <100%: при данных операциях дек гораздо быстрее списка
+ '''
+
+# 3 получение элемента из списка и дека
+
+def li_pop_i():
+    for i in test_li:
+        li[i]
+
+def de_pop_i():
+    for i in test_li:
+        de[i]
+
+d_pop_i = timeit(de_pop_i, globals = globals(), number=1)
+l_pop_i = timeit(li_pop_i, globals = globals(), number=1)
+print(f'получение элемента из списка проходит за {l_pop_i}\n\
+получение элемента из дека проходит за {d_pop_i}\n\
+{d_pop_i*100/l_pop_i} > 100 % ')
+
+
+'''
+получение элемента из списка проходит за 1.059999704011716e-06
+получение элемента из дека проходит за 1.4300003385869786e-06
+134.90572998982395 > 100 % 
+Выводы из списка по индексу элемент получить гораздо быстрее
+'''
