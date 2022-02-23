ca527bb31b05597ab241c0521e3cc8642a1f3f3b
Kalachev <alex.kalachyov2005@gmail.com>
2022-02-23 16:43:56 +0300

    Homework 1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,38 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+class Stack_plate():
+    def __init__(self):
+        self.limit = 3
+        self.stack = [[]]
+
+    def append_val(self, val):
+        if len(self.stack[-1]) > self.limit:
+            new_inner = []
+            new_inner.append(val)
+            self.stack.append(new_inner)
+        else:
+            self.stack[-1].append(val)
+
+    def __str__(self):
+        return str(self.stack)
+
+    def get_val(self):
+        res = self.stack[-1][-1]
+        del self.stack[-1][-1]
+        if len(self.stack[-1]) < 1:
+            del self.stack[-1]
+        return res
+
+
+st = Stack_plate()
+st.append_val('st1')
+st.append_val('st2')
+st.append_val('smth')
+st.append_val(123)
+st.append_val(-1)
+
+
+
+
+print(st)
