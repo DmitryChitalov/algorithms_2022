780c62dcd633575d90058f39fdbc39ffa84357cb
Kalachev <alex.kalachyov2005@gmail.com>
2022-02-23 22:53:16 +0300

    hw7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,60 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_update(lst_obj):
+    n = 1
+    j = 0
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                j += 1
+        if j == 0:
+            return lst_obj
+        n += 1
+    return lst_obj
+
+
+# замеры 10
+orig_list_10 = [randint(-100, 100) for _ in range(10)]
+print(timeit("bubble_sort(orig_list_10[:])", globals=globals(), number=1000))
+print(timeit("bubble_sort_update(orig_list_10[:])", globals=globals(), number=1000))
+
+# замеры 100
+orig_list_100 = [randint(-100, 100) for _ in range(100)]
+print(timeit("bubble_sort(orig_list_100[:])", globals=globals(), number=1000))
+print(timeit("bubble_sort_update(orig_list_100[:])", globals=globals(), number=1000))
+
+# замеры 1000
+orig_list_1000 = [randint(-100, 100) for _ in range(1000)]
+print(timeit("bubble_sort(orig_list_1000[:])", globals=globals(), number=1000))
+print(timeit("bubble_sort_update(orig_list_1000[:])", globals=globals(), number=1000))
+
+print(orig_list_1000)
+print(bubble_sort_update(orig_list_1000[:]))
+
+
+# 0.0100278
+# 0.011486200000000002
+# 0.671733
+# 0.7984500999999999
+# 71.83192489999999
+# 82.72695850000001
+# Не во всех замерах доработка была эффективна.
+# Доработка будет помогать в зависимости от того, насколько массив уже упорядочен.
+# Важно учитывать, что чем меньше массив, тем больше шансов на уменьшение времени.
