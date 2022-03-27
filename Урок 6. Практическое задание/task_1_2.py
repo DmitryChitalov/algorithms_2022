a4d7273b58d9c8c5972bb2fb648849d7dd5e9f3c
big_sosa <howarddan11@mail.ru>
2022-03-27 15:28:55 +0300

    add

--- Урок 6. Практическое задание/task_1_2.py
+++ Урок 6. Практическое задание/task_1_2.py
@@ -3,3 +3,49 @@
 
 Это файл для второго скрипта
 """
+from sys import getsizeof
+from collections import namedtuple
+
+
+#  Не оптимизированная
+def num_translate():
+    numbers = {
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять',
+    }
+    print(getsizeof(numbers))
+    return numbers.get('ten')
+
+
+# Оптимизированная
+def num_translate_tuple():
+    dict_ = namedtuple('dict_', ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'))
+    d = dict_(one='один',
+              two='два',
+              three='три',
+              four='четыре',
+              five='пять',
+              six='шесть',
+              seven='семь',
+              eight='восемь',
+              nine='девять',
+              ten='десять'
+    )
+    print(getsizeof(d))
+    return d.ten
+
+
+num_translate()
+num_translate_tuple()
+
+"""
+с namedtuple мы выигрываем по памяти в 3 раза
+"""
--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -3,3 +3,51 @@
 
 Это файл для третьего скрипта
 """
+
+
+"""2. Реализовать класс Road (дорога).
+определить атрибуты: length (длина), width (ширина);
+значения атрибутов должны передаваться при создании экземпляра класса;
+определить метод расчёта массы асфальта, необходимого для покрытия всей дороги;
+использовать формулу: длина * ширина * масса асфальта для покрытия одного
+кв. метра дороги асфальтом, толщиной в 1 см * число см толщины полотна;"""
+
+from pympler import asizeof
+
+
+# Не оптимизированная
+class Road:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def weight(self):
+        return f'{(self.length * self.width * 25 * 5) / 1000} тонн'
+
+
+r_1 = Road(28, 5155)
+print(asizeof.asizeof(r_1))
+
+
+# Оптимизировання
+class Road2:
+    __slots__ = ['length', 'width']
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def weight(self):
+        return f'{(self.length * self.width * 25 * 5) / 1000} тонн'
+
+
+r_2 = Road2(28, 5155)
+print(asizeof.asizeof(r_2))
+
+
+"""
+С помощью слотов выйграли по памяти в три раза
+
+Результаты: 1 - 328
+            2 - 112
+"""
