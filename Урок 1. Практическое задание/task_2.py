14c4e5be080f91fc7c9bf1ac2fde67857918b225
andrejkalabin <admin@24vgk.ru>
2022-05-14 17:44:12 +0300

    выполнил задание

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,28 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+# Сложность O(n)
+def get_min_number(lst):
+    min_number = lst[0]                                     # O(1)
+    for i in lst:                                           # O(n)
+        if i < min_number:                                  # O(len(i)
+            min_number = i                                  # O(1)
+    return min_number                                       # O(1)
+
+
+# Сложность O(n**2)
+def get_min_number_2(lst):
+    min_number_2 = lst[0]                                   # O(1)
+    for i in lst:                                           # O(n)
+        for j in range(lst.index(i) + 1, len(lst) - 1, 1):  # O(n)
+            if min_number_2 > lst[j]:                       # O(len(lst[j])
+                min_number_2 = lst[j]                       # O(1)
+    return min_number_2                                     # O(1)
+
+
+first_list = [100, 50, 3, 4, 23, 10]
+
+print(get_min_number(first_list))
+
+print(get_min_number_2(first_list))
