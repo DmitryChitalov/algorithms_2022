a13a9bda1015943b0f2ef185d8f888e134c1e7a4
DrozdForever <drozd.forever@gmail.com>
2022-05-14 14:26:49 +0700

    Homework

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -1,17 +1,27 @@
-"""
-Задание 2.
-
-Реализуйте два алгоритма.
-
-Оба должны обеспечивать поиск минимального значения для списка.
-
-Сложность первого алгоритма должна быть O(n^2) - квадратичная.
-
-Сложность второго алгоритма должна быть O(n) - линейная.
+from random import randint
 
 
-Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
--- нельзя использовать встроенные функции min() и sort()
--- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
--- проставьте сложности каждого выражения в двух ваших алгоритмах
-"""
+def search_min(lst):  # O(n^2)
+    min_val = 10 ** 6  # O(1)
+    for ind, num in enumerate(lst):  # O(n)
+        for j in lst[ind+1:]:  # O(n)
+            if j <= num and j < min_val:  # O(1)
+                min_val = j  # O(1)
+            elif j >= num and num < min_val:  # O(1)
+                min_val = num  # O(1)
+    return min_val  # O(1)
+
+
+def search_min_v2(lst):  # O(n)
+    min_val = 10 ** 6  # O(1)
+    for i in lst:  # O(n)
+        if i < min_val:  # O(1)
+            min_val = i  # O(1)
+    return min_val  # O(1)
+
+
+some_list = [randint(-50, 50) for _ in range(10)]
+
+print(min(some_list))
+print(search_min(some_list))
+print(search_min_v2(some_list))
