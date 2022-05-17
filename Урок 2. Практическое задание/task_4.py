ac3b83bf44e2e765da182205b95f33c7f6a357c1
neatek <neatek@icloud.com>
2022-05-17 07:12:51 +0300

    Урок 2. Практическое задание.

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -7,6 +7,53 @@
 Введите количество элементов: 3
 Количество элементов - 3, их сумма - 0.75
 
-Решите через рекурсию. В задании нельзя применять циклы.
-Нужно обойтисть без создания массива!
+!!! Решите через рекурсию.
+!!! В задании нельзя применять циклы.
+!!! Нужно обойтисть без создания массива
+"""
+
+class MyApp:
+
+    def __init__(self):
+        self.number = 1
+        self.step = 1
+        self.total = 0
+        self.sum = 0
+
+    def process(self):
+        # Can be better with try/except, too lazy
+        self.step = int(input('Введите количество элементов:'))
+
+        self.total = self.step
+        self.count_num(self.number)
+        self.create_step()
+
+    def count_num(self, number):
+        print(number)
+        self.sum += number
+
+    def create_step(self):
+        less = self.number / 2
+        self.number -= less
+        self.number *= -1
+        self.step -= 1
+        self.count_num(self.number)
+        if self.step > 1:
+            self.create_step()
+        else:
+            self.result()
+
+    def result(self):
+        print('Количество элементов - %i, их сумма - %.2f' % (self.total, self.sum))
+
+
+app = MyApp()
+app.process()
+
+"""
+    Введите количество элементов:3
+    1
+    -0.5
+    0.25
+    Количество элементов - 3, их сумма - 0.75
 """
--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,58 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+import random
+
+
+class MyApp:
+
+    def __init__(self):
+        self.random = random.randint(0, 100)
+        self.wrong = 0
+
+    def start(self):
+        print('Вы должны отгадать число (от 0-100) не более чем за 10 попыток. Удачи!')
+        self.try_catch()
+
+    def try_catch(self):
+        if self.wrong >= 10:
+            self.exit()
+            return
+
+        try:
+            self.check_number(int(input('Введите число: ')))
+        except BaseException:
+            self.try_catch()
+
+    def exit(self):
+        print('Игра окончена. Попытки исчерпаны. Это число %i' % self.random)
+
+    def wrong_try(self, number):
+        high = self.random > number
+        if high:
+            print("Неудачная попытка. (%i/10). Число больше." % self.wrong)
+        else:
+            print("Неудачная попытка. (%i/10). Число меньше." % self.wrong)
+        self.try_catch()
+
+    def check_number(self, number):
+        if self.random != number:
+            self.wrong += 1
+            self.wrong_try(number)
+        else:
+            print('Да! Вы правильно угадали число. Это %i.' % self.random)
+
+
+app = MyApp()
+app.start()
+
+
+"""
+    Вы должны отгадать число (от 0-100) не более чем за 10 попыток. Удачи!
+    Введите число: 50
+    Неудачная попытка. (1/10). Число больше.
+    Введите число: 70
+    Неудачная попытка. (2/10). Число меньше.
+    Введите число: 60
+    Да! Вы правильно угадали число. Это 60.
+"""
