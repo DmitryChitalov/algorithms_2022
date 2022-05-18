0fc0688b58235079f1a5192de08bbadd0ab2c352
Anatoliy-A-M <95370835+Anatoliy-A-M@users.noreply.github.com>
2022-05-18 14:30:12 +0300

    Assigment 5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,37 +17,49 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
-class Stack_Plates:
-    def __init__(self):
+class Stack:
+    def __init__(self, n):
         self.plates = []
+        self.max_num = n
+        self.full_stacks = []
+
+    def last_size(self):
+        return self.plates[len(self.plates) - 1]
+
+    def height(self):
+        return len(self.plates)
 
     def is_empty(self):
         return self.plates == []
 
-    def push_in(self, plate):
-        self.plates.append(plate)
-
-    def pop_out(self):
+    def pick_one_up(self):
         return self.plates.pop()
 
-    def get_val(self):
-        return self.plates[len(self.plates) - 1]
+    def put_on_top(self, plate_size):
+        if self.height() == self.max_num:
+            self.full_stacks.append(self.plates)
+            self.plates = []
+        self.plates.append(plate_size)
 
-    def stack_size(self):
-        return len(self.plates)
+    def show_stacks(self):
+        self.full_stacks.append(self.plates)
+        print(self.full_stacks)
+        self.full_stacks = self.full_stacks[:-1]
 
-
-def plates_stack(count_of_plates):
-    stack_obj = Stack_Plates()
-
-    for plate in range(1, count_of_plates + 1):
-        stack_obj.push_in(plate)
-        if stack_obj.stack_size() == 10:
-            print(stack_obj.plates)
-            stack_obj = Stack_Plates()
-    if stack_obj.is_empty():
-        return
-    print(stack_obj.plates)
-
-
-plates_stack(99)
+stack_1 = Stack(6)
+print(stack_1.is_empty())
+stack_1.put_on_top(1)
+stack_1.put_on_top(2)
+stack_1.put_on_top(3)
+stack_1.put_on_top(4)
+stack_1.put_on_top(5)
+stack_1.put_on_top(6)
+stack_1.put_on_top(7)
+print(stack_1.last_size())
+stack_1.put_on_top(8)
+stack_1.put_on_top(9)
+stack_1.pick_one_up()
+stack_1.put_on_top(10)
+print(stack_1.height())
+print(stack_1.is_empty())
+stack_1.show_stacks()
