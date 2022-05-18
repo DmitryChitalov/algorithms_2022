fed921bdc1cd8e04ed96b85e54f907a56b493040
Vladimir <vlwasd@yandex.ru>
2022-05-18 18:18:40 +0300

    Lesson 1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,28 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+# Первая функция выполняется быстрее так как самым сложным алгоритмом, является операция sorted() - O(n log n),
+# в отличии от второго способа в котором сложность O(n^2)
+
+
+def max_profit_1(user_dict):                                            # Сложность: O(n log n)
+    sorted_dict = sorted(user_dict, key=user_dict.get, reverse=True)    # O(n log n)
+    for i in sorted_dict:                                               # O(n)
+        return i, user_dict.get(i)                                      # O(1)
+
+
+def max_profit_2(user_dict):                        # Сложность: O(n^2)
+    for i in range(len(user_dict)):                 # O(n)
+        max_value = 0                               # O(1)
+        for key, value in user_dict.items():        # O(n)
+            if max_value < value:                   # O(1)
+                max_value = value                   # O(1)
+                key_max_value = key                 # O(1)
+    return key_max_value, max_value                 # O(1)
+
+
+# comp_dict = {'SuperComp': 100, 'MegaComp': 120, 'SuperMegaComp': 80}
+#
+#
+# print(max_profit_1(comp_dict))
+# print(max_profit_2(comp_dict))
