70d0fe3418984c8e5196a4dcc9ef63fe0abe87b3
Anatoliy-A-M <95370835+Anatoliy-A-M@users.noreply.github.com>
2022-05-18 09:50:24 +0300

    Assigment 5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,37 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+class Stack_Plates:
+    def __init__(self):
+        self.plates = []
+
+    def is_empty(self):
+        return self.plates == []
+
+    def push_in(self, plate):
+        self.plates.append(plate)
+
+    def pop_out(self):
+        return self.plates.pop()
+
+    def get_val(self):
+        return self.plates[len(self.plates) - 1]
+
+    def stack_size(self):
+        return len(self.plates)
+
+
+def plates_stack(count_of_plates):
+    stack_obj = Stack_Plates()
+
+    for plate in range(1, count_of_plates + 1):
+        stack_obj.push_in(plate)
+        if stack_obj.stack_size() == 10:
+            print(stack_obj.plates)
+            stack_obj = Stack_Plates()
+    if stack_obj.is_empty():
+        return
+    print(stack_obj.plates)
+
+
+plates_stack(99)
