4d1cd2543a9c00a316e004088986c3c43044eb54
T1monation <4timonaton@gmail.com>
2022-05-19 22:36:30 +0300

    Home work 3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,89 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+
+"""
+Прошу строго не судит данный код, это мой первы опыт взаимодействия с БД при помощи Питоши)
+Алгоритм создает БД mysql с единственной таблицей users, в которой 3 колонки: id, login, password_hash.
+При создании хеша пароля, "солил" при помощи логина. Данный код можно дорабатывать безконечно, проводить кучу 
+проверок и сравнений, но надо вовремя останавливаться)) Поставленную зачачу код решает!
+"""
+
+from mysql.connector import connect, Error
+import hashlib
+
+
+def query_mysql_select(query_script):
+    try:
+        with connect(host="localhost", user='root', password='drifting69', ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query_script)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
+
+
+def query_mysql_transact(query_script):
+    try:
+        with connect(host="localhost", user='root', password='drifting69', ) as connection:
+            with connection.cursor() as cursor:
+                for el in query_script:
+                    cursor.execute(el)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+
+def create_db():
+    db_drop = 'DROP DATABASE IF EXISTS test_hm'
+    db_create = 'CREATE DATABASE test_hm'
+    db_use = 'USE test_hm'
+    db_table_create = """
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY, 
+        login VARCHAR(250) UNIQUE,
+        password_hash VARCHAR(250)
+    )"""
+    db_init_command = [db_drop, db_create, db_use, db_table_create]
+    query_mysql_transact(db_init_command)
+
+
+def show_db():
+    db_show = 'SELECT id, login, password_hash FROM test_hm.users'
+    result = query_mysql_select(db_show)
+    for el in result:
+        print(el)
+
+
+def create_user():
+    print('Создание пользователя')
+    login = input('Введите логин: ')
+    password = input('Введите пароль: ')
+    user_hash = hashlib.sha256(login.encode('utf-8') + password.encode('utf-8')).hexdigest()
+    query_insert = f"INSERT INTO test_hm.users (login, password_hash) VALUES ('{login}', '{user_hash}')"
+    query_mysql_transact([query_insert])
+
+
+def autorize():
+    print('Авторизация пользователя.')
+    login = input('Введите имя пользователя: ')
+    # Вынимаем из БД хеш пароля
+    query_select = f"SELECT password_hash FROM test_hm.users WHERE login = '{login}'"
+    from_db_hash = query_mysql_select(query_select)
+    password = input('Введите пароль: ')
+    # Вычисляем хеш по введенному логину и паролю
+    user_hash = hashlib.sha256(login.encode('utf-8') + password.encode('utf-8')).hexdigest()
+    print('Авторизация пройденна !') if from_db_hash[0][0] == user_hash else \
+        print('Непраильное имя пользователя или пароль!')
+
+
+# Создадим БД:
+create_db()
+# Создадим нового пользователя
+create_user()
+create_user()
+create_user()
+# Прочитаем созданную таблицу users
+show_db()
+autorize()
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,45 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+import hashlib
+import time
+
+
+# Выполним задание с помощью ООП
+class UrlTable:
+    urls_dict = {}
+    user_salt = ''
+
+    def __init__(self):  # Каждый экземпляр класса будем создовать со своей солью
+        self.user_salt = input('Введите соль: ')
+
+    def url_add(self, user_url):  # Функция добавления url-адреса в таблицу
+        if user_url in self.urls_dict:
+            print(self.urls_dict[user_url])  # если адрес есть в словаре, выводим его в консоль
+        else:  # если адреса в словаре нет, добавляем его
+            self.urls_dict.setdefault(user_url, hashlib.sha512(user_url.encode('utf-8') +
+                                                               self.user_salt.encode('utf-8')).hexdigest())
+
+    @property
+    def show_urls(self):  # вывод имеющейся таблицы адресов
+        print(f'Таблица кешированных адресов на время: {time.ctime()}')
+        for key in self.urls_dict:
+            print(f'Адресс: {key}, хеш: {self.urls_dict[key]}')
+
+    @property
+    def show_salt(self):  # напоминалка введенной соли
+        print(f'Пользовательская соль: {self.user_salt}')
+
+
+a = UrlTable()
+a.url_add('http://www.microsoft.com')
+a.show_urls
+a.url_add('https://www.google.com')
+a.url_add('http://www.google.com')
+a.url_add('http://www.google.com')
+a.url_add('http://yandex.ru')
+a.show_urls
+a.url_add('http://www.microsoft.com')
+a.url_add('http://www.microsoft.com')
+a.show_salt
