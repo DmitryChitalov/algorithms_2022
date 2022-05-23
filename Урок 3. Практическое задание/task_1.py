07e5078bb4be7149ce97511fd6a40858b9b31271
Anatoliy-A-M <95370835+Anatoliy-A-M@users.noreply.github.com>
2022-05-23 14:08:59 +0300

    Assigment 1

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -28,3 +28,63 @@ b) получение элемента списка, оцените сложно
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+import time
+
+def stopwatch(func):
+    def wrapper(args=None):
+        start = time.time()
+        obj = func(args)
+        end = time.time()
+        print(f"Время операции {func} - {end - start} сек")
+        return obj
+
+    return wrapper
+
+
+# а) заполнение списка и словаря
+@stopwatch
+def list_complation(*args):
+    lst = [i for i in range(10 ** 7)]
+    return lst
+
+
+@stopwatch
+def dict_complation(*args):
+    dict = {i: i for i in range(10 ** 7)}
+    return dict
+
+
+# b) получение элемента списка и словаря
+
+@stopwatch
+def take_el_from_list(lst):
+    return lst[9999998]
+
+
+@stopwatch
+def take_el_from_dict(dc):
+    return dc[9999999]
+
+
+# c) удаление элемента списка и словаря
+
+@stopwatch
+def delete_el_from_list(lst: list):
+    lst.pop(9999999)
+    return lst
+
+
+@stopwatch
+def delete_el_from_dict(dc: dict):
+    dc.pop(9999999)
+    return dc
+
+
+test_lst = list_complation()
+test_dict = dict_complation()
+
+take_el_from_list(test_lst)
+take_el_from_dict(test_dict)
+
+test_lst = delete_el_from_list(test_lst)
+test_dict = delete_el_from_dict(test_dict)
