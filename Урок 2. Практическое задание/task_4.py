bdd9d7180821c42a911e50666e9feeff9f546761
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2022-05-23 11:03:28 +0300

    Update task_4.py

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -10,3 +10,15 @@
 Решите через рекурсию. В задании нельзя применять циклы.
 Нужно обойтисть без создания массива!
 """
+
+def recursia(number=int(input('Введите количество элементов: ')), i=0, result=0, result_number=1):
+    if number == i:
+        return fr'Количество элементов: {number}, Сумма: {result}'
+    if number != i:
+        result += result_number
+        result_number /= -2
+        i += 1
+        return recursia(number, i, result, result_number)
+
+
+print(recursia())
