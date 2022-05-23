b516fd02654cb8250752303c95745001d68466ce
Valeriya <105391739+ValeryGav@users.noreply.github.com>
2022-05-23 22:05:42 +0300

    Update task_2.py
    task2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,39 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+
+from uuid import uuid4
+import hashlib
+import csv
+
+def savePassword(password):
+    salt = uuid4().hex
+    res = hashlib.sha256(salt.encode('utf8') + password.encode('utf8')).hexdigest()
+    print 'В базе данных хранится строка: ' + str(res)
+
+    with open('names.csv', 'w') as csvfile:
+        keyResList = [salt, res]
+        writer = csv.writer(csvfile, delimiter=' ')
+        writer.writerows(keyResList)
+
+    checkPassword()
+
+
+def checkPassword():
+    list_ = []
+    password = str(raw_input('Введите пароль еще раз для проверки: '))
+    with open('names.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ')
+        for row in spamreader:
+            list_.append(''.join(row))
+
+    res = hashlib.sha256(list_[0] + password.encode('utf8')).hexdigest()
+
+    if list_[1] == res:
+        print 'Вы ввели правильный пароль'
+    else:
+        print 'Вы ввели некорректный пароль'
+
+if name == 'main':
+    newPassword = str(raw_input('Введите пароль: '))
+    savePassword(password=newPassword)
