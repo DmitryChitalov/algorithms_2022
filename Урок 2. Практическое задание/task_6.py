b67bee28dda712a0cc3cd29d336759c7dd97b2ba
Софья Чащихина <105418002+schash1@users.noreply.github.com>
2022-05-23 11:15:59 +0300

    Update task_6.py

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,25 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+from random import randint
+
+
+def random(random_number=randint(0, 100), i=0):
+    if i < 10:
+        num = (int(input('Введите число от 0 до 100: ')))
+        if num == random_number:
+            return fr'Вы угадали число {random_number}.'
+        if num < random_number:
+            i += 1
+            print('Ваше число меньше')
+            return random(random_number, i)
+        if num > random_number:
+            i += 1
+            print('Ваше число больше')
+            return random(random_number, i)
+    else:
+        return 'Попыток больше нет. Вы проиграли!'
+
+
+print(random())
