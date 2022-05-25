a203eb8ea0f02bf99bf3b41ba53d2506b56885cd
Valeriya <105391739+ValeryGav@users.noreply.github.com>
2022-05-25 23:10:19 +0300

    Update task_3.py
    task3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,9 @@
 """
 
 
+from timeit import timeit
+
+
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
         return
@@ -35,3 +38,31 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(number, num_str=''):
+    if number == 0:
+        return num_str
+    else:
+        part_num = number % 10
+        num_str += '{}'.format(part_num)
+        number = number // 10
+        return revers_4(number=number, num_str=num_str)
+
+
+print(timeit("revers(7412)", setup="from __main__ import revers", number=1000))
+print(timeit("revers_2(7412)", setup="from __main__ import revers_2", number=1000))
+print(timeit("revers_3(7412)", setup="from __main__ import revers_3", number=1000))
+print(timeit("revers_4(7412)", setup="from __main__ import revers_4", number=1000))
+
+"""
+Результат:
+
+0.0006236350000108359
+0.0003958830000101443
+0.0001885970000330417
+0.0010624899999811532
+
+Самая эффективная функция - revers_3
+"""
+
