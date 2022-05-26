00ddd279f6f9c62e1847d42da953e4ad4275135c
Anatoliy-A-M <95370835+Anatoliy-A-M@users.noreply.github.com>
2022-05-26 11:12:58 +0300

    Assigment 4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -38,17 +38,15 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-# достигнуто ускорение за счёт использования comprehensions
 def func_3():
-    new_array = []
-    [new_array.append(array.count(i)) for i in array]
-    elem = array[new_array.index(max(new_array))]
-    return elem
-
+    return max(array, key=lambda i: array.count(i))
 
 # print(func_1())
 # print(func_2())
 #print(func_3())
-print(timeit("func_1", globals=globals())) #->0.03182100004050881
-print(timeit("func_2", globals=globals())) #->0.028320999932475388
-print(timeit("func_3", globals=globals())) #->0.026120899943634868
+
+print(timeit("func_1", globals=globals())) 
+print(timeit("func_2", globals=globals())) 
+print(timeit("func_3", globals=globals())) 
+
+# Задачу удалось написать в одну строку, но к сожалению скорость улучшить не получилось.
