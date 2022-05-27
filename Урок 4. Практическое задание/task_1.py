b38efcf941a95637168ed9f4340934682516fbfa
neatek <neatek@icloud.com>
2022-05-27 19:39:26 +0300

    Урок 4. Практическое задание.

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,7 +11,7 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
-
+from timeit import timeit
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +19,31 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+def func_2(nums):
+    return [idx for idx, i in enumerate(nums) if i % 2 == 0]
+
+
+numss = [1,1,1,1,1,1,1,1,1]
+print(func_1(numss))
+print(func_2(numss))
+
+print(
+    timeit(
+        "func_1([1,1,1,1,1,1,1,1,1])",
+        setup='from __main__ import func_1',
+        number=100000))
+
+print(
+    timeit(
+        "func_2([1,1,1,1,1,1,1,1,1])",
+        setup='from __main__ import func_2',
+        number=100000))
+
+"""
+[]
+[]
+0.0986506
+0.09083469999999999
+мы не создаем список новый и применили List Comprehension - и это быстрее
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,7 +11,7 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
-
+from timeit import timeit
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
@@ -31,7 +31,46 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
+
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join(list(reversed([i for i in str(enter_num)])))
+
+
+
+print(revers_4(123456789))
+
+print(
+    timeit(
+        'revers(123456789)',
+        setup='from __main__ import revers',
+        number=10000))
+print(
+    timeit(
+        'revers_2(123456789)',
+        setup='from __main__ import revers_2',
+        number=10000))
+print(
+    timeit(
+        'revers_3(123456789)',
+        setup='from __main__ import revers_3',
+        number=10000))
+
+print(
+    timeit(
+        'revers_4(123456789)',
+        setup='from __main__ import revers_4',
+        number=10000))
+"""
+0.030070100000000002
+0.018858399999999997
+0.003654699999999997
+0.010387099999999996
+
+самый быстрый 3ий, так как в остальных случаях много операций, либо используется не эффективный тип данных
+"""
