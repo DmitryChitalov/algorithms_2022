15eb810d3e51b67c3a565479acbbb062284d50a6
Almaz Gilmullin <gilmullin.almaz@gmail.com>
2022-05-28 22:19:32 +0300

    task_5 28_05_

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,118 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+########################################################################
+from collections import deque
+from timeit import timeit
+
+my_list = list()
+my_dq = deque()
+my_num = 10 ** 3
+my_num2 = 10 ** 2
+
+
+# 1) сравниваем операции append, pop, extend:
+
+def create_array(lst, n):
+    for i in range(n):
+        lst.append(i)
+
+
+def pop_array(lst, n):
+    for i in range(n):
+        lst.pop()
+
+
+def extend_array(lst, n):
+    for i in range(n):
+        lst.extend([i + 1])
+
+
+# 2) сравниваем операции appendleft, popleft, extendleft:
+
+def ins_lst(lst, n):
+    for i in range(n):
+        lst.insert(0, i)
+
+
+def app_left_dq(dq, n):
+    for i in range(n):
+        dq.appendleft(i)
+
+
+def pop_lst(lst, n):
+    for i in range(n):
+        lst.pop(0)
+
+
+def pop_left_dq(dq, n):
+    for i in range(n):
+        dq.popleft()
+
+
+def ext_left_lst(lst, n):
+    for i in range(n):
+        lst[:0] = [i]
+
+
+def ext_left_dq(dq, n):
+    my_str = str('')
+    for i in range(n):
+        my_str += str(i)
+        dq.extendleft(my_str)
+
+
+# 3) сравниваем операции получения элемента списка и дека:
+
+def get_lst(lst, n):
+    for i in range(n):
+        lst[i] = i
+    return lst
+
+
+def get_dq(dq, n):
+    for i in range(n):
+        dq[i] = i
+    return dq
+
+
+print('append, pop, extend:')
+print('my_list: ', timeit("create_array(my_list, my_num)", globals=globals(), number=10000))
+print('my_dq: ', timeit("create_array(my_dq, my_num)", globals=globals(), number=10000))
+print()
+print('my_list: ', timeit("pop_array(my_list, my_num)", globals=globals(), number=10000))
+print('my_dq: ', timeit("pop_array(my_dq, my_num)", globals=globals(), number=10000))
+print()
+print('my_list: ', timeit("extend_array(my_list, my_num)", globals=globals(), number=10000))
+print('my_dq: ', timeit("extend_array(my_dq, my_num)", globals=globals(), number=10000))
+print()
+
+print('appendleft, popleft, extendleft:')
+print('my_list: ', timeit("ins_lst(my_list, my_num2)", globals=globals(), number=10))
+print('my_dq: ', timeit("app_left_dq(my_dq, my_num2)", globals=globals(), number=10))
+print()
+print('my_list: ', timeit("pop_lst(my_list, my_num2)", globals=globals(), number=10))
+print('my_dq: ', timeit("pop_left_dq(my_dq, my_num2)", globals=globals(), number=10))
+print()
+print('my_list: ', timeit("ext_left_lst(my_list, my_num2)", globals=globals(), number=10))
+print('my_dq: ', timeit("ext_left_dq(my_dq, my_num2)", globals=globals(), number=10))
+print()
+print('get:')
+print('my_list: ', timeit("get_lst(my_list, my_num)", globals=globals(), number=10000))
+print('my_dq: ', timeit("get_dq(my_dq, my_num)", globals=globals(), number=10000))
+print()
+
+"""
+Вывод:
+1) сравнивая операции append, pop, extend списка и дека можно
+прийти к выводу, что дек опережает список при выполнении 
+операции append. При операциях pop, extend у них паритет.
+2) сравнивая операции appendleft, popleft, extendleft списка и дека можно
+прийти к выводу, что дек опережает список при выполнении 
+этих операции. Особенно при выполнении  операции extendleft.
+3) сравнивая операции получения элемента списка и дека 
+видно, что список опережает дек.
+Что доказывает основное правило: если вам нужно
+что-то быстро дописать или вытащить, используйте deque.
+Если вам нужен быстрый случайный доступ, используйте list
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,46 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+############################################################################
+from collections import OrderedDict
+from timeit import timeit
+
+
+# Операция заполнения словаря
+
+def fill_dict(us_dict, us_num):
+    for i in range(us_num):
+        us_dict[i] = i
+
+
+num = 10 ** 4
+simple_dict = dict()
+ordered_dict = OrderedDict()
+
+print('Операции заполнения словарей:')
+print(timeit("fill_dict(simple_dict, num)", globals=globals(), number=1000))
+print(timeit("fill_dict(ordered_dict, num)", globals=globals(), number=1000))
+print()
+
+
+# Операция изменения словаря
+
+def change_dict(us_dict, us_num):
+    for i in range(us_num):
+        us_dict.pop(i)
+    for j in range(us_num):
+        us_dict[j] = 'fill'
+
+
+print('Операции изменения словарей:')
+print(timeit("change_dict(simple_dict, num)", globals=globals(), number=1000))
+print(timeit("change_dict(ordered_dict, num)", globals=globals(), number=1000))
+
+'''
+Аналитика:
+    Исходя из полученных результатов, мы можем сказать, что обычный словарь 
+операции заполнения и изменения выполняет быстрее.
+    Смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях есть, если
+нам важен именно порядок следования элементов. А также возможность 
+оперативного переупорядочивания элементов.
+'''
