7a76d6b54ba605fbd0dd08489db474ed04bc06e4
Troover <murdoc-94@mail.ru>
2022-05-29 18:32:16 +0300

    hw_lesson_4

---
+++ lesson 4/2_task.py
@@ -0,0 +1,91 @@
+"""
+Задание 2.
+Приведен код, который формирует из введенного числа
+обратное по порядку входящих в него цифр.
+Задача решена через рекурсию
+Выполнена попытка оптимизировать решение мемоизацией
+Сделаны замеры обеих реализаций.
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
+П.С. задание не такое простое, как кажется
+"""
+
+from timeit import timeit
+from random import randint
+
+
+def recursive_reverse(number):
+    if number == 0:
+        return str(number % 10)
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+num_100 = randint(10000, 1000000)
+num_1000 = randint(1000000, 10000000)
+num_10000 = randint(100000000, 10000000000000)
+
+print('Не оптимизированная функция recursive_reverse')
+print(
+    timeit(
+        "recursive_reverse(num_100)",
+        setup='from __main__ import recursive_reverse, num_100',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_1000)",
+        setup='from __main__ import recursive_reverse, num_1000',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_10000)",
+        setup='from __main__ import recursive_reverse, num_10000',
+        number=10000))
+
+
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorate
+
+
+@memoize
+def recursive_reverse_mem(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
+
+
+print('Оптимизированная функция recursive_reverse_mem')
+print(
+    timeit(
+        'recursive_reverse_mem(num_100)',
+        setup='from __main__ import recursive_reverse_mem, num_100',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_1000)',
+        setup='from __main__ import recursive_reverse_mem, num_1000',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_10000)',
+        setup='from __main__ import recursive_reverse_mem, num_10000',
+        number=10000))
+
+
+"""
+    При запуске кода randint выдаёт рандомное число в заданном диапазоне, 
+    декоратор при каждом запуске функции записывает полученные значения в кэш,
+    т.е. если задано число 123456789, на первом этапе в словарь попадёт цифра 9: "9"
+    на втором 98: "98" и т.д. до конца нашего числа.
+    При повторении запуска кода, результат уже берётся из кэша.
+    Т.е. из 1000 попыток запуска кода, в 999 случаях результат берётся из кэша.
+    На этом и основано повышение скорости выполнения.
+    При единичном запуске кода мемоизация не требуется.
+"""
