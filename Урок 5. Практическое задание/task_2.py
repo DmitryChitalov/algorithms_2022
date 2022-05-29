73d2ba8879ae0a168405c638f1f11fa85d1a9f34
a.timofeev <tauman08@gmail.com>
2022-05-29 15:22:21 +0300

    дз5 к пятому уроку

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,40 @@ reduce
 __mul__
 __add__
 """
+from collections import defaultdict
+
+def sum():
+    lst = [('one',list(input('Введите первое число: '))),('two',list(input('Введите второе число: ')))]
+    nums = defaultdict(list)
+    print(*nums)
+    for key,values in lst:
+        nums[key] = values
+    print(nums)
+
+    sum = hex(int(''.join(nums['one']),16) + int(''.join(nums['two']),16))
+    mult = hex(int(''.join(nums['one']),16) * int(''.join(nums['two']),16))
+    print(f' сумма: {list(str(sum))}')
+    print(f' Произведение: {list(str(mult))}')
+
+class MyOperations():
+
+    def __init__(self,num):
+        self.value = list(str(num))
+
+    def __add__(self, other):
+        sum = hex(int(''.join(self.value),16) + int(''.join(other.value),16))
+        return MyOperations(sum)
+
+    def __mul__(self, other):
+        mult = hex(int(''.join(self.value), 16) * int(''.join(other.value), 16))
+        return MyOperations(mult)
+
+
+if __name__ == '__main__':
+
+sum()
+
+obj = MyOperations('f') + MyOperations(1)
+print(f'summa: {obj.value}')
+obj = MyOperations('f') * MyOperations('f')
+print(f'multiplaction: {obj.value}')
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,30 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+from timeit import timeit
+from collections import OrderedDict
+
+def pop_dic(dict):
+    for i in range(len(dict)):
+        dict.pop(i)
+
+
+
+
+my_dict = {i:f'test_{i}' for i in range(10000)}
+my_od = OrderedDict([(i,f'test_{i}') for i in range(10000)])
+
+print(f'Получение элемента словарь: {timeit("my_dict.get(100)",number=100000,globals=globals())}')
+print(f'Получение элемента OrderedDict: {timeit("my_od.get(100)",number=100000,globals=globals())}')
+
+print(f'метод pop словарь: {timeit("pop_dic(my_dict)",number=1,globals=globals())}')
+print(f'метод pop OrderedDict: {timeit("pop_dic(my_od)",number=1,globals=globals())}')
+
+print(f'метод items() словарь: {timeit("my_dict.items()",number=1000,globals=globals())}')
+print(f'метод items() OrderedDict: {timeit("my_od.items()",number=1000,globals=globals())}')
+
+"""
+По итогам замеров OrderedDict практически идентичен словарю и так как с версии 3.6 словарь сохраняет порядок, то 
+особого смысла использовать OrderedDict нет
+"""
+
