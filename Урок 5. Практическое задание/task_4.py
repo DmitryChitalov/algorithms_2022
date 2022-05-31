f180d09ebb20a3090d64ae453f70e08347a0967b
Valeriya <105391739+ValeryGav@users.noreply.github.com>
2022-05-31 21:14:07 +0300

    Update task_4.py
    task4

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,57 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+def dict1():
+    my_dict = {}
+    my_dict['apple'] = 3
+
+def dict2():
+    my_dict = {'apple': 3}
+    my_dict.pop('apple')
+
+def dict3():
+    my_dict = {'apple': 3}
+    my_new_dict = my_dict.copy()
+
+def order_dict1():
+    my_dict = OrderedDict()
+    my_dict['apple'] = 4
+
+def order_dict2():
+    my_dict = OrderedDict({'apple': 4})
+    my_dict.pop('apple')
+
+def order_dict3():
+    my_dict = OrderedDict({'apple': 4})
+    my_new_dict = my_dict.copy()
+
+if __name__ == '__main__':
+    print 'Сравниваем три функции (добавление элемента, pop и copy) для dict и OrderDict: '
+    print 'dict:'
+    print timeit('dict1()', setup='from main import dict1', number=10000)
+    print timeit('dict2()', setup='from main import dict2', number=10000)
+    print timeit('dict3()', setup='from main import dict3', number=10000)
+    """
+    Вывод:
+    dict:
+        0.001327
+        0.0021453
+        0.0028345
+    Самая быстрая функция - получение элемента
+    """
+    print 'OrderDict:'
+    print timeit('order_dict1()', setup='from main import order_dict1', number=10000)
+    print timeit('order_dict2()', setup='from main import order_dict2', number=10000)
+    print timeit('order_dict3()', setup='from main import order_dict3', number=10000)
+    """
+    Вывод:
+    OrderDict:
+        0.0450079
+        0.0652484
+        0.1093582
+    Функции OrderDict намного медленее чем у обычного dict
+    """
