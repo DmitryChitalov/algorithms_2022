32011253830a01adbfe9ad4438ea18af60c6d32c
Lev Sobolenkov <dangerrrleo@gmail.com>
2022-06-01 19:27:17 +0300

    Добавлена 1 дз

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,79 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class TaskBoard:
+    def __init__(self):
+        self.tasks = []
+        self.modification = []
+        self.done = []
+
+    def size(self):
+        task_board = {
+            'Новых задач': len(self.tasks),
+            'Задачи на доработке': len(self.modification),
+            'Выполненные задачи': len(self.done)
+        }
+        return task_board
+
+    def add_task(self, task):
+        self.tasks.insert(0, task)
+
+    def get_task(self):
+        return self.tasks.pop()
+
+    def done_task(self, task):
+        self.done.insert(0, task)
+
+    def task_to_done(self):
+        return self.done.pop()
+
+    def task_modification(self, task):
+        self.modification.insert(0, task)
+
+    def task_to_modification(self):
+        return self.modification.pop()
+
+
+board = TaskBoard()
+print(board.size())
+
+board.add_task(f'Задача № 1')
+
+board.add_task(f'Задача № 2')
+
+board.add_task(f'Задача № 3')
+
+print(board.size())
+
+task_to_done = board.get_task()
+print(task_to_done)
+board.done_task(task_to_done)
+
+print(board.size())
+
+task_to_done_2 = board.get_task()
+print(task_to_done_2)
+board.done_task(task_to_done_2)
+
+print(board.size())
+
+
+task_to_modification = board.task_to_done()
+print(task_to_modification)
+board.task_modification(task_to_modification)
+
+print(board.size())
+
+correct_task = board.task_to_modification()
+print(correct_task)
+board.done_task(correct_task)
+
+print(board.size())
+
+task_to_done_3 = board.get_task()
+print(task_to_done_3)
+board.done_task(task_to_done_3)
+
+print(board.size())
--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -13,3 +13,65 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --код с нуля писать не нужно, требуется доработать пример с урока
 """
+
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        if elem != ' ':
+            self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+if __name__ == '__main__':
+    dc_obj = DequeClass()
+    print(dc_obj.is_empty())  # -> True
+
+    # добавить элементы в хвост
+    dc_obj.add_to_rear(10)
+    dc_obj.add_to_rear('my_str')
+
+    # добавить элементы в голову
+    dc_obj.add_to_front(None)
+    dc_obj.add_to_front(True)
+
+    # размер дека
+    print(dc_obj.size())  # -> 4
+    print(dc_obj.is_empty())  # -> False
+
+from task_7 import DequeClass
+
+
+def pal_checker(string):
+    dc_obj = DequeClass()
+    for el in string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+
+print(pal_checker("молоко делили ледоколом"))
