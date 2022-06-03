5257231ae027d56ef9dbb58bf9cc28e70915ece7
Mikhail-Zh <python-developer@mail.ru>
2022-06-03 20:30:55 +0500

    Выполнил домашнее задание к уроку №7 Алгоритмы на Python

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,52 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+"""
+Время выполнения:
+При длине массива (2m + 1), где m = 5: 0.001645099837332964 сек.
+При длине массива (2m + 1), где m = 50: 0.03460709983482957 сек.
+При длине массива (2m + 1), где m = 500: 0.5130623001605272 сек.
+"""
+
+from timeit import timeit
+from random import randint
+
+
+def shellsort(a, m):
+    def new_increment(a):
+        i = int(len(a) / 2)
+        yield i
+        while i != 1:
+            if i == 2:
+                i = 1
+            else:
+                i = int(round(i / 2.2))
+            yield i
+
+    for increment in new_increment(a):
+        for i in range(increment, len(a)):
+            for j in range(i, increment - 1, -increment):
+                if a[j - increment] < a[j]:
+                    break
+                a[j], a[j - increment] = a[j - increment], a[j]
+    return f'медиана равна: {a[m]}'
+
+
+m = 5
+array_11 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(shellsort(array_11[:], m))
+print(f"Время выполнения со списком из {2 * m + 1} 'элементов: "
+      f"{timeit(f'shellsort(array_11[:], m)', globals=globals(), number=100)} сек.\n")
+
+m = 50
+array_101 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(shellsort(array_101[:], m))
+print(f"Время выполнения со списком из {2 * m + 1} 'элементов: "
+      f"{timeit(f'shellsort(array_101[:], m)', globals=globals(), number=100)} сек.\n")
+
+m = 500
+array_1001 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(shellsort(array_1001[:], m))
+print(f"Время выполнения со списком из {2 * m + 1} 'элементов: "
+      f"{timeit(f'shellsort(array_1001[:], m)', globals=globals(), number=100)} сек.")
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,38 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+"""
+Время выполнения:
+При длине массива (2m + 1), где m = 5: 0.0005113000515848398 сек.
+При длине массива (2m + 1), где m = 50: 0.011920100077986717 сек.
+При длине массива (2m + 1), где m = 500: 0.7763685998506844 сек.
+"""
+
+from timeit import timeit
+from random import randint
+
+
+def search_median(lst_obj, m):
+    for _ in range(len(lst_obj) - (m + 1)):
+        lst_obj.remove(max(lst_obj))
+    return f'медиана равна: {max(lst_obj)}'
+
+
+m = 5
+array_11 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(search_median(array_11[:], m))
+print(f"Время выполнения со списком из {2 * m + 1} элементов: "
+      f"{timeit(f'search_median(array_11[:], m)', globals=globals(), number=100)} сек.\n")
+
+m = 50
+array_101 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(search_median(array_101[:], m))
+print(f"Время выполнения со списком из {2 * m + 1} элементов: "
+      f"{timeit(f'search_median(array_101[:], m)', globals=globals(), number=100)} сек.\n")
+
+m = 500
+array_1001 = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(search_median(array_1001[:], m))
+print(f"Время выполнения со списком из {2 * m + 1} элементов: "
+      f"{timeit(f'search_median(array_1001[:], m)', globals=globals(), number=100)} сек.")
