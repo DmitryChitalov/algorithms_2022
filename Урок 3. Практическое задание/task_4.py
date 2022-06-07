387a328db1bc8029d2c86d76b698c2259c8709c8
Tigreshkas <st.tigra@mail.ru>
2022-06-07 16:20:27 +0500

    fix homework_3
    fix task 1, 2

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,24 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+from uuid import uuid4
+import hashlib
+
+
+cash_dict = {}
+salt = uuid4().hex
+
+
+def url_cash(key_url):
+    if key_url in cash_dict.keys():
+        print(f'{cash_dict[key_url]}')
+    else:
+        cash_dict[key_url] = hashlib.sha512(salt.encode('utf-8') + key_url.encode('utf-8')).hexdigest()
+        print(f'Страница записана в кэш. Хеш {cash_dict[key_url]}')
+
+
+url_cash('mail.ru')
+url_cash('labirint.ru')
+url_cash('gb.ru')
+url_cash('labirint.ru')
