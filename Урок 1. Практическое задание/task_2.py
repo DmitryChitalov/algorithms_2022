67481de1a4ef2fcf5a3b343befa7db8c1acef192
GarrikDanilov <garrik_danilov@mail.ru>
2022-08-15 00:22:51 +0300

    task_2

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,32 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+def min_squre(list_in):
+    copy_list = []                                     # О(1) - константная
+    copy_list.extend(list_in)                          # O(N) - линейная
+
+    for _ in range(len(copy_list)):                    # O(N**2) - квадратичная
+        for idx in range(len(copy_list) - 1):          # O(N) - линейная
+            if copy_list[idx] > copy_list[idx + 1]:    # О(1) - константная
+                buf = copy_list[idx]                   # О(1) - константная
+                copy_list[idx] = copy_list[idx + 1]    # О(1) - константная
+                copy_list[idx + 1] = buf               # О(1) - константная
+
+    min_val = copy_list[0]                             # О(1) - константная
+    return min_val                                     # О(1) - константная
+
+
+def min_lin(list_in):
+    min_val = list_in[0]     # О(1) - константная
+    for val in list_in:      # O(N) - линейная
+        if val < min_val:    # О(1) - константная
+            min_val = val    # О(1) - константная
+    return min_val           # О(1) - константная
+
+
+if __name__ == '__main__':
+    test_list = [2, 6, 9, 1, 3, 7]
+
+    print(min_squre(test_list))
+    print(min_lin(test_list))
