e3d8cb3f343fdbfe1e35de756ddb277233e6a864
kenterberiiskii <a.mizyakov@gmail.com>
2022-08-16 21:46:45 +0300

    Update task_6.py

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,47 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        # x.insert(0, 3)
+        self.stack.insert(0, item)
+
+    def pop(self):
+        removed = self.stack.pop()
+        return removed
+
+    def stack_size(self):
+        return len(self.stack)
+
+    def stack_remove(self, element, item):
+        element.stack.insert(0, item.pop())
+
+    def show_elements(self):
+        return (self.stack)
+
+
+
+
+if __name__ == '__main__':
+    stack_base = Stack()
+    stack_remake = Stack()
+    stack_ready = Stack()
+
+    print(stack_base.stack_size())
+    print(stack_remake.stack_size())
+    print(stack_ready.stack_size())
+    stack_base.push('1')
+    stack_base.push('2')
+    stack_base.push('22')
+    print(stack_base.stack_size())
+
+    stack_base.stack_remove(stack_remake, stack_base)
+
+    print(stack_base.stack_size())
+    print(stack_remake.stack_size())
+    print(stack_ready.stack_size())
+    print(stack_remake.show_elements())
