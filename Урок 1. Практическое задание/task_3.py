d47e6529a31c28c10139e439b03955d2559cf3b7
kimalio <kimalio@proton.me>
2022-08-16 17:08:22 +0400

    Lesson 01

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,30 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+def top_three_O_n_sq(firms, top_size=3):  # O(n^2)
+    top, f = [], firms.copy()
+    if len(f) <= top_size:
+        return f
+    else:
+        while len(top) != top_size:
+            k = list(f.keys())[list(f.values()).index(max(f.values()))]
+            v = f[list(f.keys())[list(f.values()).index(max(f.values()))]]
+            top.append({k: v})
+            del f[k]
+    return top
+
+def top_three_O_n(firms, top_size=3):  # O(n)
+    return sorted(firms.items(), key=lambda kv: kv[1])[::-1][:top_size]
+
+
+if __name__ == '__main__':
+    companies = {'Horns and hooves': 200_000,
+                 'Flippers and tails': 500_000,
+                 'Mars': 100_000,
+                 'Space-X': 400_000,
+                 'Bugulma': 900_000}
+
+    print(top_three_O_n_sq(companies))
+    print(top_three_O_n(companies))
+
+    
