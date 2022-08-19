4f624e87d583b71a361a7970cb69f6637ebf9ba1
Denchik2021 <education2021@internet.ru>
2022-08-19 21:26:21 +0500

    Домашнее задание по 1 уроку Якупов Д.И.
    ДЗ по 1 уроку

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -16,4 +16,36 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+some_company = {'BP': 450000, 'Shell': 800000, 'Rosneft': 350000, 'Lukoil': 11000000, 'Bashneft': 750000}
+print(len(some_company))
 
+
+# сложность O(n log n)
+
+def by_values(item):
+    return item[1]                                                      # O(1)
+
+
+max_job_dollars = {}                                                    # O(1)
+i = 0                                                                   # O(1)
+for x, m in sorted(some_company.items(), key=by_values, reverse=True):  # O(n log n)
+    if i < 3:                                                           # O(len(i)
+        max_job_dollars.setdefault(x, m)                                # O(1)
+    i = i + 1                                                           # O(1)
+
+print(max_job_dollars)                                                  # O(1)
+
+
+# второй способ
+#сложность: O(n)
+try:
+    some_company = {'BP': 450000, 'Shell': 800000, 'Rosneft': 350000, 'Lukoil': 11000000, 'Bashneft': 750000}
+    print('\nвторое решение:')                                          #O(1)
+    print(max(some_company, key=some_company.get))                      #O(n)
+
+    v = list(some_company.values())                                     #O(len(...))
+    k = list(some_company.keys())                                       #O(len(...))
+    print(k[v.index(max(v[0:3]))])                                      #O(n)
+
+except Exception as e:
+    print(e)
