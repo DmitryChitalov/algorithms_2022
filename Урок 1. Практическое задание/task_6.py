27b34b18c7ac38377bc77110b01892a42a6890cb
Roman <korchigin5@gmail.com>
2022-08-19 23:58:15 +0300

    Korchigin_Roman_lesson1

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,61 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class TaskBoardClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+    def remove(self, other, item):
+        other.elems.insert(0, item.from_queue)
+        self.elems.pop()
+
+
+if __name__ == '__main__':
+    basic_task = TaskBoardClass()
+    modification_task = TaskBoardClass()
+    solved_task = TaskBoardClass()
+    print(basic_task.is_empty())
+
+    # помещаем обЪекты в базовую очередь
+    basic_task.to_queue('first')
+    basic_task.to_queue('second')
+    basic_task.to_queue('third')
+    basic_task.to_queue('fourth')
+    basic_task.to_queue('fifth')
+    print(basic_task.is_empty())
+    print(basic_task.size())
+    print(modification_task.is_empty())
+    print(solved_task.is_empty())
+
+    # отправляем задания на доработку
+    basic_task.remove(modification_task, basic_task)
+    print(basic_task.size())
+    print(modification_task.size())
+    basic_task.remove(modification_task, basic_task)
+    print(basic_task.elems)
+
+    # отправляем задания в список решенных задач
+    basic_task.remove(solved_task, basic_task)
+    print(basic_task.size())
+    print(solved_task.size())
+
+    # задание доработали, отправили в решенное
+    modification_task.remove(solved_task, modification_task)
+    print(basic_task.size())
+    print(modification_task.size())
+    print(solved_task.size())
+
