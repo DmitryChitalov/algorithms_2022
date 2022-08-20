d7d2bdaf605f87e7667369ff61e48daea52a9543
daniil_dn <dosindaniilm@gmail.com>
2022-08-20 21:16:32 +0300

    hw2 update

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,23 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+import random
+
+
+def user_quest(to_guess: int, attempt: int):
+    user_in = input(f'you have {attempt} attempts. Enter number to guess: \n')
+    if user_in.isdigit() and int(user_in) == to_guess:
+        print('Guessed')
+    elif attempt == 1:
+        print("Looser, The number is ", to_guess)
+        return
+    elif not user_in.isdigit():
+        print("это не число")
+        user_quest(to_guess, attempt)
+    user_quest(to_guess, attempt - 1)
+
+
+
+if __name__ == "__main__":
+    to_guess = random.randint(0, 100)
+    print(user_quest(to_guess, 10))
