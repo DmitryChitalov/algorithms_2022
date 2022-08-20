5e48d6bbee1718ad477cca5f5f91bdaccb124f89
fxshad <fxshad@mail.ru>
2022-08-20 18:27:25 +0700

    lesson_2_recursion

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -10,3 +10,16 @@
 Решите через рекурсию. В задании нельзя применять циклы.
 Нужно обойтисть без создания массива!
 """
+
+
+def sum_sequence(step, number=1.0, sign=-1, sum_seq=0.0):
+    if step == 0:
+        return sum_seq
+    sum_seq = sum_seq + number
+    number = (number / 2) * sign
+    step -= 1
+    return sum_sequence(step, number, sign, sum_seq)
+
+
+print(sum_sequence(3))
+
