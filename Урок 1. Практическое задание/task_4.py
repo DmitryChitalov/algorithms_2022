95c58981f284cf0192ed99b406cd6899b7f774fd
GarrikDanilov <garrik_danilov@mail.ru>
2022-08-20 14:39:21 +0300

    task_4

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,41 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+# О(1) - константная
+def authorization_const(users, login, password):
+    if users.get(login):                                                                        # О(1) - константная
+        user = users.get(login)                                                                 # О(1) - константная
+        if user['password'] == password:                                                        # О(1) - константная
+            return 'Авторизация пройдена' if user['state'] else 'Активируйте учетную запись'    # О(1) - константная
+        else:
+            return 'Неверный пароль'                                                            # О(1) - константная
+    return 'Неверный логин'                                                                     # О(1) - константная
+
+
+# O(N) - линейная
+def authorization_lin(users, login, password):
+    for logins, info in users.items():                                                            # O(N) - линейная
+        if logins == login:                                                                       # О(1) - константная
+            if info['password'] == password:                                                      # О(1) - константная
+                return 'Авторизация пройдена' if info['state'] else 'Активируйте учетную запись'  # О(1) - константная
+            else:
+                return 'Неверный пароль'                                                          # О(1) - константная
+    return 'Неверный логин'                                                                       # О(1) - константная
+
+
+if __name__ == '__main__':
+    users = {'user_1': {'password': 123, 'state': False}, 'user_2': {'password': 456, 'state': True},
+             'user_3': {'password': 789, 'state': True}}
+
+    print('Константная сложность:')
+    print(authorization_const(users, 'user_1', 123))
+    print(authorization_const(users, 'user_2', 222))
+    print(authorization_const(users, 'user_3', 789))
+    print('\n')
+
+    print('Линейная сложность:')
+    print(authorization_lin(users, 'user_1', 123))
+    print(authorization_lin(users, 'user_2', 222))
+    print(authorization_lin(users, 'user_3', 789))
