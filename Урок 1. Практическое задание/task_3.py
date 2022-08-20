e01866d2f423a911b80b1591f7249b24f286cfe1
Andrey Milykh <amilykh@mail.ru>
2022-08-20 16:30:27 +0300

    Milykh_Andrey_dz_1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,54 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+""" 
+Примечание. Сортировка не позволяет изменять порядок словаря на месте.
+Записываем упорядоченные пары в совершенно новый пустой словарь.
+В Python 3.7 и более поздних версиях словари сортируются по порядку
+вставки элементов. В более ранних версиях они были неупорядоченными.
+"""
+def max_profits_1(companies, count):
+    """Функция должная вернуть минимальное значение для списка.
+    Sort the values
+
+    Сложность: O(N^2)
+    """
+    sorted_values = sorted(companies.values(), reverse=True)
+    li = []
+    [li.append(x) for x in sorted_values if x not in li]
+    li = li[:count]
+    sorted_dict = {}
+
+    for i in li:
+        if count == 0:
+            break
+        for k in companies.keys():
+            if companies[k] == i:
+                sorted_dict[k] = companies[k]
+                count = count - 1
+                if  count == 0:
+                    break
+
+    return sorted_dict
+
+
+def max_profits_2(companies, count):
+    """Функция должная вернуть минимальное значение для списка.
+    Sort the values
+
+    Сложность: O(NlogN)
+    """
+    sorted_dict = {}
+    # список ключей, отсортированных по убыванию
+    sorted_keys = sorted(companies, key=companies.get, reverse=True)
+    sorted_keys = sorted_keys[:count]
+
+    for i in sorted_keys:
+        sorted_dict[i] = companies[i]
+
+    return sorted_dict
+
+
+companies1 = {'A': 10, 'B': 50, 'C': 30, 'D': 20, 'E': 40, 'F': 50, 'G': 30 }
+print(max_profits_1(companies1, 4))
+print(max_profits_2(companies1, 5))
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,49 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+def authorization_1(users, name, password):
+    """
+    Сложность: O(N)
+    """
+    for key, value in users.items():
+        if key == name:
+            if value['password'] != password:
+                return "Не верный пароль!"
+            elif not value['activation']:
+                return "Учетная запись не активирована! Пройдите активацию!"
+            else:
+                return "Ресурс предоставлен"
+
+
+    return "Пользователь с данной учетной записью не существует!"
+
+
+def authorization_2(users, name, password):
+    """
+    Сложность: O(1)
+    """
+    if users.get(name):
+        if users[name]['password'] != password:
+            return "Не верный пароль!"
+        elif not users[name]['activation']:
+            return "Учетная запись не активирована! Пройдите активацию!"
+        else:
+            return "Ресурс предоставлен"
+    else:
+        return "Пользователь с данной учетной записью не существует!"
+
+users1 = {
+    'user1': {'password': '111', 'activation': True},
+    'user2': {'password': '222', 'activation': True},
+    'user3': {'password': '333', 'activation': False}
+}
+
+print(authorization_1(users1, 'user1', '111'))
+print(authorization_1(users1, 'user1', '222'))
+print(authorization_1(users1, 'user3', '333'))
+print(authorization_1(users1, 'user4', '444'))
+
+print(authorization_2(users1, 'user1', '111'))
+print(authorization_2(users1, 'user1', '222'))
+print(authorization_2(users1, 'user3', '333'))
+print(authorization_2(users1, 'user4', '444'))
