fa53460c32b686573edd1b21c4257ccfa1fdb59d
ton1k090 <ton1k090@mail.ru>
2022-08-20 14:43:51 +0300

    Lesson_1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,34 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+class StackClass:
+    def __init__(self, max_size):
+        self.elems = [[]]
+        self.max_size = max_size
+
+    def push_in(self, el):
+        if len(self.elems[len(self.elems) - 1]) < self.max_size:
+            self.elems[len(self.elems) - 1].append(el)
+        else:
+            self.elems.append([])
+            self.elems[len(self.elems) - 1].append(el)
+
+
+    def __str__(self):
+        return str(self.elems)
+
+    def stack_size(self):
+        return len(self.elems)
+
+
+a = StackClass(5)
+a.push_in(1)
+a.push_in(1)
+a.push_in(1)
+a.push_in(1)
+a.push_in(1)
+a.push_in(1)
+a.push_in(1)
+print(a)
+print()
+print(a.stack_size())
