f60e96d3882328e1ed78da522c462162384e0c6d
babenk0ff <babenk0fff@gmail.com>
2022-08-21 20:08:55 +0300

    Solution for task_6 added

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,34 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+from random import randint
+
+
+def guess_number(counter=10, number=None, generate=True):
+    if counter == 0:
+        print(f'Вы не угадали! Было загадано число {number}')
+        return
+
+    if generate:
+        number = randint(0, 100)
+        print('Отгадайте число от 0 до 100 за 10 попыток')
+
+    try:
+        guess = int(input(f'Попыток осталось: {counter}. Введите число: '))
+    except ValueError:
+        print('Ошибка! Введено не число!')
+        guess_number(counter, number=number, generate=False)
+    else:
+        if guess == number:
+            print(f'Вы угадали - загадано число {number}!')
+            return
+        elif guess < number:
+            print('Неверно! Загаданное число больше')
+            return guess_number(counter - 1, number=number, generate=False)
+        elif guess > number:
+            print('Неверно! Загаданное число меньше')
+            return guess_number(counter - 1, number=number, generate=False)
+
+
+guess_number()
