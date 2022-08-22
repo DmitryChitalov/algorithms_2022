91fc4f75308a0c2e84cd3347ccad2de2dd340ca7
Yaroslav Russu <yaroslav.russu@mail.ru>
2022-08-22 11:51:20 +1000

    Третье задание выполнено и оформлено к сдаче

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -28,12 +28,23 @@ import hashlib
 
 letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c',
            'v', 'b', 'n', 'm']
-n = 10
+n = 4
 s = ''.join([random.choice(letters) for x in range(n)])
-set_s = set([s[i:j + i + 1] for i in range(len(s)) for j in range(len(s) + 1)])
-print(len(set_s))
-res = []
-for i in set_s:
-    h = hashlib.sha256(i.encode())
-    res.append(h)
-print(len(res))
+# s = 'papa'
+
+"""Если следовать ТЗ, то ход выполнения должен быть такой, но для 'papa' вместо 6 уникальных подстрок мы имеет 15
+хешей неуникальных подстрок"""
+substrings_h = set()
+for i in range(len(s)+1):
+    for j in range(len(s)):
+        res = s[i:j + i + 1]
+        if res != '' and res != s:
+            substrings_h.add((hashlib.sha256(res.encode())).hexdigest())
+
+# print(substrings_h)
+
+"""Возможно правильнее было бы сначала подготовить множество подстрок, а потом преобразовать их в hash"""
+substrings_set = set([s[i:j + i + 1] for j in range(len(s)) for i in range(len(s)+1)
+                      if (s[i:j + i + 1]) != '' and (s[i:j + i + 1]) != s])
+substrings_hash = [hashlib.sha256(i.encode()).hexdigest() for i in substrings_set]
+# print(substrings_hash)
