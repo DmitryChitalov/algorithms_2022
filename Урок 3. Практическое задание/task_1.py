3463318661667fac1996ed30cefacf30f451ae13
kovantigo-ptz <kovantigo-ptz@mail.ru>
2022-08-23 18:32:13 +0300

    add homework

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -28,3 +28,72 @@ b) получение элемента списка, оцените сложно
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+
+
+from time import time
+
+
+def stopwatch(function):
+
+    def wrapper():
+        start = time()
+        result = function()
+        end = time()
+        print(f'Время работы: {end - start:.10f}')
+        return result
+    return wrapper
+
+
+@stopwatch
+def fill_list():   # O(1)
+    result_list = []
+    for i in range(10 ** 5):
+        result_list.append(i)
+    return result_list
+
+
+@stopwatch
+def fill_dict():   # O(1)
+    result_dict = {}
+    for i in range(10 ** 5):
+        result_dict.setdefault(i, None)
+    return result_dict
+
+
+res_list = fill_list()
+res_dict = fill_dict()
+# ------------------------------
+
+
+@stopwatch
+def elem_list():    # O(N)
+    for i in range(len(res_list)):
+        print(res_list[i], end=' ')
+
+
+@stopwatch
+def val_dict(): # O(N)
+    for key in res_dict.keys():
+        print(res_dict[key], end=' ')
+
+
+elem_list()
+val_dict()
+# ------------------------------
+
+
+@stopwatch
+def del_elem_list():    # O(N)
+    for i in range(len(res_list)):
+        res_list.remove(i)
+    return res_list
+
+
+@stopwatch
+def del_key_val_dict():    # O(N)
+    for key in range(len(res_dict.keys())):
+        del res_dict[key]
+
+
+del_elem_list()    # 0.8854355812
+del_key_val_dict()    # 0.0156199932
