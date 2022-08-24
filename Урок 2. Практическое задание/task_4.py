0d238193fd84e7aa9f356cedf436ffc6a6b5136f
Ostap <mistrun@yandex.ru>
2022-08-24 18:59:54 +0300

    Shubin_lesson_2
    не сделал 5, 6, 7 задание

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -12,10 +12,35 @@
 """
 
 
-def numbers_el(n, sum_el):
-    result = sum_el / int(n)
-    print(result)
+def numbers_el(n):
+    n = int(n)
+
+    if n % 2 == 0:
+        first_el_row = -1
+        final_row = ((first_el_row / 2) ** (n-1))  # число n ряда
+        print(f'финальное число ряда {final_row} от четного числа')
+        count_sum = (first_el_row * ((-0.5) ** n - 1)) / (-0.5 - first_el_row)  # формула суммы прогрессии где 0.5 = 1/2
+        print(count_sum)
+
+    elif n % 2 != 0:
+        first_el_row = 1
+        final_row = ((first_el_row / 2) ** (n - 1))  # число n ряда
+        print(f'финальное число ряда {final_row} от не четного числа')
+        count_sum = (first_el_row * ((-0.5) ** n - 1)) / (-0.5 - first_el_row)  # формула суммы прогрессии где 0.5 = 1/2
+        print(count_sum)
 
 
-numbers_el(input("Введите количество элементов: "), 0.75)
+numbers_el(input("Введите количество элементов: "))
 
+
+def func_progression(n, first_el_row=1):
+    n = int(n)
+
+    if n > 52:
+        print(f'{0.6} в периоде')  # n = 52 будет являться числом в периоде
+    else:
+        count_sum = (first_el_row * ((-0.5) ** n - 1)) / (-0.5 - first_el_row)  # формула суммы прогрессии где 0.5 = 1/2
+        print(count_sum)
+
+
+func_progression(input('Введите количества элементов: '))
--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,71 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+import random
+
+
+def game(nubmer=random.randint(1, 100)):
+    print(nubmer)
+    count = 0
+    max_count = int(input("введите количество попыток :"))
+    if max_count > 0:
+        count += 1
+        max_count -= 1
+        user_number = int(input(f"попытка № {count}, осталось {max_count} введите число: "))
+
+        if user_number == nubmer:
+            print("Победа")
+
+        else:
+            print("вы проиграли ")
+            return game()
+
+
+game()
+
+# import random
+#
+# number = None
+# userNumber = None
+# count = 0
+# level = int(input("введите количество попыток :"))
+# maxCount = level
+#
+#
+# print(f"Всего попыток {maxCount}")
+#
+# number = random.randint(1, 100)
+#
+# while True:
+#     count += 1
+#     maxCount -= 1
+#     print(number)
+#     userNumber = int(input(f"попытка № {count}, осталось {maxCount} введите число: "))
+#
+#     if number == userNumber:
+#         print("Победа")
+#         user = int(input("Еще разок ? если да то 1 если нет то 2: "))
+#         if user == 1:
+#             count = 0
+#             maxCount = level
+#             number = random.randint(1, 100)
+#             continue
+#         else:
+#             break
+#
+#     if maxCount < 1:
+#         print("вы проиграли ")
+#         user = int(input("Еще разок ? если да то 1 если нет то 2: "))
+#         if user == 1:
+#             count = 0
+#             maxCount = level
+#             number = random.randint(1, 100)
+#             continue
+#         else:
+#             break
+#
+#     elif number < userNumber:
+#         print("введите число меньше")
+#
+#     elif number > userNumber:
+#         print("введите число больше")
