3645592f08033ff984e3afb4f0722a4fcd146433
fxshad <fxshad@mail.ru>
2022-08-24 17:29:21 +0700

    lesson_3_hashes

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,25 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+import hashlib
+
+memory = {}
+
+
+def cash_web(url, mem):
+    if url in mem:
+        res = mem.get(url)
+        return res
+    else:
+        res = hashlib.sha512('salt'.encode('utf-8') + url.encode('utf-8')).hexdigest()
+        mem[url] = res
+
+
+print(cash_web('https://gb.ru/lessons/259239', memory))
+print(cash_web('https://mail.ru/', memory))
+print(cash_web('https://yandex.ru/', memory))
+print(cash_web('https://gb.ru/lessons/259239', memory))
+print(cash_web('https://gb.ru/lessons/259239', memory))
+print(cash_web('https://yandex.ru/', memory))
+print(memory)
+
