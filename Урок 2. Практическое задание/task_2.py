86fa5abf1cf3969904664bd1c34730689394b49c
Sergey <so.bunin@gmail.com>
2022-08-25 00:05:32 +0300

    Оптимизирован алгоритм

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -18,22 +18,23 @@
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
 
+def ch_even(n, oddnum=0, evennum=0):
+    if (n % 2) == 1:
+        oddnum += 1
+    else:
+        evennum += 1
+    return oddnum, evennum
+
+
 def num_dig(n, oddnum=0, evennum=0):
     if len(str(n)) == 1:
-        if (n % 2) == 1:
-            oddnum += 1
-        else:
-            evennum += 1
-        return (oddnum, evennum)
+        oddnum, evennum = ch_even(n, oddnum, evennum)
+        return oddnum, evennum
     else:
-        if (n % 10 % 2) == 1:
-            oddnum += 1
-        else:
-            evennum += 1
+        oddnum, evennum = ch_even(n, oddnum, evennum)
         n //= 10
         return num_dig(n, oddnum, evennum)
 
 
 m = int(input('Введите число: '))
 print(num_dig(m))
-
