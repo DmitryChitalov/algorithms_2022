54fd0b3a21cf3bf04bd7e190a976bc4279e100d5
KiselevVitaly <94475116+KiselevVitaly@users.noreply.github.com>
2022-08-25 20:47:46 +0300

    Update task_7.py

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -13,3 +13,42 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --код с нуля писать не нужно, требуется доработать пример с урока
 """
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+ 
+def check(data):
+    string = data.replace(" ", "")
+    dc = DequeClass()
+    for char in string:
+        dc.add_to_rear(char)
+        
+    switch = True
+    
+    while dc.size() > 1 and switch:
+        begin = dc.remove_from_front()
+        end = dc.remove_from_rear()
+        if begin != end:
+            switch = False
+            
+    return switch
+  
