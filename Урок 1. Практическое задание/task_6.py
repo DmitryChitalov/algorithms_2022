bff9f1e2fce1704e0e7ef0519c14bb406677a444
Vladimir Grebenev <grebenev-81@mail.ru>
2022-08-25 23:22:35 +0300

    Урок 1. Задание 1, 2, 3, 4, 5, 6, 7 готово.

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -6,7 +6,7 @@
 
 Реализуйте класс-структуру "доска задач".
 
-Структура должна предусматривать наличие несольких очередей задач, например
+Структура должна предусматривать наличие нескольких очередей задач, например
 1) базовой, откуда задачи берутся, решаются и отправляются в список решенных
 2) очередь на доработку, когда нерешенные задачи из первой очереди отправляются
 на корректировку решения
@@ -17,3 +17,56 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+class TaskBoardClass:
+    def __init__(self):
+        self.todo = []
+        self.done = []
+        self.rework = []
+        print('создана новая доска')
+
+    def is_empty(self):
+        return self.todo == []
+
+    def to_queue(self, item):
+        self.todo.insert(0, item)
+        return f'Список задач обновлён:: {self.todo}'
+
+    def from_queue(self, queue):
+        if queue == 'todo':
+            self.done.insert(0, self.todo.pop())
+        if queue == 'rework':
+            self.done.insert(0, self.rework.pop())
+        return f'Список выполненных задач обновлён: {self.done}'
+
+    def to_rework(self):
+        self.rework.insert(0, self.todo.pop())
+        return f'Список задач на доработку обновлён: {self.rework}'
+
+    def size(self):
+        return f'Задач в работе: {len(self.todo)+len(self.rework)}'
+
+
+if __name__ == '__main__':
+    TBC_homeworks = TaskBoardClass()
+    print(TBC_homeworks.to_queue('выбросить мусор'))
+    print(TBC_homeworks.to_queue('полить деревья'))
+    print(TBC_homeworks.to_queue('поиграть с детьми'))
+    print(TBC_homeworks.to_queue('наладить самокат'))
+    print(TBC_homeworks.is_empty())
+    print(TBC_homeworks.size())
+    print(TBC_homeworks.from_queue('todo'))
+    print(TBC_homeworks.from_queue('todo'))
+    print(TBC_homeworks.from_queue('todo'))
+    print(TBC_homeworks.to_rework())
+    print(TBC_homeworks.size())
+    print(TBC_homeworks.from_queue('rework'))
+    print(TBC_homeworks.size())
+
+
+
+
+
+
+
+
