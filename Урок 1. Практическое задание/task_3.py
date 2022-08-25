b441229cc96709f0c3af89a86df56fa6ce183074
[tash] <info@geekbrains.ru>
2022-08-25 21:07:14 +0300

    all tasks

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,22 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+profit = {'audi': 123456789, 'kia': 12345, 'volvo': 123456, 'VAZ': 1234, 'bmw': 1234567}
+
+# # решение 1:
+# def func(arr): # O(nlogn)
+#     res = sorted(profit, key=profit.get)[-3:] # O(n) = O(nlogn) + O(n) = O(nlogn)
+#     return res #O(1)
+# func(profit)
+
+# решение 2:
+def func_2(arr): #O(1) + 3 x O(n) + O(1) + O(n) + 0(1) = 3O(n) = O(n)
+    lst = [] #O(1)
+    for i in range(3): #3
+        maxm = max(arr, key=profit.get) #O(n)
+        lst.append(maxm) #O(1)
+        profit.pop(max(profit, key=profit.get)) #O(n) + O(1) = O(n)
+    return lst #O(1)
+# func_2(profit)
+
+'''Второе решение эффективнее, т. к. имеет сложность О(n),  а первое - O(nlogn)'''
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,37 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+d = {'Tash': ['tash', 'activated'], 'Ivan': ['Ivan', 'not activated'],
+     'Petr': ['petr', 'activated'], 'Masha': ['masha','not activated']}
+
+def check(key, password):#O(n)
+    if key in d.keys() and password == d[key][0]:#O(n) + O(1) = O(n)
+        if d[key][1] == 'not activated':#O(1)
+            print(f'{key}, your login and password are okay'#O(1)
+                    f' but you need to finish registration!')
+        else:
+            print('Your login and password are okay!'#O(1)
+                      ' You can keep using our service!')
+    else:
+        print('Your login or password are incorrect! Please try again!')#O(1)
+# check('Tash', 'tash')
+
+def check_2(key, password):#O(1)
+    if d.get(key) and password == d[key][0]:#O(1)
+        if d[key][1] == 'not activated':#O(1)
+            print(f'{key},your login and password are okay but'#O(1)
+                  f' you need to finish registration!')
+        else:
+            print(f'{key},your login and password are okay!'#O(1)
+                  ' You can keep using our service!')
+    else:
+        print('Your login or password are incorrect! Please try again!')#O(1)
+
+# check_2('Ivan', 'Ivan')
+
+
+
+"""Первое решение эффективнее, его сложность О(n), второе решение имеет сложность O(1)"""
+
+
+
