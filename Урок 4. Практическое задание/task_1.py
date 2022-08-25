e765e5862188cfd63b2833053e6c017253755760
DolgopolovRA <ra.dolgopolov@yandex.ru>
2022-08-25 18:43:38 +0300

    dz_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +21,24 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+lst = list(range(100000))
+func_1(lst)
+func_2(lst)
+print(f'Время работы функции {func_1.__name__} - {timeit("func_1(lst)", globals=globals(), number=1000)}')
+print(f'Время работы функции {func_2.__name__} - {timeit("func_2(lst)", globals=globals(), number=1000)}')
+
+"""
+
+В функции func_2 формирование списка сделал с помощью list comprehension, который быстрее for-циклов, 
+что и демонстрирует замер, давая разницу  ~ 5 сек.
+Время работы функции func_1 - 21.74166379999997
+Время работы функции func_2 - 17.096172800000204
+
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,6 +9,9 @@
 Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
+from collections import Counter
+from timeit import timeit
+
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -37,5 +40,43 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    elem, nums = Counter(array).most_common(1)[0]
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {nums} раз(а)'
+
+
+def func_4():
+    el = sorted(array, key=lambda x: array.count(x))[-1]
+    return f'Чаще всего встречается число {el}, ' \
+           f'оно появилось в массиве {array.count(el)} раз(а)'
+
+
+def func_5():
+    nums, el = max({array.count(key): key for key in set(array)}.items())
+    return f'Чаще всего встречается число {el}, ' \
+           f'оно появилось в массиве {nums} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+print(func_5())
+
+print(f'Время работы функции {func_1.__name__} - {timeit("func_1()", globals=globals())}')
+print(f'Время работы функции {func_2.__name__} - {timeit("func_2()", globals=globals())}')
+print(f'Время работы функции {func_3.__name__} - {timeit("func_3()", globals=globals())}')
+print(f'Время работы функции {func_4.__name__} - {timeit("func_4()", globals=globals())}')
+print(f'Время работы функции {func_5.__name__} - {timeit("func_5()", globals=globals())}')
+
+
+"""
+Время работы функции func_1 - 3.3639583999993192
+Время работы функции func_2 - 4.346648899998399
+Время работы функции func_3 - 7.4237278000000515
+Время работы функции func_4 - 5.131565100000444
+Время работы функции func_5 - 3.851553199998307
+
+Сделал замеры, ускорить задачу не получилось
+"""
