cb5c7d3afae03e90ececbc840ecb163d03be9d87
ssergo99 <ssergo99.git@gmail.com>
2022-08-25 12:57:25 +0300

    Домашнее задание по 4 уроку Щипак СГ

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -12,6 +12,10 @@
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
 
+from timeit import timeit
+
+enter_num_start = 23456
+
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
@@ -35,3 +39,29 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def get_reverse_dig(num, rev_str=' '):
+    if num // 10 == 0:
+        return str(rev_str) + str(num % 10)
+    else:
+        return get_reverse_dig(num // 10, (str(rev_str) + str(num % 10)))
+
+
+print(timeit("revers(enter_num_start)", setup="from __main__ import revers, enter_num_start",
+             number=1000))
+print(timeit("revers_2(enter_num_start)", setup="from __main__ import revers_2, enter_num_start",
+             number=1000))
+print(timeit("revers_3(enter_num_start)", setup="from __main__ import revers_3, enter_num_start",
+             number=1000))
+print(timeit("get_reverse_dig(enter_num_start)", setup="from __main__ import get_reverse_dig, enter_num_start",
+             number=1000))
+
+"""
+
+
+Вывод: самый оптимальный вариант - использование среза последовательности [::-1], т.к. этот вариант 
+фактически разворачивает строку за 1 раз, не используя циклы или рекурсии
+
+
+"""
