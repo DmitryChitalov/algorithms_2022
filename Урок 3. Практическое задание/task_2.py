08ffaff67f84f85e1d660103e02634aa91542781
mistyback0 <mback.edu@gmail.com>
2022-08-26 12:19:26 +0300

    Completed homework for lesson3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,37 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+
+def l3_v2():
+    import hashlib
+    import json
+
+    data = {}
+    name_input = input('Введите имя пользователя')
+    hash_input = hashlib.sha256(input('Установите пароль').encode()).hexdigest()
+    print(hash_input)
+    data[name_input] = hash_input
+
+    with open('passwds.json', 'w', encoding='utf-8') as nf:
+        json_str = json.dumps(data, ensure_ascii=False, indent=2)
+        nf.write(json_str)
+
+    name_compare = input('Имя пользователя')
+    hash_compare = hashlib.sha256(input('Пароль').encode()).hexdigest()
+
+    with open('passwds.json', 'r', encoding='utf-8') as nf:
+        json_str = nf.read()
+        data_set = json.loads(json_str)
+
+    try:
+        print(data_set[name_compare] == hash_compare)
+        if data_set[name_compare] == hash_compare:
+            print('Пароль верный')
+        else:
+            print('Неверный пароль')
+    except KeyError:
+        print('Неверное имя пользователя\nПовторный запуск программы\n')
+        return l3_v2()
+
+
+l3_v2()
