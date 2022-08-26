4b445a411b7ff72939d81cfde5aaa39e46a03506
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2022-08-26 12:33:58 +0300

    Четвертое задание по курсу Алгоритмы

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,8 +12,18 @@
 П.С. задание не такое простое, как кажется
 """
 
+"""
+Ответ: На правах версии, тк до конца не уверен в правильности своих выводов, буду с нетерпением ждать разбора
+домашнего задания.
+Чтобы понять что происходит я запускал функции с разными условиями пока не дошел до cProfile.
+Запустив run отдельно функции обнаружил в колонке ncalls, что функция без мемоизации запускается 14 раз,
+а с декоратором 1 раз. Возможно, что во втором случае (с мемоизацией) считается время исполнения только одного прохода
+функции, а не всех 14 раз, необходимых для получения результата. Либо оттого, что считается время исполнения самого 
+декоратора, а не всей задекорированной функции. Соответственно, и результаты замеров получились быстрее.
+"""
 from timeit import timeit
 from random import randint
+from cProfile import run
 
 
 def recursive_reverse(number):
@@ -54,13 +64,14 @@ def memoize(f):
         else:
             cache[args] = f(*args)
             return cache[args]
+
     return decorate
 
 
 @memoize
 def recursive_reverse_mem(number):
     if number == 0:
-        return ''
+        return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
@@ -80,3 +91,6 @@ print(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+run('recursive_reverse(num_10000)')
+run('recursive_reverse_mem(num_10000)')
