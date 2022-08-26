a7b2263762b0870ccdd5e465952ddefea2d112c2
Oleg <olazar61@gmail.com>
2022-08-27 01:50:52 +0300

    Homework_1

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,50 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+def checking_account_1(login, password, acc):  # O(n)
+    if login not in acc.keys():  # O(n)
+        return f'Пользователь не найден. Пройдите авторизацию.'  # O(1)
+    elif password == str(acc[login][0]) and acc[login][1]:  # O(1)
+        return f'Добро пожаловать, {login}!'  # O(1)
+    elif password == str(acc[login][0]) and not acc[login][1]:  # O(1)
+        return f'Активируйте Вашу учётную запись, {login}!'  # O(1)
+    elif password != str(acc[login][0]) and not acc[login][1]:  # O(1)
+        return f'Введён неверный пароль, {login}!'  # O(1)
+    else:
+        return f'Логин {login} занят. Зарегистрируйтесь и пройдите авторизацию под другим логином!'  # O(1)
+
+
+def checking_account_2(login, password, acc):  # O(1)
+    if not acc.get(login):  # O(1)
+        return f'Пользователь не найден. Пройдите авторизацию.'  # O(1)
+    elif password == str(acc[login][0]) and acc[login][1]:  # O(1)
+        return f'Добро пожаловать, {login}!'  # O(1)
+    elif password == str(acc[login][0]) and not acc[login][1]:  # O(1)
+        return f'Активируйте Вашу учётную запись, {login}!'  # O(1)
+    elif password != str(acc[login][0]) and not acc[login][1]:  # O(1)
+        return f'Введён неверный пароль, {login}!'  # O(1)
+    else:
+        return f'Логин {login} занят. Зарегистрируйтесь и пройдите авторизацию под другим логином!'  # O(1)
+
+
+accounts = {'oleg': [111111, True], 'vlad': [333333, False]}
+
+print(checking_account_1('oleg', '111111', accounts))
+print(checking_account_1('oleg', '222222', accounts))
+print(checking_account_1('vlad', '111111', accounts))
+print(checking_account_1('vlad', '333333', accounts))
+print(checking_account_1('serg', '333333', accounts))
+print("_____________________________________________")
+print(checking_account_2('oleg', '111111', accounts))
+print(checking_account_2('oleg', '222222', accounts))
+print(checking_account_2('vlad', '111111', accounts))
+print(checking_account_2('vlad', '333333', accounts))
+print(checking_account_2('serg', '333333', accounts))
+
+"""
+Функция checking_account_2 работает эффективнее функции checking_account_1,
+за счёт работы встроенной функции get (O(1)), в отличие от сложности
+перебора (O(n)).
+"""
