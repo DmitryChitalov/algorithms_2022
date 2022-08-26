2abcdef07b9897ddda4685160b2d8a1f9460e069
kenterberiiskii <a.mizyakov@gmail.com>
2022-08-26 10:05:56 +0300

    Update task_1.py
    Использовал enumerate.Получил кортеж, который в силу своей неизменяемости эффективнее списка при работе с памятью

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,7 +11,7 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
-
+import timeit
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +19,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+# def func_2(nums):
+#     return f'{[i for i in range(len(nums)) if nums[i] % 2 == 0]}'
+
+def func_3(nums):
+    my_list = []
+    for k,v in enumerate(nums):
+        if v % 2 == 0:
+            my_list.append(k)
+    return my_list
+
+
+my_arr = [1, 2, 3, 4, 5, 6, 7, 8]
+print(func_1(my_arr))
+print(func_3(my_arr))
+print(timeit.timeit('func_1(my_arr)', number=10000, globals=globals()))
+print(timeit.timeit('func_3(my_arr)', number=10000, globals=globals()))
+
+# Использовал enumerate.Получил кортеж, который в силу своей неизменяемости эффективнее списка при работе с памятью
