936e953c86f0e26031d02a733cfb6310c404c409
Chernyaeva <moiri@mail.ru>
2022-08-27 14:14:21 +0300

    lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,7 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import timeit
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +20,59 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+def func_2(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if not nums[i] % 2:
+            new_arr.append(i)
+    return new_arr
+
+def func_3(nums):
+    new_arr = [i for i in range(len(nums)) if not nums[i] % 2]
+    return new_arr
+
+
+def func_4(nums):
+    new_arr = []
+    l = len(nums) - 1
+    while l:
+        if not nums[l] % 2:
+            new_arr.append(l)
+        l = l - 1
+    return new_arr
+
+my_list = [3,2,5,6,4,8,9,11,6,9]
+my_big_list = list(range(1, 100))
+
+# print(func_1(my_list))
+print('оригинальная функция')
+print(timeit('func_1(my_list)', globals=globals()))
+print(timeit('func_1(my_big_list)', globals=globals()))
+
+# print(func_2(my_list))
+print('Замена == 0 на not')
+print(timeit('func_2(my_list)', globals=globals()))
+print(timeit('func_2(my_big_list)', globals=globals()))
+
+# print(func_3(my_list))
+print('list comprehension')
+print(timeit('func_3(my_list)', globals=globals()))
+print(timeit('func_3(my_big_list)', globals=globals()))
+
+# print(func_4(my_list))
+print('while')
+print(timeit('func_4(my_list)', globals=globals()))
+print(timeit('func_4(my_big_list)', globals=globals()))
+
+
+"""
+Аналитика. 
+1. Замена == 0 на not ускоряет работу функции. Проверка числа на 0 работает быстрее, чем сравнение целых чисел.
+   Независимо от объёма данных.
+2. List comprehensions работает быстрее, чем цикл for, на большом объёме данных, потому что это встроенная функция.
+   Если объём даннных небольшой - оригинальная функция работает быстрее.
+3. Цикл while работает быстрее на небольшом объёме данных, потому что не использует итерацию.
+   На большом объёме данных оригинальная функция работает быстрее.
+   
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,9 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 
+from timeit import timeit
+
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +40,58 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    elem = max(set(array), key=array.count)
+    return f'Чаще всего встречается число {elem}, '\
+           f'оно появилось в массиве {array.count(elem)} раз(а)'
+
+
+def func_4():
+    mydict = {}
+    maxnum = 0
+    for i in array:
+        num = mydict.get(i)
+        if num:
+            num = num + 1
+            mydict[i] = num
+            if num > maxnum:
+                maxnum = num
+                maxval = i
+        else:
+            mydict.setdefault(i, 1)
+    return f'Чаще всего встречается число {maxval}, '\
+           f'оно появилось в массиве {maxnum} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+print('Small arrray:')
+print(timeit('func_1()', globals=globals()))
+print(timeit('func_2()', globals=globals()))
+print(timeit('func_3()', globals=globals()))
+print(timeit('func_4()', globals=globals()))
+
+array = [6, 1, 3, 1, 3, 4, 5, 1, 3, 1, 3, 4, 5, 1, 3, 1, 3, 4, 5, 1, 3, 1, 3, 4, 5, 1, 3, 1, 3, 4, 5, 1]
+
+print('Big arrray:')
+print(timeit('func_1()', globals=globals()))
+print(timeit('func_2()', globals=globals()))
+print(timeit('func_3()', globals=globals()))
+print(timeit('func_4()', globals=globals()))
+
+'''
+Получилось ускорить выполнение задачи двумя способами. 
+Функция 3 работает быстрее, чем первые 2, и на большом и на небольшом объёме данных.
+Функция 4 работает быстрее на большом объёме данных.
+Аналитика:
+Функции 1 и 2 имеют сложность О(n^2), так как при переборе элементов массива вызывается array.count,
+          что создаёт вложенный цикл.
+Функция 3 также имеет сложность О(n^2), но превращает список в сэт, тем самым убирая повторы, 
+          и затем по каждому элементу сэта вызывает array.count().
+          Такми образом array.count вызывается только один раз для каждого уникального элемента списка.
+Функция 4 перебирвает список только один раз, составля словарь с количеством повторов элементов списка.
+ 
+'''
