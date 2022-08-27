a5da19bb117495cc1e50874f2d3d8ae60de09a30
GarrikDanilov <garrik_danilov@mail.ru>
2022-08-27 12:14:53 +0300

    task_1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,7 +11,7 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
-from timeit import Timer
+from timeit import timeit
 
 
 def func_1(nums):
@@ -28,14 +28,11 @@ def func_2(nums):
 
 if __name__ == '__main__':
     num = [i for i in range(100)]
-    t1 = Timer(stmt='func_1(num)', setup='from __main__ import func_1, num')
-    print('Время func_1', t1.timeit(number=100))
-
-    t2 = Timer(stmt='func_2(num)', setup='from __main__ import func_2, num')
-    print('Время func_2', t2.timeit(number=100))
+    print('Время func_1', timeit('func_1(num)', globals=globals(), number=100))
+    print('Время func_2', timeit('func_2(num)', globals=globals(), number=100))
 
     """
-        Время func_1 0.003659700000000002
-        Время func_2 0.002307500000000004 - В функции вместо цикла for применен List Comprehension: создание списка и 
+        Время func_1 0.0032746000000000025
+        Время func_2 0.0016747000000000012 - В функции вместо цикла for применен List Comprehension: создание списка и 
         его заполнение происходит одновременно. 
     """
