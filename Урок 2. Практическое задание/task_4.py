6dd225f0691a67291d39dcf6f076f7b2978b6c13
[tash] <info@geekbrains.ru>
2022-08-27 10:18:10 +0300

    1-3, 6

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -10,3 +10,24 @@
 Решите через рекурсию. В задании нельзя применять циклы.
 Нужно обойтисть без создания массива!
 """
+num = int(input("Введите  целое число"))
+count = 0
+num_1 = 1
+res = 1
+num_2 = 0
+def rec():
+    global count
+    count += 1
+    if count < num:
+        global num_1
+        global res
+        global num_2
+        num_2 = num_1 * (-0.5)
+        res = res + num_2
+        num_1 = num_2
+        num_2 = num_2 * (-0.5)
+        return rec()
+    else:
+        print(res)
+
+rec()
