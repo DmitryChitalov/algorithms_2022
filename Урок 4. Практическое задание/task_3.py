7b70c46d7e1df266e065945bcded1464c9dc581e
fxshad <fxshad@mail.ru>
2022-08-28 18:54:47 +0700

    lesson_4_timeit

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,7 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
@@ -35,3 +36,42 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    revers_num = "".join(reversed(str(enter_num)))
+    return revers_num
+
+
+n = 780054300
+
+
+r1 = """
+revers(n)
+"""
+
+r2 = """
+revers_2(n)
+"""
+
+r3 = """
+revers_3(n)
+"""
+
+
+r4 = """
+revers_4(n)
+"""
+
+
+
+print(timeit(r1, globals=globals(), number=10000))
+print(timeit(r2, globals=globals(), number=10000))
+print(timeit(r3, globals=globals(), number=10000))
+print(timeit(r4, globals=globals(), number=10000))
+
+# Среднее время выполнения revers: 0.016 сек -- наименее эфективная, очевидно из-за рекурсии
+# Среднее время выполнения revers_2: 0.010 сек  -- средняя по эфективности, работает по принципу revers, но без рекурсии.Цикл while оказался эфективней
+# Среднее время выполнения revers_3: 0.0022 сек -- эфективная, перезапись строки в обратном порядке оказалась эфективней чем while
+# Среднее время выполнения revers_4: 0.0047 сек -- средняя по эфективности, функция reversed потребляет больше всего ресурсов
+
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -9,6 +9,8 @@
 Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
+from timeit import timeit
+
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -37,5 +39,40 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    new_array = [array.count(n) for n in array]
+    return f'Чаще всего встречается число {array[new_array.index( max(new_array))]}, ' \
+           f'оно появилось в массиве {max(new_array)} раз(а)'
+
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+
+
+f1 = """
+func_1()
+"""
+
+f2 = """
+func_2()
+"""
+
+
+f3 = """
+func_3()
+"""
+
+print(timeit(f1, globals=globals(), number=100000))
+print(timeit(f2, globals=globals(), number=100000))
+print(timeit(f3, globals=globals(), number=100000))
+
+# Результаты замеров:
+# func_1: 0.09930220013484359
+# func_2: 0.13643720000982285
+# func_3: 0.13771019992418587
+# Неудалось ускорить алгоритм
+
+
+
