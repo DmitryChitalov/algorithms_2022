138646fd2a51880c3b8501de45a01b2023b0bce2
sklleon <sklleon@mail.ru>
2022-08-29 18:39:18 +0300

    homework lesson_1 at 29-08-22 Sklayrevskii Leonid

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,38 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+profit_company = {'Nike': 5000, 'Adidas': 6000, 'Puma': 2000, 'Reebok': 1800,'HH': 2200,'HM': 2400,'Zara': 5000, 'SL': 3000}
+print(len(profit_company))
+
+# Первый способ: Cложность O(n log n)
+
+
+def by_value(item):
+    return item[1]                                                          # O(1)
+
+
+max_profit = {}                                                             # O(1)
+i = 0                                                                       # O(1)
+for k, v in sorted(profit_company.items(), key=by_value, reverse=True):     # O(n + n log n)
+    if i < 3:                                                               # O(len(i)
+        max_profit.setdefault(k, v)                                         # O(1)
+    i = i + 1                                                               # O(1)
+print(max_profit)                                                           # O(1)
+
+
+# Второй способ: Сложность O (n**2)
+
+global max_value                                                            # O(1)
+global key_max_value                                                        # O(1)
+
+max_profit_2 = {}                                                           # O(1)
+while len(max_profit_2) < 3:                                                # O(n)
+    max_value = 0                                                           # O(1)
+    for key, value in profit_company.items():                               # O(n)
+        if max_value < value:                                               # O(len(max_value))
+            max_value = value                                               # O(1)
+            key_max_value = key                                             # O(1)
+    max_value = profit_company.pop(key_max_value)                           # O(1)
+    max_profit_2.setdefault(key_max_value, max_value)                       # O(1)
+
+print(max_profit_2)
