a03080e54ba740153935efa7e550825126212842
mistyback0 <mback.edu@gmail.com>
2022-08-30 12:46:16 +0300

    Completed homework for lesson4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -12,6 +12,8 @@
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
 
+from timeit import timeit
+
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
@@ -35,3 +37,34 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+print(revers(35060))
+print(timeit('revers(350)', globals=globals(), number=1000000), '\n')
+
+print(revers_2(35060))
+print(timeit('revers_2(350)', globals=globals(), number=1000000), '\n')
+
+print(revers_3(35060))
+print(timeit('revers_3(350)', globals=globals(), number=1000000), '\n')
+
+#Собственный вариант решения:
+
+
+def revers_4(enter_num):
+    enter_num = str(enter_num)
+    emp_list = []
+    for x in enter_num:
+        emp_list.insert(0, x)
+    res_str = ''.join(emp_list)
+    return res_str
+
+
+print(revers_4(35060))
+print(timeit('revers_4(350)', globals=globals(), number=1000000))
+
+
+"""
+Не смотря на то, что предложенный вариант решения revers_4 при замерах модулем timeit оказался быстрее функций revers_1 и revers_2,
+самой эффективной реализацией является revers_3, т.к использует встроенные функции языка, которые всегда работают быстрее, что подтвержают замеры времени выполнения.
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,8 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 
+from timeit import timeit
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +39,20 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+print(func_1(), timeit('func_1', globals=globals(), number=1000000))
+print(func_2(), timeit('func_2', globals=globals(), number=1000000))
+
+# Собственный вариант решения
+
+
+def func_3():
+    val = max(array, key=lambda x: array.count(x))
+    return f'Чаще всего встречается число {val}, ' \
+           f'оно появилось в массиве {array.count(val)} раз(а)'
+
+
+print(func_3(), timeit('func_3', globals=globals(), number=1000000))
+
+'''
+Все представленные решения показывают примерно одинаковые показатели по скорости выполнения алгоритма
+'''
