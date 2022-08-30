785981ef8cc22ceada177e3717bdabfefa79a790
Sergey <so.bunin@gmail.com>
2022-08-31 01:30:03 +0300

    DZ_4_4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -1,15 +1,12 @@
 """
 Задание 4.
-
 Приведены два алгоритма. В них определяется число,
 которое встречается в массиве чаще всего.
-
 Сделайте профилировку каждого алгоритма через timeit
-
 Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
-Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
+Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
-
+from timeit import timeit
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -30,12 +27,22 @@ def func_2():
     for el in array:
         count2 = array.count(el)
         new_array.append(count2)
-
     max_2 = max(new_array)
     elem = array[new_array.index(max_2)]
     return f'Чаще всего встречается число {elem}, ' \
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    num_3 = [array.count(i) for i in array]
+    return f'Чаще всего встречается число {array[num_3.index(max(num_3))]}, ' \
+           f'оно появилось в массиве {max(num_3)} раз(а)'
+
+
 print(func_1())
+print(timeit("func_1()", number=100000, globals=globals()))
 print(func_2())
+print(timeit("func_2()", number=100000, globals=globals()))
+print(func_3())
+print(timeit("func_3()", number=100000, globals=globals()))
+"""ускорить 2-ой алгоритм получилось незначительно"""
