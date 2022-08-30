e1239d7ca3f4f3bb487dddffcb5ea350cc8d158a
Sergey <so.bunin@gmail.com>
2022-08-31 01:29:01 +0300

    DZ_4_3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,21 +1,17 @@
 """
 Задание 3.
-
 Приведен код, формирующий из введенного числа
 обратное по порядку входящих в него
 цифр и вывести на экран.
-
-Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
+Сделайте профилировку каждого алгоритма через timeit
 Обязательно предложите еще свой вариант решения!
-
-Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
+Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
-
+from timeit import timeit
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
@@ -35,3 +31,20 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(n, b=0):
+    if n != 0:
+        b = b * 10 + n % 10
+        n //= 10
+        return revers_4(n, b)
+    return b
+
+
+print(timeit("revers(4623524)", number=1000000, globals=globals()))
+print(timeit("revers_2(4623524)", number=1000000, globals=globals()))
+print(timeit("revers_3(4623524)", number=1000000, globals=globals()))
+print(timeit("revers_4(4623524)", number=1000000, globals=globals()))
+"""Первый и третий вариант наименее эффективные, т.к. используют "тяжелую" рекурсию,
+второй вариант - средней эффективности, т.к. содержит один цикл с простыми арифметическими действиями,
+третий вариант - наиболее эффективный, т.к. содержит всего два действия  с О(n)"""
