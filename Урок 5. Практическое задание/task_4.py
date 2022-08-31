0a76bb4fd52cdea3b09d78ad7c6aaa1a70f3fa5a
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-09-01 00:37:04 +0500

    Lesson_05 completed

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,36 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+
+obj_dict = {x: x+1 for x in range(10000)}
+ordered_dict = OrderedDict({x: x+1 for x in range(10000)})
+
+
+def popitem_dict():
+    obj_dict.popitem()
+
+
+def popitem_od():
+    ordered_dict.popitem()
+
+
+def pop_dict(n):
+    obj_dict.pop(n)
+
+
+def pop_od(n):
+    ordered_dict.pop(n)
+
+
+print(timeit("popitem_dict()", globals=globals(), number=1000))
+print(timeit("popitem_od()", globals=globals(), number=1000))
+print(timeit("for i in range(1000):" "pop_dict(i)", globals=globals(), number=1))
+print(timeit("for i in range(1000):" "pop_od(i)", globals=globals(), number=1))
+
+"""
+OrderedDict чуть медленнее чем обычный словарь, после версии Python 3.6 - OD становится не актуален
+"""
