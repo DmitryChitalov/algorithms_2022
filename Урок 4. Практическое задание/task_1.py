62ca3a1f81049c16d38e23d839adc075d8fcca35
ton1k090 <ton1k090@mail.ru>
2022-08-31 20:28:21 +0300

    Lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,6 +11,12 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
+from timeit import timeit
+
+nums = [x for x in range(1, 100)]
+
+
+'''Итератор с функцией append'''
 
 
 def func_1(nums):
@@ -19,3 +25,18 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+print(timeit('func_1(nums)', globals=globals(), number=1000))
+
+print()
+
+'''# Списковое включение, работает быстрее итератора с функцией append'''
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+print(timeit('func_2(nums)', globals= globals(), number=1000))
