2a12b4df9f158c9df92cef07fb91d30054171396
Artem Pospeev <artem.pospeev@icloud.com>
2022-09-01 12:18:07 +0300

    Completed hw_3(Pospeev_Artem)
    Алгоритмы и структуры данных. 23.08

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -22,3 +22,25 @@
 р
 а
 """
+
+from hashlib import sha1
+
+
+def uniq_substring(input_str):
+    result_set = set()
+    for i in range(len(input_str)):
+        for j in range(i, len(input_str) + 1):
+            sliced_string = input_str[i:j]
+            if sliced_string != '' and len(sliced_string) != len(input_str):
+                result_set.add(sha1(sliced_string.encode()).hexdigest())
+                result_set.add(sha1(sliced_string[::-1].encode()).hexdigest())
+    return len(result_set)
+
+
+if __name__ == '__main__':
+    while True:
+        input_data = input('Введите любую строку для подсчета уникальных подстрок:\n'
+                           '1 - для выхода\n')
+        if input_data == '1':
+            break
+        print(f'Количество уникальных подстрок: {uniq_substring(input_data)}')
