49c34a86bc6f8d8cde4ec8297ea92cd3ba069755
Roman <korchigin5@gmail.com>
2022-09-02 00:14:42 +0300

    Korchigin_Roman_lesson3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,41 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+# Функция uuid4() модуля uuid генерирует случайный UUID и возвращает объект UUID.
+# Для преобразования объекта UUID в строку используйте встроенную функцию str().
+
+from hashlib import sha256
+from uuid import uuid4
+
+salt = uuid4().hex
+cache_obj = {}
+
+
+def check_cache(url):
+    hash_url = sha256(salt.encode() + url.encode()).hexdigest()
+    if hash_url in cache_obj.keys():
+        return True
+    else:
+        return {hash_url: url}
+
+
+url_1 = input('Введите url: ')
+check_cache(url_1)
+
+try:
+    cache_obj.update(check_cache(url_1))
+except TypeError:
+    print("Данная страница уже есть в кэше")
+
+print(cache_obj)
+
+url_2 = input('Введите url: ')
+check_cache(url_2)
+
+try:
+    cache_obj.update(check_cache(url_2))
+except TypeError:
+    print("Данная страница уже есть в кэше")
+
+print(cache_obj)
