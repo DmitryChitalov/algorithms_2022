0f51a7335efd8a8b4d7e50807f571a22f200500c
YurKolya <100020131+YurKolya@users.noreply.github.com>
2022-09-02 15:01:06 +0300

    Solution

---
+++ Урок 2. Практическое задание/task_6.py
@@ -0,0 +1,19 @@
+import random
+
+def recur_method(count, numb):
+    """Рекурсия"""
+    print(f"Попытка №{count}")
+    answer = int(input("Введите число от 0 до 100: "))
+    if count == 10 or answer == numb:
+        if answer == numb:
+            print("Верно!")
+        print(f"Загаданное число: {numb}")
+    else:
+        if answer > numb:
+            print(f"Загаданное число меньше чем {numb}")
+        else:
+            print(f"Загаданное число больше чем {numb}")
+        recur_method(count + 1, numb)
+
+
+recur_method(1, random.randint(0, 100))
