32d5792c1c58a0e7440b131587236023de4031da
Artem Pospeev <artem.pospeev@icloud.com>
2022-09-03 19:48:05 +0300

    hw-4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -26,22 +26,22 @@ num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
 num_10000 = randint(100000000, 10000000000000)
 
-print('Не оптимизированная функция recursive_reverse')
-print(
-    timeit(
-        "recursive_reverse(num_100)",
-        setup='from __main__ import recursive_reverse, num_100',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_1000)",
-        setup='from __main__ import recursive_reverse, num_1000',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_10000)",
-        setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
+# print('Не оптимизированная функция recursive_reverse')
+# print(
+#     timeit(
+#         "recursive_reverse(num_100)",
+#         setup='from __main__ import recursive_reverse, num_100',
+#         number=10000))
+# print(
+#     timeit(
+#         "recursive_reverse(num_1000)",
+#         setup='from __main__ import recursive_reverse, num_1000',
+#         number=10000))
+# print(
+#     timeit(
+#         "recursive_reverse(num_10000)",
+#         setup='from __main__ import recursive_reverse, num_10000',
+#         number=10000))
 
 
 def memoize(f):
@@ -64,19 +64,73 @@ def recursive_reverse_mem(number):
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
+# print('Оптимизированная функция recursive_reverse_mem')
+# print(
+#     timeit(
+#         'recursive_reverse_mem(num_100)',
+#         setup='from __main__ import recursive_reverse_mem, num_100',
+#         number=10000))
+# print(
+#     timeit(
+#         'recursive_reverse_mem(num_1000)',
+#         setup='from __main__ import recursive_reverse_mem, num_1000',
+#         number=10000))
+# print(
+#     timeit(
+#         'recursive_reverse_mem(num_10000)',
+#         setup='from __main__ import recursive_reverse_mem, num_10000',
+#         number=10000))
+
+'''
+Во всех замерах выше мы берем и передаём 10000 одно и тоже случайное число в функцию, что приводит к искажению
+результатов замера. Для более точного и наглядного исследования надобности мемоизации для функции recursive_reverse
+предлагаю передавать не одно и тоже случайное число, а каждый раз разные случайные числа из тех же диапазонов.
+'''
+print('Не оптимизированная функция recursive_reverse')
+print(
+    timeit(
+        "recursive_reverse(randint(10000, 1000000))",
+        setup='from __main__ import recursive_reverse, randint',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(randint(1000000, 10000000))",
+        setup='from __main__ import recursive_reverse, randint',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(randint(100000000, 10000000000000))",
+        setup='from __main__ import recursive_reverse, randint',
+        number=10000))
+
 print('Оптимизированная функция recursive_reverse_mem')
 print(
     timeit(
-        'recursive_reverse_mem(num_100)',
-        setup='from __main__ import recursive_reverse_mem, num_100',
+        'recursive_reverse_mem(randint(10000, 1000000))',
+        setup='from __main__ import recursive_reverse_mem, randint',
         number=10000))
 print(
     timeit(
-        'recursive_reverse_mem(num_1000)',
-        setup='from __main__ import recursive_reverse_mem, num_1000',
+        'recursive_reverse_mem(randint(1000000, 10000000))',
+        setup='from __main__ import recursive_reverse_mem, randint',
         number=10000))
 print(
     timeit(
-        'recursive_reverse_mem(num_10000)',
-        setup='from __main__ import recursive_reverse_mem, num_10000',
+        'recursive_reverse_mem(randint(100000000, 10000000000000))',
+        setup='from __main__ import recursive_reverse_mem, randint',
         number=10000))
+
+
+'''
+Не оптимизированная функция recursive_reverse
+0.04229019989725202
+0.03618469997309148
+0.0589693000074476
+Оптимизированная функция recursive_reverse_mem
+0.05849959992337972
+0.04384319996461272
+0.1314450999489054
+
+Наглядно видно, что в некоторых случаях мемоизацию не то, что не даёт прибавки по времени,
+а наоборот, замедляет работу функции
+'''
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,32 +1,30 @@
 """
 Задание 3.
-
 Приведен код, формирующий из введенного числа
 обратное по порядку входящих в него
 цифр и вывести на экран.
-
 Сделайте профилировку каждого алгоритма через timeit
-
 Обязательно предложите еще свой вариант решения!
-
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
 
+from timeit import timeit
 
-def revers(enter_num, revers_num=0):
+N = 23 ** 100  # Число для "отзеркаливания"
+
+
+def revers(enter_num, revers_num=''):
     if enter_num == 0:
-        return
+        return revers_num
     else:
-        num = enter_num % 10
-        revers_num = (revers_num + num / 10) * 10
+        revers_num += str(enter_num % 10)
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers(enter_num, revers_num)
 
 
-def revers_2(enter_num, revers_num=0):
+def revers_2(enter_num, revers_num=''):
     while enter_num != 0:
-        num = enter_num % 10
-        revers_num = (revers_num + num / 10) * 10
+        revers_num += str(enter_num % 10)
         enter_num //= 10
     return revers_num
 
@@ -35,3 +33,27 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    reverse_num = ''
+    for el in str(enter_num):
+        reverse_num = el + reverse_num
+    return reverse_num
+
+
+print(f'Рекурсия : {timeit("revers(N)", globals=globals(),number=10000)}')
+print(f'Цикл : {timeit("revers_2(N)", globals=globals(),number=10000)}')
+print(f'Срез строки : {timeit("revers_3(N)", globals=globals(),number=10000)}')
+print(f'Посимвольный перебор строки : {timeit("revers_4(N)", globals=globals(),number=10000)}')
+
+'''
+Рекурсия : 0.6549074000213295
+Цикл : 0.43801270006224513
+Срез строки : 0.01126370020210743
+Посимвольный перебор строки : 0.10998630011454225
+
+Как мы видим из замеров: самым высоким показателем производительности обладает срез: в среднем в 10/40 раз быстрее,
+чем прочие способы. Преимущество скорости достигается за счет того, что иттерации в срезе происходят на более
+быстром языке (Си)
+'''
