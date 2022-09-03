06ad45379ae5030c5d16a886c02f77e8b156bf95
kenterberiiskii <a.mizyakov@gmail.com>
2022-09-03 19:19:38 +0300

    Update task_1_4.py

--- Урок 6. Практическое задание/task_1_4.py
+++ Урок 6. Практическое задание/task_1_4.py
@@ -30,3 +30,35 @@
 
 Это файл для четвертого скрипта
 """
+from memory_profiler import memory_usage
+
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        m1 = memory_usage()
+        print(m1)
+        res = func(args[0])
+        m2 = memory_usage()
+        print(m2)
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
+
+    return wrapper
+
+
+@decor
+# 2.39453125 без tuple - 2.171875 c tuple - незначительно (возможно погрешность измерений)
+def func_2(nums):
+    my_list = []
+    for k, v in enumerate(nums):
+        if v % 2 == 0:
+            my_list.append(k)
+    return my_list
+
+
+my_arr = tuple(range(100000))
+
+
+res2, mem_diff2 = func_2(my_arr)
+print(f'{mem_diff2} MenInBlack')
+# 2.39453125 без tuple - 2.171875 c tuple - незначительно (возможно погрешность измерений)
