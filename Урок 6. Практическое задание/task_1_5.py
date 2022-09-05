e21854786b421ca47911331e1415f3f1105ef0ff
Yaroslav Russu <yaroslav.russu@mail.ru>
2022-09-05 15:37:48 +1000

    Задания выполены

--- Урок 6. Практическое задание/task_1_5.py
+++ Урок 6. Практическое задание/task_1_5.py
@@ -30,4 +30,76 @@
 
 Это файл для пятого скрипта
 """
+import random
+from memory_profiler import profile
+import hashlib
 
+
+def random_url():
+    symblos = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+    return ''.join([random.choice(symblos) for _ in range(random.randint(5, 15))])
+
+
+class UrlCache:
+    def __init__(self):
+        self.cache_dict = {}
+        self.salt = 'UrlCache'
+
+    def add_url_to_cache(self, new_url: str):
+        """Функция принимает в себя url и добавляет его хеш в словарь с другими хешами"""
+        new_record = {new_url: hashlib.sha512(new_url.encode() + self.salt.encode()).hexdigest()}
+        self.cache_dict.update(new_record)
+        return f'Сайт {new_url} добавлен в кеш'
+
+    def check_cache(self, pattern: str):
+        """Функция проверяет наличие хеша url в словаре с другими хешами"""
+        if pattern in self.cache_dict:
+            return f'Хеш {pattern}: {self.cache_dict[pattern]}'
+        else:
+            return self.add_url_to_cache(pattern)
+
+
+@profile
+def create_objects():
+    cache = UrlCache()
+    for i in range(10000):
+        cache.check_cache(f'https://www.{random_url()}.ru/')
+
+
+create_objects()
+
+
+class UrlSlotCache():
+    def __init__(self, new_url: str):
+        self.cache_dict = {}
+        self.salt = 'UrlCache'
+        self.url = new_url
+
+    def add_url_to_cache(self):
+        """Функция принимает в себя url и добавляет его хеш в словарь с другими хешами"""
+        new_record = {self.url: hashlib.sha512(self.url.encode() + self.salt.encode()).hexdigest()}
+        self.cache_dict.update(new_record)
+        return f'Сайт {self.url} добавлен в кеш'
+
+    def check_cache(self):
+        """Функция проверяет наличие хеша url в словаре с другими хешами"""
+        if self.url in self.cache_dict:
+            return f'Хеш {self.url}: {self.cache_dict[self.url]}'
+        else:
+            return self.add_url_to_cache()
+
+
+@profile
+def create_objects_slot():
+    for i in range(10000):
+        url = UrlSlotCache(f'https://www.{random_url()}.ru/')
+        url.check_cache()
+
+
+create_objects_slot()
+
+
+"""Кажется я правильно произвел замену существующего образования классов на применение конструкции слотов.
+Но это не точно. В прежней редакции Mem usage в ходе работы функции увеличивалась на 2.9 MiB.
+В новой редакции Mem usage остался в прежнем объеме
+  """
