cc4efba11b792e5629fe24041852db413782f980
Ostap <mistrun@yandex.ru>
2022-09-05 16:11:33 +0300

    Shubin_lesson_4
    Все задания выполнены

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,7 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 import timeit
+from collections import Counter
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -39,18 +40,35 @@ def func_2():
 
 
 def func_3():
-    elem = max(set(array))
-    return f'Чаще всего встречается число {elem}, ' \
-           f'оно появилось в массиве {array.count(elem)} раз(а)'
+    result = Counter(array).most_common(1)
+    return f'Чаще всего встречается число {result[0][0]}, ' \
+           f'оно появилось в массиве {result[0][1]} раз(а)'
+
+
+def func_4():
+    result = max(set(array), key=array.count)
+    return f'Чаще всего встречается число {result}, ' \
+           f'оно появилось в массиве {array.count(result)} раз(а)'
 
 
 print(func_1())
 print(func_2())
 print(func_3())
-#
-# count_func_1 = timeit.timeit('func_1()', setup='from __main__ import func_1, array')
-# count_func_2 = timeit.timeit('func_2()', setup='from __main__ import func_2, array')
-#
-# print(count_func_1, "Результат 1 функции, ")
-#
-# print(count_func_2, "Результат 2 функции, ")
+print(func_4())
+
+count_func_1 = timeit.timeit('func_1()', setup='from __main__ import func_1, array')
+count_func_2 = timeit.timeit('func_2()', setup='from __main__ import func_2, array')
+count_func_3 = timeit.timeit('func_3()', setup='from __main__ import func_3, array')
+count_func_4 = timeit.timeit('func_4()', setup='from __main__ import func_4, array')
+print(count_func_1, "Результат 1 функции, ")
+print(count_func_2, "Результат 2 функции, ")
+print(count_func_3, "Результат 3 функции, ")
+print(count_func_4, "Результат 4 функции, ")
+
+# Аналитика
+# В функции № 3 использовал ипорт модуля и кортежи, что заметно увеличило время выполнения
+# В Функции № 4 сложность квадратичная, это самая быстрая из всех функций
+# 3.7910486000000003 Результат 1 функции,
+# 5.156621599999999 Результат 2 функции,
+# 9.537008 Результат 3 функции,
+# 3.3450510000000016 Результат 4 функции,
