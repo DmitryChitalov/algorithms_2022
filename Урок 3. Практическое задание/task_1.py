a908b3b19a84f800ade95cf0231645fdc9a828ac
farush <farusha.q@gmail.com>
2022-09-06 02:01:37 +0300

    Homework 3 is Done

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -28,3 +28,67 @@ b) получение элемента списка, оцените сложно
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+from random import randint
+from time import time
+
+def measure_time(f):
+    def measuring(*args):
+        start = time()
+        f_return = f(*args)
+        end = time()
+        print(f'{f.__name__} \n{end - start}')
+        return f_return
+    return measuring
+
+
+
+@measure_time
+def list_creation(n):
+    lst = [randint(0, 10000) for i in range(n)] # не нашел для list comprehension. Предполагаю,что O(n) т.к. есть for i in range
+    return lst
+
+@measure_time
+def dict_creation(n):
+    dct = {key: randint(0, 10000) for key in range(n)} # : Предполагаю,что также O(n) как в случае с list comprehension
+    return dct
+
+
+lst = list_creation(10)
+dct = dict_creation(10)
+
+
+@measure_time
+def get_from_list(lst):
+    for i in range(len(lst)): # O(n) если без цикла для одного элемента, то О(1)
+        print(f'{lst[i]},', end=' ')
+
+    return None
+
+@measure_time
+def get_from_dict(dct): # O(n) если без цикла для одного элемента, то О(1)
+    for key, val in dct.items():
+        print(f'{key}: {dct[key]},', end=' ')
+    return None
+
+
+get_from_list(lst)
+get_from_dict(dct)
+
+@measure_time
+def del_from_list(lst):
+    for i in range(len(lst)): #О(n)
+        lst.pop() #O(1)
+    return None
+
+@measure_time
+def del_from_dict(dct):
+    dct_lst = [] #O(1)
+    for key in dct.keys(): #О(n)
+        dct_lst.append(key)  #O(1)
+    for i in range(len(dct_lst)): #О(n)
+        dct.pop(dct_lst[i]) #O(1)
+    return None
+
+
+del_from_list(lst)
+del_from_dict(dct)
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,21 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+import hashlib
+
+cached_urls = {}
+
+def check_cache(url):
+    hashed_url = hashlib.sha512('abracadabra'.encode() + url.encode()).hexdigest()
+    if url in cached_urls:
+        return hashed_url
+    else:
+        cached_urls[url] = hashed_url
+        return 'url cached'
+
+print(check_cache('www.homework.org'))
+print(check_cache('www.homework.org'))
+print(check_cache('www.homework.ru'))
+print(check_cache('www.homework.ru'))
+
