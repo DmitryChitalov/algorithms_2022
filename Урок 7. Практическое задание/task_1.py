8cd7969ae022cd78524697ac9953f752b998cfc1
Chernyaeva <moiri@mail.ru>
2022-09-06 16:26:52 +0300

    lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,46 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+
+import random
+from timeit import timeit
+
+my_array = [random.randrange(-100, 100) for i in range(100)]
+print(f'исходный массив: {my_array}')
+
+
+def bubble_sort(some_array):
+    for j in range(len(some_array) - 1):
+        for i in range(len(some_array) - 1 - j):
+            if some_array[i] < some_array[i+1]:
+                some_array[i], some_array[i + 1] = some_array[i+1], some_array[i]
+    return some_array
+
+
+print('Не оптимизировано')
+print(bubble_sort(my_array[:]))
+print(timeit('bubble_sort(my_array[:])', globals=globals(), number=10000))
+
+print(f'исходный массив: {my_array}')
+
+
+def bubble_sort2(some_array):
+    for j in range(len(some_array) - 1):
+        sort_counter = 0
+        for i in range(len(some_array) - j - 1):
+            if some_array[i] < some_array[i+1]:
+                some_array[i], some_array[i + 1] = some_array[i+1], some_array[i]
+                sort_counter += 1
+        if sort_counter == 0:
+            return some_array
+    return some_array
+
+
+print('Оптимизировано')
+print(bubble_sort2(my_array[:]))
+print(timeit('bubble_sort2(my_array[:])', globals=globals(), number=10000))
+
+'''
+Оптимизация замедлила работу алгоритма из-за дополнительных проверок. 
+Она бы помогла, если бы числа в исходном массиве были упорядочены.
+'''
