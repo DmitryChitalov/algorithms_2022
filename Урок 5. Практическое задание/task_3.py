304e6380ede036540f32792bfd740ca6e50e3f06
roghaldorn <roghaldorn@gmail.com>
2022-09-06 19:17:21 +0300

    lesson_5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,105 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+
+from collections import deque
+from timeit import timeit
+
+test_list = []
+test_deque = deque()
+
+# 1)
+test_data = list(str(98765 ** 99))
+
+
+def append_test(lst, data):
+    for elem in data:
+        lst.append(elem)
+
+
+def pop_test(lst):
+    for i in range(1000):
+        lst.pop()
+
+
+def extend_test(lst, data):
+    for elem in data:
+        lst.extend(elem)
+
+
+print(timeit("append_test(test_list, test_data)", globals=globals(), number=1000))
+print(timeit("append_test(test_deque, test_data)", globals=globals(), number=1000))  # быстрее
+
+print(timeit("pop_test(test_list)", globals=globals(), number=100))
+print(timeit("pop_test(test_deque)", globals=globals(), number=100))  # быстрее
+
+print(timeit("extend_test(test_list, test_data)", globals=globals(), number=1000))
+print(timeit("extend_test(test_deque, test_data)", globals=globals(), number=1000))  # быстрее
+
+"""
+Вывод 1: для операций append, pop и extend дека отрабатывает быстрее списка, что согласуется с документацией
+"""
+
+
+# 2)) сравнить операции
+# appendleft, popleft, extendleft дека и соответствующих им операций списка
+# и сделать выводы что и где быстрее
+
+
+def appendleft_list(lst, data):
+    for elem in data:
+        lst = list(elem) + lst
+
+
+def appendleft_deque(lst, data):
+    for elem in data:
+        lst.appendleft(elem)
+
+
+def popleft_list(lst):
+    for i in range(500):
+        lst.remove(lst[0])
+
+
+def popleft_deque(lst):
+    for i in range(500):
+        lst.popleft()
+
+
+def extendleft_list(lst, data):
+    lst = data + lst
+
+
+def extendleft_deque(lst, data):
+    lst.extendleft(data)
+
+
+print(timeit("appendleft_list(test_list, test_data)", globals=globals(), number=1))
+print(timeit("appendleft_deque(test_deque, test_data)", globals=globals(), number=100))  # быстрее
+
+print(timeit("popleft_list(test_list)", globals=globals(), number=1))
+print(timeit("popleft_deque(test_deque)", globals=globals(), number=100))  # быстрее
+
+print(timeit("extendleft_list(test_list, test_data)", globals=globals(), number=1000))
+print(timeit("extendleft_deque(test_deque, test_data)", globals=globals(), number=1000))  # быстрее
+
+"""
+Вывод 2: встроенные методы деки работают быстрее аналогичных реализаций у списков.
+"""
+
+
+# 3) сравнить операции получения элемента списка и дека
+# и сделать выводы что и где быстрее
+
+def get_elem_test(lst, tst_lst=[]):
+    for i in range(1000):
+        tst_lst.append(lst[i])
+
+
+print(timeit("get_elem_test(test_list)", globals=globals(), number=10000))  # быстрее
+print(timeit("get_elem_test(test_deque)", globals=globals(), number=10000))
+
+"""
+Вывод 3: как и указано в документации, взятие элементов по индексу в списке происходит быстрее чем в деке, 
+хотя разница по времени не очень большая.
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,54 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+dict_test = {}
+ord_dict_test = OrderedDict()
+test_data = [i for i in range(1000000)]
+
+
+def add_elem(dct, data):
+    """
+    Заполнение словаря.
+    """
+    for elem in data:
+        dct[elem] = str(elem)
+
+
+print(timeit("add_elem(dict_test, test_data)", globals=globals(), number=10))  # быстрее
+print(timeit("add_elem(ord_dict_test, test_data)", globals=globals(), number=10))
+
+
+def pop_dct(dct):
+    """
+    Удаление элементов словаря
+    """
+    for i in range(10):
+        dct.popitem()
+
+
+print(timeit("pop_dct(dict_test)", globals=globals(), number=1000))  # быстрее
+print(timeit("pop_dct(ord_dict_test)", globals=globals(), number=1000))
+
+
+def dct_value(dct, test_lst=[]):
+    """
+    Взятие элемента
+    """
+    for elem in dct.values():
+        test_lst.append(elem)
+
+
+print(timeit("dct_value(dict_test)", globals=globals(), number=100))  # быстрее
+print(timeit("dct_value(ord_dict_test)", globals=globals(), number=100))
+
+"""
+Вывод: во всех примерах выше словарь показал более быструю скорость выполнения.
+В Python 3.6+ OrderedDict имеет смысл как:
+a) Указатель на важность порядков элементов словаря
+b) Для операций переупорядочивания(согласно документации модуля collections)
++ до питона 3.8 у OrderedDict присутствовал метод __reversed__(), который отсутсвовал у обычного словаря.
+"""
