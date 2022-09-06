3a6fbde82775bc57ac06ac353ee2807c7bd4d1f0
slade <paran87@mail.ru>
2022-09-07 00:19:01 +0300

    2

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -9,3 +9,38 @@
 Опищите эту проблему и найдите простой путь ее решения.
 Опишите этот путь и покажите его применение
 """
+
+
+def funct(k):
+    n = k - 1
+    return ((-1) ** n) / (2 ** n)
+
+
+def recurs(n, summ=0):
+    if n != 0:
+        summ = summ + funct(n)
+        return recurs(n - 1, summ)
+    else:
+        return summ
+
+
+print(recurs(3))
+
+
+# Вариант 2
+def recurs2(n):
+    if n == 1:
+        return funct(n)
+    return funct(n) + recurs2(n - 1)
+
+
+print(recurs2(3))
+
+
+# Вариант 3
+
+def recurs3(n):
+    return funct(n) if (n == 1) else funct(n) + recurs3(n - 1)
+
+
+print(recurs3(3))
