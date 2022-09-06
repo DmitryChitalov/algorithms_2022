216eebd903fdadd4fc9c35bdac9e5125b414807c
Roman <korchigin5@gmail.com>
2022-09-06 23:41:14 +0300

    Korchigin_Roman_lesson4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,16 +11,18 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
         revers(enter_num, revers_num)
+        return revers(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -35,3 +37,32 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+# четвертая функция через тернарный оператор
+def revers_4(enter_num):
+    return str(enter_num) if enter_num < 10 else str(enter_num % 10) + revers_4(enter_num // 10)
+
+
+enternum = 32144891
+
+print(revers(enternum))
+print(revers_2(enternum))
+print(revers_3(enternum))
+print(revers_4(enternum))
+
+print(timeit("revers(enternum)", "from __main__ import revers, enternum", number=1000))
+print(timeit("revers_2(enternum)", "from __main__ import revers_2, enternum", number=1000))
+print(timeit("revers_3(enternum)", "from __main__ import revers_3, enternum", number=1000))
+print(timeit("revers_4(enternum)", "from __main__ import revers_4, enternum", number=1000))
+
+"""
+Время исполнения функции revers = 0.12306139999964216
+Время исполнения функции revers_2 = 0.0018924999994851532
+Время исполнения функции revers_3 = 0.0004233999998177751
+Время исполнения функции revers_3 = 0.0031703999993624166
+
+Самый быстрый способ №3,который достигается при помощи среза,реализован на встроенных функциях. На втором месте по 
+производительности способ №2 который реализован через цикл. Хуже всего себя показали рекурсивные функции 
+которые вызывают себя сами.
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,7 +10,10 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 
-array = [1, 3, 1, 3, 4, 5, 1]
+from timeit import timeit
+from collections import Counter
+
+array = [1, 3, 1, 3, 4, 5, 3, 9, 6, 1]
 
 
 def func_1():
@@ -37,5 +40,36 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+# С помощью класса Counter() модуля collections
+def func_3():
+    return f'Самое часто встречающееся число и количество раз его появления: {Counter(array).most_common(1)[0]}'
+
+
+# Через функцию max.
+def func_4():
+    number = max(array, key=array.count)
+    quantity = array.count(number)
+    return f'Чаще всего встречается число {number}, оно появилось в массиве {quantity} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+print(timeit("func_1()", "from __main__ import func_1", number=1000))
+print(timeit("func_2()", "from __main__ import func_2", number=1000))
+print(timeit("func_3()", "from __main__ import func_3", number=1000))
+print(timeit("func_4()", "from __main__ import func_4", number=1000))
+
+"""
+Время исполнения функции func_1 - 0.0027018000109819695
+Время исполнения функции func_2 - 0.0037096000014571473
+Время исполнения функции func_3 - 0.007532899995567277
+Время исполнения функции func_4 - 0.002664800005732104
+
+Лучшие результаты показывает первая и четвертая функции. Вторая функция на втором месте. Все они использую функцию 
+.count, но во второй функции также используется метод .append, который дополнительно формирует список. 
+Самая медленная третья, она реализованна c помощью класса Counter модуля collections. Скорее всего из-за того 
+что Counter использует протокол итерации Python и каждый символ должен быть приобразован в обьект Python.
+"""
