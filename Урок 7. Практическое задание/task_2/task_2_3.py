c80e3134496697bd092145dad127c799664dbe36
kenterberiiskii <a.mizyakov@gmail.com>
2022-09-07 17:53:32 +0300

    Update task_2_3.py

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,38 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+from random import randint
+from timeit import timeit
+import statistics
+
+orig_list = [92, 89, 83, 69, 29, -25, -34, -49, -50, -100, 6]
+
+def stat_median(lst_obj):
+    return statistics.median(lst_obj)
+
+# замеры 10
+orig_list = [randint(-100, 100) for _ in range(10)]
+print(
+    timeit(
+        "stat_median(orig_list[:])",
+        globals=globals(),
+        number=1000))
+# замеры 100
+orig_list = [randint(-100, 100) for _ in range(100)]
+print(
+    timeit(
+        "stat_median(orig_list[:])",
+        globals=globals(),
+        number=1000))
+# замеры 1000
+orig_list = [randint(-100, 100) for _ in range(1000)]
+print(
+    timeit(
+        "stat_median(orig_list[:])",
+        globals=globals(),
+        number=1000))
+"""
+0.0019038000609725714
+0.0083147999830544
+0.1833440000191331
+"""
