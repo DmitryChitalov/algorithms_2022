64df8919e1fbdc99e9069b0401fda68eaa8746a4
fxshad <fxshad@mail.ru>
2022-09-07 18:17:30 +0700

    lesson_7_sort_new_pull

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,52 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+from timeit import timeit
+from random import randint
+
+
+lst = []
+for _ in range(1000):
+    lst.append(randint(-100, 100))
+print(lst)
+
+
+def func_1(lst_obj):
+    for i in range(len(lst_obj) - 1):
+        for j in range(len(lst_obj) - i - 1):
+            if lst_obj[j] < lst_obj[j + 1]:
+                lst_obj[j], lst_obj[j + 1] = lst_obj[j + 1], lst_obj[j]
+    return lst_obj
+
+
+f1 = """
+func_1(lst[:])"""
+
+print(timeit(f1, globals=globals(), number=50), 'func_1')
+
+
+def func_2(lst_obj):
+    for i in range(len(lst_obj) - 1):
+        swap = True
+        for j in range(len(lst_obj) - i - 1):
+            if lst_obj[j] < lst_obj[j + 1]:
+                lst_obj[j], lst_obj[j + 1] = lst_obj[j + 1], lst_obj[j]
+                swap = True
+            if not swap:
+                break
+    return lst_obj
+
+
+f2 = """
+func_2(lst[:])"""
+print(func_2(lst))
+print(timeit(f2, globals=globals(), number=50), 'func_2')
+
+"""
+Результат выполнения:
+2.3687133999774233 func_1 -- исходная функция сортировки
+1.5060096999513917 func_2 -- доработанная
+В результате доработки время выполнения уменьшилось примерно на 40%
+"""
+
+
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,57 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+arr_10 = []
+for _ in range(11):
+    arr_10.append(randint(1, 10))
+
+arr_100 = []
+for _ in range(101):
+    arr_100.append(randint(1, 100))
+
+arr_1000 = []
+for _ in range(1001):
+    arr_1000.append(randint(1, 100))
+
+print(arr_10)
+print(arr_100)
+print(arr_1000)
+
+
+def unsort_med(lst_obj):
+    while True:
+        for elem in lst_obj:
+            count = 0
+            for x in lst_obj:
+                if x < elem:
+                    count += 1
+            if count == len(lst_obj) // 2:
+                return elem
+        lst_obj.remove(max(lst_obj))
+
+
+print(f'med:{(unsort_med(arr_10))}')
+print(f'med:{(unsort_med(arr_100))}')
+print(f'med:{(unsort_med(arr_1000))}')
+
+u1 = """
+unsort_med(arr_10)"""
+
+u2 = """
+unsort_med(arr_100)"""
+
+u3 = """
+unsort_med(arr_1000)"""
+
+print(timeit(u1, globals=globals(), number=1000), 'u1')
+print(timeit(u2, globals=globals(), number=1000), 'u2')
+print(timeit(u3, globals=globals(), number=1000), 'u3')
+
+
+
+
+
