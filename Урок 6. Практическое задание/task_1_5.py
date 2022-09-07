172b8c94118f0e007c49b844fe395df4febb4e3e
andrey_milykh <amilykh@mail.ru>
2022-09-07 10:59:40 +0300

    Milykh_Andrey_dz_6

--- Урок 6. Практическое задание/task_1_5.py
+++ Урок 6. Практическое задание/task_1_5.py
@@ -30,3 +30,92 @@
 
 Это файл для пятого скрипта
 """
+# Исходный код: Урок 5. Практическое задание task_1.py
+#
+# Способ 5 минимизации расходования памяти. Использование возможностей
+# модуля recordclass
+#
+from sys import getsizeof
+from collections import namedtuple
+from recordclass import recordclass
+
+
+def enter_information(b_namedtuple=True):
+    _companies = []
+    _lst = ['profit1', 'profit2', 'profit3', 'profit4']
+
+    count = int(input("Введите количество предприятий для расчета прибыли: "))
+    for i in range(count):
+        name = input("Введите название предприятия: ")
+        str_profits = (input("Через пробел введите прибыль данного "
+                             "предприятия за каждый квартал"
+                             "(Всего 4 квартала): ")).split()
+        int_profits = list(map(int, str_profits))
+        if b_namedtuple:
+            Company = namedtuple(name, _lst)
+            company = Company(*int_profits)
+            print(f"Объём занимаемой объектом namedtuple памяти: "
+                  f"{getsizeof(company)} байт(а)")
+        else:
+            Company = recordclass(name, _lst)
+            company = Company(*int_profits)
+            print(f"Объём занимаемой объектом recordclass памяти: "
+                  f"{getsizeof(company)} байт(а)")
+        _companies.append(company)
+    return _companies
+
+
+def get_average_annual_profit(lst_companies):
+    result = 0
+    for company in lst_companies:
+        result += company.profit1 + company.profit2 \
+                  + company.profit3 + company.profit4
+    return result / len(lst_companies)
+
+
+def show_efficiency(lst_companies, limit_value):
+    lst_bad = []
+    lst_good = []
+    lst_average = []
+    for company in lst_companies:
+        company_annual_profit = company.profit1 + company.profit2 \
+                                + company.profit3 + company.profit4
+        if company_annual_profit < limit_value:
+            lst_bad.append(str(company).split('(')[0])
+        elif company_annual_profit > limit_value:
+            lst_good.append(str(company).split('(')[0])
+        else:
+            lst_average.append(str(company).split('(')[0])
+
+    if lst_good:
+        print(f"Предприятия, с прибылью выше среднего значения: "
+              f"{''.join(lst_good)}")
+    if lst_bad:
+        print(f"Предприятия, с прибылью ниже среднего значения: "
+              f"{''.join(lst_bad)}")
+    if lst_average:
+        print(f"Предприятия, с прибылью равной среднему значению: "
+              f"{''.join(lst_average)}")
+
+
+def use_namedtuple():
+    companies = enter_information()
+    average_annual_profit = get_average_annual_profit(companies)
+    print(f"Средняя годовая прибыль всех предприятий: {average_annual_profit}")
+    show_efficiency(companies, average_annual_profit)
+
+
+def use_recordclass():
+    companies = enter_information(b_namedtuple=False)
+    average_annual_profit = get_average_annual_profit(companies)
+    print(f"Средняя годовая прибыль всех предприятий: {average_annual_profit}")
+    show_efficiency(companies, average_annual_profit)
+
+
+def main():
+    use_namedtuple()  # вызвать решение на основе namedtuple
+    use_recordclass()  # вызвать решение на основе recordclass
+
+
+if __name__ == "__main__":
+    main()
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -6,6 +6,90 @@
 Вам нужно обнаружить проблему в процессе этого. Но проблема связана не с тем,
 что рекурсия потребляет много памяти, а с самим процессом замеров.
 
-Опищите эту проблему и найдите простой путь ее решения.
+Опишите эту проблему и найдите простой путь ее решения.
 Опишите этот путь и покажите его применение
+"""
+from memory_profiler import profile
+from memory_profiler import memory_usage
+from time import perf_counter
+MEBIBYTE = 1048576  # Bytes
+
+
+def check(func):
+    """
+    Декоратор для замера скорости исполнения кода и потребляемой памяти
+    (с ипользованием memory_profiler)
+    """
+    def wrapper(*args, **kwargs):
+        memory = memory_usage()
+        start_time = perf_counter()
+        result = func(*args, **kwargs)
+        end_time = perf_counter()
+        run_time = end_time - start_time
+        memory_2 = memory_usage()
+        memory_used = memory_2[0] - memory[0]
+        print(f"Функция {func.__name__!r} использовала память:"
+              f" {memory_used} Mib или {memory_used * MEBIBYTE} Bytes")
+        print(f"Функция {func.__name__!r} выполнена за {run_time:.8f} c")
+
+        return result
+    return wrapper
+
+
+"""
+Если вызвать здесь профилирование памяти с помощью функции profile,
+то у нас будет столько таблиц, сколько вызовов рекурсивной функции.
+"""
+
+
+# @check
+# @profile
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n*recur_factorial(n-1)
+
+
+# @profile
+def main():
+    # take input from the user
+    num = int(input("Enter a number: "))
+    print()
+    # check is the number is negative
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        print(f"The factorial of {num} is {call_factorial(num)}")
+
+
+# фунция обёртка для рекурсивной функции
+@check
+@profile
+def call_factorial(num):
+    return recur_factorial(num)
+
+
+"""
+Для num = 5:
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    68     18.2 MiB     18.2 MiB           1   @check
+    69                                         @profile
+    70                                         def call_factorial(num):
+    71     18.3 MiB      0.0 MiB           1       return recur_factorial(num)
+
+
+Функция 'call_factorial' использовала память: 0.28125 Mib или 294912.0 Bytes
+Функция 'call_factorial' выполнена за 0.08157189 c
+"""
+
+
+if __name__ == "__main__":
+    main()
+"""
+The factorial of 5 is 120
 """
