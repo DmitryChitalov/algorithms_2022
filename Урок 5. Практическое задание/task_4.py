a7873e80b3d4dc312d4651b12d114e495f5fd11b
Viktor <tomahabk59@gmail.com>
2022-09-07 07:39:08 +0500

    Lesson_5 completed

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,37 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from timeit import timeit
+from collections import OrderedDict
+
+o_dct = OrderedDict()
+dct = dict()
+
+print('Заполнение словарей')
+print(timeit('for i in range(1000): o_dct[i] = i', globals=globals(), number=1000))
+print(timeit('for i in range(1000): dct[i] = i', globals=globals(), number=1000))
+
+print('Получение значений по ключу')
+print(timeit('for i in range(1000): a = o_dct[i]', globals=globals(), number=1000))
+print(timeit('for i in range(1000): b = dct[i]', globals=globals(), number=1000))
+
+print('Удаление элемента')
+print(timeit('for _ in range(10): o_dct.popitem()', globals=globals(), number=100))
+print(timeit('for _ in range(10): dct.popitem()', globals=globals(), number=100))
+
+"""
+Словари работают практически одинаково, с версии Python 3.6 
+словарь упорядочивает элементы автоматически, необходимости
+использовать OrderDict нет.
+
+Заполнение словарей
+OrderDict 0.007270900008734316
+dict 0.007473499979823828
+Получение значений по ключу
+OrderDict 0.004161999968346208
+dict 0.004891099990345538
+Удаление элемента
+OrderDict 0.00015310000162571669
+dict 0.00010259996633976698
+"""
