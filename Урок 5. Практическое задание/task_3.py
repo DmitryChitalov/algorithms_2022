e4758dbe72573d3565318dcb4581c88b09f8e1a7
Isakov_Boris <iborisisakov@gmail.com>
2022-09-07 20:55:39 +0300

    Homework_lesson_5_Isakov_Boris

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,149 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+
+from timeit import timeit
+from collections import deque
+
+simple_lst = [i for i in range(10 ** 5)]
+simple_deq = deque([i for i in range(10 ** 5)])
+count = 10 ** 4
+
+
+# 1) сравнить операции append, pop, extend списка и дека и сделать выводы что и где быстрее
+def append_list(some_list):
+    for i in range(count):
+        some_list.append(i)
+    return some_list
+
+
+def append_deque(some_deque):
+    for i in range(count):
+        some_deque.append(i)
+    return some_deque
+
+
+print("сравнение  операции 'append' для list и deque")
+print(timeit('append_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('append_deque(simple_deq.copy())', globals=globals(), number=100))
+
+# Время выполнения практически совпадает операции 'append' для list и deque
+
+
+def pop_list(some_list):
+    for i in range(count):
+        some_list.pop()
+    return some_list
+
+
+def pop_deque(some_deque):
+    for i in range(count):
+        some_deque.pop()
+    return some_deque
+
+
+print("сравнение  операции 'pop' для list и deque")
+print(timeit('pop_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('pop_deque(simple_deq.copy())', globals=globals(), number=100))
+
+# Время выполнения операции 'pop' для list и deque практически совпадает.
+
+
+def extend_list(some_list):
+    for i in range(count):
+        some_list.extend([1, 2, 3])
+    return some_list
+
+
+def extend_deque(some_deque):
+    for i in range(count):
+        some_deque.extend([1, 2, 3])
+    return some_deque
+
+
+print("сравнение  операции 'extend' для list и deque")
+print(timeit('extend_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('extend_deque(simple_deq.copy())', globals=globals(), number=100))
+
+#Время выполнения операции 'extend' для списка незначительно меньше, чем у очереди.
+
+
+# 2) сравнить операции appendleft, popleft, extendleft дека и соответствующих им операций списка и сделать выводы что и где быстрее
+
+def appendleft_list(some_list):
+    for i in range(count):
+        some_list.insert(0, i)
+    return some_list
+
+
+def appendleft_deque(some_deque):
+    for i in range(count):
+        some_deque.appendleft(i)
+    return some_deque
+
+
+print("сравнение  операции 'appendleft' для list и deque")
+print(timeit('appendleft_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('appendleft_deque(simple_deq.copy())', globals=globals(), number=100))
+
+# Время выполнения очередью операции 'appendleft' значительно меньше, чем у соответствующих ей операций списка.
+
+
+def popleft_list(some_list):
+    for i in range(count):
+        some_list.pop(i)
+    return some_list
+
+
+def popleft_deque(some_deque):
+    for i in range(count):
+        some_deque.popleft()
+    return some_deque
+
+
+print("сравнение  операции 'popleft' для list и deque")
+print(timeit('popleft_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('popleft_deque(simple_deq.copy())', globals=globals(), number=100))
+
+#Время выполнения очередью операции 'popleft' значительно меньше, чем у соответствующих ей операций списка.
+
+
+def extendleft_list(some_list):
+    for i in range(count):
+        some_list.extend([1, 2, 3])
+        # some_list.sort()
+    return some_list
+
+
+def extendleft_deque(some_deque):
+    for i in range(count):
+        some_deque.extendleft([1, 2, 3])
+    return some_deque
+
+
+print("сравнение  операции 'extendleft' для list и deque")
+print(timeit('extendleft_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('extendleft_deque(simple_deq.copy())', globals=globals(), number=100))
+
+#Время выполнения очередью операции 'extendleft' незначительно больше, чем у соответствующих ей операций списка.
+
+
+#3) сравнить операции получения элемента списка и дека и сделать выводы что и где быстрее
+
+def get_item_list(some_list):
+    for i in range(count):
+        some_list[i] = i
+    return some_list
+
+
+def get_item_deque(some_deque):
+    for i in range(count):
+        some_deque[i] = i
+    return some_deque
+
+
+print("сравнение  операций доступа к случайным элементам для list и deque")
+print(timeit('get_item_list(simple_lst.copy())', globals=globals(), number=100))
+print(timeit('get_item_deque(simple_deq.copy())', globals=globals(), number=100))
+
+# Список быстрее осуществляет доступ к случайному элементу по сравнению с очередью.
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,42 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+import time
+from time import perf_counter
+from collections import OrderedDict
+
+
+def function_time(function):
+    def wrapped(*args, **kwargs):
+        start_time = perf_counter()
+        res = function(*args, **kwargs)
+        all_time = time.perf_counter() - start_time
+        print(f"Время выполнения '{str(function).split(' ')[1]}' составило:", end=' ')
+        print(all_time, "секунд")
+        return res
+    return wrapped
+
+
+@function_time
+def change_simple_dict(some_dict):
+    for key in range(10000):
+        some_dict.pop(key)
+    for key in range(10001, 20002):
+        some_dict[key] = 'change'
+
+
+@function_time
+def change_ordered_dict(some_dict):
+    for key in range(10000):
+        some_dict.pop(key)
+    for key in range(10001, 20002):
+        some_dict[key] = 'change'
+
+
+simple_dict = {i: i for i in range(10 ** 5)}
+ordered_dict = OrderedDict(simple_dict)
+
+change_simple_dict(simple_dict)
+change_ordered_dict(ordered_dict)
+
+# Начиная с версии Python 3.6  использование OrderedDict оправдано, если нужны move_to_end(key, last=True), popitem(last=True))
