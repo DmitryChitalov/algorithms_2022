a2cd7be938bcceaed7491d60d93b4b547f9caad8
Shubin_Ostap <mistrun@yandex.ru>
2022-09-07 13:41:09 +0300

    Shubin_lesson_4
    Выполнил все задачи

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,6 +11,10 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
+import timeit
+
+
+num_100 = range(1, 100)
 
 
 def func_1(nums):
@@ -19,3 +23,20 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+count_time_1 = timeit.timeit('func_1(num_100)', setup='from __main__ import func_1, num_100')
+count_time_2 = timeit.timeit('func_2(num_100)', setup='from __main__ import func_2, num_100')
+
+print(count_time_1, "Время 1 функции")
+print(count_time_2, "Время 2 функции")
+
+# Аналитика
+# 18.63887419970706 Время 1 функции
+# 16.245855399873108 Время 2 функции
+# используем генератор списка для ускорения работы, как видно по замерам, время работы улучшили.
