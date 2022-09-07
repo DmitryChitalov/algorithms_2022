129342589fb0196212932618e599bd31294dafe7
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-09-07 22:36:06 +0500

    Lesson_06 completed

--- Урок 6. Практическое задание/task_1_2.py
+++ Урок 6. Практическое задание/task_1_2.py
@@ -29,4 +29,110 @@
 генераторы, numpy, использование слотов, применение del, сериализация и т.д.
 
 Это файл для второго скрипта
+
+
+Python-basic - https://github.com/Frvzr/gb_python_basics/blob/master/Koposhilov_Ivan_dz_5/task_5_4.py
+
+Представлен список чисел. Необходимо вывести те его элементы, значения которых больше предыдущего
+"""
+
+from time import perf_counter
+import sys
+from memory_profiler import profile, memory_usage
+from random import randint
+
+
+def decor(func):
+    def wrapper(*args):
+        m1 = memory_usage()
+        res = func(args[0])
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
+
+    return wrapper
+
+
+@profile
+def get_numbers_1(src: list):
+    """
+    Генератор, выводящий значения из списка, которые больше предыдущего значения
+    :src: list с значениями
+    :return: generator
+    """
+
+    start = perf_counter()
+    result = []
+    for i in range(0, len(src) - 1):
+        if src[i] < src[i + 1]:
+            result.append(src[i + 1])
+    print(sys.getsizeof(result), perf_counter() - start)  # 120 0.0000165
+    return result
+
+
+@profile
+def get_numbers_2(src: list):
+    start = perf_counter()
+    result = (src[i + 1]
+              for i in range(0, len(src) - 1) if src[i] < src[i + 1])
+    print(sys.getsizeof(result), perf_counter() - start)  # 112 0.0000075
+    return result
+
+
+# Оптимизированный вариант
+@decor
+def get_numbers_3(src: list):
+    """
+    Генератор
+    """
+    for num in src:
+        if src[num] < src[num + 1]:
+            yield num
+
+
+src = [randint(1, 100000) for _ in range(1, 1000000)]
+if __name__ == "__main__":
+    generator, mem_diff = get_numbers_3(src)
+    start = perf_counter()
+    for num in generator:
+        print(num, end=' ')
+    print(perf_counter() - start)
+    print(f'\nВыполнение заняло {mem_diff} Mib')
+    get_numbers_1(src)
+    get_numbers_2(src)
+
+
+"""
+Задание было оптимизированно генератором, LC увеличивает скорость
+
+Выполнение заняло 0.0078125 Mib
+4167352 39.1552479
+Filename: c:\Users\user\Desktop\test1.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+   124     58.5 MiB     58.5 MiB           1   @profile
+   125                                         def get_numbers_1(src: list):       
+   132     58.5 MiB      0.0 MiB           1       start = perf_counter()
+   133     58.5 MiB      0.0 MiB           1       result = []
+   134     62.7 MiB -11119.7 MiB      999999       for i in range(0, len(src) - 1):
+   135     62.7 MiB -11119.7 MiB      999998           if src[i] < src[i + 1]:
+   136     62.7 MiB  -5554.7 MiB      499885               result.append(src[i + 1])
+   137     62.7 MiB      0.0 MiB           1       print(sys.getsizeof(result), perf_counter() - start)  # 120 0.0000165
+   138     62.7 MiB      0.0 MiB           1       return result
+
+
+112 7.959999999940237e-05
+Filename: c:\Users\user\Desktop\test1.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+   141     58.5 MiB     58.5 MiB           1   @profile
+   142                                         def get_numbers_2(src: list):
+   143     58.5 MiB      0.0 MiB           1       start = perf_counter()
+   144     58.5 MiB      0.0 MiB           2       result = (src[i + 1]
+   145     58.5 MiB      0.0 MiB           1                 for i in range(0, len(src) - 1) if src[i] < src[i + 1])
+   146     58.5 MiB      0.0 MiB           1       print(sys.getsizeof(result), perf_counter() - start)  # 112 0.0000075
+   147     58.5 MiB      0.0 MiB           1       return result
+
 """
--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -30,3 +30,78 @@
 
 Это файл для третьего скрипта
 """
+
+
+"""
+Python Basic - https://github.com/Frvzr/gb_python_basics/blob/master/Koposhilov_Ivan_dz_2/task_2_4.py
+
+# На вход будет подаваться список, содержащий искажённые данные с должностями и именами сотрудников:
+# Известно, что имя сотрудника всегда в конце строки!
+# Обработать все элементы списка и вернуть в качестве результата список, состоящий из фраз вида:
+# Подумать, как получить имена сотрудников из элементов списка, как привести их к корректному виду.
+"""
+
+
+
+
+from memory_profiler import profile, memory_usage
+from collections import namedtuple
+def decor(func):
+    def wrapper(*args):
+        m1 = memory_usage()
+        res = func(args[0])
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
+    return wrapper
+
+
+@profile
+def convert_name_extract(list_in: list) -> list:
+    """Извлекает имена из элементов и формирует список приветствий."""
+    list_out = []
+    for item in list_in:
+        join_item = ''.join(item)
+        split_item = join_item.split()
+        list_out.append(f'Привет, {split_item[-1].capitalize()}!')
+    return list_out
+
+# Оптимизированная функция
+
+
+@decor
+def convert_name_extract_3(list_in: list):
+    """
+    Генератор
+    """
+    list_out = [f'Привет, {"".join(split_item).split()[-1].capitalize()}!'
+                for split_item in list_in]
+    yield list_out
+
+
+my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА',
+           'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+
+
+if __name__ == "__main__":
+    convert_name_extract(my_list)
+
+    generator, mem_diff = convert_name_extract_3(my_list)
+    for item in generator:
+        print(item, end='')
+    print(f'\nВыполнение заняло {mem_diff} Mib')
+"""
+В оптимизированной функции использовал LC + генератор, результат 0.00390625 Mib
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    72     19.6 MiB     19.6 MiB           1   @profile
+    73                                         def convert_name_extract_2(list_in: list) -> list:
+    75     19.6 MiB      0.0 MiB          12       list_out = [f'Привет, {"".join(split_item).split()[-1].capitalize()}!'
+    76     19.6 MiB      0.0 MiB           1                   for split_item in list_in]
+    77     19.6 MiB      0.0 MiB           1       return list_out
+
+
+['Привет, Игорь!', 'Привет, Марина!', 'Привет, Николай!', 'Привет, Аэлита!']
+Выполнение заняло 0.00390625 Mib
+"""
