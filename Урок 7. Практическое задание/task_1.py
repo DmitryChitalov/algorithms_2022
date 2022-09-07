3b0e721c452f4a34c8368e14e2b881bb5a104c42
fxshad <fxshad@mail.ru>
2022-09-07 13:52:23 +0700

    lesson_7_sort

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,47 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+from timeit import timeit
+from random import randint
+
+
+lst = []
+for _ in range(1000):
+    lst.append(randint(-100, 100))
+print(lst)
+
+
+def func_1(lst_obj):
+    for i in range(len(lst_obj) - 1):
+        for j in range(len(lst_obj) - i - 1):
+            if lst_obj[j] < lst_obj[j + 1]:
+                lst_obj[j], lst_obj[j + 1] = lst_obj[j + 1], lst_obj[j]
+    return lst_obj
+
+
+f1 = """
+func_1(lst)"""
+
+print(timeit(f1, globals=globals(), number=50), 'fiunc_1')
+
+
+def func_2(lst_obj):
+    for i in range(len(lst_obj) - 1):
+        for j in range(len(lst_obj) - i - 1):
+            if lst_obj[j] < lst_obj[j + 1]:
+                lst_obj[j], lst_obj[j + 1] = lst_obj[j + 1], lst_obj[j]
+            break
+    return lst_obj
+
+f2 = """
+func_2(lst)"""
+print(func_2(lst))
+print(timeit(f2, globals=globals(), number=50), 'fiunc_2')
+
+"""
+Результат выполнения:
+1.4808988000149839 -- исходная функция сортировки
+0.012850600003730506 -- доработанная
+В результате доработки время выполнения существенно уменьшилось, чем больше массив тем больший эффект
+дает такая доработка
+"""
--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,76 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def shellsort(lst_obj):
+    def new_increment(array):
+        i = int(len(array) / 2)
+        yield i
+        while i != 1:
+            if i == 2:
+                i = 1
+            else:
+                i = int(round(i/2.2))
+            yield i
+    for increment in new_increment(lst_obj):
+        for i in range(increment, len(lst_obj)):
+            for j in range(i, increment - 1, -increment):
+                if lst_obj[j - increment] < lst_obj[j]:
+                    break
+                lst_obj[j], lst_obj[j - increment] = lst_obj[j - increment], lst_obj[j]
+    return lst_obj
+
+
+arr_10 = []
+for _ in range(11):
+    arr_10.append(randint(1, 10))
+shellsort(arr_10)
+s1 = """
+shellsort(arr_10)
+median(arr_10)
+"""
+
+arr_100 = []
+for _ in range(101):
+    arr_100.append(randint(1, 100))
+shellsort(arr_100)
+s2 = """
+shellsort(arr_100)
+median(arr_100)"""
+
+arr_1000 = []
+for _ in range(1001):
+    arr_1000.append(randint(1, 100))
+shellsort(arr_1000)
+s3 = """
+shellsort(arr_1000)
+median(arr_1000)"""
+
+print(arr_10)
+print(arr_100)
+print(arr_1000)
+
+
+def median(lst_obj):
+    n = len(lst_obj)
+    if n % 2 == 1:
+        return lst_obj[n // 2]
+    else:
+        i = n // 2
+        return (lst_obj[i - 1] + lst_obj[i]) / 2
+
+
+print(f'med:{(median(arr_10))}')
+print(f'med:{(median(arr_100))}')
+print(f'med:{(median(arr_1000))}')
+
+print(timeit(s1, globals=globals(), number=1000), 's1')
+print(timeit(s2, globals=globals(), number=1000), 's2')
+print(timeit(s3, globals=globals(), number=1000), 's3')
+
+
+
+
