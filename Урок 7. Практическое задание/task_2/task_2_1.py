af11b833a9696a2056aa1361aad0da7414ea3153
Pavel <reallife1990msk@mail.ru>
2022-09-08 15:55:47 +0300

    hw_7
    by Pavel Hauruk

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,34 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+def make_lst(m):
+    return [randint(-100, 100) for i in range(2*m+1)]
+
+
+def shell(seq):
+    inc = len(seq) // 2
+    while inc:
+        for i, el in enumerate(seq):
+            while i >= inc and seq[i - inc] > el:
+                seq[i] = seq[i - inc]
+                i -= inc
+            seq[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return seq
+
+# data = [22, 7, 2, -5, 8, 4,9]
+# shell(data)
+# print(data)
+# #data  # [-5, 2, 4, 7, 8, 22]
+
+print(timeit("shell(make_lst(10))", globals=globals(), number=100))
+print(timeit("shell(make_lst(100))", globals=globals(), number=100))
+print(timeit("shell(make_lst(1000))", globals=globals(), number=100))
+
+a=shell(make_lst(10))
+print(f'медиана:{a[int((len(a)-1)/2)]}')
+
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,36 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def make_lst(m):
+    return [randint(-100, 100) for i in range(2*m+1)]
+
+
+def without_sort(lst_obj):
+    temp = lst_obj
+    left_list = []
+    right_list = []
+    for i in range(len(temp)):
+        for j in range(len(temp)):
+            if temp[i] > temp[j]:
+                left_list.append(temp[j])
+            if temp[i] < temp[j]:
+                right_list.append(temp[j])
+            if temp[i] == temp[j] and i > j:
+                left_list.append(temp[j])
+            if temp[i] == temp[j] and i < j:
+                right_list.append(temp[j])
+        if len(left_list) == len(right_list):
+            return temp[i]
+        left_list.clear()
+        right_list.clear()
+
+
+print(timeit("without_sort(make_lst(10))", globals=globals(), number=100))
+print(timeit("without_sort(make_lst(100))", globals=globals(), number=100))
+print(timeit("without_sort(make_lst(1000))", globals=globals(), number=100))
+
+
