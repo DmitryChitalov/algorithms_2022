c0cc80d4d2d2878a04f3ab608cbfcc9a032e52ba
farush <farusha.q@gmail.com>
2022-09-09 00:13:29 +0300

    Homework 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,16 +11,16 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
-
+from timeit import timeit
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        return revers(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -32,6 +32,55 @@ def revers_2(enter_num, revers_num=0):
 
 
 def revers_3(enter_num):
-    enter_num = str(enter_num)
-    revers_num = enter_num[::-1]
+    #enter_num = str(enter_num)
+    revers_num = str(enter_num)[::-1]
     return revers_num
+
+def revers_4(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+print('время для рекурсивного метода с делением на десять')
+print(
+    timeit(
+        'revers(9876543210)',
+        globals = globals(),
+        number=1000000))
+print('*'*30)
+print('время для метода с циклом с делением на 10')
+print(
+    timeit(
+        'revers_2(9876543210)',
+        globals = globals(),
+        number=1000000))
+print('*'*30)
+print('время для метода с присвоением среза')
+print(
+    timeit(
+        'revers_3(9876543210)',
+        globals = globals(),
+        number=1000000))
+print('*'*30)
+print('время для метода с использовнием встроенной функции reversed')
+print(
+    timeit(
+        'revers_4(9876543210)',
+        globals = globals(),
+        number=1000000))
+"""
+время для рекурсивного метода с делением на десять
+2.872303660000398
+******************************
+время для метода с циклом с делением на 10
+1.9000550260007003
+******************************
+время для метода с присвоением среза
+0.342629841001326
+******************************
+время для метода с использовнием встроенной функции reversed
+0.8606314469998324
+
+Самый затратный метод - рекурсивный
+
+Самый быстрый - присвоение среза
+
+"""
