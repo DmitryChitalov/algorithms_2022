223999eb763ad7e26c739f2aa371e291161ba4c1
babenk0ff <babenk0fff@gmail.com>
2022-09-08 18:19:20 +0300

    Solutions added for homework 7

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,23 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+import random
+from timeit import timeit
+
+
+def min_method(lst: list):
+    temp_list = lst[:]
+    return [temp_list.pop(temp_list.index(min(temp_list))) for _ in range(len(lst) // 2 + 1)][-1]
+
+
+m = 5
+test_list = [random.randint(-100, 100) for _ in range(2 * m + 1)]
+print(test_list)
+print(f'Медиана: {min_method(test_list[:])}')
+
+# Замеры
+for m in [10, 100, 1000]:
+    test_list = [random.randint(-100, 100) for _ in range(m)]
+    print(f'Поиск медианы в списке из {m} элементов: ', end=' ')
+    print(timeit('min_method(test_list[:])', globals=globals(), number=1000))
