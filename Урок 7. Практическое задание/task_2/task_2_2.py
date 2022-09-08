52af380608f024fa32cc3128d240d124ef3a24ba
Andrey Milykh <amilykh@mail.ru>
2022-09-08 22:32:30 +0300

    Milykh_Andrey_dz_7

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,51 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def del_max_items(li):
+    """Возврат медианы массива путём удаления максимальных элементов"""
+    temp_li = li
+    for i in range(len(li) // 2):
+        temp_li.remove(max(temp_li))
+    return max(temp_li)
+
+
+# -----------------------------------------------------------------------------
+m = 10
+origin_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+# print(origin_list)
+result = del_max_items(origin_list)
+print(f"Медиана списка из 11 элементов: {result}")
+# Измерение среднего время выполнения куска кода.
+result_time = timeit(stmt="del_max_items(origin_list[:])",
+                    number=100,
+                    globals=globals())
+print(f"{'%.8f' % result_time} seconds")
+
+# -----------------------------------------------------------------------------
+m = 100
+origin_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+# Измерение среднего время выполнения куска кода.
+result_time = timeit(stmt="del_max_items(origin_list[:])",
+                     number=100,
+                     globals=globals())
+print(f"{'%.8f' % result_time} seconds")
+
+# -----------------------------------------------------------------------------
+m = 1000
+origin_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+# Измерение среднего время выполнения куска кода.
+result_time = timeit(stmt="del_max_items(origin_list[:])",
+                     number=100,
+                     globals=globals())
+print(f"{'%.8f' % result_time} seconds")
+
+"""
+Медиана списка из 11 элементов: -36
+0.00117190 seconds
+0.04160020 seconds
+3.62440460 seconds
+"""
--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,54 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+from random import randint
+from timeit import timeit
+from statistics import median
+
+
+def built_in_median(some_li):
+    return median(some_li[:])
+
+
+# -----------------------------------------------------------------------------
+m = 10
+origin_list = [randint(0, 100) for _ in range(2 * m + 1)]
+result = built_in_median(origin_list[:])
+print(f"Медиана списка из 11 элементов: {result}")
+result_time = timeit(stmt="built_in_median(origin_list[:])",
+                     globals=globals(),
+                     number=100)
+print(f"{'%.8f' % result_time} seconds")
+del origin_list
+
+# -----------------------------------------------------------------------------
+m = 100
+origin_list = [randint(0, 100) for _ in range(2 * m + 1)]
+# result = built_in_median(origin_list[:])
+# print(f"median is: {result}")
+result_time = timeit(stmt="built_in_median(origin_list[:])",
+                     globals=globals(),
+                     number=100)
+print(f"{'%.8f' % result_time} seconds")
+del origin_list
+
+# -----------------------------------------------------------------------------
+m = 1000
+origin_list = [randint(0, 100) for _ in range(2 * m + 1)]
+# result = built_in_median(origin_list[:])
+# print(f"median is: {result}")
+result_time = timeit(stmt="built_in_median(origin_list[:])",
+                     globals=globals(),
+                     number=100)
+print(f"{'%.8f' % result_time} seconds")
+del origin_list
+
+"""
+Медиана списка из 11 элементов: 51
+0.00012540 seconds
+0.00130520 seconds
+0.01987760 seconds
+
+Из трёх способов использование встроенной функции
+оказалось самым эффективным способом!
+"""
