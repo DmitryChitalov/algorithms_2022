b26899d3f5bf2818d6236734aa07767e21ee70f8
sslava <ricar@mail.ru>
2022-09-08 14:17:42 +0300

    Решение заданий 5 урока

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -28,3 +28,91 @@
 Предприятия, с прибылью выше среднего значения: Рога
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+from collections import namedtuple, defaultdict
+
+
+def profit_company_calc():
+    """
+    Нахождения по нужному количеству компаний
+    средней прибыли за квартал и информации больше или меньше
+    эта прибыль средней по всем компаниям
+    :return: информация о прибыли компаний
+    """
+    try:
+        num_comp = int(input('Введите количество предприятий: '))
+    except ValueError:
+        return print('Вы ввели не число')
+    else:
+        my_tuple = namedtuple('info_comp', 'name quarter_1 quarter_2 quarter_3 quarter_4')
+        comp_profit = {}
+        for i in range(num_comp):
+            company_info = my_tuple(
+                name=input('Введите название компании: '),
+                quarter_1=int(input('Введите прибыль за 1 квартал: ')),
+                quarter_2=int(input('Введите прибыль за 2 квартал: ')),
+                quarter_3=int(input('Введите прибыль за 3 квартал: ')),
+                quarter_4=int(input('Введите прибыль за 4 квартал: ')))
+            comp_profit[company_info] = (company_info.quarter_1 + company_info.quarter_2 +
+                                         company_info.quarter_3 + company_info.quarter_4) / 4
+        total_avr_profit = 0
+        for val in comp_profit.values():
+            total_avr_profit += val
+        total_avr_profit = total_avr_profit / num_comp
+
+        for key, val in comp_profit.items():
+            if val < total_avr_profit:
+                print(f'У компании {key.name} средняя прибыль {val} ниже среденей '
+                      f'по компаниям {total_avr_profit}')
+            elif val > total_avr_profit:
+                print(f'У компании {key.name} средняя прибыль {val} выше среденей '
+                      f'по компаниям {total_avr_profit}')
+            else:
+                print(f'У компании {key.name} средняя прибыль {val} равна среденей '
+                      f'по компаниям {total_avr_profit}')
+
+        return print('Вывод информации завершен')
+
+
+def profit_company_calc_anothe():
+    """
+    Нахождения по нужному количеству компаний
+    средней прибыли за квартал и информации больше или меньше
+    эта прибыль средней по всем компаниям используя defaultdict
+    :return: информация о прибыли компаний
+    """
+    try:
+        num_comp = int(input('Введите количество предприятий: '))
+    except ValueError:
+        return print('Вы ввели не число')
+    else:
+        comp_profit_avr = defaultdict(int)
+        comp_profit = defaultdict(list)
+        for i in range(num_comp):
+            name = input('Введите название компании: ')
+            for j in range(4):
+                profit = int(input(f'Введите прибыль за {j + 1} квартал: '))
+                comp_profit[name].append(profit)
+            comp_profit_avr[name] = sum(comp_profit[name]) / 4
+        total_avr_profit = 0
+        for val in comp_profit_avr.values():
+            total_avr_profit += val
+        total_avr_profit = total_avr_profit / num_comp
+
+        for key, val in comp_profit_avr.items():
+            if val < total_avr_profit:
+                print(f'У компании {key} средняя прибыль {val} ниже среденей '
+                      f'по компаниям {total_avr_profit}')
+            elif val > total_avr_profit:
+                print(f'У компании {key} средняя прибыль {val} выше среденей '
+                      f'по компаниям {total_avr_profit}')
+            else:
+                print(f'У компании {key} средняя прибыль {val} равна среденей '
+                      f'по компаниям {total_avr_profit}')
+
+        return print('Вывод информации завершен')
+
+
+if __name__ == '__main__':
+    profit_company_calc()
+    profit_company_calc_anothe()
