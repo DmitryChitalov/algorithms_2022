211d7f8ec402686079b0bf61da1aa877bb33d12e
User <ushakov.ip@yandex.ru>
2022-09-08 23:20:49 +0300

    dz_4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,16 +11,17 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
+from timeit import timeit
 
 
-def revers(enter_num, revers_num=0):
+def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -35,3 +36,15 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+
+enter_num = 1234567890
+
+print('Число наоборот:', timeit(f'revers_1({enter_num})', globals=globals()))
+print('Число наоборот:', timeit(f'revers_2({enter_num})', globals=globals()))
+print('Число наоборот:', timeit(f'revers_3({enter_num})', globals=globals()))
+print('Число наоборот:', timeit(f'revers_4({enter_num})', globals=globals()))
