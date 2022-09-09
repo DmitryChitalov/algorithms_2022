18b5d8a60af02398d5083b605fbaea744bb2ce73
Sergey <so.bunin@gmail.com>
2022-09-09 23:01:43 +0300

    DZ_7_2_2

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -13,3 +13,48 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from memory_profiler import profile
+from time import time
+
+
+def timer1(func):
+    def time_mes(*args, **kwargs):
+        start = time()
+        act = func(*args, **kwargs)
+        stop = time()
+        res = stop - start
+        print(f'Функция выполняется за {res} c')
+        return act
+    return time_mes
+
+
+def gen_int(n):
+    a = [randint(0, 10000) for _ in range(n)]
+    return a
+
+
+# @profile
+@timer1
+def med_nosort(m):
+    arr = gen_int(2 * m + 1)
+    for el in arr:
+        left = []
+        right = []
+        mid = []
+        while len(left) <= m and len(right) <= m:
+            for i in range (2*m+1):
+                if el > arr[i]: left.append(arr[i])
+                elif el < arr[i]: right.append(arr[i])
+                else: mid.append(arr[i])
+            if len(left) == len(right):
+                return el
+
+
+print("Поиск без сортировки")
+print("Для m=10")
+print(f"Получаем массив c медианой {med_nosort(10)}")
+print("Для m=100")
+print(f"Получаем массив c медианой {med_nosort(100)}")
+print("Для m=1000")
+print(f"Получаем массив c медианой {med_nosort(1000)}")
