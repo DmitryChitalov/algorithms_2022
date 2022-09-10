0ffd1aab08bf4e59e18e3038d249b56142d8667a
Valerulia <tryk@rambler.ru>
2022-09-10 22:02:58 +0300

    Добавлены файлы ПЗ1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,62 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+
+
+class PlatesStack:
+    def __init__(self):
+        self.plates = [[]]
+
+    def is_empty(self):
+        return self.plates == [[]]
+
+    def push_in(self, plates):
+        if len(self.plates[-1]) == 5:
+            self.plates.append([plates])
+        else:
+            self.plates[-1].append(plates)
+
+    def pop_out(self):
+        result = self.plates[-1].pop()
+        if len(self.plates[-1]) == 0:
+            self.plates.pop()
+        return result
+
+    def get_val(self):
+        return self.plates[-1][-1]
+
+    def stack_size(self):
+        j = len(self.plates)
+        if j > 1:
+            j = (j-1)*5 + len(self.plates[-1])
+        return j
+
+
+if __name__ == '__main__':
+
+    pl1 = PlatesStack()
+    print(pl1.is_empty())  # пустая стопка тарелок
+    pl1.push_in('red1')
+    pl1.push_in('green1')
+    pl1.push_in('green2')
+    pl1.push_in('blue1')
+    pl1.push_in('yellow1')
+    pl1.push_in('red2')
+    pl1.push_in('orange1')
+    pl1.push_in('blue2')
+    pl1.push_in('yellow2')
+    pl1.push_in('orange2')
+    pl1.push_in('orange3')
+    pl1.push_in('orange4')
+    print(pl1.get_val())  # первая тарелка с вершины последней стопки
+    print(pl1.stack_size())  # количество тарелок
+    print(pl1.pop_out())  # убираем последнюю тарелку
+    print(pl1.pop_out())  # убираем последнюю тарелку
+    print(pl1.pop_out())  # убираем последнюю тарелку
+    print(pl1.stack_size())  # количество тарелок
+
+
+
+
+
+
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,67 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+
+class BoardTasks:
+    def __init__(self):
+        self.base_queue = QueueClass()
+        self.rev_queue = QueueClass()
+        self.solved_tasks = []
+
+    def task_solving(self):
+        task = self.base_queue.from_queue()
+        self.solved_tasks.append(task)
+
+    def task_revision(self):
+        task = self.base_queue.from_queue()
+        self.rev_queue.to_queue(task)
+
+    def task_add(self, task):
+        self.base_queue.to_queue(task)
+
+
+if __name__ == '__main__':
+
+    desk1 = BoardTasks()
+
+    print(desk1.base_queue.elems)
+    desk1.task_add('Task1')
+    desk1.task_add('Task2')
+    desk1.task_add('Task3')
+    print(desk1.base_queue.elems)
+
+    desk1.task_solving()
+    desk1.task_revision()
+    print(desk1.base_queue.elems)
+    print(desk1.rev_queue.elems)
+    print(desk1.solved_tasks)
+
+
+
+
+
+
+
+
+
+
+
+
