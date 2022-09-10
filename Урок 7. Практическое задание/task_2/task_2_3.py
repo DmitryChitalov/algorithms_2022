a829c262617120b6baf2a4677be2fc1bff20c9a7
MarselMi <marselmihajlov@yandex.ru>
2022-09-10 21:38:50 +0300

    7 commit

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,22 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+from random import randint
+import statistics
+from timeit import timeit
+
+
+mass_1 = 2 * 10 + 1
+mass_2 = 2 * 100 + 1
+mass_3 = 2 * 1000 + 1
+lst1 = [randint(-900, 900) for _ in range(mass_1)]
+lst2 = [randint(-900, 900) for _ in range(mass_2)]
+lst3 = [randint(-900, 900) for _ in range(mass_3)]
+
+
+
+print(timeit('statistics.median(lst1)', globals=globals(), number=2))
+print(timeit('statistics.median(lst2)', globals=globals(), number=2))
+print(timeit('statistics.median(lst3)', globals=globals(), number=2))
+'''Встроенные функции снова оправдали ожидания своей скоростью, но с малыми 
+списками дела обстоят сложнее'''
