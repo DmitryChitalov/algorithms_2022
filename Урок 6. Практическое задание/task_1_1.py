c542e0eeb30fa68f9c64181abbcd2804605fd030
Arseny <arsenky@yandex.ru>
2022-09-11 17:46:29 +0300

    lesson6 complited

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -2,7 +2,7 @@
 Задание 1.
 
 Вам нужно взять 5 любых скриптов, написанных ВАМИ в рамках работы над ДЗ
-курсов Алгоритмы и Основы Python
+курсов Алгоритмы и Основы
 
 На каждый скрипт нужно два решения - исходное и оптимизированное.
 
@@ -30,3 +30,49 @@
 
 Это файл для первого скрипта
 """
+
+from memory_profiler import profile
+
+# задание из первого урока курса основ:
+# Вычислить сумму кубов нечётных чисел от 1 до 1000 (ниже 10000 - искусственный инкремент),
+# сумма цифр которых делится нацело на 7
+
+@profile
+def shell():
+    number_list = []
+    for i in range(1, 100000, 2):
+        number_list.append(i ** 3)
+
+    numbers_summ = 0
+    for number in number_list:
+        number_copy = number
+        figure_summ = 0
+        while number_copy > 0:
+            figure_summ += number_copy % 10
+            number_copy = number_copy // 10
+        if figure_summ % 7 == 0:
+            numbers_summ += number
+    print('Сумма чисел из списка, сумма цифр которых делится нацело на 7 =', numbers_summ)
+
+# оптимизируем программу используя генератор вместо списка bumber_list, замеры подтверждают экономию памяти
+
+@profile
+def shell_2():
+    def number_list():
+        for i in range(1, 100000, 2):
+            yield i ** 3
+
+    numbers_summ = 0
+    for number in number_list():
+        number_copy = number
+        figure_summ = 0
+        while number_copy > 0:
+            figure_summ += number_copy % 10
+            number_copy = number_copy // 10
+        if figure_summ % 7 == 0:
+            numbers_summ += number
+    print('Сумма чисел из списка, сумма цифр которых делится нацело на 7 =', numbers_summ)
+
+
+shell()
+shell_2()
--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -2,7 +2,7 @@
 Задание 1.
 
 Вам нужно взять 5 любых скриптов, написанных ВАМИ в рамках работы над ДЗ
-курсов Алгоритмы и Основы Python
+курсов Алгоритмы и Основы
 
 На каждый скрипт нужно два решения - исходное и оптимизированное.
 
@@ -30,3 +30,76 @@
 
 Это файл для третьего скрипта
 """
+
+from collections import namedtuple
+from memory_profiler import profile
+
+
+@profile
+def average_profit():
+    n = int(input('Введите количество предприятий: '))
+    main_dct = {}
+    companies = namedtuple('companies', 'name profit_1 profit_2 profit_3 profit_4')
+    for i in range(n):
+        print('Введите построчно имя компании и прибыль за 4 квартала: ')
+        company = companies(
+            name=input(),
+            profit_1=int(input()),
+            profit_2=int(input()),
+            profit_3=int(input()),
+            profit_4=int(input()))
+        main_dct[company.name] = (company.profit_1 + company.profit_2 + company.profit_3 + company.profit_4) / 4
+    profit_sum = 0
+    for value in main_dct.values():
+        profit_sum += value
+    aver_profit = profit_sum / n
+    print('Общая средняя прибыль: ', aver_profit)
+    list1 = []
+    list2 = []
+    for name in main_dct.keys():
+        if main_dct[name] > aver_profit:
+            list1.append(name)
+        else:
+            list2.append(name)
+    print('Компании с прибылью больше средней: ', list1)
+    print('Компании с прибылью меньше  средней: ', list2)
+
+
+# оптимизируем код, удаляя вручную ссылки, используя del, сразу же как переменная перестает быть нужной
+# пометил такие строки коментарием
+
+@profile
+def average_profit_1():
+    n = int(input('Введите количество предприятий: '))
+    main_dct = {}
+    companies = namedtuple('companies', 'name profit_1 profit_2 profit_3 profit_4')
+    for i in range(n):
+        print('Введите построчно имя компании и прибыль за 4 квартала: ')
+        company = companies(
+            name=input(),
+            profit_1=int(input()),
+            profit_2=int(input()),
+            profit_3=int(input()),
+            profit_4=int(input()))
+        main_dct[company.name] = (company.profit_1 + company.profit_2 + company.profit_3 + company.profit_4) / 4
+    del companies  # оптимизация
+    profit_sum = 0
+    for value in main_dct.values():
+        profit_sum += value
+    aver_profit = profit_sum / n
+    del profit_sum  # оптимизация
+    del n  # оптимизация
+    print('Общая средняя прибыль: ', aver_profit)
+    list1 = []
+    list2 = []
+    for name in main_dct.keys():
+        if main_dct[name] > aver_profit:
+            list1.append(name)
+        else:
+            list2.append(name)
+    print('Компании с прибылью больше средней: ', list1)
+    print('Компании с прибылью меньше  средней: ', list2)
+
+
+average_profit()
+average_profit_1()
