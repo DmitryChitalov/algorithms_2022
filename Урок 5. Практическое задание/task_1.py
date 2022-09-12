db9a0e9beb2332c8a4cd1be177a5d0f9e44a705b
Ostap <mistrun@yandex.ru>
2022-09-12 20:02:57 +0300

    Shubin_lesson_5
    task 1, 2

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -28,3 +28,60 @@
 Предприятия, с прибылью выше среднего значения: Рога
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import namedtuple
+
+number_enterprises = int(input('Введите количество предприятий: '))
+list_companies = []
+tuple_companies = namedtuple('Enterprises', 'name_company, quarter_1, quarter_2, quarter_3, quarter_4, sum_years')
+result_profit = 0
+
+
+def data_request(number):
+    if number > 0:
+        name_company = input('Введите название предприятия:')
+        quarter_1 = int(input("Введите прибыль за 1 квартал: "))
+        quarter_2 = int(input("Введите прибыль за 2 квартал: "))
+        quarter_3 = int(input("Введите прибыль за 3 квартал: "))
+        quarter_4 = int(input("Введите прибыль за 4 квартал: "))
+        sum_years = quarter_1 + quarter_2 + quarter_3 + quarter_4
+
+        companies = tuple_companies(name_company=name_company,
+                                    quarter_1=quarter_1,
+                                    quarter_2=quarter_2,
+                                    quarter_3=quarter_3,
+                                    quarter_4=quarter_4,
+                                    sum_years=sum_years)
+
+        list_companies.append(companies)
+
+        return data_request(number - 1)
+    else:
+        print("данные приняты в обработку ")
+        print(list_companies)
+
+
+data_request(number_enterprises)
+
+
+def average_profit(companies, result):
+    for i in companies:
+        result += i.sum_years
+    result_profit = result / len(companies)
+    print(f'Средняя прибыль всех компаний: {result_profit}')
+
+    for i in companies:
+        if result_profit > i.sum_years:
+            print(f"Предприятия, с прибылью ниже среднего значения:{i.name_company}")
+        elif result_profit < i.sum_years:
+            print(f"Предприятия, с прибылью выше среднего значения:{i.name_company}")
+        else:
+            print(f"Предприятия, с прибылью равной среднему значению:{i.name_company}")
+
+
+average_profit(list_companies, result_profit)
+
+
+
+
+
+
