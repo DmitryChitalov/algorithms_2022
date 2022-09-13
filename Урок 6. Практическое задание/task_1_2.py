cf2e3b06d5088f9bab3d938afef63c21554ea12d
Oleg <olazar61@gmail.com>
2022-09-13 15:09:33 +0300

    Homework_6_Azarnykh_Oleg
    Азарных Олег Вячеславович
    Курс: "Алгоритмы и структуры данных на Python. Базовый курс"
    Начало курса: 23.08.2022
    Урок № 6

--- Урок 6. Практическое задание/task_1_2.py
+++ Урок 6. Практическое задание/task_1_2.py
@@ -1,32 +1,69 @@
 """
-Задание 1.
+КУРС: "Основы языка Python". Урок 4: "Работа с модулями и пакетами". Задание 2.
+Написать функцию `currency_rates()`, принимающую в качестве аргумента код валюты
+(например, USD, EUR, SGD, ...) и возвращающую курс этой валюты по отношению к рублю.
+Использовать библиотеку `requests`.
+В качестве API можно использовать `http://www.cbr.ru/scripts/XML_daily.asp`.
+* Можно ли, используя только методы класса `str`, решить поставленную задачу?
+* Функция должна возвращать результат числового типа, например `float`...
+"""
 
-Вам нужно взять 5 любых скриптов, написанных ВАМИ в рамках работы над ДЗ
-курсов Алгоритмы и Основы Python
-
-На каждый скрипт нужно два решения - исходное и оптимизированное.
-
-Вы берете исходное, пишете что это за задание и с какого оно курса.
-Далее выполняете профилирование скрипта средствами memory_profiler
-
-Вы оптимизируете исходное решение.
-Далее выполняете профилирование скрипта средствами memory_profiler
-
-Вам нужно написать аналитику, что вы сделали для оптимизации памяти и
-чего добились.
+import requests
+from memory_profiler import memory_usage
 
 
-ВНИМАНИЕ:
-1) скрипты для оптимизации нужно брать только из сделанных вами ДЗ
-курсов Алгоритмы и Основы
-2) нельзя дублировать, коды, показанные на уроке
-3) для каждого из 5 скриптов у вас отдельный файл, в нем должна быть версия до
-и версия после оптимизации
-4) желательно выбрать те скрипты, где есть что оптимизировать и не брать те,
-где с памятью и так все в порядке
-5) не нужно писать преподавателю '''я не могу найти что оптимизировать''', это
-отговорки. Примеров оптимизации мы перечислили много: переход с массивов на
-генераторы, numpy, использование слотов, применение del, сериализация и т.д.
+def decor(func):
+    def wrapper(*args):
+        m1 = memory_usage()
+        res = func(*args)
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
 
-Это файл для второго скрипта
+    return wrapper
+
+
+# https://github.com/AzarnykhOleg/Python-Basics/pull/5
+@decor
+def currency_rates(code: str) -> float:
+    """возвращает курс валюты `code` по отношению к рублю"""
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    currency_dict = {}
+    currency_list = response.text.split('ID=')
+    del currency_list[0]
+    for element in currency_list:
+        exchange_rate = element[(element.find('<Value>') + 7):element.find('</Value>')]
+        exchange_rate = float(exchange_rate.replace(',', '.'))
+        amendment = int(element[(element.find('<Nominal>') + 9):element.find('</Nominal>')])
+        currency_dict[(element[(element.find('<CharCode>') + 10):
+                               element.find('</CharCode>')])] = exchange_rate / amendment
+        result_value = currency_dict.get(code.upper(), None)
+    return result_value
+
+
+# Обновлённый вариант
+@decor
+def currency_rates_adv(code: str) -> float:
+    """возвращает курс валюты `code` по отношению к рублю"""
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    currency_list = response.text.split('ID=')
+    del currency_list[0]
+    for element in currency_list:
+        if element[(element.find('<CharCode>') + 10):element.find('</CharCode>')] == code:
+            exchange_rate = element[(element.find('<Value>') + 7):element.find('</Value>')]
+            exchange_rate = float(exchange_rate.replace(',', '.'))
+            amendment = int(element[(element.find('<Nominal>') + 9):element.find('</Nominal>')])
+    return exchange_rate / amendment
+
+
+res, mem_diff = currency_rates("USD")
+print(res)
+print(f"Выполнение заняло {mem_diff} Mib")  # -> Выполнение заняло 0.6796875 Mib
+print('----------------- Новый вариант -----------------')
+res, mem_diff = currency_rates_adv("USD")
+print(res)
+print(f"Выполнение заняло {mem_diff} Mib")  # -> Выполнение заняло 0.0 Mib
+
+"""
+Отказ от создания словаря для хранения данных позволил сэкономить 0.6796875 Mib.
 """
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -9,3 +9,60 @@
 Опищите эту проблему и найдите простой путь ее решения.
 Опишите этот путь и покажите его применение
 """
+from memory_profiler import memory_usage
+from pympler.asizeof import asizeof
+
+
+def decor(func):
+    def wrapper(*args):
+        m1 = memory_usage()
+        res = func(*args)
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
+
+    return wrapper
+
+
+#  Функция № 1.
+@decor
+def theorem(n):
+    def theorem_1(n, sum_n=0):
+        if n < 1:
+            return sum_n
+        return theorem_1(n - 1, sum_n + n)
+
+    return f'{theorem_1(n)} - левая часть равенства,\n{int(n * (n + 1) / 2)} - правя часть равенства'
+
+
+#  Функция № 2.
+@decor
+def revers(enter_num, revers_num=0):  # Функция даёт неверный результат при больших числах
+    if enter_num == 0:
+        return revers_num
+    else:
+        num = enter_num % 10
+        revers_num = int((revers_num + num / 10) * 10)
+        enter_num //= 10
+    return revers(enter_num, revers_num)
+
+
+res, mem_diff = theorem(15)
+print(res)
+print(f"Выполнение заняло {mem_diff} Mib")  # Выполнение заняло 0.0078125 Mib
+print(asizeof(theorem(15)))  # 280
+print('----------------- Другая рекурсивная функция: -----------------')
+res, mem_diff = revers(123456789123456)
+print(res)
+print(f"Выполнение заняло {mem_diff} Mib")  # Выполнение заняло 0.01171875 Mib
+print(asizeof(revers(123456789123456)))  # 1440
+
+"""
+    При замерах методами memory_profiler Функции № 2 (рекурсивной) аргументы функции попадали
+столько же раз, сколько запускалась рекурсия, а результаты пробрасывались через место
+вызова функции (print()  и т.д.). В результате - выдача неожиданного результата работы функции
+и результаты замеров, которые вряд ли можно считать объективными.
+    При замерах, произведённых на Функции № 1 (рекурсивная функция вложена в другую функцию
+без рекурсии), такого эффекта не возникает.
+    Это и есть выход, я думаю.
+"""
