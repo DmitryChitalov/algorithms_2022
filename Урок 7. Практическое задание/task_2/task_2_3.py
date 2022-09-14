e583a2aa396d125d64879bb165db62f7b2bd59ca
Zhelonkina <gorina.a.a@mail.ru>
2022-09-14 22:44:21 +0300

    ДЗ7 Желонкина задачи 1, 2.1, 2.2, 2.3

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,30 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+import statistics
+from random import randint
+from timeit import timeit
+
+m = 10
+my_list = [randint(1, 100) for _ in range(2 * m + 1)]
+print('рандомный список ', 'из ', m, ' элементов: \n', my_list)
+print('Медиана: ', statistics.median(my_list[:]))
+print('время на выполнение составило: ',
+      round(timeit("statistics.median(my_list[:])", globals=globals(), number=1000), 3))
+
+m = 100
+my_list = [randint(1, 100) for _ in range(2 * m + 1)]
+print('рандомный список ', 'из ', m, ' элементов: \n', my_list)
+print('Медиана: ', statistics.median(my_list[:]))
+print('время на выполнение составило: ',
+      round(timeit("statistics.median(my_list[:])", globals=globals(), number=1000), 3))
+
+m = 1000
+my_list = [randint(1, 100) for _ in range(2 * m + 1)]
+print('рандомный список ', 'из ', m, ' элементов: \n', my_list)
+print('Медиана: ', statistics.median(my_list[:]))
+print('время на выполнение составило: ',
+      round(timeit("statistics.median(my_list[:])", globals=globals(), number=1000), 3))
+
+# Самый эффективный способом является применение встроенной функции
+# Лучше использавать встроенные функции сортировки, т.к. они реализованы
