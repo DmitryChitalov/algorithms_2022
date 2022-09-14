e59b310fc4e5e199718cd2314938add7cce46ebd
Виталий Терещенко <banderolvitali@gmail.com>
2022-09-14 10:45:39 +0300

    Задания 7 урока

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -14,3 +14,32 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+import random
+from timeit import timeit
+
+
+def gnome_sort(my_array):
+    l = len(my_array)
+    i = 1
+    while i < l:
+        if i == 0:
+            i = 1
+        if my_array[i] > my_array[i-1]:
+            my_array[i], my_array[i-1] = my_array[i-1], my_array[i]
+            i -= 1
+        else:
+            i += 1
+    return my_array
+
+
+m = 5
+some_array = [random.randrange(-100, 100) for i in range(2*m + 1)]
+print(timeit("(gnome_sort(some_array[:]))[m]", globals=globals(), number=1))
+m = 50
+some_array = [random.randrange(-100, 100) for i in range(2*m + 1)]
+print(timeit("(gnome_sort(some_array[:]))[m]", globals=globals(), number=1))
+m = 500
+some_array = [random.randrange(-100, 100) for i in range(2*m + 1)]
+print(timeit("(gnome_sort(some_array[:]))[m]", globals=globals(), number=1))
+
