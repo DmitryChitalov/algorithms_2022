e0c22e1a949427648db73f57c1ac157e573664d7
Lenai65209 <lenai65209@rambler.ru>
2022-09-14 12:07:06 +0300

    Иконникова Елена. Задание к уроку 7. Группа 2141. Курс Алгоритмы от 23.08.2022

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,76 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from sys import setrecursionlimit
+from timeit import timeit
+
+setrecursionlimit(10000)
+
+
+def gnome_sorts(my_list):
+    index = 1
+    i = 0
+    n = len(my_list)
+    while i < n - 1:
+        if my_list[i] <= my_list[i + 1]:
+            i, index = index, index + 1
+        else:
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+            i = i - 1
+            if i < 0:
+                i, index = index, index + 1
+    return my_list
+
+
+def my_median(my_lst):
+    len_my_lst = len(my_lst)
+    sort_my_copy_lst = gnome_sorts(my_lst[:])
+    return sort_my_copy_lst[len_my_lst // 2]
+
+
+def inp_m_get_my_lst(m):
+    len_my_lst = 2 * m + 1
+    orig_list = [randint(-100, 100) for _ in range(len_my_lst)]
+    return orig_list
+
+
+orig_list_10 = inp_m_get_my_lst(10)
+print(f'gnome_sorts(orig_list_10[:]): {gnome_sorts(orig_list_10[:])}.')
+print(f'my_median(orig_list_10): {my_median(orig_list_10)}.')
+print('orig_list_10 ', orig_list_10)
+# замеры 10
+print(
+    timeit(
+        "my_median(orig_list_10[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list_100 = inp_m_get_my_lst(100)
+print(f'my_median(orig_list_100[:]): {my_median(orig_list_100[:])}.')
+# замеры 100
+print(
+    timeit(
+        "my_median(orig_list_100[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list_1000 = inp_m_get_my_lst(1000)
+print(f'my_median(orig_list_1000[:]): {my_median(orig_list_1000[:])}.')
+# замеры 1000
+print(
+    timeit(
+        "my_median(orig_list_1000[:])",
+        globals=globals(),
+        number=1000))
+
+"""
+gnome_sorts(orig_list_10[:]): [-73, -72, -70, -53, -37, -20, -16, -13, 0, 2, 11, 33, 34, 34, 57, 72, 79, 83, 91, 97, 100].
+my_median(orig_list_10): 11.
+orig_list_10  [100, -70, -20, 34, 72, -72, -73, -37, 57, -13, -53, 0, 83, 11, 2, 33, 79, -16, 34, 91, 97]
+0.06191299999999997
+my_median(orig_list_100[:]): -7.
+7.5726251
+my_median(orig_list_1000[:]): 0.
+909.2737175999999
+"""
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,66 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+# from operator import itemgetter
+from random import randint
+from timeit import timeit
+
+
+def inp_m_get_my_lst(m):
+    len_my_lst = 2 * m + 1
+    orig_list = [randint(-100, 100) for _ in range(len_my_lst)]
+    return orig_list
+
+
+def median_without_sorting(my_lst):
+    copy_my_lst = my_lst[:]
+    repetitions = len(my_lst[:]) // 2 + 1
+    i = len(copy_my_lst)
+    while i >= repetitions:
+        # ind_min_el, min_pop_el = min(enumerate(copy_my_lst), key=itemgetter(1))
+        # странно, но с этой строкой в 2 раза медленее по замерам.
+        min_pop_el = min(copy_my_lst)
+        ind_min_el = copy_my_lst.index(min_pop_el)
+        if i != repetitions:
+            copy_my_lst.pop(ind_min_el)
+            i -= 1
+        else:
+            return copy_my_lst.pop(ind_min_el)
+
+
+orig_list_10 = inp_m_get_my_lst(10)
+print(f'median_without_sorting(orig_list_10): {median_without_sorting(orig_list_10)}.')
+print('orig_list_10', orig_list_10)
+# замеры 10
+print(
+    timeit(
+        "median_without_sorting(orig_list_10[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list_100 = inp_m_get_my_lst(100)
+print(f'median_without_sorting(orig_list_100[:]): {median_without_sorting(orig_list_100[:])}.')
+# замеры 100
+print(
+    timeit(
+        "median_without_sorting(orig_list_100[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list_1000 = inp_m_get_my_lst(1000)
+print(f'median_without_sorting(orig_list_1000[:]): {median_without_sorting(orig_list_1000[:])}.')
+# замеры 1000
+print(
+    timeit(
+        "median_without_sorting(orig_list_1000[:])",
+        globals=globals(),
+        number=1000))
+
+"""
+median_without_sorting(orig_list_10[:]): -38.
+0.021191700000000008
+median_without_sorting(orig_list_100[:]): 7.
+1.1645812
+median_without_sorting(orig_list_1000[:]): 3.
+88.9141718
+"""
