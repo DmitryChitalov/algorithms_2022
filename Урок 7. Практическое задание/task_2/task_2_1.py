1110a3c2ecc83c4be354642507b9c815c68d7560
sslava <ricar@mail.ru>
2022-09-14 11:11:25 +0300

    Решенеи задач 7 урока

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,130 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+import math
+from timeit import timeit
+from random import randint
+
+
+def create_list(num):
+    return [randint(0, 100) for _ in range(2 * num + 1)]
+
+
+def gnome(array):
+    """
+    Гномья сортировка
+    :param array: массив
+    :return: отсортированный массив
+    """
+    i = 1
+    size_lst = len(array)
+    while i < size_lst:
+        if array[i - 1] <= array[i]:
+            i += 1
+        else:
+            array[i - 1], array[i] = array[i], array[i - 1]
+            if i > 1:
+                i -= 1
+    return array
+
+
+def shell_sort(array):
+    """
+    Сортировка Шелла
+    :param array: массив
+    :return: отсортированный массив
+    """
+    n = len(array)
+    k = int(math.log2(n))
+    interval = 2 ** k - 1
+    while interval > 0:
+        for i in range(interval, n):
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+            array[j] = temp
+        k -= 1
+        interval = 2 ** k - 1
+    return array
+
+
+def heapify(array, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and array[i] < array[l]:
+        largest = l
+    if r < n and array[largest] < array[r]:
+        largest = r
+
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+        heapify(array, n, largest)
+
+
+def heap_sort(array):
+    """
+    Сортировка кучей
+    :param array: массив
+    :return: отсортированный массив
+    """
+    n = len(array)
+    for i in range(n // 2, -1, -1):
+        heapify(array, n, i)
+    for i in range(n - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
+    return array
+
+
+if __name__ == '__main__':
+    my_lst = [1, 2, 3, 4, 5]
+    my_lst1 = [5, 2, 1, 4, 3]
+    my_lst2 = [70, 49, 30, 10, 39, 36, 75, 80, 93, 32, 31, 47, 91, 54, 74, 24, 39, 12, 88, 76, 36]
+    print(gnome(my_lst[:])[2])
+    print(gnome(my_lst1[:])[2])
+    print(gnome(my_lst2[:])[10])
+    print(shell_sort(my_lst[:])[2])
+    print(shell_sort(my_lst1[:])[2])
+    print(shell_sort(my_lst2[:])[10])
+    print(heap_sort(my_lst[:])[2])
+    print(heap_sort(my_lst1[:])[2])
+    print(heap_sort(my_lst2[:])[10])
+
+    orig_list = create_list(10)
+
+    # замеры 10
+    print(timeit("gnome(orig_list[:])[10]", globals=globals(), number=1000))
+    print(timeit("shell_sort(orig_list[:])[10]", globals=globals(), number=1000))
+    print(timeit("heap_sort(orig_list[:])[10]", globals=globals(), number=1000))
+
+    orig_list = create_list(100)
+
+    # замеры 100
+    print(timeit("gnome(orig_list[:])[100]", globals=globals(), number=1000))
+    print(timeit("shell_sort(orig_list[:])[100]", globals=globals(), number=1000))
+    print(timeit("heap_sort(orig_list[:])[100]", globals=globals(), number=1000))
+
+    orig_list = create_list(1000)
+
+    # замеры 1000
+    print(timeit("gnome(orig_list[:])[1000]", globals=globals(), number=1000))
+    print(timeit("shell_sort(orig_list[:])[1000]", globals=globals(), number=1000))
+    print(timeit("heap_sort(orig_list[:])[1000]", globals=globals(), number=1000))
+
+    """
+    Результаты замеров:
+    m=10 Сортировка Гноья: 0.04678620002232492
+    m=10 Сортировка Шелла: 0.016286199912428856
+    m=10 Сортировка Кучей: 0.043166999937966466
+    m=100 Сортировка Гноья: 3.607114400016144
+    m=100 Сортировка Шелла: 0.22857549996115267
+    m=100 Сортировка Кучей: 0.5781914000399411
+    m=1000 Сортировка Гноья: 423.3251139000058
+    m=1000 Сортировка Шелла: 4.822601700201631
+    m=1000 Сортировка Кучей: 9.578077499987558
+
+    """
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,52 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def create_list(num):
+    return [randint(0, 100) for _ in range(2 * num + 1)]
+
+
+def find_medians(lst):
+    median_idx = len(lst) // 2
+    for i in range(median_idx):
+        lst.remove(max(lst))
+    my_median = max(lst)
+    return my_median
+
+
+if __name__ == '__main__':
+    my_lst = [1, 2, 3, 4, 5]
+    my_lst1 = [5, 2, 1, 4, 3]
+    my_lst2 = [70, 49, 30, 10, 39, 36, 75, 80, 93, 32, 31, 47, 91, 54, 74, 24, 39, 12, 88, 76, 36]
+    print(find_medians(my_lst[:]))
+    print(find_medians(my_lst1[:]))
+    print(find_medians(my_lst2[:]))
+
+    orig_list = create_list(10)
+    print(orig_list)
+    print(find_medians(orig_list[:]))
+
+    # замеры 10
+    print(timeit("find_medians(orig_list[:])", globals=globals(), number=1000))
+
+    orig_list = create_list(100)
+
+    # замеры 100
+    print(timeit("find_medians(orig_list[:])", globals=globals(), number=1000))
+
+    orig_list = create_list(1000)
+
+    # замеры 1000
+    print(timeit("find_medians(orig_list[:])", globals=globals(), number=1000))
+
+    """
+    Результаты замеров:
+    
+    m=10 : 0.010440699988976121
+    m=100 : 0.39781470014713705
+    m=1000 : 26.365068699931726
+    
+    """
