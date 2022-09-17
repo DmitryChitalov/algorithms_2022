63b6a2c60ac9af077834cd6b8ec4d2f6779c8181
Kuanysh <anuarbekov.kuanysh@mail.ru>
2022-09-17 21:08:55 +0600

    Lesson5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,48 @@ reduce
 __mul__
 __add__
 """
+
+
+from collections import defaultdict
+
+
+hex_nums = defaultdict(list)
+for i in range(2):
+    hex_num = input('Введите шестнадцатиричное число: ')
+    hex_nums[hex_num] = list(hex_num)
+hex_sum = hex(sum([int(i, 16) for i in hex_nums.keys()]))
+res_sum = list(str(hex_sum)[2:].upper())
+int_prod = 1
+for i in hex_nums.keys():
+    int_prod *= int(i, 16)
+hex_prod = hex(int_prod)
+res_prod = list(str(hex_prod)[2:].upper())
+print('Сумма чисел: ', res_sum)
+print('Произведение: ', res_prod)
+
+
+# 2)
+class HexNum:
+
+    def __init__(self, a_str):
+        self.arr = list(a_str.upper())
+        self.int = int(''.join(self.arr), 16)
+
+    def __str__(self):
+        return str(self.arr)
+
+    def __add__(self, other):
+        hex_sum = hex(self.int + other.int)
+        str_sum = str(hex_sum)[2:]
+        return HexNum(str_sum)
+
+    def __mul__(self, other):
+        hex_sum = hex(self.int * other.int)
+        str_sum = str(hex_sum)[2:]
+        return HexNum(str_sum)
+
+
+hex_1 = HexNum(input('Введите первое шестнадцатиричное число: '))
+hex_2 = HexNum(input('Введите второе шестнадцатиричное число: '))
+print('Сумма чисел: ', hex_1 + hex_2)
+print('Произведение: ', hex_1 * hex_2)
