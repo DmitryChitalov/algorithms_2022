2965b0d5ca6f59291b8167415ddd507e45566472
slade <paran87@mail.ru>
2022-09-19 00:51:13 +0300

    Задание 2.2

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,43 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from collections import Counter
+import random
+import timeit
+
+
+def random_mass(num: int):
+    mas = []
+    for i in range(num * 2 + 1):
+        mas.append(int(random.uniform(-100, 100)))
+    return mas
+
+
+def my_mode(mass):
+    c = Counter(mass)
+    return [k for k, v in c.items() if v == c.most_common(1)[0][1]]
+
+
+i = 10
+mas = my_mode(random_mass(i))
+print("Медиана равна", mas)
+per = """
+mas = my_mode(random_mass(i))
+"""
+print(timeit.timeit(setup='', stmt=per, number=10000, globals=globals()))
+
+i = 100
+mas = my_mode(random_mass(i))
+print("Медиана равна", mas)
+per = """
+mas = my_mode(random_mass(i))
+"""
+print(timeit.timeit(setup='', stmt=per, number=10000, globals=globals()))
+
+i = 1000
+mas = my_mode(random_mass(i))
+print("Медиана равна", mas)
+per = """
+mas = my_mode(random_mass(i))
+"""
+print(timeit.timeit(setup='', stmt=per, number=100, globals=globals()))
