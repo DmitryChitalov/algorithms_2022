b630042c37fade19b7000153da192f3299044562
geekbrains <info@geekbrains.ru>
2022-09-22 19:21:33 +0300

    Lesson_7

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,29 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+
+from random import randint
+from timeit import timeit
+from statistics import median
+
+# 10 элементов
+m = 5
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit('median(orig_list[:])', globals=globals(), number=1000))   # 0.0007430999539792538
+print(f'Медиана - {median(orig_list[:])}')                              # Медиана - 23
+
+# 100 элементов
+m = 50
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit('median(orig_list[:])', globals=globals(), number=1000))   # 0.004520399961620569
+print(f'Медиана - {median(orig_list[:])}')                              # Медиана - 4
+
+# 1000 элементов
+m = 500
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit('median(orig_list[:])', globals=globals(), number=1000))   # 0.08666729996912181
+print(f'Медиана - {median(orig_list[:])}')                              # Медиана - -2
+
+"""
+Эффективнее оказался способ с использованием встроенной функции median из модуля statistics
+"""
