9fde9face11e7d288554f60971986ccfcebe2689
JulieXie1111 <titovayulia96@yandex.com>
2022-09-24 17:56:27 +1000

    Lesson_1
    В принципе разобралась. Что было не понятно подсмотрела и построчно все разобрала.

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -13,3 +13,48 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --код с нуля писать не нужно, требуется доработать пример с урока
 """
+
+
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def clear(self):
+        return self.items == []
+
+    def add_to_front(self, el):
+        self.items.append(el)
+
+    def add_to_rear(self, el):
+        self.items.insert(0, el)
+
+    def remove_front(self):
+        self.items.pop()
+
+    def remove_rear(self):
+        self.items.pop(0)
+
+    def item_count(self):
+        return len(self.items)
+
+
+def pal_checker(string):
+    dc_obj = Deque()
+    new_str = string.replace(' ', '')
+    for el in new_str:
+        dc_obj.add_to_rear(el)
+
+    is_equal = True
+
+    while dc_obj.item_count() > 1 and is_equal:
+
+        first = dc_obj.remove_front()
+        last = dc_obj.remove_rear()
+
+        if first != last:
+            is_equal = False
+
+        return is_equal
+
+
+print(pal_checker('молоко делили ледоколом'))
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -6,6 +6,35 @@
 Вам нужно обнаружить проблему в процессе этого. Но проблема связана не с тем,
 что рекурсия потребляет много памяти, а с самим процессом замеров.
 
-Опищите эту проблему и найдите простой путь ее решения.
+Опишите эту проблему и найдите простой путь ее решения.
 Опишите этот путь и покажите его применение
 """
+
+# Сложность O(n)
+
+some_list = [15, 10, 3, 6, 5, 7, 2]
+
+
+def min_val(lst):
+    min_value = lst[0]
+    for i in lst:
+        if i < min_value:
+            min_value = i
+    return min_value
+
+
+# O(n^2).
+
+def min_val2(lst):
+    lst_length = len(lst)
+    for i in range(0, lst_length):
+        for j in range(0, lst_length - 1):
+            if lst[j] > lst[j + 1]:
+                temp = lst[j]
+                lst[j] = lst[j + 1]
+                lst[j + 1] = temp
+
+    return lst[0]
+
+
+print(min_val2(some_list))
