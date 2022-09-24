06d5097d84643b8de95e0f2126ee738f50403cf3
User <ushakov.ip@yandex.ru>
2022-09-24 16:00:22 +0300

    dz_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,32 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+import random
+
+
+def bubble_sort(lst):
+    n = 1
+
+    while n < len(lst):
+        count = 0
+
+        for i in range(len(lst) - 1 - (n - 1)):
+
+            if lst[i] < lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                count += 1
+
+        if count == 0:
+            break
+
+        n += 1
+
+
+SIZE = 10
+MIN_ITEM = -100
+MAX_ITEM = 99
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+
+print('Массив:', array, sep='\n')
+bubble_sort(array)
+print('После сортировки:', array, sep='\n')
