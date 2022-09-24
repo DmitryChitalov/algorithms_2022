a6ba19756cc38934f56ffdebf4d025838827ef32
Shubin_Ostap <mistrun@yandex.ru>
2022-09-24 20:35:48 +0300

    Shubin_lesson_7
    Добавил аналитику

--- Урок 7. Практическое задание/test.py
+++ Урок 7. Практическое задание/test.py
@@ -1,15 +1,2 @@
-def bubble_sort(lst_obj):
-    n = 1
-    while n < len(lst_obj):
-        for i in range(len(lst_obj)-n):
-            if lst_obj[i] > lst_obj[i+1]:
-                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
-        n += 1
-    return f'{orig_list} - отсортированный список'
+numbers_list = [-93, -87, -61, -23, -72, -2, 36, -83, 13, 90, -99, -69, -56, -95, 52, -74, 80, 33, -42, -81, -37]
 
-
-orig_list = [randint(-100, 100) for i in range(10)]
-print(f'{orig_list} - исходный список')
-print(bubble_sort(orig_list))
-time_func = timeit("bubble_sort(orig_list[:])", globals=globals(), number=1000)
-print(f'{time_func} - время выполнения функции')
