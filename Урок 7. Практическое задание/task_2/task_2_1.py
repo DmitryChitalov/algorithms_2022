674b2aac5fa89c804cf197a56eb0a3ae5db3e7dc
kimalio <kimalio@proton.me>
2022-09-25 21:03:24 +0400

    Lesson_07

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,53 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+
+# гномья сортировка
+def dwarf_sort(lst):
+    n, i = len(lst), 0
+    while i + 1 < n:
+        if lst[i + 1] >= lst[i]:
+            i += 1
+        else:
+            lst[i], lst[i + 1] = lst[i + 1], lst[i]
+            if i > 0:
+                i -= 1
+            else:
+                i += 1
+    return lst
+
+
+# 10
+m = 5
+my_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(dwarf_sort(my_list[:]))
+print(f'{timeit("dwarf_sort(my_list[:])", globals=globals(), number=100)} сек.')
+print(f'Медиана (10): {dwarf_sort(my_list[:])[m]}')
+print('_' * 50)
+
+# 100
+m = 50
+my_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(dwarf_sort(my_list[:]))
+print(f'{timeit("dwarf_sort(my_list[:])", globals=globals(), number=100)} сек.')
+print(f'Медиана (100): {dwarf_sort(my_list[:])[m]}')
+print('_' * 50)
+
+# 1000
+m = 500
+my_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(dwarf_sort(my_list[:]))
+print(f'{timeit("dwarf_sort(my_list[:])", globals=globals(), number=100)} сек.')
+print(f'Медиана (1000): {dwarf_sort(my_list[:])[m]}')
+
+
+# 0.0007526249999999998 сек.
+# 0.053056959 сек.
+# 6.158973792 сек.
+
+
+
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,40 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+
+def find_median(lst):
+    arr = lst.copy()
+    while len(arr) > len(lst) // 2 + 1:
+        arr.remove(max(arr))
+    return max(arr)
+
+
+# 10
+m = 5
+my_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'{timeit("find_median(my_list[:])", globals=globals(), number=100)} сек.')
+print(f'Медиана (10): {find_median(my_list[:])}')
+print('_' * 50)
+
+# 100
+m = 50
+my_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'{timeit("find_median(my_list[:])", globals=globals(), number=100)} сек.')
+print(f'Медиана (100): {find_median(my_list[:])}')
+print('_' * 50)
+
+# 1000
+m = 500
+my_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'{timeit("find_median(my_list[:])", globals=globals(), number=100)} сек.')
+print(f'Медиана (1000): {find_median(my_list[:])}')
+
+# 0.00026887500000000175 сек.
+# 0.009896958 сек.
+# 0.594641875 сек.
+
+# отнсительно неплохо показывает себя на больших списках
