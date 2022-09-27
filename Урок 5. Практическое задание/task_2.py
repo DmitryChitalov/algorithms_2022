cc8347aafd7d60f4cccea41ad821e47ead168f47
Elena Konstantinova <elekonst1@gmail.com>
2022-09-27 18:13:14 -0400

    Интересные задания

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,24 @@ reduce
 __mul__
 __add__
 """
+
+class HexOperation:
+    def __init__(self, numb_1, numb_2):
+        self.numb_1 = numb_1
+        self.numb_2 = numb_2
+    
+    def __add__(self, other):
+        return list(hex(int(''.join(self.numb_1), 16) + int(''.join(other.numb_2), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.numb_1), 16) * int(''.join(other.numb_2), 16)))[2:]
+
+hex_num_1 = list(input("Введите первое шестнадцатеричное число: "))
+hex_num_2 = list(input("Введите второе шестнадцатеричное число: "))
+
+sum_hex_num = HexOperation(hex_num_1, hex_num_2) + HexOperation(hex_num_1, hex_num_2)
+
+mult_hex_num = HexOperation(hex_num_1, hex_num_2) * HexOperation(hex_num_1, hex_num_2)
+
+print(f'Сумма числел равно: {sum_hex_num}')
+print(f'Произведение числел равно: {mult_hex_num}')
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,56 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+from collections import defaultdict
+
+from timeit import timeit
+
+
+regular_dict = {}
+for i in range(100):
+    regular_dict[i] = i + 1
+
+
+
+def_dict = defaultdict(int)
+for i in range(100):
+    def_dict[i] = i + 1
+
+
+
+def copy_dict(dictionary):
+    return dictionary.copy()
+
+print(timeit('copy_dict(regular_dict.copy())', globals=globals(), number=100))
+print(timeit('copy_dict(def_dict.copy())', globals=globals(), number=100))
+
+#0.00030017100000000185
+#0.0014416599999999974
+
+def dict_items(dictionary):
+    return dictionary.items()
+
+print(timeit('dict_items(regular_dict.copy())', globals=globals(), number=100))
+print(timeit('dict_items(def_dict.copy())', globals=globals(), number=100))
+
+
+#0.0001515850000000027
+#0.0009232679999999979
+
+def changing_dict(dictionary):
+    for i in range(len(dictionary)):
+        dictionary[i] = i + 10
+    return dictionary
+
+print(timeit('changing_dict(regular_dict.copy())', globals=globals(), number=100))
+print(timeit('changing_dict(def_dict.copy())', globals=globals(), number=100))
+
+#0.0010771580000000017
+#0.0023695339999999995
+
+"""
+При операциях создания копии, получения пары значений ключ значение и изменения 
+значений в словаре, обычный словарь работает быстрее чем defaultdict(), поэтому 
+его использование для стандартных операций со словарем не имеет смысла.
+"""
