dedbd373f7b1525be47cea72ed06f98e0527b0f7
Norovyatkin Ilya <dragon.nest9821@gmail.com>
2022-09-29 16:17:06 +0600

    Complete task_4

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,58 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+def access_1(my_dict):
+    """Функция обеспечивает проверку возможности допуска пользователя к ресурсу.
+    Решение 1:
+    Задаем условие проверки данных о пользователе.
+    Если оно выполняется, пользователь
+    получает доступ к ресурсу.
+    Сложность: O(1).
+    """
+    if my_dict['activation'] == 'True':  # O(1)
+        return (f'User {my_dict["login"]} is admitted to the resource.')  # O(1)
+    return (f'User {my_dict["login"]} is not allowed to access the resource. Get authenticated.')  # O(1)
+
+
+user_1 = {'login': 'Nancy', 'pass': 123, 'activation': 'False'}
+user_2 = {'login': 'Kim', 'pass': 456, 'activation': 'True'}
+
+print(access_1(user_1))
+print(access_1(user_2))
+
+
+def access_2(my_dict):
+    """Функция обеспечивает проверку возможности допуска
+    пользователя к ресурсу.
+    Решение 2:
+    Просим пользователя ввести логин и пароль.
+    Проверяем введёные пользователем данные.
+    Если данные совпадают с информацией в словаре,
+    пользователь получает доступ к ресурсу.
+    Сложность: O(n).
+    """
+    for key, value in my_dict.items():  # O(n)
+        login_user = input('Введите имя: ')
+        password_user = int(input('Введите пароль: '))
+        if key == login_user:  # O(1)
+            if value['pass'] == password_user and value['activation'] == 'True':  # O(1)
+                return (f'User {login_user} is admitted to the resource.')
+            elif value['pass'] == password_user and value['activation'] != "True":  # O(1)
+                return (f'User {login_user} is not allowed to access the resource. Get authenticated.')
+            elif value['pass'] != password_user:  # O(1)
+                return "Invalid password. Try again"
+        return (f'User {login_user} is not found')  # O(1)
+
+
+user_3 = {'Michael': {'pass': 789, 'activation': 'False'}}
+user_4 = {'Larry': {'pass': 321, 'activation': 'True'}}
+
+print(access_2(user_3))
+print(access_2(user_4))
+
+"""
+Решение 1 эффективнее. Так как мы не производим итерацию словаря.
+Так же у решения 1 темп роста количества операцй для произведения сортировки O(1), он меньше чем у решения №2 O(n).
+"""
