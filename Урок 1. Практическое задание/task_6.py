e1d5ddba6bd471cbf4b4aba79c1f7a552440604c
Norovyatkin Ilya <dragon.nest9821@gmail.com>
2022-09-29 16:22:36 +0600

    Complete task_5.6.7

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,47 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class TaskBoard:
+    def __init__(self):
+        self.elems = []
+
+    def __str__(self):
+        if not self.elems:
+            return 'Все задания сделаны.'
+        else:
+            return f'Список дел: {self.elems}.'
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_task(self, task):
+        self.elems.insert(0, task)
+
+    def del_task(self):
+        if self.elems:
+            task = self.elems.pop()
+            print(f'Задание "{task}" сделано.')
+        else:
+            print('Заданий больше нет')
+
+    def size(self):
+        return len(self.elems)
+
+
+if __name__ == '__main__':
+    to_do_list = TaskBoard()
+    to_do_list.add_task('Поесть')
+    to_do_list.add_task('Вынести мусор')
+    print(to_do_list)
+    to_do_list.del_task()
+    to_do_list.add_task('Почистить зубы')
+    to_do_list.del_task()
+    to_do_list.del_task()
+    print(to_do_list)
+    to_do_list.add_task('Найти работу')
+    print(to_do_list)
+    to_do_list.del_task()
+    to_do_list.del_task()
+    print(to_do_list)
