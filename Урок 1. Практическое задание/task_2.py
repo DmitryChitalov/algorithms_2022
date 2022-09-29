0b79b082798cc9f89c0afa396482f3853c3d8471
Konstantin Zaytsev <tarans.fishborn@gmail.com>
2022-09-29 13:00:23 +0300

    Домашняя работа №1. Зайцев К.В.

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,36 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+from random import randint
+
+
+def search_min(val):
+    """
+    O(n^2).
+    """
+    for el in val:               # O(n)
+        is_min = True            # O(1)
+        for elem in val:         # O(n)
+            if elem < el:        # O(1)
+                is_min = False   # O(1)
+        if is_min:               # O(1)
+            return el
+    return 0
+
+
+def find_min_2(val):
+    """
+    O(n).
+    """
+    min_value = val[0]          # O(1)
+    for elem in val:            # O(n)
+        if min_value > elem:    # O(1)
+            min_value = elem    # O(1)
+    return min_value            # O(1)
+
+
+my_list = [randint(0, 20) for i in range(20)]
+print(my_list)
+print(search_min(my_list))
+print(search_min_2(my_list))
--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -13,3 +13,46 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --код с нуля писать не нужно, требуется доработать пример с урока
 """
+
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_back(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_back(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+def check(string):
+    dc = DequeClass()
+    replace_string = string.replace(' ', '')
+    for el in replace_string:
+        dc.add_to_back(el)
+
+    still_equal = True
+
+    while dc.size() > 1 and still_equal:
+        first = dc.remove_from_front()
+        last = dc.remove_from_back()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+
+print(check("молоко делили ледоколом"))
