17caab3e2eb286298c5affab9e02ea9c635eed9a
Zhanna Nizhegorodova <105349123+ZhannaNizhegorodova@users.noreply.github.com>
2022-09-30 17:18:16 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,47 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+companies = {"apple": 3500,
+             "google": 2000,
+             "yandex": 1500,
+             "amazon": 3000,
+             "vk": 1900
+
+             }
+
+
+# Способ 1
+# Сложность: O(n^2) — квадратичное время
+
+def search_the_richest(user_dict):
+    list_values = sorted(companies.values(), reverse=True)[:3]  # O(n Log n)
+    new_dict = {}  # O(1)
+    for el in list_values:  # O(n)
+        for k, v in companies.items():  # O(n)
+            if v == el:  # O(1)
+                new_dict[k] = v  # O(1)
+    return new_dict  # O(1)
+
+
+print(search_the_richest(companies))
+
+
+# Способ 2
+# Сложность: O(n*log n) — линейно-логарифмическое время
+
+def search_the_richest_2(user_dict):
+    sorted_companies = sorted(companies, key=companies.get, reverse=True)[:3]  # O(n*log n)
+    new_dict = {}  # O(1)
+    for i in sorted_companies:  # O(1)
+        new_dict[i] = companies.get(i)  # O(1)
+    return new_dict  # O(1)
+
+
+print(search_the_richest_2(companies))
+
+'''
+Второй способ решения эффективнее, 
+т.к. при сложности O(n*log n) с ростом количества элементов время выполнения растет не так быстро, 
+как при сложности O(n^2).
+'''
+
