050d759b93d63bed67905e727fd6023a004ac952
ecrvmal <ecrvmal@yandex.ru>
2022-10-01 00:05:56 +0300

    Algorithms_Vlad_Mal_Lesson_1_Commit_2

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -37,10 +37,10 @@ def check_2(lst_obj):
     что такой элемент отстутствует
     в оставшихся справа элементах
 
-    Сложность:                             O(N)
+    Сложность:                             O(N^2)
     """
     for j in range(len(lst_obj)):          # O(N)
-        if lst_obj[j] in lst_obj[j+1:]:    # O(1)
+        if lst_obj[j] in lst_obj[j+1:]:    # O(N)
             return False                   # O(1)
     return True                            # O(1)
 
