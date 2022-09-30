ac5139e44604470158f718cc3a05874de14c62ff
Kamdiser <103354155+Kamdiser@users.noreply.github.com>
2022-09-30 15:55:27 +0300

    Add files via upload

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,19 +1,45 @@
 """
 Задание 5. На закрепление навыков работы со стеком
-
 Реализуйте собственный класс-структуру "стопка тарелок".
-
 Мы можем складывать тарелки в стопку и при превышении некоторого значения
 нужно начать складывать тарелки в новую стопку.
-
 Структура должна предусматривать наличие нескольких стопок.
 Создание новой стопки происходит при достижении предыдущим
 стеком порогового значения.
-
 После реализации структуры, проверьте ее работу на различных сценариях.
-
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --реализуйте по аналогии с примером, рассмотренным на уроке
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+
+
+class StackPlates:
+    def __init__(self, vol_stack):
+        self.vol_stack = vol_stack
+        self.stack = []
+
+    def add_stack(self, vol, varibl):
+        if len(self.stack) == 0:
+            self.stack.append(list(str(varibl)))
+            vol = vol-1
+        for i in range(1, vol+1):
+            stack = self.stack.pop()
+            if len(list(stack)) == self.vol_stack:
+                self.stack.append(stack)
+                self.stack.append(list(str(varibl)))
+            if len(list(stack)) < self.vol_stack:
+                stack.append(str(varibl))
+                self.stack.append(stack)
+
+
+if __name__ == '__main__':
+    # Создаем стек с размером стопки
+    a = StackPlates(5)
+    # добавляем количество тарелок - vol и тип тарелки - varibl
+    a.add_stack(6, 8)
+    a.add_stack(6, 2)
+    a.add_stack(3, 256)
+    a.add_stack(5, 1)
+
+    print(a.stack)
