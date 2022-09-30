479b3cf0e90ad74707b736fb0ebc42e9df0fa22d
Zhanna Nizhegorodova <105349123+ZhannaNizhegorodova@users.noreply.github.com>
2022-09-30 17:17:37 +0300

    Update task_2.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,31 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+first_list = [100, 50, 3, 4, 23, 10]
+
+
+# Сложность: O(n^2) два уровня вложенности
+
+def min_number(user_list):
+    min_el = user_list[0]
+    for i in user_list:  # O(n)
+        for j in range(len(user_list)):  # O(n)
+            if min_el > user_list[j]:  # O(1)
+                min_el = user_list[j]  # O(1)
+    return min_el  # O(1)
+
+
+# Сложность: O(n) линейное время - чем больше элементов, тем дольше выполняется поиск
+
+def min_number_2(user_list):
+    min_number = user_list[0]  # O(1)
+    for i in range(len(user_list)):  # O(n)
+        if user_list[i] < min_number:  # O(1)
+            min_number = user_list[i]  # O(1)
+    return min_number  # O(1)
+
+
+print(min_number(first_list))
+
+print(min_number_2(first_list))
