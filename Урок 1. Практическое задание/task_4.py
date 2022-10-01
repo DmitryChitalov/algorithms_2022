0f78521b6a5d052390a293e2060b4fad2d2682eb
vmrcx <hlh7520@gmail.com>
2022-10-01 17:35:43 +0300

    task_4

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,52 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+users = [
+    {'Login': 'atkhnn', 'Password': '1230', 'is_activated': 'True'},
+    {'Login': 'ektiv', 'Password': '8901', 'is_activated': 'True'},
+    {'Login': 'cvtr', 'Password': '5678', 'is_activated': 'True'},
+    {'Login': 'vikishow', 'Password': '2345', 'is_activated': 'False'},
+    {'Login': 'wedro', 'Password': '7890', 'is_activated': 'False'},
+    {'Login': 'orfil', 'Password': '4567', 'is_activated': 'True'},
+    {'Login': 'kirabell', 'Password': '1234', 'is_activated': 'False'}
+]
+
+login = []
+password = []
+is_activated = []
+for i in users:
+    login.append(i['Login'])
+    password.append(i['Password'])
+    is_activated.append(i['is_activated'])
+
+#Сложность: O(n)
+
+def authentication(login, password):
+    for i in users:                                                                    #O(n)
+        if i.get('Login') == login:                                                    #O(1)
+            if i.get('Password') == password:                                          #O(1)
+                if i.get('is_activated') == 'True':                                    #O(1)
+                    return f'Доступ разрешен'                                          #O(1)
+                else:
+                    return f'Ваш аккаунт не активирован. Хотите пройти активацию?'     #O(1)
+            else:
+                return f'Введите корректный пароль'                                    #O(1)
+
+print(authentication(login[0], password[2]))
+
+#Сложность: O(n^2)
+
+def authentication_2(login, password):
+    for i in login:                                                                                 #O(n)
+        for t in password:                                                                          #O(n)
+            if login.index(i) == password.index(t) and is_activated[login.index(i)] == 'True':      #O(1)
+                return f'Доступ разрешен'                                                           #O(1)
+            elif login.index(i) == password.index(t) and is_activated[login.index(i)] == 'False':   #O(1)
+                return f'Ваш аккаунт не активирован. Хотите пройти активацию?'                      #O(1)
+            elif login.index(i) != password.index(t):                                               #O(1)
+                return f'Введите корректный пароль'                                                 #O(1)
+
+print(authentication_2(login[3], password[3]))
+
+#Вывод: эффективнее будет использовать первое решение, так как первое решение - линейная сложность, а второе - квадратичная
