6cd1020eb51ffc63b5132f8867d73257148acbb8
vmrcx <hlh7520@gmail.com>
2022-10-01 17:35:33 +0300

    task_3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,47 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+# Сложность: O(n^2)
+
+companies = {
+    'company_1': 1000005,
+    'company_2': 345873,
+    'company_3': 4000150,
+    'company_4': 105000,
+    'company_5': 4356791,
+    'company_6': 113459}
+
+list_income = list(companies.values())           # O(n)
+list_income.sort(reverse=True)                   # O(n log n)
+
+
+def get_key(*args):
+    keys = []                                        # O(1)
+    for arg in args:                                 # O(n)
+        for key, value in companies.items():         # O(n)
+            if arg == value:                         # O(1)
+                keys.append(key)                     # O(1)
+    return keys
+
+best_companies = list_income[0:3]
+top_companies = get_key(*best_companies)
+print(top_companies)
+
+
+# Сложность: O(n log n)
+
+list_income = list(companies.values())         # O(n)
+list_income.sort(reverse=True)                 # O(n log n)
+
+
+def get_key(val):
+    for key, value in companies.items():         # O(n)
+        if val == value:                         # O(1)
+            return key
+
+top_1, top_2, top_3 = get_key(list_income[0]), get_key(list_income[1]), get_key(list_income[2])
+
+print(top_1, top_2, top_3, sep=', ')
+
+# Вывод: эффективнее будет второй вариант решения, так как линейно-логарифмическая сложность требует меньше времени на выполнение, чем квадратичная.
+
