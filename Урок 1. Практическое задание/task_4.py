dd6ac9cf1c239570af364fca6ce07493d4d730aa
Evgeniy Doroshenko <kraspy@yandex.ru>
2022-10-02 21:53:46 +0700

    task_4 complete

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -47,16 +47,33 @@ users = {
 }
 
 
-def auth_0(users, name, password):
+def auth_0(users, name, password): # O(1)
     if users.get(name):
         if users[name]['pass'] == password and users[name]['active']:        # O(1)
-            return 'Пользователь авторизован'                                # O(1)
+            return 'Пользователь "{name}" авторизован'                                # O(1)
         elif users[name]['pass'] == password and not users[name]['active']:  # O(1)
-            return 'Пользователь не активен'                                 # O(1)
+            return 'Пользователь "{name}" не активен'                                 # O(1)
         elif users[name]['pass'] != password:                                # O(1)
-            return 'Неправильный пароль'
-    else:
-        return f'{name} does not exist'
+            return '"{name}": Неправильный пароль'                                     # O(1)
+        else:
+            return f'Пользователь "{name}" не найден'                        # O(1)
 
 
+def auth_1 (users, name, password): # O(N)
+    for user, status in users.items():                                       # O(N)
+        if user == name:                                                     # O(1)
+            if status['pass'] == password and status['active']:              # O(1)
+                return f'Пользователь "{name}" авторизован'                           # O(1)
+            elif status['pass'] == password and not status['active']:        # O(1)
+                return f'Пользователь "{name}" не активен'                            # O(1)
+            elif status['pass'] != password:                                 # O(1)
+                return f'"{name}": Неправильный пароль'                                # O(1)
+            else:
+                return f'Пользователь "{name}" не найден'                    # O(1)
+
+print(auth_0(users, 'user_0', '123'))
 print(auth_0(users, 'user_2', '789'))
+print(auth_0(users, 'user_3', '777'))
+print(auth_1(users, 'user_0', '123'))
+print(auth_1(users, 'user_2', '789'))
+print(auth_1(users, 'user_3', '777'))
