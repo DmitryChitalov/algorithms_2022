f8d47b77935ef82cca6bdb74786c23f2272591d6
RozhkovaDarya <102472078+RozhkovaDarya@users.noreply.github.com>
2022-10-02 18:17:15 +0200

    Update task_5.py

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,49 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+public class Stack
+{
+    LinkedList _items = new LinkedList();
+    public void Push(T value)
+    {
+        throw new NotImplementedException();
+    }
+    public T Pop()
+    {
+        throw new NotImplementedException();
+    }
+    public T Peek()
+    {
+        throw new NotImplementedException();
+    }
+    public int Count
+    {
+        get;
+    }
+}
+
+
+public void Push(T value)
+{
+    _items.AddLast(value);
+}
+public T Pop()
+{
+    if (_items.Count == 0)
+    {
+        throw new InvalidOperationException("The stack is empty");
+    }
+    T result = _items.Tail.Value;
+    _items.RemoveLast();
+    return result;
+}
+# Сложность: O(1)
+
+public int Count
+{
+    get
+    {
+        return _items.Count;
+    }
+}
+# Сложность: O(1)
