fd0f4d30fb4bdd5069f38d0f94e4ac3afc048628
leshkovia <leshkovia@gmail.com>
2022-10-02 20:26:33 +0300

    task 6 is complete!

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,27 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+import random
+
+random_number = round(random.random() * 100)
+
+
+def mystery(data):
+    global tries
+    user_choice = int(input(f"Угадай число от 0 до 100 (У тебя {10 - tries} попыток)? >>> "))
+    if tries == 10:
+        print(f"Проигрыш! Наше число >>> {data}")
+        exit()
+    elif user_choice > data:
+        print("Слишком много")
+    elif user_choice < data:
+        print("Слишком мало")
+    else:
+        print(f"Победа! И впрямь {data} ;)")
+        exit()
+    tries += 1
+    return mystery(data)
+
+
+tries = 0
+mystery(random_number)
