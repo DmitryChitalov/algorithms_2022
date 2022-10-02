700b2a4e8ce5f18640a401281e305185c5d34493
Belyakov Roman <belyakov1981@mail.ru>
2022-10-02 13:09:46 +0300

    done_task_1

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -27,3 +27,39 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+def calculations():
+
+    operator = input('Введите арифметический оператор или q для выхода: ')
+
+    if operator == 'q':
+        print('Вычисления завершены!')
+        return exit(0)
+    elif operator != '+' and operator != '-' and operator != '*' and operator != '/':
+        print('Введите +, -, * или /')
+        calculations()
+
+    try:
+        a, b = float(input('Введите число a: ')), float(input('Введите число b: '))
+
+        if operator == '+':
+            print(f'Сумма чисел {a} и {b} равна {a + b}')
+            calculations()
+        elif operator == '-':
+            print(f'Разность чисел {a} и {b} равна {a - b}')
+            calculations()
+        elif operator == '*':
+            print(f'Произведение чисел {a} и {b} равно {a * b}')
+            calculations()
+        elif operator == '/':
+            try:
+                print(f'Частное от деления чисел {a} и {b} равно {a / b}')
+                calculations()
+            except ZeroDivisionError:
+                print('На ноль делить нельзя!')
+                calculations()
+    except ValueError:
+        print('Введите числа!')
+        calculations()
+
+calculations()
