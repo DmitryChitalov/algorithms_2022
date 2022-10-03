b20bd228331dd5bcc2b419f90b44b076754f5cb0
daniil_dn <dosindaniilm@gmail.com>
2022-10-03 21:43:15 +0300

    hw7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -38,14 +38,14 @@ def mid_find(lst_in):
     if ln % 2 == 0:
         first = lst_in[ln // 2 - 1]
         second = lst_in[ln // 2]
-        med = (first + second) / 2
+        return (first + second) / 2
     else:
-        med = lst_in[ln // 2]
+        return lst_in[ln // 2]
 
 
 orig_list_10 = [randint(-100, 100) for _ in range(11)]
 orig_list_100 = [randint(-100, 100) for _ in range(101)]
 orig_list_1000 = [randint(-100, 100) for _ in range(1001)]
-print(timeit.timeit(stmt='mid_find(orig_list_10)', globals=globals()))
-print(timeit.timeit(stmt='mid_find(orig_list_100)', globals=globals()))
-print(timeit.timeit(stmt='mid_find(orig_list_1000)', globals=globals()))
+print(timeit.timeit(stmt='mid_find(orig_list_10)', globals=globals(), number=10000))
+print(timeit.timeit(stmt='mid_find(orig_list_100)', globals=globals(), number=10000))
+print(timeit.timeit(stmt='mid_find(orig_list_1000)', globals=globals(), number=10000))
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -21,17 +21,14 @@ from statistics import median
 
 def mid_find(lst_in):
     ln = len(lst_in)
-    if ln % 2 == 0:
-        first = lst_in[ln // 2 - 1]
-        second = lst_in[ln // 2]
-        med = (first + second) / 2
-    else:
-        med = lst_in[ln // 2]
+    for _ in range(ln // 2):
+        lst_in.pop(lst_in.index(max(lst_in)))
+    return max(lst_in)
 
 
 orig_list_10 = [randint(-100, 100) for _ in range(11)]
 orig_list_100 = [randint(-100, 100) for _ in range(101)]
 orig_list_1000 = [randint(-100, 100) for _ in range(1001)]
-print(timeit.timeit(stmt='mid_find(orig_list_10)', globals=globals()))
-print(timeit.timeit(stmt='mid_find(orig_list_100)', globals=globals()))
-print(timeit.timeit(stmt='mid_find(orig_list_1000)', globals=globals()))
+print(timeit.timeit(stmt='mid_find(orig_list_10)', globals=globals(), number=10000))
+print(timeit.timeit(stmt='mid_find(orig_list_100)', globals=globals(), number=10000))
+print(timeit.timeit(stmt='mid_find(orig_list_1000)', globals=globals(), number=10000))
