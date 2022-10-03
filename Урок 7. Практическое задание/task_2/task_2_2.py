16212026fe1f41d98fdf6a11d5dec2d7a381e969
Still190494 <Still190494@ya.ru>
2022-10-04 00:09:03 +0300

    lesson_7

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,61 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+
+from random import randint
+from timeit import timeit
+
+
+def median_search(seq):
+    new_list = seq
+    for _ in range(len(orig_list) // 2):
+        new_list.remove(max(new_list))
+    return max(new_list)
+
+
+m = 10
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'Медиана :{median_search(orig_list)}')
+# замеры 10
+print(
+    timeit(
+        "median_search(orig_list[:])",
+        globals=globals(),
+        number=1000))
+m = 100
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'Медиана :{median_search(orig_list)}')
+# замеры 100
+print(
+    timeit(
+        "median_search(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+m = 1000
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'Медиана :{median_search(orig_list)}')
+# замеры 1000
+print(
+    timeit(
+        "median_search(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+"""
+
+Результаты тестов.
+(m = 10)
+0.0003142000059597194
+(m = 100)
+0.00037460000021383166
+(m = 1000)
+0.006531800027005374
+
+Исходя из того, что было сказано на уроке.., вроде, правильно, удаляются максимальные элементы до m и
+медианой становится максимальный из оставшихся.
+Но не понятно почему медианой получаются абсолютно разные по индексам элементы...ну то есть из оставшегося списка
+бывало, что и [0] или [-1] элемент попадался медианой когда принтами покрывал.
+
+"""
