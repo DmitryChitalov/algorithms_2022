5db16470a52c58fc4a751f19830f388d86a62231
vmrcx <hlh7520@gmail.com>
2022-10-03 21:34:15 +0300

    task6

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,26 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+from random import randint
+
+
+def guessing_game(count, random_number):
+
+    print(f'Попытка №{count}')
+    user_number = int(input('Введите число от 0 до 100:'))
+
+    if user_number == random_number or count == 10:
+        if user_number == random_number:
+            print('Победа!')
+        else:
+            print('Проигрыш')
+    else:
+        count += 1
+        if user_number > random_number:
+            print('Меньше!')
+        else:
+            print('Больше!')
+        return guessing_game(count, random_number)
+
+guessing_game(1, randint(0, 100))
