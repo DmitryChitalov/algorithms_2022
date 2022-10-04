0fe17e3a25322e30d63d471e395bd9fe90a6bee8
Belyakov Roman <belyakov1981@mail.ru>
2022-10-04 23:20:26 +0300

    update_task_1

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -35,31 +35,34 @@ def calculations():
     if operator == 'q':
         print('Вычисления завершены!')
         return exit(0)
-    elif operator != '+' and operator != '-' and operator != '*' and operator != '/':
-        print('Введите +, -, * или /')
-        calculations()
+    else:
+        if operator in '+-*/':
 
-    try:
-        a, b = float(input('Введите число a: ')), float(input('Введите число b: '))
-
-        if operator == '+':
-            print(f'Сумма чисел {a} и {b} равна {a + b}')
-            calculations()
-        elif operator == '-':
-            print(f'Разность чисел {a} и {b} равна {a - b}')
-            calculations()
-        elif operator == '*':
-            print(f'Произведение чисел {a} и {b} равно {a * b}')
-            calculations()
-        elif operator == '/':
             try:
-                print(f'Частное от деления чисел {a} и {b} равно {a / b}')
-                calculations()
-            except ZeroDivisionError:
-                print('На ноль делить нельзя!')
-                calculations()
-    except ValueError:
-        print('Введите числа!')
-        calculations()
+                a, b = float(input('Введите число a: ')), float(input('Введите число b: '))
+
+                if operator == '+':
+                    print(f'Сумма чисел {a} и {b} равна {a + b}')
+                    calculations()
+                elif operator == '-':
+                    print(f'Разность чисел {a} и {b} равна {a - b}')
+                    calculations()
+                elif operator == '*':
+                    print(f'Произведение чисел {a} и {b} равно {a * b}')
+                    calculations()
+                elif operator == '/':
+                    try:
+                        print(f'Частное от деления чисел {a} и {b} равно {a / b}')
+                    except ZeroDivisionError:
+                        print('На ноль делить нельзя!')
+                    finally:
+                       return calculations()
+            except ValueError:
+                print('Введите числа!')
+                return calculations()
+
+        else:
+            print('Введите +, -, * или /')
+            return calculations()
 
 calculations()
