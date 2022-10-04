fb5db4f860f785186f142f3bc007f89fd8584af4
yourgod <m.xpym@mail.ru>
2022-10-04 20:43:05 +0300

    Решено 5-6

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,53 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+class QueueClass:
+    def __init__(self):
+        self.elem = []
+
+    def is_empty(self):
+        return self.elem == []
+
+    def to_q(self, item):
+        self.elem.insert(0, item)
+
+    def from_queue(self):
+        return self.elem.pop()
+
+    def size(self):
+        return len(self.elem)
+
+
+class BoardTasks:
+    def __init__(self):
+        self.base_q = QueueClass()
+        self.rev_q = QueueClass()
+        self.solved_task = []
+
+    def task_solving(self):
+        task = self.base_q.from_queue()
+        self.solved_task.append(task)
+
+    def task_revision(self):
+        task = self.base_q.from_queue()
+        self.rev_q.to_q(task)
+
+    def task_add(self, task):
+        self.base_q.to_q(task)
+
+
+if __name__ == '__main__':
+
+    desk1 = BoardTasks()
+
+    print(desk1.base_q.elem)
+    desk1.task_add('Task1')
+    desk1.task_add('Task2')
+    desk1.task_add('Task3')
+    print(desk1.base_q.elem)
+
+    desk1.task_solving()
+    desk1.task_revision()
+    print(desk1.base_q.elem)
+    print(desk1.rev_q.elem)
+    print(desk1.solved_task)
