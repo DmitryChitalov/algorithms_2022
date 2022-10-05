9f996f315177378cbee8f9a2f00f7520881bc983
Belyakov Roman <belyakov1981@mail.ru>
2022-10-05 23:51:17 +0300

    update_task_1

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -28,3 +28,63 @@ b) получение элемента списка, оцените сложно
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+
+import time
+import random
+
+
+def check_time(func):
+    def wrap(*args):
+        time_start = time.time()
+        ret = func(*args)
+        time_end = time.time()
+        print(func.__name__, time_end - time_start)
+        return ret
+
+    return wrap
+
+list_data = [num for num in range(10000)]
+#print(list_data)
+
+list_ex = []
+dict_ex = {}
+
+# Заполнение списка через append.
+# Сложность O(n).
+@check_time
+def fill_list(list_ex, list_data):
+    for i in list_data:               # O(n))
+        list_ex.append(i)             # O(1)
+
+    return list_ex
+
+# Заполнение списка через insert.
+# Сложность O(n).
+@check_time
+def fill_list_1(list_ex, list_data):
+    for i in list_data:               # O(n))
+        list_ex.insert(0, i)          # O(1)
+
+    return list_ex
+
+# Заполнение словаря через setdefault.
+# Сложность O(n).
+@check_time
+def fill_dict(dict_ex, list_data):
+    for i in range(10000):                        # O(n)
+        dict_ex.setdefault(i, list_data[i])      # O(1)
+
+    return dict_ex
+fill_list(list_ex, list_data)
+# print(list_ex)
+list_ex = []
+fill_list_1(list_ex, list_data)
+# print(list_ex)
+fill_dict(dict_ex, list_data)
+# print(dict_ex)
+
+# Заполнение списка из 10000 элементов через append составило - 0.0007376670837402344.
+# Заполнение списка из 10000 элементов через insert составило - 0.04088926315307617.
+# Заполнение солваря из 10000 элементов через setdefault составило - 0.0019953250885009766.
+# Наиболее быстрый способ заполнения списка через append.
+# И в целом заполнение словаря более длительный процесс в связи с созданием ХЕШ-значения KEYs, примерно в 2 раза больше.
