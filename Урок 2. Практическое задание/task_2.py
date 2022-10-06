0b293296bdea6456a4bec9079cef09e72a91ca7d
RozhkovaDarya <102472078+RozhkovaDarya@users.noreply.github.com>
2022-10-06 20:17:06 +0200

    Update task_2.py

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -17,3 +17,17 @@
 Введите число: 123
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
+def count_numb2(n, even=0, odd=0):
+    if n == 0:
+        print(f"Количество четных и нечетных цифр в числе равно: ({even}, {odd})")
+    else:
+        cur_n = n % 10
+        n = n // 10
+        if cur_n % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+        return count_numb2(n, even, odd)
+
+n = int(input("Введите число: "))
+count_numb2(n)
