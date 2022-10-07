59fa0a74087e2e7510845c3ee21fdf5814e2b3c0
Still190494 <Still190494@ya.ru>
2022-10-07 18:38:01 +0300

    lesson_8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -13,6 +13,18 @@
 """
 
 
+class MyErrorValidation(Exception):
+    def __init__(self, obj_root, child):
+        self.obj_root = obj_root
+        self.child = child
+
+    def __str__(self):
+        if self.obj_root > self.child:
+            return f'{self.child} должен быть в левой ветке'
+        if self.obj_root < self.child:
+            return f'{self.child} должен быть в правой ветке'
+
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -24,8 +36,10 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
+        if new_node >= self.root:
+            raise MyErrorValidation(self.root, new_node)
         # если у узла нет левого потомка
-        if self.left_child == None:
+        if self.left_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
@@ -39,8 +53,10 @@ class BinaryTree:
 
     # добавить правого потомка
     def insert_right(self, new_node):
+        if new_node < self.root:
+            raise MyErrorValidation(self.root, new_node)
         # если у узла нет правого потомка
-        if self.right_child == None:
+        if self.right_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
@@ -69,14 +85,27 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
+r = BinaryTree(10)
 print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
+r.insert_left(1)
+r.insert_left(2)
+r.insert_left(3)
+r.insert_left(4)
+r.insert_left(5)
+r.insert_left(6)
 print(r.get_left_child().get_root_val())
 r.insert_right(12)
-print(r.get_right_child())
+r.insert_right(13)
+r.insert_right(14)
+r.insert_right(15)
+r.insert_right(16)
+r.insert_right(17)
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
 print(r.get_right_child().get_root_val())
+print(r.get_left_child())
+print(r.get_right_child())
+
+# Проверки работоспособности валидации:
+# r.insert_right(7)
+# r.insert_left(18)
