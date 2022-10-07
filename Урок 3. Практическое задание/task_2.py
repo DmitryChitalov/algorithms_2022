5d204d748598ead55cd3e4d9bb7c1e3081708c8b
vmrcx <hlh7520@gmail.com>
2022-10-07 15:35:20 +0300

    task2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,35 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+
+from hashlib import sha256
+import csv
+
+def user_hash():
+    login = input('Введите логин: ')
+    password = input('Введите пароль: ')
+    hash_password = sha256(login.encode() + password.encode()).hexdigest()
+    return login, hash_password
+
+def user_data():
+    login, hash_password = user_hash()
+    print(f'В базе данных хранится строка: {hash_password}')
+    with open("users.csv", mode="w") as w_file:
+        file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+        file_writer.writerow([login, hash_password])
+
+def verification():
+    login, verification_hash = user_hash()
+    with open("users.csv", mode="r") as r_file:
+        file = csv.reader(r_file, delimiter=",")
+        f = 0
+        for n in file:
+            if login == n[0] and verification_hash == n[1]:
+                print('Данные верны, вы вошли в систему')
+                f = 1
+                break
+        if f == 0:
+            print('Пользователь с такими данными не найден')
+
+user_data()
+verification()
