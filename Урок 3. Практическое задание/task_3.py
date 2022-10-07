1ce74ca5281dbcd74cbfe9490b0632b03b558647
vmrcx <hlh7520@gmail.com>
2022-10-07 15:35:25 +0300

    task3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -22,3 +22,15 @@
 р
 а
 """
+import hashlib
+
+def uniqueness(unique_value):
+    len_value = len(unique_value)
+    hash_set = set()
+    for i in range(len_value):
+        for l in range(i + 1, len_value + 1):
+            if unique_value[i:l] != unique_value:
+                hash_set.add(hashlib.sha256(unique_value[i:l].encode('utf-8')).hexdigest())
+    return len(hash_set)
+
+print(uniqueness('papa'))
