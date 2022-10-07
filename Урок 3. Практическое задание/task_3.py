8e101ea3104a7d1525b90d23c1e5802ccc375e5c
Belyakov Roman <belyakov1981@mail.ru>
2022-10-07 18:42:44 +0300

    done_task_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -22,3 +22,29 @@
 р
 а
 """
+
+
+# Напишем функцию определяющую количество уникальных хэш в строке из латинских букв.
+def unique_str(string):
+    for i in range(len(string)):
+        for j in range(i, len(string)):
+            if not string[i:j + 1] == string:
+                uni_str.add(hash(string[i:j + 1]))
+    return f'{string} - {len(uni_str)} уникальных подстрок'
+
+
+uni_str = ''
+uni_str = set(uni_str)
+
+# Проверим работу функции.
+string = 'papa'
+print(unique_str(string))
+print(uni_str)
+
+string = 'tetra'
+print(unique_str(string))
+print(uni_str)
+
+string = 'system'
+print(unique_str(string))
+print(uni_str)
