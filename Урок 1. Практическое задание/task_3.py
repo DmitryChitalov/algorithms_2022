ecf2f8c1fbff63600afbceb4b81ccb3f8f28508c
Viktor_isupov_wk_win10 <v.isupov@mail.ru>
2022-10-07 14:25:25 +0300

    Выполнил ДЗ урок 1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,34 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+company_list = {'Компания1': 9000, 'Компания2': 30000,
+                'Компания3': 8000, 'Компания4': 8100,
+                'Компания5': 5000, 'Компания6': 7000}
+
+# O(N)
+
+def top3(dict_1):
+    list1 = sorted(dict_1.values(), reverse=True)[:3]   # O(log N)
+    top_company = {}                                    # O(1)
+    for key, val in company_list.items():               # O(N)
+        if val in list1:                                # O(N)
+            top_company.update({key:val})               # O(len(..))
+    return top_company                                  # O(1)
+
+print(top3(company_list))
+
+
+# O(N**2)
+
+def top3_2(dict_1):
+    list1 = sorted(dict_1.values(), reverse=True)[:3]   # O(log N)
+    top_company = {}                                    # O(1)
+    for el in list1:                                    # O(N)
+        for key, val in company_list.items():           # O(N)
+            if el == val:                               # O(N)
+                top_company[key] = val                  # O(1)
+    return top_company                                  # O(1)
+
+print(top3_2(company_list))
+
+# Первый способ лучше , руководствуясь натацией О большое
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,87 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+class QueueClass:
+    def __init__(self):
+        self.base = []
+        self.process = []
+        self.finish = []
+
+    def is_empty(self):
+        print("Список задач пуст?: ")
+        return True if (self.base == []) is True \
+                       and (self.process == []) is True else False
+
+    def to_base(self, item):
+        print(f" {item} - Добавлен в очередь: ")
+        self.base.insert(0, item)
+
+    def to_process(self, item):
+        if item in self.base:
+            val = self.base.index(item)
+            self.process.insert(0, self.base.pop(val))
+            print('Перенесен на доработку: ')
+            return item
+        elif item in self.finish:
+            val2 = self.finish.index(item)
+            self.process.insert(0, self.finish.pop(val2))
+            print('Перенесен на доработку: ')
+            return item
+        else:
+            return print('Задача не обнаружена')
+
+    def to_finish(self, item):
+        if item in self.base:
+            val = self.base.index(item)
+            self.finish.insert(0, self.base.pop(val))
+            print('Перенесен в завершенные: ')
+            return item
+        elif item in self.process:
+            val2 = self.process.index(item)
+            self.finish.insert(0, self.process.pop(val2))
+            print('Перенесен в завершенные: ')
+            return item
+        else:
+            return print('Задача не обнаружена')
+
+    def size(self, queue):
+        if queue == 'base':
+            print('Кол-во задач в базовой очереди: ')
+            return len(self.base)
+        if queue == 'process':
+            print('Кол-во задач в работе: ')
+            return len(self.process) + len(self.base)
+        if queue == 'finish':
+            print('Кол-во завершонных задач: ')
+            return len(self.finish)
+
+
+if __name__ == '__main__':
+    qc_obj = QueueClass()
+    print(qc_obj.is_empty())  # -> True. Очередь пустая
+
+    # помещаем объекты в очередь
+    qc_obj.to_base('my_obj')
+    qc_obj.to_base(4)
+    qc_obj.to_base(True)
+    qc_obj.to_base('Задача1')
+    qc_obj.to_base('Задача2')
+    qc_obj.to_base('Задача3')
+
+    print(qc_obj.is_empty())  # -> False. Очередь пустая
+
+    print(qc_obj.size('base'))  # -> 3
+
+    print(qc_obj.to_finish('my_obj'))  # -> my_obj
+
+    print(qc_obj.size('finish'))  # -> 2
+
+    print(qc_obj.to_process('Задача1'))
+    print(qc_obj.to_process('Задача2'))
+
+    print(qc_obj.to_finish('Задача1'))
+
+    print(qc_obj.size('process'))
+
+
