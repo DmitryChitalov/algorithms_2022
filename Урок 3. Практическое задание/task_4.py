c83dfc37a8333b848ef72a9f5c0e90b456f8c1ef
Evgeniy Doroshenko <kraspy@yandex.ru>
2022-10-07 19:54:38 +0700

    tasks for lesson 3 completed

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,24 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+from uuid import uuid4
+import hashlib
+salt = uuid4().hex
+cache = {}
+
+
+def cache_url(url):
+    hash = hashlib.sha512(salt.encode() + url.encode()).hexdigest()
+    if cache.get(url):
+        print(f'Хэш для URL {url}:\n{hash}')
+    else:
+        cache[url] = hash
+
+
+cache_url('https://www.ya.ru/')
+cache_url('https://www.ya.ru/')
+cache_url('https://gb.ru/')
+cache_url('https://gb.ru/')
+print('\nТекущий кэш:')
+print(cache)
