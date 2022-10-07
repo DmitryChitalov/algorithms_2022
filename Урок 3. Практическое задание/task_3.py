f121626542342175a673fb128eb21b5437340dfc
Norovyatkin Ilya <dragon.nest9821@gmail.com>
2022-10-07 11:18:38 +0600

    complete task_3_3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -22,3 +22,30 @@
 р
 а
 """
+
+
+import hashlib
+
+
+def set_string(work_str):
+    """
+    Создание множества хэш для подстрок заданной строки
+    :param work_str: заданная строка
+    :return: множество хэш подстрок
+    """
+    res_set_str = set()
+    res_set_hash = set()
+    for i in range(len(work_str)):
+        for j in range(i + 1, len(work_str) + 1):
+            if work_str[i:j] != work_str:
+                res_set_hash.add(hashlib.sha256(work_str[i:j].encode('utf-8')).hexdigest())
+                res_set_str.add(work_str[i:j])
+                print(work_str[i:j])
+    print(res_set_str)
+    print(res_set_hash)
+    return res_set_str
+
+
+if __name__ == '__main__':
+    my_str = input('Введите слово: ')
+    print(f'Количество уникальных подстрок в строке - {my_str} равно {len(set_string(my_str))}')
