f4da29a30ea8c19028440408d8b93fefd9efcfbf
Belyakov Roman <belyakov1981@mail.ru>
2022-10-08 18:50:02 +0300

    done_task_2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,69 +22,78 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
-
-import sqlite3
-import hashlib
+from sqlite3 import connect, OperationalError, IntegrityError
+from hashlib import sha256
+from os.path import join, dirname
 
 # Решееие с использованием базы sqlite.
-'''
-class DbHash:
+class HashClass:
     def __init__(self):
-        self.conn = sqlite3.connect("test_al.db")
-        self.cursor = self.conn.cursor()
+        self.db_obj = join(dirname(__file__), 'DB_Hash')
+        self.conn = connect(self.db_obj)
+        self.crs = self.conn.cursor()
 
     def create_table(self):
-        self.cursor.execute("DROP TABLE IF EXISTS users;")
-        self.cursor.execute("CREATE TABLE users(u_login varchar(255), password_hash varchar(255))")
-
-    def create_hash(self):
-        login = input('Введите логин: ')
-        us_pass = input('Введите пароль: ')
-        hash_passwd = hashlib.sha256(login.encode() + us_pass.encode()).hexdigest()
-        return login, hash_passwd
-
-    def sql_insert(self):
-
-        login, hash_pass = self.create_hash()
-
-        insert_log_hash = 'INSERT INTO users(u_login, password_hash) VALUES(?,?)'
-        log_hash = (login, hash_pass)
+        create_stmt = "CREATE TABLE user_info(user_login varchar(255) unique, user_password varchar(255));"
         try:
-            self.cursor.execute(insert_log_hash, log_hash)
-        except sqlite3.IntegrityError:
+            self.crs.execute(create_stmt)
+        except OperationalError:
+            print('Таблица уже есть! Не создаём её.')
+        else:
+            self.conn.commit()
+            print('Операция прошла успешно таблица добавлена в базу данных DB_Hash!')
+
+    @staticmethod
+    def create_hash():
+        login = input('Введите логин: ')
+        password = input('Введите пароль: ')
+        hash_obj = sha256(login.encode() + password.encode()).hexdigest()
+        return login, hash_obj
+
+    def register(self):
+        print('Регистрация!')
+        login, reg_hash = self.create_hash()
+
+        insert_stmt = 'INSERT INTO user_info(user_login, user_password) VALUES(?,?)'
+        user_info = (login, reg_hash)
+
+        try:
+            self.crs.execute(insert_stmt, user_info)
+        except IntegrityError:
             print('Такой login уже существует. Войдите в систему или введите другой login.')
         else:
             self.conn.commit()
-            print('Вы зарегестрированы')
+            print('Вы зарегестрированы!')
 
     def log_in(self):
+        print('Вход на сайт!')
+        login, check_hash = self.create_hash()
 
-        login, hash_pass = self.create_hash()
+        select_stmt = 'SELECT user_password FROM user_info WHERE user_login = ?'
 
-        select_log_hash = 'SELECT password_hash FROM users WHERE u_login = ?'
+        self.crs.execute(select_stmt, (login,))
 
-        self.cursor.execute(select_log_hash, (login,))
+        out_hash = self.crs.fetchone()
 
-        db_hash = self.cursor.fetchone()
-        db_hash = str(*db_hash)
-
-        if hash_pass == db_hash:
-            print('Вы ввели правильный пароль')
+        if check_hash == out_hash[0]:
+            print('Вы ввели правильный пароль!')
         else:
-            print('Вы ввели неправильный пароль')
+            print('Вы ввели неправильный пароль или не зарегестрировались!')
 
-data_base_1 = DbHash()
-data_base_1.create_table()
-data_base_1.sql_insert()
-data_base_1.log_in()
-'''
+
+network = HashClass()
+network.create_table()
+network.register()
+network.register()
+network.register()
+network.log_in()
 
 
 # Решение с хранением логина и хэша-пароля в файле log_pass_hash.csv.
 def create_hash():
     login = input('Введите логин: ')
     us_pass = input('Введите пароль: ')
-    hash_passwd = hashlib.sha256(login.encode() + us_pass.encode()).hexdigest()
+    hash_passwd = sha256(login.encode() + us_pass.encode()).hexdigest()
     return login, hash_passwd
 
 
@@ -107,7 +116,7 @@ def log_in(dict_hash):
 
     if u_login in dict_hash:
         u_password = input('Введите пароль: ')
-        hash_u_password = hashlib.sha256(u_login.encode() + u_password.encode()).hexdigest()
+        hash_u_password = sha256(u_login.encode() + u_password.encode()).hexdigest()
         if hash_u_password == dict_hash[u_login]:
             print('Доступ разрешён!')
             return exit(0)
