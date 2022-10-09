d82526f31b7ef4487dcf8654dff0f1be8a6d7acc
RozhkovaDarya <102472078+RozhkovaDarya@users.noreply.github.com>
2022-10-09 20:12:06 +0200

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,45 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+class HashClass:
+    def __init__(self):
+        self.db_obj = join(dirname(__file__), 'demo.sqlite')
+        self.conn = connect(HashClass.db_obj)
+        self.crs = self.conn.cursor()
+
+    def create_table(self):
+        create_stmt = 'DROP TABLE IF EXISTS user_info'\
+                      'CREATE TABLE user_info ('\
+                      'user_login varchar(255)'\
+                      'unique, user_password varchar(255));'
+        
+
+    @staticmethod
+    def get_hash():
+        passwd = input('пароль: ')
+        login = input('логин: ')
+        hash_obj = sha256(login.encode() + passwd.encode()).hexdigest()
+        return login, hash_obj
+
+    def registr(self):
+        login, reg_hash = self.get_hash()
+        insert_stmt = 'INSERT INTO user_info (user_login, user_password)' \
+                      'VALUES (?, ?)'
+        user_info = (login, reg_hash)
+        try:
+            self.crs.execute(insert_stmt, user_info)
+        except IntegrityError:
+            print('вход')
+        else:
+            self.conn.commit()
+            print('Вы зарегистрировались')
+
+    def log_in(self):
+        login, check_hash = self.get_hash()
+        select_stmt = "SELECT user_password FROM user_info WHERE user_login = ?"
+        self.crs.execute(select_stmt, (login,))
+        out_hash = self.crs.fetchone()
+        if check_hash == out_hash[0]:
+            print('Ура!')
+        else:
+            print('неверный пароль')
