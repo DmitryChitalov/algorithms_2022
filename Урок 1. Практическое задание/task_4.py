3a648a65b8112df70e0a7dee73fc6cc436e806a1
Ivan Strelkov <donotreply@mail.ru>
2022-10-09 19:12:45 +0300

    StrelkovIVhw01
    StrelkovIVhw01

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,40 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+def check_auth1(user_dict: dict, login: str, password: str):
+    """O(n)"""
+    for user_login, user_params in user_dict.items():  # O(n)
+        if user_login == login and user_params.get('password') == password:  # O(1)
+            if user_params.get('is_active'):  # O(1)
+                return 'OK'  # O(1)
+            else:
+                return 'Activation required'  # O(1)
+    return 'Incorrect login/password'  # O(1)
+
+
+def check_auth2(user_dict: dict, login: str, password: str):
+    """ O(1)"""
+    user_data = user_dict.get(login)  # O(1)
+    if user_data:
+        if user_data.get('password') == password:  # O(1)
+            if user_data.get('is_active'):  # O(1)
+                return 'OK'  # O(1)
+            else:
+                return 'Activation required'  # O(1)
+    return 'Incorrect login/password'  # O(1)
+
+
+if __name__ == '__main__':
+    userdict = {'user' + str(i): {'password': 'fdfd', 'is_active': bool(i % 2)}
+                for i in range(15)}
+    print(userdict)
+    print(check_auth1(userdict, 'user1', 'fdfd'))
+    print(check_auth1(userdict, 'user1t', 'fdfd'))
+    print(check_auth1(userdict, 'user1', 'fdf'))
+    print(check_auth1(userdict, 'user2', 'fdfd'))
+    print(check_auth2(userdict, 'user1', 'fdfd'))
+    print(check_auth2(userdict, 'user2', 'fdfd'))
+
+# Второй вариант лучше, т.к. доступ к словарю по ключу эффективнее перебора
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,60 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+
+
+class StackOfPlates:
+    def __init__(self, max_plate_count):
+        self.max_size = max_plate_count
+        self.piles = []
+        self.piles_count = 0
+        self.plates_count = 0
+
+    def __str__(self):
+        return str(self.piles)
+
+    def is_empty(self):
+        return self.piles_count == 0
+
+    def push(self, plate):
+        if self.piles_count == 0 or (self.plates_count % self.max_size) == 0:
+            self.piles.append([plate])
+            self.piles_count += 1
+            self.plates_count += 1
+        else:
+            self.piles[self.piles_count - 1].append(plate)
+            self.plates_count += 1
+
+    def pop(self):
+        result = None
+        if self.piles_count > 0:
+            result = self.piles[self.piles_count - 1].pop()
+            self.plates_count -= 1
+            if (self.plates_count % self.max_size) == 0:
+                self.piles.pop()
+                self.piles_count -= 1
+        return result
+
+    def get_piles_count(self):
+        return self.piles_count
+
+    def get_plates_count(self):
+        return self.plates_count
+
+
+if __name__ == '__main__':
+    my_plate_stack = StackOfPlates(3)
+    for i in range(1, 11):
+        my_plate_stack.push('Т' + str(i))
+
+    print(my_plate_stack)
+    for i in range(9):
+        print(f'Убрали {my_plate_stack.pop()}: '
+              f'{my_plate_stack.get_piles_count()}, {my_plate_stack.get_plates_count()}')
+        if i % 2 == 0:
+            new_plate = "Тd" + str(i)
+            my_plate_stack.push(new_plate)
+            print(
+                f'Добавили {new_plate}: '
+                f'{my_plate_stack.get_piles_count()}, {my_plate_stack.get_plates_count()}')
+    print(my_plate_stack)
