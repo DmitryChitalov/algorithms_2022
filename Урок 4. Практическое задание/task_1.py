f21b812cbd0725e3e039d09681748c7a4e1cd737
KDV777 <k-d@bk.ru>
2022-10-09 10:51:09 +0500

    lesson 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import Timer
+
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +21,29 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+def func_3(nums):
+    len_nums = len(nums)
+    return [i for i in range(len_nums) if nums[i] % 2 == 0]
+
+
+print(func_2((1, 2, 4, 5, 6)))
+print(func_3((1, 2, 4, 5, 6)))
+
+t1 = Timer(stmt='func_1', setup='from __main__ import func_1')
+print("Не оптимизированная функция func_1", t1.timeit(number=10000000), 'seconds')
+
+t1 = Timer(stmt='func_2', setup='from __main__ import func_2')
+print("Оптимизированная функция func_2", t1.timeit(number=10000000), 'seconds')
+
+t1 = Timer(stmt='func_3', setup='from __main__ import func_3')
+print("Оптимизированная функция func_3", t1.timeit(number=10000000), 'seconds')
+
+# Не оптимизированная функция func_1 0.2125482999254018 seconds
+# Оптимизированная функция func_2 0.19864979991689324 seconds
+# ВЫВОД: За счет использования LC нам удалось снизить время выполнения кода
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,9 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 
+from timeit import Timer
+from collections import Counter
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +40,37 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    res = max(array, key=array.count)
+    return f'Чаще всего встречается число {res}, ' \
+           f'оно появилось в массиве {array.count(res)} раз(а)'
+
+
+def func_4():
+    return f'Чаще всего встречается число {Counter(array).most_common(1)[0][0]}, ' \
+           f'оно появилось в массиве {Counter(array).most_common(1)[0][1]} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+t1 = Timer(stmt='func_1', setup='from __main__ import func_1')
+print("функция func_1  ", t1.timeit(number=10000000), 'seconds')
+
+t2 = Timer(stmt='func_2', setup='from __main__ import func_2')
+print("функция func_2  ", t1.timeit(number=10000000), 'seconds')
+
+t3 = Timer(stmt='func_3', setup='from __main__ import func_3')
+print("функция func_3  ", t1.timeit(number=10000000), 'seconds')
+
+t4 = Timer(stmt='func_4', setup='from __main__ import func_4')
+print("функция func_4  ", t1.timeit(number=10000000), 'seconds')
+
+# функция func_1   0.23617290006950498 seconds
+# функция func_2   0.21995980013161898 seconds
+# функция func_3   0.216214100131765 seconds
+# функция func_4   0.223549900110811 seconds
+
+# в третьем варианте (func_3) замеры показали маленькое улучшение производительности
