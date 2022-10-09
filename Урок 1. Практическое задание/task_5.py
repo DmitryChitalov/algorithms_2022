ac62ba12e5ad9343831b321424cf620faaf63ab4
Vasiliy Andreev <93121991+MyNameIs-Witcher@users.noreply.github.com>
2022-10-09 20:18:23 +0300

    Выполнение task_5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,3 +1,5 @@
+from random import randint
+
 """
 Задание 5. На закрепление навыков работы со стеком
 
@@ -17,3 +19,45 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+
+from random import randint
+
+class StackClass:
+  def __init__(self):
+    self.elems = list()
+  
+  def push_in(self, el):
+    self.elems.append(el)
+  
+  def stack_size(self):
+    return len(self.elems)
+  
+  def clear_stack(self):
+    self.elems = list() 
+ 
+
+if __name__ == '__main__':
+  
+  stacks = list()
+  stack = StackClass()
+  
+  amount_of_dishes = randint(0, 100)
+  max_size = randint(0,10)
+  
+  for i in range(1, amount_of_dishes + 1):
+    now_size = stack.stack_size()
+    if now_size < max_size:
+      stack.push_in(f'dish_{i}')
+    else:
+      stacks.append(stack.elems)
+      stack.clear_stack()
+      stack.push_in(f'dish_{i}')
+
+  stacks.append(stack.elems)
+  print(amount_of_dishes, max_size)
+  print(stacks)
+      
+ 
+  
+  
+  
