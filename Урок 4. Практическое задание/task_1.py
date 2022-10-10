4418db51dcb6d192e69c4565e742d7d48130dba8
leshkovia <leshkovia@gmail.com>
2022-10-10 15:36:29 +0300

    task 1 in progress...

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import timeit, default_timer
+
 
 def func_1(nums):
     new_arr = []
@@ -21,4 +23,15 @@ def func_1(nums):
     return new_arr
 
 
-"""Hello"""
+def my_func(nums):
+    return [el for el in range(len(nums)) if nums[el] % 2 == 0]
+
+
+print(func_1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9]))
+print(
+    timeit("func_1", setup="from __main__ import func_1",
+           timer=default_timer, number=10000000))
+print(my_func([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9]))
+print(
+    timeit("my_func", setup="from __main__ import my_func",
+           timer=default_timer, number=10000000))
