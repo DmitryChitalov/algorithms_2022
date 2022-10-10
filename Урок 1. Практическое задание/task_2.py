324a2797aba0dd5f9a2874e901c78fdd404a0417
Yuri <rubtsov1606@mail.ru>
2022-10-10 11:16:54 +0300

    Home Work1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,22 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+def min_of_list_1(user_list):
+
+    for i in range(len(user_list)):
+        for j in range(len(user_list)):
+            if user_list[i] < user_list[j]:
+                min_el = user_list[i]
+            else:
+                min_el = user_list[i]
+
+    return min_el
+
+
+def min_of_list_2(user_list):
+
+    min_el = user_list[0]
+    for i in range(len(user_list)):
+        if user_list[i] < min_el:
+            min_el = user_list[i]
+    return min_el
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -16,4 +16,24 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+companies = {'ARS': 526879.45, 'SECT': 895674.32, 'FOX': 789456213.78, 'ZZZ': 478956213.56, 'XXX': 4789555555.55}
 
+# 1 способ
+list_values = sorted(list(companies.values()), reverse=True)[:3]                     # O(n)
+for el in list_values:                                                               # O(1)
+    for k, v in companies.items():                                                   # O(n)
+        if v == el:
+            print(k, ':', v)                                                         # O(1)
+
+
+# O(n)  + O(1) + O(n)  + O(1) = O(n)
+
+# 2 способ
+sorted_companies = sorted(companies, key=companies.get, reverse=True)[:3]    # O(n*log n)
+for i in sorted_companies:                                                   # O(1)
+    print(i, ':', companies.get(i))                                          # O(1)
+
+# O(n*log n) + O(1) + O(1) = O (n *log n)
+
+
+# Эффективнее 2 способ, т.к. он лаконичнее и итоговая сложность O(n * log n) меньше, чем O(n)
