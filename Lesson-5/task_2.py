93df2b240bd5b7fd4eb16b1c22e357ee306a2821
Yana <misschernenok@gmail.com>
2022-10-10 20:28:33 +0300

    DZ_5

---
+++ Lesson-5/task_2.py
@@ -0,0 +1,42 @@
+"""
+Задание 2.
+Написать программу сложения и умножения двух шестнадцатеричных чисел.
+При этом каждое число представляется как массив,
+элементы которого это цифры числа.
+Например, пользователь ввёл A2 и C4F.
+Сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
+Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
+произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
+Подсказка:
+Попытайтесь решить это задание в двух вариантах
+1) через collections
+defaultdict(list)
+int(, 16)
+reduce
+2) через ООП
+вспомните про перегрузку методов
+__mul__
+__add__
+"""
+# У меня получилось сделать только через collections, задание вообще не из простых =)
+
+from collections import defaultdict
+
+
+def hex_decimal_number():
+    hex_dict, hex_sum, hex_mul = defaultdict(list), 0, 1
+    hex_1 = input('Введите первое число: ')
+    hex_2 = input('Введите второе число: ')
+    hex_dict[int(hex_1, 16)] = list(hex_1)
+    hex_dict[int(hex_2, 16)] = list(hex_2)
+    for hex_num in hex_dict:
+        hex_sum += hex_num
+    for hex_num in hex_dict:
+        hex_mul *= hex_num
+
+    print(f'Сумма чисел {list(hex_1)} и {list(hex_2)} равна {list(hex(hex_sum))[2:]}\n'
+          f'Произведение чисел {list(hex_1)} и {list(hex_2)} равна {list(hex(hex_mul))[2:]}')
+
+
+if __name__ == "__main__":
+    hex_decimal_number()
