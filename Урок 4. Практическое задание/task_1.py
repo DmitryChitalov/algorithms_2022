b2735094d693d37d0e48cda6d27cb2db48caaaa0
vmrcx <hlh7520@gmail.com>
2022-10-12 01:46:36 +0300

    task_1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,7 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import timeit
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +20,15 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+nums = 0, 1, 2, 3, 4, 5, 6, 7
+print(timeit('func_1(nums)', number=100000, globals=globals()))
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+print(timeit('func_2(nums)', number=100000, globals=globals()))
+
+# Первая функция: 0.20315479999408126
+# Вторая функция: 0.1533573999768123
+# За счет истопользования list comprehensions во втором варианте получилось снизить время выполнения кода.
