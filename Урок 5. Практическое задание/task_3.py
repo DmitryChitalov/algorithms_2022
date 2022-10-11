140b8b7683fd60522a215dd5ce988644f650eec1
Dax <John-Xenos@yandex.ru>
2022-10-11 21:36:52 +0300

    Горбунов Е.А. Практическое задание 5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,36 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+
+import timeit
+from collections import deque
+
+# 1
+l = list('задача')
+d = deque('задача')
+print("1. append для list:", timeit.timeit('l.append("_")', number=10000, globals=globals()))
+print("1. append для deque:", timeit.timeit('d.append("_")', number=10000, globals=globals()))
+print('1. append: Время list практически равно deque')
+print("1. pop для list:", timeit.timeit('l.pop()', number=10000, globals=globals()))
+print("1. pop для deque:", timeit.timeit('d.pop()', number=10000, globals=globals()))
+print('1. pop: Время list чуть меньше deque')
+print("1. extend для list:", timeit.timeit('l.extend("_3")', number=10000, globals=globals()))
+print("1. extend для deque:", timeit.timeit('d.extend("_3")', number=10000, globals=globals()))
+print('1. extend: Время list кратно больше deque')
+# 2
+l = list('задача')
+d = deque('задача')
+print("2. appendleft для list:", timeit.timeit('l.insert(0, "_")', number=10000, globals=globals()))
+print("2. appendleft для deque:", timeit.timeit('d.appendleft("_")', number=10000, globals=globals()))
+print('2. appendleft: Время list кратно больше deque')
+print("2. popleft для list:", timeit.timeit('l.pop(0)', number=10000, globals=globals()))
+print("2. popleft для deque:", timeit.timeit('d.popleft()', number=10000, globals=globals()))
+print('2. popleft: Время list кратно больше deque')
+print("2. extendleft для list:", timeit.timeit('l[:0]="2_"', number=10000, globals=globals()))
+print("2. extendleft для deque:", timeit.timeit('d.extendleft("_2")', number=10000, globals=globals()))
+print('2. extendleft: Время list кратно больше deque')
+
+"""
+Везде, где работа с элементами списка приводит к изменению порядкового номера элементов в списке
+время выполнения таких действий в списке выше, чем то же действие в deque
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,51 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+
+# https://www.awesomeandrew.ru/2021/07/10/ordereddict-vs-dict-%D0%B2-python-%D0%B2%D1%8B%D0%B1%D0%B8%D1%80%D0%B0%D0%B5%D0%BC-%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B8%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BC%D0%B5%D0%BD/?ysclid=l94iqk7wbl992406050
+
+import timeit
+from collections import OrderedDict
+
+numbers_1 = dict({"one": 1, "two": 2, "three": 3})
+numbers_2 = OrderedDict({"one": 1, "two": 2, "three": 3})
+"""
+print(numbers_1)
+print(numbers_2)
+
+numbers_1 = dict([("one", 1), ("two", 2), ("three", 3)])
+numbers_2 = OrderedDict([("one", 1), ("two", 2), ("three", 3)])
+print(numbers_1)
+print(numbers_2)
+
+numbers_1 = dict(one=1, two=2, three=3)
+numbers_2 = OrderedDict(one=1, two=2, three=3)
+print(numbers_1)
+print(numbers_2)
+
+keys = ["one", "two", "three"]
+numbers_2 = OrderedDict.fromkeys(keys, 0)
+print(numbers_2)
+"""
+
+print("Добавление нового элемента в dict:", timeit.timeit('numbers_1["four"]=4', number=10000, globals=globals()))
+print("Добавление нового элемента в OrderedDict:", timeit.timeit('numbers_2["four"]=4', number=10000, globals=globals()))
+print('Добавление элемента: Время dict немного меньше OrderedDict')
+print("Удаление элемента в dict:", timeit.timeit('del numbers_1["four"]', number=1, globals=globals()))
+print("Удаление элемента в OrderedDict:", timeit.timeit('del numbers_2["four"]', number=1, globals=globals()))
+print('Удаление элемента: Время dict кратно меньше OrderedDict')
+
+"""
+В большинстве случаев при использовании версии Python 3.6 и выше использование OrderedDict нецелесообразно.
+Нашел только несколько причин использования OrderedDict:
+1. Обозначение смысла кода:
+    если вы используете OrderedDict, а не dict, то ваш код поясняет, что важен порядок следования элементов в словаре.
+    Вы определенно сообщаете, что ваш код требует и полагается на порядок элементов в словаре.
+2. Контроль над порядком элементов:
+    если вам нужно переупорядочить или переупорядочить элементы в словаре, вы можете использовать .move_to_end()
+    и расширенный вариант .popitem().
+3. Поведение при проверке равенства equality элементов: 
+    если ваш код сравнивает словари на предмет их равенства (эквивалентности), и при сравнении важен порядок элементов, 
+    то словари класса OrderedDict это правильный выбор.
+4. Необходима обратная совместимость.
+"""
