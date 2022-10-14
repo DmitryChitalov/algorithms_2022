599172e22cceb83916e6cc8b25f847f48a4ac954
p.baranov <pavel-info@yandex.ru>
2022-10-14 18:29:36 +0300

    Баранов Павел Сергеевич 260922 Урок 5

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,40 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+from timeit import timeit
+from collections import OrderedDict
+
+
+# dc = {x: x for x in range(100)}
+
+def append_dc(n ,val):
+    for j in range(n):
+        val[j] = j
+
+def get_dc(n, val):
+    for j in range(n):
+         i = val[j]
+
+def pop_dc(n, val):
+    for j in range(n):
+        val[j] = j
+    for j in range(n):
+        val.pop(j)
+
+
+
+
+dc = {}
+order_dc = OrderedDict()
+print('append     dict', timeit('append_dc(100, dc)', globals=globals(), number=10000))
+print('append ord_dict', timeit('append_dc(100, order_dc)', globals=globals(), number=10000))
+
+print('get     dict', timeit('get_dc(100, dc)', globals=globals(), number=10000))
+print('get ord_dict', timeit('get_dc(100, order_dc)', globals=globals(), number=10000))
+
+print('pop     dict', timeit('pop_dc(100, dc)', globals=globals(), number=10000))
+print('pop ord_dict', timeit('pop_dc(100, order_dc)', globals=globals(), number=10000))
+
+'''
+Не значительно выигрывает pop у orderdict
+'''
