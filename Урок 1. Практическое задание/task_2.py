8beae2a4674f695fce50c24aa23bc2d2f0602a97
Vasiliy Andreev <93121991+MyNameIs-Witcher@users.noreply.github.com>
2022-10-15 17:09:32 +0300

    выполнение task_2

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -7,11 +7,24 @@
 
 Сложность первого алгоритма должна быть O(n^2) - квадратичная.
 """
+# Сложность: O(n^2)
+
+def min_val_ver1(list_of_values: list) -> str:
+  min = list_of_values[0]
+  for i in range(0, len(list_of_values)): # O(n)
+    for k in range(0, len(list_of_values)): # O(n)
+      if list_of_values[i] <= list_of_values[k]: # O(1)
+        if list_of_values[i] <= min: # O(1)
+          min = list_of_values[i] # O(1)
+  return min # O(1)
+
 
 """
 Сложность второго алгоритма должна быть O(n) - линейная.
 """
-def min_val_n2(list_of_values):
+# Сложность: O(n)
+
+def min_val_ver2(list_of_values: list) -> str:
   min = list_of_values[0] # O(1)
   for i in range(1, len(list_of_values)): # O(n)
     if list_of_values[i] < min:# O(1)
