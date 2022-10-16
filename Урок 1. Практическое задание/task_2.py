3f5adec7e5201a8e68d2cbaa53720569c3182df0
kribodia <kribodia@mail.ru>
2022-10-16 21:05:14 +0200

    Сделал ДЗ 1.2

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,31 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+
+# O(n^2)
+
+def min_of_list1(my_list):
+    for i in my_list:  # O(n)
+        min_val = True  # O(1)
+        for j in my_list:  # O(n)
+            if i > j:  # O(1)
+                min_val = False  # O(1)
+        if min_val:  # O(1)
+            return i  # O(1)
+
+
+# O(n)
+
+def min_of_list2(my_list):
+    min_val = my_list[1]  # O(1)
+    for i in my_list:  # O(n)
+        if i < min_val:  # O(1)
+            min_val = i  # O(1)
+    return min_val  # O(1)
+
+
+my_list = [12, 2, 3, 4, 5]
+print(my_list)
+print(min_of_list1(my_list))
+print(min_of_list2(my_list))
