cd5cdfa577c72a6aa6a500e9ac784771f54768ae
p.baranov <pavel-info@yandex.ru>
2022-10-16 08:12:10 +0300

    update 5

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -65,39 +65,3 @@ func_2()
 '''
 # print(timeit.timeit("func_1()", globals=globals(), number=10))
 # print(timeit.timeit("func_2()", globals=globals(), number=10))
-
-import timeit
-from memory_profiler import profile
-
-import numpy
-
-
-@profile
-def func_1():
-    nums = [j * 10 for j in range(50000)]
-    new_arr = []
-    for i in range(len(nums)):
-        if nums[i] % 2 == 0:
-            new_arr.append(i)
-    return new_arr
-
-
-@profile
-def func_2():
-    nums = numpy.array([j * 10 for j in range(50000)])
-    new_arr = numpy.array
-    for i in range(len(nums)):
-        if nums[i] % 2 == 0:
-            numpy.append(new_arr, i)
-    return new_arr
-
-
-func_1()
-func_2()
-'''
-Заменил list на array из numpy. Значительный выигрыш по памяти и проигрыш по быстродействию.
-# 0.06307230005040765
-# 2.5453929000068456
-'''
-# print(timeit.timeit("func_1()", globals=globals(), number=10))
-# print(timeit.timeit("func_2()", globals=globals(), number=10))
