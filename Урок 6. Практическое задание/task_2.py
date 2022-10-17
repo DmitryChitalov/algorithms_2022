daa0268bd845db6e95445d8bd40b79d68a84ebf0
leshkovia <leshkovia@gmail.com>
2022-10-17 19:35:57 +0300

    task 6 is complete!

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -10,15 +10,44 @@
 Опишите этот путь и покажите его применение
 """
 
+from memory_profiler import profile
 
-def reverse_numbers(data):
-    last_num = data % 10
-    first_num = data // 10
-    if data == 0:
-        return ""
+"""less_2_task_3"""
 
-    return str(last_num) + reverse_numbers(first_num)
 
+@profile()
+def wrapped(number):
+    def reverse_numbers(data):
+        last_num = data % 10
+        first_num = data // 10
+        if data == 0:
+            return ""
+
+        return str(last_num) + reverse_numbers(first_num)
+
+    return reverse_numbers(number)
+
+
+# Line #    Mem usage    Increment  Occurrences   Line Contents
+# =============================================================
+#     18     18.7 MiB     18.7 MiB           1   @profile()
+#     19                                         def wrapped(number):
+#     20     18.7 MiB      0.0 MiB           8       def reverse_numbers(data):
+#     21     18.7 MiB      0.0 MiB           7           last_num = data % 10
+#     22     18.7 MiB      0.0 MiB           7           first_num = data // 10
+#     23     18.7 MiB      0.0 MiB           7           if data == 0:
+#     24     18.7 MiB      0.0 MiB           1               return ""
+#     25
+#     26     18.7 MiB      0.0 MiB           6           return str(last_num) + reverse_numbers(first_num)
+#     27
+#     28     18.7 MiB      0.0 MiB           1       return reverse_numbers(number)
+#
+#
+# 005001
 
 user_choice = int(input("Введите число для реверса: "))
-print(reverse_numbers(user_choice))
+print(wrapped(user_choice))
+
+"""Проблема замера расходования памяти при работе скрипта с рекурсией состоит в том, что скрипт постоянно
+самовызывается, провоцируя очередной замер профайлера. Чтобы этого избежать, как и обсуждалось на уроке,
+обернём скрипт с рекурсией в отдельную функцию-обёртку для единого замера расхода RAM"""
