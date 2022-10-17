3e83372346819ae736d6cf63b51ce82e142da134
leshkovia <leshkovia@gmail.com>
2022-10-17 12:53:06 +0300

    task 1 is complete!

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -30,13 +30,54 @@
 
 Это файл для первого скрипта
 """
+from memory_profiler import profile
+from numpy import array
+from pympler.asizeof import asizeof
+from sys import getsizeof
 
 
+@profile()
 def add_dict(n):
     my_dict = {}
     for el in range(n):
-        my_dict[-el] = el
+        my_dict[el] = el
     return my_dict
 
 
-add_dict(100500)
+@profile()
+def add_dict_optimized(n):
+    my_tuple = tuple(el for el in range(n))
+    return my_tuple
+
+
+print(f"Размер словаря >>> {getsizeof(add_dict(100500))}")
+# Line #    Mem usage    Increment  Occurrences   Line Contents
+# =============================================================
+#     39     31.2 MiB     31.2 MiB           1   @profile()
+#     40                                         def add_dict(n):
+#     41     31.2 MiB      0.0 MiB           1       my_dict = {}
+#     42     39.2 MiB      0.1 MiB      100501       for el in range(n):
+#     43     39.2 MiB      8.0 MiB      100500           my_dict[el] = el
+#     44     39.2 MiB      0.0 MiB           1       return my_dict
+#
+#
+# Размер словаря >>> 5242976
+
+print(f"Размер кортежа >>> {getsizeof(add_dict_optimized(100500))}")
+
+# Line #    Mem usage    Increment  Occurrences   Line Contents
+# =============================================================
+#     47     31.3 MiB     31.3 MiB           1   @profile()
+#     48                                         def add_dict_optimized(n):
+#     49     35.4 MiB      4.1 MiB      201003       my_tuple = tuple(el for el in range(n))
+#     50     35.4 MiB      0.0 MiB           1       return my_tuple
+#
+#
+# Размер кортежа >>> 804048
+
+"""
+В данном задании произвёл оптимизацию скрипта заполняющего словарь значениями, путём 
+размещения данных в кортеж, который сам по себе имеет меньший размер на выходе + само 
+заполнение расходует меньшее количество памяти. Эффект заметен при достаточно больших вводных
+данных!
+"""
