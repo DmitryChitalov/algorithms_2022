3cf00b89aefc245c35bd22b0c61663fa0682f710
Norovyatkin Ilya <dragon.nest9821@gmail.com>
2022-10-18 20:42:56 +0600

    Complete task_6_1(1.2.3.4.5)

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -30,3 +30,104 @@
 
 Это файл для первого скрипта
 """
+
+"""
+В качестве примера выбрана 2 задача из 1 урока по Основам python
+"""
+
+
+from memory_profiler import profile
+
+
+@profile
+def task_1_2():
+    cube_list = []
+    sum_list = 0
+
+    for i in range(0, 50000):
+        if i % 2 != 0:
+            cube_list.append(i ** 3)
+
+    for element in cube_list:
+        sum_elements = 0
+        cycle_element = element
+        while cycle_element / 10 > 0:
+            sum_elements += cycle_element % 10
+            cycle_element = cycle_element // 10
+        if sum_elements % 7 == 0:
+            sum_list += element
+
+    print(sum_list)
+
+
+@profile
+def task_1_2_optimize():
+    sum_list = 0
+    cube_list = (i ** 3 for i in range(0, 50000) if i % 2 != 0)
+
+    for element in cube_list:
+        sum_elements = 0
+        cycle_element = element
+        while cycle_element / 10 > 0:
+            sum_elements += cycle_element % 10
+            cycle_element = cycle_element // 10
+        if sum_elements % 7 == 0:
+            sum_list += element
+
+    print(sum_list)
+
+
+task_1_2()
+task_1_2_optimize()
+
+"""
+94743520095335132
+Filename: /home/fox/Рабочий стол/algorithms_2022/Урок 6. Практическое задание/task_1_1.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    42     19.4 MiB     19.4 MiB           1   @profile
+    43                                         def task_1_2():
+    44     19.4 MiB      0.0 MiB           1       cube_list = []
+    45     19.4 MiB      0.0 MiB           1       sum_list = 0
+    46                                         
+    47     20.7 MiB      0.5 MiB       50001       for i in range(0, 50000):
+    48     20.7 MiB      0.0 MiB       50000           if i % 2 != 0:
+    49     20.7 MiB      0.8 MiB       25000               cube_list.append(i ** 3)
+    50                                         
+    51     20.7 MiB      0.0 MiB       25001       for element in cube_list:
+    52     20.7 MiB      0.0 MiB       25000           sum_elements = 0
+    53     20.7 MiB      0.0 MiB       25000           cycle_element = element
+    54     20.7 MiB      0.0 MiB      356690           while cycle_element / 10 > 0:
+    55     20.7 MiB      0.0 MiB      331690               sum_elements += cycle_element % 10
+    56     20.7 MiB      0.0 MiB      331690               cycle_element = cycle_element // 10
+    57     20.7 MiB      0.0 MiB       25000           if sum_elements % 7 == 0:
+    58     20.7 MiB      0.0 MiB        2978               sum_list += element
+    59                                         
+    60     20.7 MiB      0.0 MiB           1       print(sum_list)
+
+
+94743520095335132
+Filename: /home/fox/Рабочий стол/algorithms_2022/Урок 6. Практическое задание/task_1_1.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    63     20.7 MiB     20.7 MiB           1   @profile
+    64                                         def task_1_2_optimize():
+    65     20.7 MiB      0.0 MiB           1       sum_list = 0
+    66     20.7 MiB      0.0 MiB       75003       cube_list = (i ** 3 for i in range(0, 50000) if i % 2 != 0)
+    67                                         
+    68     20.7 MiB      0.0 MiB       25001       for element in cube_list:
+    69     20.7 MiB      0.0 MiB       25000           sum_elements = 0
+    70     20.7 MiB      0.0 MiB       25000           cycle_element = element
+    71     20.7 MiB      0.0 MiB      356690           while cycle_element / 10 > 0:
+    72     20.7 MiB      0.0 MiB      331690               sum_elements += cycle_element % 10
+    73     20.7 MiB      0.0 MiB      331690               cycle_element = cycle_element // 10
+    74     20.7 MiB      0.0 MiB       25000           if sum_elements % 7 == 0:
+    75     20.7 MiB      0.0 MiB        2978               sum_list += element
+    76                                         
+    77     20.7 MiB      0.0 MiB           1       print(sum_list)
+
+    Создание списка заменил на генератор.
+    Как показывают замеры экономия памяти составила 1.3 MiB
+"""
--- Урок 6. Практическое задание/task_1_2.py
+++ Урок 6. Практическое задание/task_1_2.py
@@ -30,3 +30,104 @@
 
 Это файл для второго скрипта
 """
+
+"""
+В качестве примера выбрана 2 задача из 1 урока по Основам python
+"""
+
+
+from memory_profiler import profile
+from numpy import array
+
+@profile
+def task_1_2():
+    cube_list = []
+    sum_list = 0
+
+    for i in range(0, 50000):
+        if i % 2 != 0:
+            cube_list.append(i ** 3)
+
+    for element in cube_list:
+        sum_elements = 0
+        cycle_element = element
+        while cycle_element / 10 > 0:
+            sum_elements += cycle_element % 10
+            cycle_element = cycle_element // 10
+        if sum_elements % 7 == 0:
+            sum_list += element
+
+    print(sum_list)
+
+
+@profile
+def task_1_2_optimize():
+    sum_list = 0
+    cube_list = array([i ** 3 for i in range(0, 50000) if i % 2 != 0])
+
+    for element in cube_list:
+        sum_elements = 0
+        cycle_element = element
+        while cycle_element / 10 > 0:
+            sum_elements += cycle_element % 10
+            cycle_element = cycle_element // 10
+        if sum_elements % 7 == 0:
+            sum_list += element
+
+    print(sum_list)
+
+
+task_1_2()
+task_1_2_optimize()
+
+"""
+94743520095335132
+Filename: /home/fox/Рабочий стол/algorithms_2022/Урок 6. Практическое задание/task_1_2.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    42     35.5 MiB     35.5 MiB           1   @profile
+    43                                         def task_1_2():
+    44     35.5 MiB      0.0 MiB           1       cube_list = []
+    45     35.5 MiB      0.0 MiB           1       sum_list = 0
+    46                                         
+    47     36.5 MiB      0.3 MiB       50001       for i in range(0, 50000):
+    48     36.5 MiB      0.0 MiB       50000           if i % 2 != 0:
+    49     36.5 MiB      0.8 MiB       25000               cube_list.append(i ** 3)
+    50                                         
+    51     36.5 MiB      0.0 MiB       25001       for element in cube_list:
+    52     36.5 MiB      0.0 MiB       25000           sum_elements = 0
+    53     36.5 MiB      0.0 MiB       25000           cycle_element = element
+    54     36.5 MiB      0.0 MiB      356690           while cycle_element / 10 > 0:
+    55     36.5 MiB      0.0 MiB      331690               sum_elements += cycle_element % 10
+    56     36.5 MiB      0.0 MiB      331690               cycle_element = cycle_element // 10
+    57     36.5 MiB      0.0 MiB       25000           if sum_elements % 7 == 0:
+    58     36.5 MiB      0.0 MiB        2978               sum_list += element
+    59                                         
+    60     36.5 MiB      0.0 MiB           1       print(sum_list)
+
+
+94743520095335132
+Filename: /home/fox/Рабочий стол/algorithms_2022/Урок 6. Практическое задание/task_1_2.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    63     36.5 MiB     36.5 MiB           1   @profile
+    64                                         def task_1_2_optimize():
+    65     36.5 MiB      0.0 MiB           1       sum_list = 0
+    66     36.8 MiB      0.3 MiB       50003       cube_list = array([i ** 3 for i in range(0, 50000) if i % 2 != 0])
+    67                                         
+    68     36.8 MiB      0.0 MiB       25001       for element in cube_list:
+    69     36.8 MiB      0.0 MiB       25000           sum_elements = 0
+    70     36.8 MiB      0.0 MiB       25000           cycle_element = element
+    71     36.8 MiB      0.0 MiB      356690           while cycle_element / 10 > 0:
+    72     36.8 MiB      0.0 MiB      331690               sum_elements += cycle_element % 10
+    73     36.8 MiB      0.0 MiB      331690               cycle_element = cycle_element // 10
+    74     36.8 MiB      0.0 MiB       25000           if sum_elements % 7 == 0:
+    75     36.8 MiB      0.0 MiB        2978               sum_list += element
+    76                                         
+    77     36.8 MiB      0.0 MiB           1       print(sum_list)
+
+Cпискок заменил на numpy.ndarray.
+Как показывают замеры экономия памяти составила 1.3 MiB
+"""
