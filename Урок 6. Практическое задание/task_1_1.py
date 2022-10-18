ed45123cd86fd11e8c8d5ed15a6f95551b4b5b7c
Konstantin Zaytsev <tarans.fishborn@gmail.com>
2022-10-18 20:32:48 +0300

    Домашнее задание №6. Зайцев К.В

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -30,3 +30,49 @@
 
 Это файл для первого скрипта
 """
+from memory_profiler import memory_usage
+# 2 дз 4 задание алгоритмы
+
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        m1 = memory_usage()
+        res = func(*args)
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        print(mem_diff)
+        return res
+
+    return wrapper
+
+
+@decor
+def func_sum_n(x, i=0, res=0, num=1):
+    if x != i:
+        res += num
+        num /= -2
+        i += 1
+        return func_sum_n(x, i, res, num)
+    if x == i:
+        return res
+
+
+@decor
+def func_sum_2(count, start=1, result=0):
+    while count > 0:
+        result += start
+        count -= 1
+        start /= -2
+    else:
+        return result
+
+
+# func_sum_n(100)
+# 0.25
+# print('\n')
+print(func_sum_2(100))
+# 0.01171875
+"""
+цикл использует меньше памяти чем рекурсия
+рекурсия использует стек для хранения вызовов, потому использует больше памяти
+"""
--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -30,3 +30,25 @@
 
 Это файл для третьего скрипта
 """
+from pympler import asizeof
+# 3 дз 1 задание алгоритмы
+
+
+#неоптимизированное решение
+def list_gen(num):
+    return [i for i in range(1, num+1)]
+
+
+# оптимизированное
+def lst_gen(n):
+    for i in range(1, n+1):
+        yield i
+
+
+print(asizeof.asizeof(lst_gen(100)))
+print(asizeof.asizeof(list_gen(100)))
+# 424
+# 4120
+"""
+применение генератора значительно снижает расход памяти
+"""
