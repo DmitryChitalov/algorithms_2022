167963495105eec798d7bd46a928509b65df2010
Zhanna Nizhegorodova <105349123+ZhannaNizhegorodova@users.noreply.github.com>
2022-10-19 13:35:42 +0300

    Update task_1_3.py

--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -30,3 +30,72 @@
 
 Это файл для третьего скрипта
 """
+from memory_profiler import profile
+
+
+# Функция с урока 2, задание 5, курс Алгоритмы
+@profile
+def output_codes_symbols(symbol_number=32, i=1):
+    if symbol_number > 127:
+        return
+    else:
+        print(ord(chr(symbol_number)), '-', chr(symbol_number), end=' ')
+        # print(f"{ord(chr(symbol_number))} - {chr(symbol_number)}", end=' ')
+        if i % 10 == 0:
+            print()
+        return output_codes_symbols(symbol_number + 1, i + 1)
+
+
+output_codes_symbols()
+
+"""
+Каждый вызов рекурсивной функции выдает подобную таблицу с замерами.
+Избавимся от рекурсии и реализуем функцию через цикл, применим f-строки.
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    36     20.7 MiB     20.6 MiB          97   @profile
+    37                                         def output_codes_symbols(symbol_number=32, i=1):
+    38     20.7 MiB      0.0 MiB          97       if symbol_number > 127:
+    39     20.7 MiB      0.0 MiB           1           return
+    40                                             else:
+    41     20.7 MiB      0.0 MiB          96           print(ord(chr(symbol_number)), '-', chr(symbol_number), end=' ')
+    42                                                 # print(f"{ord(chr(symbol_number))} - {chr(symbol_number)}", end=' ')
+    43     20.7 MiB      0.0 MiB          96           if i % 10 == 0:
+    44     20.6 MiB      0.0 MiB           9               print()
+    45     20.8 MiB      0.0 MiB          96           return output_codes_symbols(symbol_number + 1, i + 1)
+"""
+
+
+@profile
+def output_codes_symbols(symbol_number=32, i=1):
+    while symbol_number < 128:
+        print(f"{ord(chr(symbol_number))} - {chr(symbol_number)}", end=' ')
+        if i % 10 == 0:
+            print()
+        symbol_number = symbol_number + 1
+        i = i + 1
+
+
+output_codes_symbols()
+
+"""
+Теперь функция output_codes_symbols вызывается однократно.
+Удалось немного снизить использование памяти.
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    50     19.8 MiB     19.8 MiB           1   @profile
+    51                                         def output_codes_symbols(symbol_number=32, i=1):
+    52     19.8 MiB      0.0 MiB          97       while symbol_number < 128:
+    53     19.8 MiB      0.0 MiB          96           print(f"{ord(chr(symbol_number))} - {chr(symbol_number)}", end=' ')
+    54     19.8 MiB      0.0 MiB          96           if i % 10 == 0:
+    55     19.8 MiB      0.0 MiB           9               print()
+    56     19.8 MiB      0.0 MiB          96           symbol_number = symbol_number + 1
+    57     19.8 MiB      0.0 MiB          96           i = i + 1
+
+
+
+Process finished with exit code 0
+
+"""
