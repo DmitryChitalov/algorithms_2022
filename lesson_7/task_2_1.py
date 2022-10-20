363a31b05701171d0387266ff9e1abd499e2453a
vetasbike <vetasbike@mail.ru>
2022-10-20 09:26:39 +0300

    Домашнее задание к уроку 7

---
+++ lesson_7/task_2_1.py
@@ -0,0 +1,63 @@
+"""
+Задание 2. Массив размером 2m + 1, где m – натуральное число,
+заполнен случайным образом. Найдите в массиве медиану.
+Медианой называется элемент ряда, делящий его на
+две равные по длине части:
+в одной находятся элементы,
+которые не меньше медианы,
+в другой – не больше медианы.
+Решите задачу тремя способами:
+1) с помощью сортировки, которую мы не рассматривали на уроке (Гномья, Шелла,
+Кучей)
+сделайте замеры на массивах длиной 10, 100, 1000 элементов
+"""
+from random import randint
+from timeit import timeit
+
+'''
+Результаты замеров:
+0.02292790001956746 => 11
+1.9976259000250138 => 101
+210.94369119999465 => 1001
+'''
+
+arr = [1, 3, 2, 4, 7, 6, 5]  # список для проверки
+
+
+def gnome_sort(list_to_sort):
+    n = 1
+    while n < len(list_to_sort):
+        if list_to_sort[n - 1] <= list_to_sort[n]:
+            n += 1
+        else:
+            list_to_sort[n - 1], list_to_sort[n] = list_to_sort[n], list_to_sort[n - 1]
+            if n > 1:
+                n -= 1  # идея гномьей сортировки, что мы будем двигаться на 1 шаг назад, если есть обмен позиций
+    return list_to_sort[len(list_to_sort) // 2]
+
+
+print(gnome_sort(arr))  # => 4
+
+orig_list = [randint(-100, 100) for _ in range(11)]
+
+print(
+    timeit(
+        "gnome_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [randint(-100, 100) for _ in range(101)]
+
+print(
+    timeit(
+        "gnome_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [randint(-100, 100) for _ in range(1001)]
+
+print(
+    timeit(
+        "gnome_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
---
+++ lesson_7/task_2_2.py
@@ -0,0 +1,56 @@
+"""
+Задание 2. Массив размером 2m + 1, где m – натуральное число,
+заполнен случайным образом. Найдите в массиве медиану.
+Медианой называется элемент ряда, делящий его на
+две равные по длине части:
+в одной находятся элементы,
+которые не меньше медианы,
+в другой – не больше медианы.
+Решите задачу тремя способами:
+2) без сортировки
+сделайте замеры на массивах длиной 10, 100, 1000 элементов
+"""
+from random import randint
+from timeit import timeit
+
+'''
+Результаты замеров:
+0.03171080001629889 => 11
+0.18328090000431985 => 101
+9.612581599969417 => 1001
+'''
+
+arr = [3, 1, 5, 4, 2, 6, 7]  # список для проверки
+
+
+def med_no_sort(some_list):
+    n = 0
+    while n <= len(some_list) / 2:
+        some_list.remove(max(some_list))
+        n += 1
+    return max(some_list)
+
+
+print(f'медиана {med_no_sort(arr[:])}')  # => 4
+
+orig_list = [randint(-100, 100) for _ in range(11)]
+print(
+    timeit(
+        "med_no_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [randint(-100, 100) for _ in range(101)]
+
+print(
+    timeit(
+        "med_no_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [randint(-100, 100) for _ in range(1001)]
+print(
+    timeit(
+        "med_no_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
