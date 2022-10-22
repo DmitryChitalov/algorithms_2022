c3d6f6dc16ca1e4670c8a4e6821dd5a812948894
Dax <John-Xenos@yandex.ru>
2022-10-22 11:26:33 +0300

    Практическое задание 7 В задании 2.2 не смог понять как сдеать замер времени, и почему вообще при замере времени ошибки

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,49 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+
+from random import randint
+from timeit import timeit
+
+numbers = [randint(-100, 100) for _ in range(100)]
+print(f'Исходный массив:\n{numbers}')
+
+# Без оптимизации
+def bubble_sort(array):
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+
+    return array
+
+# С оптимизацией
+def bubble_sort_optimized(array):
+    for i in range(len(array) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                flag = False
+
+        if flag == True:
+            break
+    return array
+
+print('\nСортировка пузырьком (в обратном направлении, скорее камнем)')
+time1 = timeit(f'bubble_sort({numbers[:]})',
+              setup='from __main__ import bubble_sort',
+              number=1000)
+print(f'Сортированный массив:\n{bubble_sort(numbers[:])}')
+print(f'Время 1000 сортировок: {time1} сек.')
+
+print('\nСортировка пузырьком оптимизированная (в обратном направлении, скорее камнем)')
+time2 = timeit(f'bubble_sort_optimized({numbers[:]})',
+              setup='from __main__ import bubble_sort_optimized',
+              number=1000)
+print(f'Сортированный массив:\n{bubble_sort_optimized(numbers[:])}')
+print(f'Время 1000 сортировок: {time2} сек.')
+
+print(f"""ЗАКЛЮЧЕНИЕ\n
+    Разница между обычной и оптимизированной составила {abs(time1 - time2)} сек.
+    В связи с чем очевидно, что оптимизация нецелесообразна.""")
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,63 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from random import choice
+from timeit import timeit
+"""
+сделайте замеры на массивах длиной 10, 100, 1000 элементов <- ТАК НЕ ПОЛУЧИТСЯ!!!
+Так как длина массива 2m + 1, т.е. будет нечетное число.
+Сделаем замеры на массивах длиной 11, 101, 1001 элементов
+"""
+
+numbers11 = [randint(-100, 100) for _ in range(11)]
+numbers101 = [randint(-100, 100) for _ in range(101)]
+numbers1001 = [randint(-100, 100) for _ in range(1001)]
+
+"""
+--- БЕЗ СОРТИРОВКИ ---
+https://habr.com/ru/post/346930/?ysclid=l9jkw5tzka892779383
+"""
+
+def quickselect_median(array, pivot_fn=choice):
+    if len(array) % 2 == 1:
+        return quickselect(array, len(array) / 2, pivot_fn)
+    else:
+        return 0.5 * (quickselect(array, len(array) / 2 - 1, pivot_fn) +
+                      quickselect(array, len(array) / 2, pivot_fn))
+
+def quickselect(array, k, pivot_fn):
+    """
+    Выбираем k-тый элемент в списке array (с нулевой базой)
+    :param array: список числовых данных
+    :param k: индекс
+    :param pivot_fn: функция выбора pivot, по умолчанию выбирает случайно
+    :return: k-тый элемент array
+    """
+    if len(array) == 1:
+        assert k == 0
+        return array[0]
+
+    pivot = pivot_fn(array)
+
+    lows = [el for el in array if el < pivot]
+    highs = [el for el in array if el > pivot]
+    pivots = [el for el in array if el == pivot]
+
+    if k < len(lows):
+        return quickselect(lows, k, pivot_fn)
+    elif k < len(lows) + len(pivots):
+        # Нам повезло и мы угадали медиану
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
+
+
+print(f'Исходный массив:\n{numbers11}')
+print(f'Медиана: {quickselect_median(numbers11[:])}')
+
+time1 = timeit(f'quickselect_median({numbers11[:]})',
+              setup='from __main__ import quickselect_median, quickselect',
+              number=1000)
+print(f'Время: {time1} сек., Медиана: {quickselect_median(numbers11[:])}')
