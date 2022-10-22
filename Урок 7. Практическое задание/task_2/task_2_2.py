0f68b850e9f6d421bb04d2721b2d06982fb884ad
Posvian <posvianski@mail.ru>
2022-10-22 21:43:11 +0300

    made task_7_2

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,48 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+m = 5
+origin_list_11 = [randint(-100, 100) for _ in range(2*m + 1)]
+print(origin_list_11)
+
+m = 50
+origin_list_101 = [randint(-100, 100) for _ in range(2*m + 1)]
+
+m = 500
+origin_list_1001 = [randint(-100, 100) for _ in range(2*m + 1)]
+
+
+def mediana(lst_obj):
+    for i in range(int(len(lst_obj) / 2)):
+        lst_obj.remove(max(lst_obj))
+    return max(lst_obj)
+
+
+# замеры для 11
+print(
+    timeit(
+        "mediana(origin_list_11[:])",
+        globals=globals(),
+        number=1000))
+
+# замеры для 101
+print(
+    timeit(
+        "mediana(origin_list_101[:])",
+        globals=globals(),
+        number=1000))
+
+# замеры для 1001
+print(
+    timeit(
+        "mediana(origin_list_1001[:])",
+        globals=globals(),
+        number=1000))
+
+# 0.0011356660106685013
+# 0.05656762499711476
+# 4.102964791003615
--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,53 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+
+from statistics import median
+from random import randint
+from timeit import timeit
+
+m = 5
+origin_list_11 = [randint(-100, 100) for _ in range(2*m + 1)]
+
+m = 50
+origin_list_101 = [randint(-100, 100) for _ in range(2*m + 1)]
+
+m = 500
+origin_list_1001 = [randint(-100, 100) for _ in range(2*m + 1)]
+
+
+def find_median(lst_obj):
+    my_med = median(lst_obj)
+    return my_med
+
+
+# замеры для 11
+print(
+    timeit(
+        "find_median(origin_list_11[:])",
+        globals=globals(),
+        number=1000))
+
+# замеры для 101
+print(
+    timeit(
+        "find_median(origin_list_101[:])",
+        globals=globals(),
+        number=1000))
+
+# замеры для 1001
+print(
+    timeit(
+        "find_median(origin_list_1001[:])",
+        globals=globals(),
+        number=1000))
+
+# 0.0006553750135935843
+# 0.0044763330079149455
+# 0.06599670799914747
+
+"""
+Самый быстрый способ на основе замеров - функция median из библиотеки 
+statistics, на втором месте оказался с пособ с поиском максимального элемента
+и удалением его, на третьем месте гномья сортировка.
+"""
