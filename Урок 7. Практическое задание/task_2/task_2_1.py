0d93ba937a32f5b588d3ae0ba1c9f58b89e83012
Viktor-home <v.isupov@mail.ru>
2022-10-22 20:49:58 +0300

    Выполнил ДЗ урок 7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,35 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+lst_10 = [randint(0, 1000) for _ in range(11)]
+lst_100 = [randint(0, 1000) for _ in range(101)]
+lst_1000 = [randint(0, 1000) for _ in range(1001)]
+
+
+def Gnom_sort_median(lst_obj):
+    i = 0
+    len_obj = len(lst_obj)
+    mediana = len(lst_obj) // 2
+    while i + 1 < len_obj:
+        if lst_obj[i + 1] >= lst_obj[i]:
+            i += 1
+        else:
+            lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+            if i > 0:
+                i -= 1
+            else:
+                i += 1
+    # print(sum(lst_obj) / len(lst_obj))
+    return lst_obj[mediana]
+
+print(Gnom_sort_median(lst_10))
+print(Gnom_sort_median(lst_100))
+print(Gnom_sort_median(lst_1000))
+print(timeit("Gnom_sort_median(lst_10)", globals=globals(), number=1000))
+print(timeit("Gnom_sort_median(lst_100)", globals=globals(), number=1000))
+print(timeit("Gnom_sort_median(lst_1000)", globals=globals(), number=1000))
+
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,24 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+lst_10 = [randint(0, 1000) for _ in range(11)]
+lst_100 = [randint(0, 1000) for _ in range(101)]
+lst_1000 = [randint(0, 1000) for _ in range(1001)]
+
+
+def get_mediana(lst_obj):
+    for i in range(len(lst_obj) // 2):
+        lst_obj.remove(max(lst_obj))
+    return max(lst_obj)
+
+
+print(get_mediana(lst_10))
+print(get_mediana(lst_100))
+print(get_mediana(lst_1000))
+print(timeit("get_mediana(lst_10)", globals=globals(), number=1000))
+print(timeit("get_mediana(lst_100)", globals=globals(), number=1000))
+print(timeit("get_mediana(lst_1000)", globals=globals(), number=1000))
