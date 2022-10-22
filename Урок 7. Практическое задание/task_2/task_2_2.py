a779f75619c709147356aa4967c060f0ecd5168a
Norovyatkin Ilya <dragon.nest9821@gmail.com>
2022-10-22 17:04:00 +0600

    Complete task_7_2(1.2.3)

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,36 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+
+from random import randint
+from timeit import timeit
+
+
+def get_mediana(obj_list):
+
+    temp = obj_list
+    for i in range(len(obj_list) // 2):
+        temp.remove(max(temp))
+    return max(temp)
+
+
+m = 10
+obj_list = [randint(1, 100) for _ in range(2*m+1)]
+print(get_mediana(obj_list[:]))
+print(timeit("get_mediana(obj_list[:])", globals=globals(), number=1000))
+
+m = 100
+obj_list = [randint(1, 100) for _ in range(2*m+1)]
+print(timeit("get_mediana(obj_list[:])", globals=globals(), number=1000))
+
+m = 1000
+obj_list = [randint(1, 100) for _ in range(2*m+1)]
+print(timeit("get_mediana(obj_list[:])", globals=globals(), number=1000))
+
+
+"""
+10 элементов - 0.007780406000165385
+100 элементов - 0.2441539700002977
+1000 элементов - 21.529072313999677
+"""
