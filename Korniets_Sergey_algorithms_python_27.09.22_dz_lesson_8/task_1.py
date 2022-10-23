b685c4c2733f267c81860206015e3bc0c475cb5a
korado2022 <korado_29@mail.ru>
2022-10-23 19:22:17 +0300

    Korniets_Sergey_algorithms_python_27.09.22_dz_lesson_8

---
+++ Korniets_Sergey_algorithms_python_27.09.22_dz_lesson_8/task_1.py
@@ -0,0 +1,56 @@
+"""
+Задание 1.
+
+Реализуйте кодирование строки по алгоритму Хаффмана.
+У вас два пути:
+1) тема идет тяжело? тогда вы можете,
+опираясь на примеры с урока, сделать свою версию алгоритма
+Разрешается и приветствуется изменение имен переменных,
+выбор других коллекций, различные изменения
+и оптимизации.
+
+2) тема понятна? постарайтесь сделать свою реализацию.
+Вы можете реализовать задачу, например,
+через ООП или предложить иной подход к решению.
+"""
+
+import heapq
+from collections import Counter, namedtuple
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def walk(self, code, acc):
+        code[self.char] = acc or "0"
+
+
+def huffman_code(s):
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, "")
+    return code
+
+text = "beep boop beer!"
+
+code_txt = huffman_code(text)
+
+for i in text:
+    print(code_txt[i], end=' ')
+print()
