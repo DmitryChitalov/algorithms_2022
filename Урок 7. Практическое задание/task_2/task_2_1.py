dfb9c8d04ae4dbcfd76d61dbdaa9b40bba9c64e5
RozhkovaDarya <102472078+RozhkovaDarya@users.noreply.github.com>
2022-10-23 18:47:39 +0200

    Update task_2_1.py

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,40 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+object = [2, 8, 5, 1, 4, 10, 0]
+
+
+def median(obj):
+    half = len(obj) // 2
+    obj.sort()
+    if not len(obj) % 2:
+        return (obj[half - 1] + obj[half]) / 2
+    return obj[half]
+
+print('Исходный массив:', object)
+print('Медиана:', median(object[:]))
+
+def shell(obj):
+    inc = len(obj) // 2
+    while inc:
+        for i, el in enumerate(obj):
+            while i >= inc and obj[i - inc] > el:
+                obj[i] = obj[i - inc]
+                i -= inc
+            obj[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+        
+shell(object)
+print(object) 
+
+object = [randint(-100, 100) for _ in range(10)]
+print(timeit('shell(object[:])', globals=globals, number=1000))
+
+object = [randint(-100, 100) for _ in range(100)]
+print(timeit('shell(object[:])', globals=globals, number=1000))
+
+object = [randint(-100, 100) for _ in range(1000)]
+print(timeit('shell(object[:])', globals=globals, number=1000))
