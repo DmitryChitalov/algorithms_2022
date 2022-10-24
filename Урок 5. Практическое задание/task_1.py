cc6190fce5afff0d6c223f81e7fbba78e361d6b5
vaganenko <vaganenko@1cbit.ru>
2022-10-25 00:29:10 +0400

    homework 5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -28,3 +28,54 @@
 Предприятия, с прибылью выше среднего значения: Рога
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import namedtuple
+
+company_count = int(input('Введите количество компаний: '))
+
+
+class Company:
+    def __init__(self):
+        self.name = input('Введите название компании: ')
+        self.profit = input('через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала):')
+        self.profit_sum = self.get_sum_profit()
+        self.profit_aver = self.profit_sum / 4
+
+    def create_list_of_profits(self):
+        return self.profit.split()
+
+    def get_sum_profit(self):
+        sum_profit = 0
+        profit_list = self.create_list_of_profits()
+        for i in profit_list:
+            sum_profit += float(i)
+        return sum_profit
+
+
+companies = namedtuple(
+    "companies",
+    "name sum_profit aver_profit")
+ar_companies = {}
+for i in range(company_count):
+    company = Company()
+    ar_companies[i] = companies(name=company.name, sum_profit=company.profit_sum, aver_profit=company.profit_aver)
+
+all_sum_profit = 0
+
+for i in ar_companies:
+    print(f'Предприятие: {ar_companies[i].name} прибыль за год - {ar_companies[i].sum_profit}, средняя прибыль'
+          f' - {ar_companies[i].aver_profit}')
+    all_sum_profit += ar_companies[i].sum_profit
+
+all_sum_aver_profit = all_sum_profit / company_count
+
+print(f'Средняя прибыль за год для всех предприятий {all_sum_aver_profit}')
+
+print('Предприятия, у которых прибыль выше среднего:')
+for i in ar_companies:
+    if (ar_companies[i].sum_profit > all_sum_aver_profit):
+        print(ar_companies[i].name)
+
+print('Предприятия, у которых прибыль ниже среднего:')
+for i in ar_companies:
+    if (ar_companies[i].sum_profit < all_sum_aver_profit):
+        print(ar_companies[i].name)
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,45 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+
+
+from timeit import timeit
+
+import collections
+
+
+my_list = []
+
+my_deque = collections.deque()
+
+
+def my_l(my):
+    return my.append(i for i in range(100000))
+
+
+def my_d(my):
+    return my.appendleft(i for i in range(100000))
+
+
+def my_l_2(my):
+    return my.insert(0, (i for i in range(100000)))
+
+
+
+print(f"{timeit('my_l(my_list)', globals=globals(), number=100000)}")
+print(f"{timeit('my_l(my_deque)', globals=globals(), number=100000)}")
+
+"""
+0.05901809997158125 -list
+0.14277710003079847 -deque
+append в деке работает медленее
+"""
+
+print(f"{timeit('my_l_2(my_list)', globals=globals(), number=100000)}")
+print(f"{timeit('my_d(my_deque)', globals=globals(), number=100000)}")
+
+"""
+1.8571822000085376 - list
+0.06057800003327429 - deque
+Вставка в начало в деке работает намного быстрее, чем в списке
+"""
