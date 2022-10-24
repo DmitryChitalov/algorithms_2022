6d009b9c61f3713eb35404a5e35d9afc25e97daf
kribodia <kribodia@mail.ru>
2022-10-24 14:25:26 +0200

    Доделал ДЗ_1 до конца

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,51 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+
+
+class Plates_stack_Class:
+    def __init__(self, max_size):
+        self.elems = [[]]
+        self.max_size = max_size
+
+    def __str__(self):
+        return str(self.elems)
+
+    def is_empty(self):
+        return self.elems == [[]]
+
+    def push_in(self, el):
+        """Заполнение с созданием списков в списке с ограничением их длинны (стэк)"""
+        if len(self.elems[len(self.elems) - 1]) < self.max_size:
+            self.elems[len(self.elems) - 1].append(el)
+        else:
+            self.elems.append([])
+            self.elems[len(self.elems) - 1].append(el)
+
+    def pop_out(self):
+        """удаление последнего элемента с последнего списка"""
+        return self.elems[len(self.elems) - 1].pop()
+
+    def get_val(self):
+        """значение текучего элемента"""
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        """количество вложенных списков"""
+        return len(self.elems)
+
+
+if __name__ == '__main__':
+    plates = Plates_stack_Class(2)
+
+    plates.push_in('1')
+    plates.push_in('2')
+    plates.push_in('3')
+    plates.push_in('4')
+    plates.push_in('5')
+    plates.push_in('6')
+    print(plates)
+    print(plates.pop_out())
+    print(plates.get_val())
+    print(plates.stack_size())
+    print(plates)
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -17,3 +17,61 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+
+class Board_of_Tasks:
+    def __init__(self):
+        self.all_tasks = QueueClass()
+        self.in_work_tasks = QueueClass()
+        self.finish_tasks = []
+
+    def add_to_all_tasks(self, item):
+        """наполнение базовой очереди"""
+        self.all_tasks.to_queue(item)
+
+    def from_all_tasks_to_in_work_tasks(self):
+        """с базовой в очередь на доработку"""
+        task = self.all_tasks.from_queue()
+        self.in_work_tasks.to_queue(task)
+
+    def from_all_tasks_to_finish_tasks(self):
+        """с базовой очереди в список завершенных"""
+        task = self.all_tasks.from_queue()
+        self.finish_tasks.append(task)
+
+    def from_in_work_tasks_to_finish_tasks(self):
+        """с очереди на доработку в список завершенных"""
+        task = self.in_work_tasks.from_queue()
+        self.finish_tasks.append(task)
+
+
+if __name__ == '__main__':
+    board_of_tasks = Board_of_Tasks()
+    board_of_tasks.add_to_all_tasks('TASK_1')
+    board_of_tasks.add_to_all_tasks('TASK_2')
+    board_of_tasks.add_to_all_tasks('TASK_3')
+    print(board_of_tasks.all_tasks.elems)
+    print(board_of_tasks.finish_tasks)
+    board_of_tasks.from_all_tasks_to_in_work_tasks()
+    print(board_of_tasks.in_work_tasks.elems)
+    board_of_tasks.from_in_work_tasks_to_finish_tasks()
+    print(board_of_tasks.finish_tasks)
+    board_of_tasks.from_all_tasks_to_in_work_tasks()
+    print(board_of_tasks.in_work_tasks.elems)
