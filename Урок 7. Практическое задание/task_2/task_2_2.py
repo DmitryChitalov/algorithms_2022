fea2856ae59028adfec55bc47ce9efebfa28c02b
Kamdiser <103354155+Kamdiser@users.noreply.github.com>
2022-10-25 12:55:24 +0300

    HT_7 task_2

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,35 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def find_median(m):
+    my_list = list(randint(1, 100) for _ in range(2 * m + 1))
+    while len(my_list) > m + 1:
+        my_list.pop(my_list.index(max(my_list)))
+    return len(my_list), max(my_list)
+
+
+print(
+    timeit(
+        "find_median(10)",
+        globals=globals(),
+        number=1000))
+print(
+    timeit(
+        "find_median(100)",
+        globals=globals(),
+        number=1000))
+print(
+    timeit(
+        "find_median(1000)",
+        globals=globals(),
+        number=1000))
+
+'''
+0.01786380005069077
+0.3424142999574542
+20.72079519997351
+'''
