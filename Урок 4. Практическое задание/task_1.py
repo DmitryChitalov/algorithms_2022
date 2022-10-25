b0a3ef5024361baf1b6fbc558c8b643e220d86d4
Yuliya Bryukhovskikh <makutu@ereb.ru>
2022-10-25 23:45:49 +0200

    lesson 4 task 1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +21,20 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+if __name__ == '__main__':
+
+    nums = [n for n in range(1000)]
+
+    time_of_func_1 = timeit("func_1(nums)", globals=globals(), number=10000)
+    time_of_func_2 = timeit("func_2(nums)", globals=globals(), number=10000)
+    percent = round(time_of_func_2/time_of_func_1*100, 2)
+    print("Время выполнения func_1:", time_of_func_1)
+    print("Для ускорения алгоритма используем list comprehension")
+    print("Время выполнения func_2:", time_of_func_2)
+    print(f"Скорость выполнения возрасла на {100-percent}%")
