2b347b60c8375116adfef1750e8fa98a77fc8952
Fo4doG <7249901@gmail.com>
2022-10-28 19:01:26 +0300

    hw_lesson_5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -28,3 +28,40 @@
 Предприятия, с прибылью выше среднего значения: Рога
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import namedtuple
+
+
+companies = namedtuple('Companies', 'id name quarter_1 quarter_2 quarter_3 quarter_4')
+
+
+def company_money():
+    count = int(input("Введите количество предприятий для расчета прибыли: "))
+    id = 0
+    gen_prof = 0
+    avarage_com = {}
+    for i in range(count):
+        id += 1
+        name = input('Введите название предприятия: ')
+        year = list(map(int,input("Через пробел введите прибыль данного предприятия"
+                                  "за каждый квартал(Всего 4 квартала):").split()))
+        company = companies(id, name=name,
+                            quarter_1=year[0],
+                            quarter_2=year[1],
+                            quarter_3=year[2],
+                            quarter_4=year[3])
+        avarage_com[company.name] = (company.quarter_1 + company.quarter_2 + company.quarter_3 + company.quarter_4)
+    for i in avarage_com.values():
+        gen_prof += i
+    gen_prof /= len(avarage_com)
+
+    profit_of_companies_lower_than_average = [i for i, j in avarage_com.items()
+                                              if j < gen_prof]
+    profit_of_companies_higher_than_average = [i for i, j in avarage_com.items()
+                                               if j >= gen_prof]
+
+    return f'Средняя годовая прибыль всех предприятий: {gen_prof}\n' \
+           f'Предприятия, с прибылью выше среднего значения: {" ".join(profit_of_companies_higher_than_average)}\n' \
+           f'Предприятия, с прибылью ниже среднего значения: {" ".join(profit_of_companies_lower_than_average)}'
+
+
+print(company_money())
