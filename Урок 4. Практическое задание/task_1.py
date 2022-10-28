458a866083dfe52566391f977fe40ff6c373d44f
AlexMer <alex.merekin@gmail.com>
2022-10-28 13:25:24 +0300

    lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,6 +11,10 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
+from timeit import timeit
+
+
+n = [num for num in range(100)]
 
 
 def func_1(nums):
@@ -19,3 +23,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+# current function time execution
+t1 = timeit("func_1(n)", globals=globals(), number=1000)
+print(f'current function time execution: {t1}')
+
+# function was updated by list comprehension format
+new_arr = [num for num in range(len(n)) if num % 2 == 0]
+
+t2 = timeit("[num for num in range(len(n)) if num % 2 == 0]", globals=globals(), number=1000)
+print(f'updated function time execution: {t2}')
+
+print(f'updated function time is different more than: {t1 / t2}')
+
+"""для оценки времени исполнения функции был использован модуль timeit,
+далее функция была скорректирована на исполнение
+через list comprehension, и скорость исполнения так же была замерена, 
+скорость измененной функции была увеличена более чем в 1,5 раза"""
+
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,9 @@
 """
 
 
+from timeit import timeit
+
+
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
         return
@@ -35,3 +38,33 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    l = []
+    for i in str(enter_num):
+        l.append(i)
+    l.reverse()
+    l = ''.join(l)
+    return l
+
+
+enter_num = 123456789
+
+print(f'revers time execution: {timeit("revers(enter_num)", globals=globals())}')
+print(f'revers_2 time execution: {timeit("revers_2(enter_num)", globals=globals())}')
+print(f'revers_3 time execution: {timeit("revers_3(enter_num)", globals=globals())}')
+print(f'revers_4 time execution: {timeit("revers_4(enter_num)", globals=globals())}')
+
+"""Учитывая результаты исполнения четырех функций:
+revers time execution: 2.6963815
+revers_2 time execution: 1.8414309579999997
+revers_3 time execution: 0.37559666699999994
+revers_4 time execution: 1.236689084
+
+Наиболее быстрое исполнение у функции revers_3 работающей на основе срезов.
+Самое длительное время исполнения у фунции на основе рекурсии.
+"""
+
+
+
