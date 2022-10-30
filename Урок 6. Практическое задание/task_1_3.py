7b5734f12df0601caf8b81f8bffb0a5bf87ce5fb
kirill <afanas.lg2@gmail.com>
2022-10-30 03:21:32 +0300

    Lesson_6 - Афанасьевский Кирилл

--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -30,3 +30,52 @@
 
 Это файл для третьего скрипта
 """
+
+from memory_profiler import profile
+from random import randrange
+import numpy
+
+
+#  Исходный код
+@profile
+def change():
+    my_list = [randrange(1000) for _ in range(100000)]
+    for i in range(0, len(my_list) // 2 * 2, 2):
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+
+#  Оптимизированный код
+@profile
+def change_numpy():
+    my_list = numpy.array([randrange(1000) for _ in range(100000)])
+    for i in range(0, len(my_list) // 2 * 2, 2):
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+
+if __name__ == '__main__':
+    change()
+    change_numpy()
+
+"""
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    40     30.8 MiB     30.8 MiB           1   @profile
+    41                                         def change():
+    42     35.2 MiB      4.4 MiB      100003       my_list = [randrange(1000) for _ in range(100000)]
+    43     35.2 MiB      0.0 MiB       50001       for i in range(0, len(my_list) // 2 * 2, 2):
+    44     35.2 MiB      0.0 MiB       50000           my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+    49     30.8 MiB     30.8 MiB           1   @profile
+    50                                         def change_numpy():
+    51     34.5 MiB      2.7 MiB      100003       my_list = numpy.array([randrange(1000) for _ in range(100000)])
+    52     33.5 MiB     -1.0 MiB       50001       for i in range(0, len(my_list) // 2 * 2, 2):
+    53     33.5 MiB      0.0 MiB       50000           my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+
+"""
+
+"""
+Сократили использования памяти с помощью библиотеки NumPy и ф-ции array.
+"""
