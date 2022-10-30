28bb49f5dcd1ba5a8e37a8e09e9f551bd8f85392
Fo4doG <7249901@gmail.com>
2022-10-30 19:28:04 +0300

    hw_lesson_7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,46 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+import random
+from timeit import timeit
+
+
+def generate_lst(n):
+    lst = [random.randint(-100, 100) for i in range(2 * n + 1)]
+    return lst
+
+
+def gnome_sort(lst):
+    index = 1
+    i = 0
+    while i < len(lst) - 1:
+        if lst[i] <= lst[i + 1]:
+            i, index = index, index + 1
+        else:
+            lst[i], lst[i + 1] = lst[i + 1], lst[i]
+            if i > 0:
+                i -= 1
+            else:
+                i, index = index, index + 1
+    mediana = lst[len(lst) // 2]
+    return lst, mediana
+
+
+# m = int(input())
+# unsorted_lst = generate_lst(m)
+# print(gnome_sort(unsorted_lst))
+
+
+lst_1 = generate_lst(10)
+lst_2 = generate_lst(100)
+lst_3 = generate_lst(1000)
+print(timeit("gnome_sort(lst_1)", globals=globals(), number=1000))
+print(timeit("gnome_sort(lst_2)", globals=globals(), number=1000))
+print(timeit("gnome_sort(lst_3)", globals=globals(), number=1000))
+
+
+"""
+10 - 0.0019659000099636614
+100 - 0.019264100003056228
+1000 - 0.3686249000020325
+"""
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,40 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+import random
+from timeit import timeit
+
+
+def generate_lst(n):
+    lst = [random.randint(-100, 100) for i in range(2 * n + 1)]
+    return lst
+
+
+"""
+Еще есть вариант через удаление максимального элемента, до того как длинна массива не станет равной m + 1,  и оттуда взять max"""
+def mediana_without_sort(lst):
+    n = len(lst) // 2
+    while len(lst) > n + 1:
+        lst.pop(lst.index(max(lst)))
+    return max(lst)
+
+
+# m = int(input())
+# lst = generate_lst(m)
+# print(lst)
+# print(mediana_without_sort(lst))
+
+
+lst_1 = generate_lst(10)
+lst_2 = generate_lst(100)
+lst_3 = generate_lst(1000)
+print(timeit("mediana_without_sort(lst_1)", globals=globals(), number=1000))
+print(timeit("mediana_without_sort(lst_2)", globals=globals(), number=1000))
+print(timeit("mediana_without_sort(lst_3)", globals=globals(), number=1000))
+
+
+"""
+10 - 0.0002167999919038266
+100 - 0.0004555000050459057
+1000 - 0.023943099979078397
+"""
