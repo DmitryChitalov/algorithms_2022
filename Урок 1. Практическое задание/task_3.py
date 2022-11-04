de48640a1abe6cc1db38f271dfb64f70cff510e8
Фадеев Егор Владимирович <Egor.Fadeev@incomsystem.ru>
2022-11-04 20:51:25 +0300

    Lesson 1 Fadeev E.V.

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,28 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+company = {
+    'Saudi Aramco': 105369,
+    'Alphabet': 76033,
+    'Apple': 94680,
+    'Microsoft': 61271,
+    'Berkshire Hathaway': 89795
+}
+
+print('\nРешение №1: сложность O(n log n)\n')
+company_sorted = sorted(company.items(), key=lambda item: item[1], reverse=True)  # O(n log n)
+for i in range(3):  # O(1)
+    print(f'Компания №{i + 1}: {company_sorted[i][0]}, прибыль {company_sorted[i][1]}')  # O(1) + O(1) = O(1)
+
+print('\nРешение №2: сложность O(n^2)\n')
+profit = list()
+for key, value in company.items():  # O(n)
+    profit.append(value)
+for i in range(len(profit)):  # O(n^2)
+    for j in range(len(profit)):
+        if profit[i] > profit[j]:
+            profit[i], profit[j] = profit[j], profit[i]
+for i in range(3):  # O(n)
+    for key, value in company.items():
+        if profit[i] == value:
+            print(f'Компания №{i + 1}: {key}, прибыль {value}')
