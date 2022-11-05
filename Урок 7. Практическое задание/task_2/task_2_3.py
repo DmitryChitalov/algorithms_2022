e54c68ec2eae04063819a9393aa5b339a3022ca0
vmrcx <hlh7520@gmail.com>
2022-11-05 06:05:18 +0300

    task_2_3

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,27 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+
+from statistics import median
+from random import randint
+from timeit import timeit
+
+def find_median(arrnum):
+    return median(arrnum[:])
+
+m = 10
+
+arrnum = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit('find_median(arrnum[:])', globals=globals(), number=100)) # 0.00030559999868273735
+
+m = 100
+
+arrnum = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit('find_median(arrnum[:])', globals=globals(), number=100)) # 0.0030718001071363688
+
+m = 1000
+
+arrnum = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(timeit('find_median(arrnum[:])', globals=globals(), number=100)) # 0.03306139982305467
+
+# Вывод: Самым эффективным способом является использование встроенной функции median
