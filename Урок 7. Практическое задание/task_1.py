c35b1cb2cfacf41d4a5ae35e8f6865b0d55e487e
AlexMer <alex.merekin@gmail.com>
2022-11-11 12:30:41 +0300

    lesson_8

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,38 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+"""Сортировка пузырьком"""
+
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_new(lst_obj):
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(lst_obj) - 1):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+                flag = True
+
+
+orig_list = [randint(-100, 100) for _ in range(100)]
+
+print(orig_list)
+print(bubble_sort(orig_list))
+
+t1 = timeit('bubble_sort(orig_list[:])', globals=globals(), number=100)
+t2 = timeit('bubble_sort_new(orig_list[:])', globals=globals(), number=100)
+
+print(f'Прирост скорости: {(t1-t2)/t1*100}%')
