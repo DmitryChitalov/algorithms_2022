3bcfe7a60b5c5d8b7d93bb09b09c258b45e2c72f
Vlacksy <19vlad89@gmail.com>
2022-11-11 15:49:15 +0500

    Homework 1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,27 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+import random
+
+
+def get_min_val_1(nums):    # O(n**2)
+    length = len(nums)                                      # O(1)
+    for i in range(length):                                 # O(n)
+        for j in range(length - i - 1):                     # O(n)
+            if nums[j] > nums[j + 1]:                       # O(1)
+                nums[j], nums[j + 1] = nums[j + 1], nums[j] # O(1)
+    return nums[0]                                          # O(1)
+
+def get_min_val_2(nums):    # O(n)
+    min_val = nums[0]   # O(1)
+    for i in nums:      # O(n)
+        if i < min_val: # O(1)
+            min_val = i # O(1)
+    return min_val      # O(1)
+
+if __name__ == '__main__':
+    lst = [random.randint(-10, 10) for i in range(10)]
+
+    print(lst)
+    print(get_min_val_1(lst))
+    print(get_min_val_2(lst))
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -14,6 +14,80 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 --реализуйте по аналогии с примером, рассмотренным на уроке
---создание нового стопки можно реализовать добавлением нового пустого массива
+--создание новой стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+class StackClass:
+
+    def __init__(self):
+        self.elems = [[]]
+        self.fullsize = 3
+
+    def is_empty(self):
+        return self.elems == [[]]
+
+    def push_in(self, el):
+        if len(self.elems[-1]) < self.fullsize:
+            return self.elems[-1].append(el)
+        else:
+            return self.elems.append([el])
+
+    def pop_out(self):
+        if len(self.elems[-1]) == 1:
+            return self.elems.pop().pop()
+        else:
+            return self.elems[-1].pop()
+
+    def get_val(self):
+        return self.elems[-1][-1]
+
+    def stack_size(self):
+        if len(self.elems[-1]) == self.fullsize:
+            return len(self.elems) * self.fullsize
+        else:
+            return len(self.elems) * self.fullsize - (self.fullsize - len(self.elems[-1]))
+
+
+if __name__ == '__main__':
+    SC_OBJ = StackClass()
+
+    print(SC_OBJ.is_empty())                # -> True - стек пустой
+
+    # наполнение стека
+    SC_OBJ.push_in(1)
+    SC_OBJ.push_in('one')
+    SC_OBJ.push_in('один')
+    SC_OBJ.push_in('2')
+    SC_OBJ.push_in('two')
+    SC_OBJ.push_in('два')
+    SC_OBJ.push_in(3)
+    SC_OBJ.push_in('three')
+
+    # значение первого элемента с конца
+    print(SC_OBJ.get_val())                 # -> three
+
+    # размер стека
+    print(SC_OBJ.stack_size())              # -> 8
+
+    print(SC_OBJ.is_empty())                # -> False - стек не пустой
+
+    # добавление еще одного элемента в стек
+    SC_OBJ.push_in('три')
+
+    # значение первого элемента с конца
+    print(SC_OBJ.get_val())                 # -> три
+
+    # размер стека
+    print(SC_OBJ.stack_size())              # -> 9
+
+    # удаление последнего элемента стека и вывод его значения
+    print(SC_OBJ.pop_out())                 # -> три
+
+    # удаление последнего элемента стека и вывод его значения
+    print(SC_OBJ.pop_out())                 # -> three
+
+    # размер стека
+    print(SC_OBJ.stack_size())              # -> 7
+
+    # элементы в стеке
+    print(SC_OBJ.elems)                     # -> [[1, 'one', 'один'], ['2', 'two', 'два'], [3]]
