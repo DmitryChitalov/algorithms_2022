0e3be6acec3c8c10186302fcc31b58bc65ceb24c
Medvedev Artem <98592883+7arts@users.noreply.github.com>
2022-11-13 19:19:53 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,13 +1,33 @@
 from memory_profiler import profile
 
 @profile
-def func(n):
-    def invert(n):
-        if n < 10:
-            return str(n)
-        else:
-            return str(n % 10) + invert(n // 10)
-    return str(n % 10) + invert(n // 10)
+def func():
+    def calc():
+        res = input("Введите операцию (+, -, *, / или 0 для выхода): ")
+        if res not in ('+', '-', '*', '/', '0'):
+            return print("Неизвестная операция"), calc()
+        if res == '0':
+            return print("Вы вышли из калькулятора")
+        try:
+            num_1 = int(input('Введите первое число: '))
+            num_2 = int(input('Введите второе число: '))
+        except ValueError:
+            return print("Ошибка: Введите число!"), calc()
+
+        if res == '+':
+            return print(f'Ваш результат {num_1 + num_2}'), calc()
+        elif res == '-':
+            return print(f'Ваш результат {num_1 - num_2}'), calc()
+        elif res == '*':
+            return print(f'Ваш результат {num_1 * num_2}'), calc()
+        elif res == '/' and num_2 != 0:
+            return print(f'Ваш результат {num_1 / num_2}'), calc()
+        try:
+            num_1 / 0
+        except ZeroDivisionError:
+            return print("На 0 делить нельзя"), calc()
+    return calc()
+func()
 
 '''
 Так как функция рекурсивная, то количество таблиц с замерами соответствует количеству вызовов
