66305cd765873a5ff5060add4b4e529fe748b061
Dmitry <budilovdmi@gmail.com>
2022-11-14 14:36:18 +1200

    HW lesson_1 Budilov

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,43 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+users = {
+        'user_1': {'password': 'password_1', 'active': True},
+        'user_2': {'password': 'password_2', 'active': False},
+        'user_3': {'password': 'password_3', 'active': False},
+        'user_4': {'password': 'password_4', 'active': True},
+        'user_5': {'password': 'password_5', 'active': False},
+        'user_6': {'password': 'password_6', 'active': True}
+    }
+
+def authentication_1(user): # Общая скорость выполнения O(1)
+    print("authentication_1:")
+    if user['username'] in users:                                       # O(1)
+        if users[user['username']]['active']:                           # O(1)
+            if user['password'] == users[user['username']]['password']: # O(1)
+                return 'Access is allowed.'                             # O(1)                                              # O(1)
+            return 'Access is denied.'                                  # O(1)
+        return 'Activate your account!'                                 # O(1)
+    return 'User not registered.'                                       # O(1)
+
+def authentication_2(user): # Общая скорость выполнения O(n)
+    print("\nauthentication_2:")
+    for key, value in users.items():                                    # O(n)
+        if user['username'] == key:                                     # O(1)
+            if value['active']:                                         # O(1)
+                if user['password'] != value['password']:               # O(1)
+                    return 'Access is denied.'                          # O(1)
+                return 'Access is allowed.'                             # O(1)
+            return 'Activate your account!'                             # O(1)
+    return 'User not registered.'                                       # O(1)
+
+# Скорость выполнения первого первой функции быстрее, 
+# так как отсутствует необходимость в переборе элементов.
+
+if __name__ == '__main__':
+
+    user = {'username': input("Username: "), 'password': input("Password: ")}
+    print('\n')
+    print(authentication_1(user))
+    print(authentication_2(user))
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -17,3 +17,79 @@
 --создание нового стопки можно реализовать добавлением нового пустого массива
 в массив стопок (lst = [[], [], [], [],....]).
 """
+
+class StackOfPlates(object):
+    """ Класс 'Стопка тарелок'. 
+
+    Args:
+        stack_limit (int): максимальный размер стопки
+    
+    Methods:
+        is_empty    -> bool : Проверка стека на наличие элементов.
+        put_plate   -> None : Создать новый стек при переполнении предыдущего или пустом стеке стеков и добавить в него элемент.
+        take_plate  -> None : Вернуть последний элемент из стека.
+        show_stacks -> str  : Показать количество элементов стопок с количество стопок.
+    """
+
+    __slots__ = ('stack_of_stacks', 'stack_limit')
+
+    def __init__(self, stack_limit: int) -> None:
+        self.stack_of_stacks: list = []
+        self.stack_limit: int = stack_limit
+
+    def is_empty(self) -> bool:
+        # Проверить стек на наличие элементов.
+        return self.stack_of_stacks == []
+
+    def put_plate(self) -> None:
+        # Создать новый стек при переполнении предыдущего или пустом стеке стеков и добавить в него элемент
+        if self.is_empty() or len(self.stack_of_stacks[-1]) == self.stack_limit:
+            self.stack_of_stacks.append([])
+        self.stack_of_stacks[-1].append('plate')
+    
+    def take_plate(self) -> None:
+        # Вернуть последний элемент из стека
+        if self.is_empty():
+            print("No plate to take.")
+        elif not self.is_empty() and len(self.stack_of_stacks[-1]) == 1:
+            self.stack_of_stacks[-1].pop()
+            self.stack_of_stacks.pop()
+        else:
+            self.stack_of_stacks[-1].pop()
+
+    def show_stacks(self) -> str:
+        # Показать количество элементов стопок с количество стопок.
+        if self.is_empty():
+            print("Stack is empty.")
+        else:
+            for number, stack in enumerate(self.stack_of_stacks):
+                print(f'Stack number {number + 1}:  {stack}')
+
+    def __str__(self) -> str:
+        if self.is_empty():
+            return 'Stack is empty'
+        else:
+            sum_of_plates: int = sum([len(stack) for stack in self.stack_of_stacks])
+            len_stack_of_stacks: int = len(self.stack_of_stacks)
+            return f"{sum_of_plates} plats in {len_stack_of_stacks} staks."
+
+
+if __name__ == '__main__':
+    
+    stacks = StackOfPlates(stack_limit=4)
+    for _ in range(9):
+        stacks.put_plate()
+    stacks.show_stacks()
+    print(stacks)
+
+    for _ in range(5):
+        stacks.take_plate()
+    stacks.show_stacks()
+    print(stacks)
+
+    for _ in range(5):
+        stacks.take_plate()
+    stacks.show_stacks()
+    print(stacks)
+    
+    
