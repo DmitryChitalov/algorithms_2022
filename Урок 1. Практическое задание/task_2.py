f49869240299aa77234d82c4f90ae515d5786ce7
Timofei Nazarov <timofejnazarov@gmail.com>
2022-11-15 18:12:36 +0300

    timofei nazarov lesson 1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,28 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+test_lst = [30, 24, 25, 6, 47, 19, 21, 10, 10, 44]
+
+
+def first_min_el(lst):                  # O(n^2)
+    min_els = [lst[0]]                  # O(1)
+    for el in lst:                      # O(n)
+        if el < min_els[0]:             # O(1)
+            min_els.insert(0, el)       # O(n)
+        else:
+            pass                        # O(1)
+    return min_els[0]                   # O(1)
+
+
+def second_min_el(lst):                             # O(n)
+    min_els = [lst[0]]                              # O(1)
+    for i in range(len(lst)):                       # O(n)
+        if lst[i] < min_els[(len(min_els) - 1)]:    # O(1)
+            min_els.append(lst[i])                  # O(1)
+        else:
+            pass
+    return min_els[(len(min_els) - 1)]              # O(1)
+
+
+print(first_min_el(test_lst))
+print(second_min_el(test_lst))
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -16,4 +16,27 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+companies_list = {'asus': 12853, 'apple': 220859, 'samsung': 3541, 'rzd': 28356,
+                  'bork': 259813, 'xiaomi': 14073, 'jeep': 250983, 'nissan': 43350}
 
+
+def first_method(companies):                                                            # O(n log n)
+    sorted_values = sorted(companies.items(), key=lambda item: item[1], reverse=True)   # O(n log n)
+    return sorted_values[:3]                                                            # O(n)
+
+
+def second_method(companies):           # O(n^2)
+    lst = []                            # O(1)
+    for k in companies:                 # O(n)
+        lst.append(companies[k])        # O(1)
+    lst.sort(reverse=True)              # O(n log n)
+    top3 = []                           # O(1)
+    for el in lst[:3]:                  # O(n)
+        for k, v in companies.items():  # O(n)
+            if v == el:                 # O(1)
+                top3.append((k, v))     # O(1)
+    return top3                         # O(1)
+
+
+print(first_method(companies_list))
+print(second_method(companies_list))
