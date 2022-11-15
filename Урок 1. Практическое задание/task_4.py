016794e94b2cd0f4239fab711974b315a8196184
Фадеев Егор Владимирович <Egor.Fadeev@incomsystem.ru>
2022-11-15 18:05:27 +0300

    homework 1 revision 1 Fadeev EV

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,40 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+
+def is_granted_1(accounts: dict, login: str, passw: str) -> str:  # O(n)
+    for key, val in accounts.items():
+        if login == key:
+            if passw == val[0]:
+                if val[1] == 'activated':
+                    return f'{key}, добро пожаловать'
+                else:
+                    return f'{key}, пройдите активацию'
+    return f'Пожалуйста, проверьте логин и пароль, или зарегистрируйтесь'
+
+
+def is_granted_2(accounts: dict, login: str, passw: str) -> str:  # O(1)
+    if accounts.get(login) is None or accounts.get(login)[0] != passw:
+        return f'Пожалуйста, проверьте логин и пароль, или зарегистрируйтесь'
+    else:
+        if not accounts.get(login)[1] == 'activated':
+            return f'{login}, пройдите активацию'
+        else:
+            return f'{login}, добро пожаловать'
+
+
+user_accounts = {
+    'ivan': ('1234', 'not activated'),
+    'egor': ('6487', 'activated'),
+    'grigorij': ('7381', 'not activated')
+}
+
+print('\n')
+print(is_granted_1(user_accounts, 'egor', '6487'))
+print(is_granted_1(user_accounts, 'grigorij33', '6488'))
+print(is_granted_1(user_accounts, 'ivan', '1234'))
+print('\n')
+print(is_granted_2(user_accounts, 'egor', '6487'))
+print(is_granted_2(user_accounts, 'grigorij33', '6488'))
+print(is_granted_2(user_accounts, 'ivan', '1234'))
