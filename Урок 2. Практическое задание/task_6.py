55d90e4daab98633218c26b276e538ad3cc52dfb
Dmitry <budilovdmi@gmail.com>
2022-11-17 22:19:59 +1200

    Task 6 complited

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,28 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+from random import randint
+
+def guess_number(counter: int=10, number: int=randint(0,100)) -> str:
+    # Угадать число за 10 попыток
+    if counter == 0:
+        return "You lose!"
+    try:
+        guess: int = int(input(f"Try to guess the number from 0 to 100! You have {counter} tries: "))
+    except ValueError as exc:
+        print(f'{exc.args[0].split()[-1]} is not a number! you lost the try.')
+        return guess_number(counter-1, number=number)
+    if guess == number:
+        return "\nCongratulations! You guessed!!!"
+    print("Too little.") if number > guess else print("Too much.")
+    return guess_number(counter-1, number=number)
+
+if __name__ == "__main__":
+    print("""  _____             _                                   
+ |_   _| __ _   _  | |_ ___     __ _ _   _  ___ ___ ___ 
+   | || '__| | | | | __/ _ \   / _` | | | |/ _ / __/ __|
+   | || |  | |_| | | || (_) | | (_| | |_| |  __\__ \__ \\
+   |_||_|   \__, |  \__\___/   \__, |\__,_|\___|___|___/
+            |___/              |___/                    
+            """)
+    print(guess_number())
