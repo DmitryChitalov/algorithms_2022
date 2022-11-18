acd6719c429eb82ef47226207d19aa07445823e3
Nikita <zephyr3589@gmail.com>
2022-11-18 11:23:26 +0300

    Урок_1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,35 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+
+companies = {'Toyota':150000,'Nissan':200000,'Honda':175000,'Suzuki':190000,'Lexus':250000}
+
+# Cложность - O(n**2)
+def max_profit_1(data):
+    sorted_values = sorted(data.values(),reverse=True)
+    sorted_dict = {}
+
+    for i in sorted_values:
+        for k in data.keys():
+            if data[k] == i:
+                sorted_dict[k] = data[k]
+                if len(sorted_dict) == 3:
+                    return sorted_dict
+
+
+print(max_profit(companies))
+
+# Сложность O(n log n)
+def by_value(data):
+    return data[1]
+
+
+max_profit = {}
+i = 0
+for k, v in sorted(companies.items(), key=by_value, reverse=True):
+    if i < 3:
+        max_profit.setdefault(k, v)
+    i = i + 1
+print(max_profit)
+
+
