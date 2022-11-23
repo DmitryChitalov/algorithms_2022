a599beb2dbfb92c1c59bc3f848b52597a3fa94ff
Sergey Mytsykov <smytsykov@awg.ru>
2022-11-23 10:00:35 +0300

    Задание 1 - 4 Лекция 4 Мыцыков Сергей https://t.me/smytsykov

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,9 +13,28 @@
 """
 
 
+from timeit import timeit
+from random import randint
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i, x in enumerate(nums) if x % 2 == 0]
+
+
+if __name__ == '__main__':
+    my_nums = [randint(1, 1000) for my_num in range(1000)]
+
+    print(timeit("func_1(my_nums)", number=10000, globals=globals()))
+    print(timeit("func_2(my_nums)", number=10000, globals=globals()))
+
+"""
+List comprehension выигрывает по скорости из-за того, то не вызывает метод append у списка
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -10,6 +10,8 @@
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу
 """
 
+from timeit import timeit
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -37,5 +39,25 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3():
+    max_val, elem = max([(array.count(elem), elem) for elem in set(array)])
+    return f'Чаще всего встречается число {max_val}, ' \
+           f'оно появилось в массиве {elem} раз(а)'
+
+
+def func_4():
+    max_val, elem = max([(elem, array.count(elem)) for elem in set(array)], key=lambda x: x[1])
+    return f'Чаще всего встречается число {max_val}, ' \
+           f'оно появилось в массиве {elem} раз(а)'
+
+
+if __name__ == '__main__':
+    print(timeit("func_1()", number=100000, globals=globals()))
+    print(timeit("func_2()", number=100000, globals=globals()))
+    print(timeit("func_3()", number=100000, globals=globals()))
+    print(timeit("func_4()", number=100000, globals=globals()))
+
+"""
+Ускорение при помощи множества будет видно на большом количестве элементов
+Так как поиск идет по 1ому элементу, можно выбрать другой элемент, но скорость уменьшается
+"""
