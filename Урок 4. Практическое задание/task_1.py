12e7f1135534cba4d899902bfd46756249e49460
Dmitriy Rovenskiy <arhitec@bk.ru>
2022-11-25 19:27:25 +0300

    lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,21 +1,49 @@
 """
 Задание 1.
-
 Приведен код, который позволяет сохранить в
 массиве индексы четных элементов другого массива
-
 Сделайте замеры времени выполнения кода с помощью модуля timeit
-
-Попробуйте оптимизировать код, чтобы снизить время выполнения
+Оптимизируйте, чтобы снизить время выполнения
 Проведите повторные замеры
-
-ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
+Добавьте аналитику: что вы сделали и почему!!!
+Без аналитики задание считается не принятым
 """
+from timeit import timeit
 
 
-def func_1(nums):
-    new_arr = []
-    for i in range(len(nums)):
+def func_1(numss):
+    new_arr_1 = []
+    for i in range(len(numss)):
         if nums[i] % 2 == 0:
-            new_arr.append(i)
-    return new_arr
+            new_arr_1.append(i)
+    return new_arr_1
+
+
+print(timeit("func_1([3, 0, 0, 0])", "from __main__ import func_1", number=1000000))
+
+
+def func_2(numss):
+    new_arr_2 = []
+    for i, j in enumerate(numss):
+        if j % 2 == 0:
+            new_arr_2.append(i)
+    return new_arr_2
+
+
+print(timeit("func_2([3, 0, 0, 0])", "from __main__ import func_2", number=1000000))
+
+nums = [3, 0, 0, 0]
+
+new_arr = [i if j % 2 == 0 else None for i, j in enumerate(nums)]
+
+print(timeit('''new_arr = [i if j % 2 == 0 else None for i, j in enumerate(nums)]''',
+             globals=globals(),
+             number=1000000))
+
+'''
+Для оптимизации сначала была использована встроенная функция "enumerate",
+замеры показали уменьшение времени работы функции,
+далее цикл был заменён на более совершенный способ формирования списка - 
+"list comprehensions" показал более высокую скорость работы по сравнению 
+с остальными функциями.
+'''
