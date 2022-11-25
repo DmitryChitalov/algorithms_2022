a5e8f848f647ad1c86713d82c89d80fa55d05e4d
timofei nazarov <timofejnazarov@gmail.com>
2022-11-25 23:17:19 +0300

    lesson_4 final

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,6 +11,8 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
+from timeit import timeit
+test_num = 123456
 
 
 def revers(enter_num, revers_num=0):
@@ -35,3 +37,32 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(num):
+    num = str(num)
+    r_num = ''
+    for i in range(len(num) - 1, -1, -1):
+        r_num += num[i]
+    return r_num
+
+
+def revers_5(num):
+    revers_num = ''.join(sorted(str(num), reverse=True))
+    return revers_num
+
+
+print(timeit("revers(test_num)", globals=globals(), number=10000))
+print(timeit("revers_2(test_num)", globals=globals(), number=10000))
+print(timeit("revers_3(test_num)", globals=globals(), number=10000))
+print(timeit("revers_4(test_num)", globals=globals(), number=10000))
+print(timeit("revers_5(test_num)", globals=globals(), number=10000))
+
+"""
+Вывод: наиболее быстрым способом является фунция 3 - использование среза. Т.к. в этом случае нам не приходится делать
+дополнительные вычисления, использовать перебор элементов в цикле или прибегать к рекурсии.
+
+Самая затратная по времени функция - с использованием рекурсии. Цикл while в данном случае работает медленнее 
+чем цикл for, решение с помощью join медленнее решения с помощью срезов, т.к. создается еще и спиок с 
+обратной сортировкой.
+"""
