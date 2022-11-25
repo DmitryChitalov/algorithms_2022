123dad6348bdb5b8406b0a41a15112750f4511c2
timofei nazarov <timofejnazarov@gmail.com>
2022-11-25 22:42:23 +0300

    Исправлены задания 1, 2 3го урока.

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -49,7 +49,7 @@ def average_time_check(function):
             end_time = time.time()
             timing = end_time - start_time
             time_lst.append(timing)
-        print(f' среднее время при запуске 10 раз {function} = {sum(time_lst)}')
+        print(f' среднее время при запуске 10 раз {function} = {sum(time_lst) / 10}')
     return wrapper
 
 
@@ -69,8 +69,17 @@ def fill_dict(num, dict_obj):   # O(1)
     return dict                 # O(1)
 
 
-# fill_lst(10**6, [])
-# fill_dict(10**6, {})
+fill_lst(10**6, [])
+fill_dict(10**6, {})
+
+"""
+среднее время при запуске 10 раз <function fill_lst at 0x0000028C28D9A440> = 0.13906598091125488
+среднее время при запуске 10 раз <function fill_dict at 0x0000028C28D9A560> = 0.1078822135925293
+
+В данном примере список заполняется при помощи метода append, а словарь заполняется при помощи доступа по индексу, 
+поэтому время заполнения словаря больше. Так же в этом примере в функцию передается пустой список или словарь, если 
+создавать объекты внутри функций создание словаря занимало бы больше времени чем создание списка.  
+"""
 
 
 # -- Пункт - b
@@ -88,16 +97,24 @@ def take_dict_el(dict_obj, value):  # O(N)
             return v                # O(1)
 
 
-snd_lst = [el for el in range(10**6)]
-snd_dict = {n: n for n in range(10**6)}
+snd_lst = [el for el in range(10**7)]
+snd_dict = {n: n for n in range(10**7)}
 
 take_lst_el(snd_lst, 15)
 take_dict_el(snd_dict, 15)
 
+"""
+среднее время при запуске 10 раз <function take_lst_el at 0x000001C6BFF3A7A0> = 0.001562509536743164
+среднее время при запуске 10 раз <function take_dict_el at 0x000001C6BFF3A8C0> = 0.0031246662139892576
+При возвращении значения из словаря тратиться больше времени, чем при возвращении значения из списка,
+т.к. словарь хранит в себе помимо значений еще и ключи. В данном примере не используется доступ по индексу
+методы pop, что могло бы ускорить получение значения.
+"""
+
 
 # -- Пункт c:
 @average_time_check
-def remove_from_lst(lst_obj, val):  # O(N^2)
+def remove_from_lst(lst_obj, val):  # O(N)
     for el in lst_obj:              # O(N)
         if el == val:               # O(1)
             lst_obj.remove(el)      # O(N)
@@ -115,3 +132,10 @@ def remove_from_dict(dict_obj, val):    # O(N)
 
 remove_from_lst(snd_lst, 256)
 remove_from_dict(snd_dict, 256)
+"""
+среднее время при запуске 10 раз <function remove_from_lst at 0x000001C56D58A9E0> = 0.482387900352478
+среднее время при запуске 10 раз <function remove_from_dict at 0x000001C56D58AB00> = 1.1064233303070068
+
+При одинаковой сложности с точки зрения O-нотации, удаление элемента из словаря занимает больше времени,
+т.к. в цикле for для словаря приходится перебирать не только значения values, но и keys.
+"""
