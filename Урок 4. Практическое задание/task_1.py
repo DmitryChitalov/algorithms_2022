7f5f3e58aab8bf2312d126683cd82566b4d24023
Krooper <98321262+Krooper@users.noreply.github.com>
2022-11-27 18:49:45 +0300

    made task_1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -11,6 +11,7 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
+from timeit import Timer
 
 
 def func_1(nums):
@@ -19,3 +20,32 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+test_list = [i for i in range(10**3)]
+# print(test_list)
+
+t1 = Timer(stmt="func_1(test_list)", setup="from __main__ import func_1, test_list")
+print("append -", t1.timeit(number=100000), "sec")
+# print(func_1(test_list))
+print('-'*100)
+
+"""
+append - 6.450531599999522 sec
+"""
+
+
+# Можно использовать генератор с ф-ей enumerate:
+def test_lst_compr_enum(nums):
+    new_arr = [key for key, value in enumerate(nums) if not value % 2]
+    return new_arr
+
+
+t2 = Timer(stmt="test_lst_compr_enum(test_list)", setup="from __main__ import test_lst_compr_enum, test_list")
+print("list comprehension + enumerate -", t2.timeit(number=100000), "sec")
+# print(test_lst_compr_enum(test_list))
+print('-'*100)
+
+"""
+list comprehension + enumerate - 5.299689699997543 sec
+"""
