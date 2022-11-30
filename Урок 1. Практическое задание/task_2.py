94e7885eca2d1a1346bb2ecefa6e595c05c6ddb4
kkpandatime <lapinadasha1@gmail.com>
2022-11-30 14:21:09 +0300

    Lesson 1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,30 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+def min_list(enum):
+    for i in enum:
+        min_value = True
+        for el in enum:
+            if i > el:
+                min_value = False
+        if min_value:
+            return i
+
+# Сложность - О(N^2)
+
+def min_list_o(enum_o):
+    min_val = enum_o[0]
+    for i in enum_o:
+        if i < min_val:
+            min_val = i
+    return min_val
+
+# Сложность - О(N)
+list = [1000, -8, 5, 6, 100]
+res = min_list(list)
+print(res)
+result = min_list_o(list)
+print(result)
+
+
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -22,3 +22,44 @@
 
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
+
+# Сложность - O(n)
+def authentication(users, login, password):
+    for k, v in users.items():      # O(n)
+        if k == login:         # O(n)
+            if v['password'] == password and v['activation']:   # O(n)
+                return "Доступ разрешен"        # O(n)
+            elif v['password'] == password and not v['activation']:     # O(n)
+                return "Пожалуйста, активируйте учетную запись"     # O(n)
+            elif v['password'] != password:     # O((len(s))
+                return "Неверный пароль"        # O(n)
+    return "Не удалось найти пользователя"      # O(n)
+
+
+# Cложность - O(1)
+def authentication_о(users, login, password):
+    if users.get(login):    # O(1)
+        if users[login]['password'] == password and users[login]['activation']:     # O(1)
+            return "Доступ разрешен"        # O(n)
+        elif users[login]['password'] == password and not users[login]['activation']:       # O(1)
+            return "Пожалуйста, активируйте учетную запись"     # O(n)
+        elif users[login]['password'] != password:      # O(1)
+            return "Неверный пароль"        # O(n)
+    else:
+        return "Не удалось найти пользователя"      # O(n)
+
+
+users_d = {'user_1': {'password': '1234', 'activation': False},
+           'user_2': {'password': '2345', 'activation': False},
+           'user_3': {'password': '3456', 'activation': True},
+           'user_4': {'password': '4567', 'activation': True},
+           'user_5': {'password': '5678', 'activation': True},
+           'user_6': {'password': '6789', 'activation': False},
+           'user_7': {'password': '7890', 'activation': False}}
+
+print(authentication(users_d, 'user_1', '3456'))
+print(authentication(users_d, 'user_4', '4567'))
+print(authentication_о(users_d, 'user_8', '0000'))
+print(authentication_о(users_d, 'user_2', '2345'))
+
+# Лучший вариант - 2-й, так как функция константная и обладает наименьшей сложностью.
