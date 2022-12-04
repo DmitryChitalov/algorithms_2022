b9675b42d7dd915a4c9294a0a787020778587d68
Krooper <98321262+Krooper@users.noreply.github.com>
2022-12-04 04:41:03 +0300

    made task_1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -18,3 +18,41 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию.
 """
+from random import randint
+from timeit import timeit
+
+
+user_lst = [randint(-100, 100) for i in range(10**3)]
+
+
+# Изначальная функция:
+def bubble_sort(lst_obj):
+    for i in range(len(lst_obj) - 1):
+        for j in range(len(lst_obj) - i - 1):
+            if lst_obj[j] < lst_obj[j + 1]:
+                lst_obj[j], lst_obj[j + 1] = lst_obj[j + 1], lst_obj[j]
+    return lst_obj
+
+
+# Используем маркер
+def bubble_sort_marker(lst_obj):
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(lst_obj) - 1):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+                flag = True
+    return lst_obj
+
+
+print(timeit("bubble_sort(user_lst)", globals=globals(), number=100))
+print(timeit("bubble_sort_marker(user_lst)", globals=globals(), number=100))
+
+"""
+Результаты:
+2.763781599998765
+0.005621000000246568
+
+Использование маркера значительно сокращает время, затрачиваемое на сортировку
+"""
