2daefa56af0fd46ca688dda212cff8d41cab1ec2
StanleyIG <oootehts@gmail.com>
2022-12-08 19:00:22 +0300

    Homework lesson_4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -21,10 +21,11 @@ def recursive_reverse(number):
         return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
 
-
+num_10 = randint(10, 100)
 num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
 num_10000 = randint(100000000, 10000000000000)
+
 
 print('Не оптимизированная функция recursive_reverse')
 print(
@@ -64,6 +65,7 @@ def recursive_reverse_mem(number):
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
+
 print('Оптимизированная функция recursive_reverse_mem')
 print(
     timeit(
@@ -80,3 +82,16 @@ print(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+"""Не оптимизированная функция recursive_reverse
+0.09787700000000002
+0.13466260000000002
+0.23891079999999998
+Оптимизированная функция recursive_reverse_mem
+0.008048599999999961
+0.007974099999999984
+0.007848900000000048
+
+Вывод, при многократном вызове функция c мемоизацией будет доставать готовые значения из словаря,
+в этом случае функция recursive_reverse_mem будет эффективнее, но для одного вызова в мемоизации
+нет необходимости"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -11,16 +11,24 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
+from timeit import timeit, default_timer, Timer
+
+
+n = 512378
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
+        return revers_num
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
-        enter_num //= 10
-        revers(enter_num, revers_num)
+        #enter_num //= 10
+        return revers(enter_num // 10, revers_num)
+
+
+
+print(revers(n))
 
 
 def revers_2(enter_num, revers_num=0):
@@ -30,8 +38,46 @@ def revers_2(enter_num, revers_num=0):
         enter_num //= 10
     return revers_num
 
+print(revers_2(n))
 
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+print(revers_3(n))
+
+
+def revers_4(enter_num):
+    return str(enter_num) if enter_num < 10 else str(enter_num % 10) + revers_4(enter_num // 10)
+
+print(revers_4(n))
+
+
+def revers_5(enter_num, revers_num=0):
+    for i in range(len(str(enter_num))):
+        num = int(enter_num) % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+
+print(revers_5(n))
+
+
+#t1 = timeit('revers(n)', globals=globals(), number=5000)
+#t2 = timeit('revers_2(n)', globals=globals(), number=5000)
+#t3 = timeit('revers_3(n)', globals=globals(), number=5000)
+print(f'функция {revers.__name__} отработала за', timeit('revers(n)', globals=globals() , number=10000), 'сек.')
+print(f'функция {revers_2.__name__} отработала за', timeit('revers_2(n)', globals=globals(), number=10000), 'сек.')
+print(f'функция {revers_3.__name__} отработала за', timeit('revers_3(n)', globals=globals(), number=10000),  'сек.')
+print(f'функция {revers_4.__name__} отработала за', timeit('revers_4(n)', globals=globals(), number=10000), 'сек.')
+print(f'функция {revers_5.__name__} отработала за', timeit('revers_5(n)', globals=globals(), number=10000), 'сек.')
+
+"""функция revers отработала за 0.08397269999999998 сек.
+функция revers_2 отработала за 0.05475260000000004 сек.
+функция revers_3 отработала за 0.018810099999999996 сек.
+функция revers_4 отработала за 0.09747109999999998 сек.
+функция revers_5 отработала за 0.09952729999999999 сек.
+
+Функция reverse_3 с переворотом строки с пмощью среза отработала быстрее всех т.к. имеет константную сложность"""
