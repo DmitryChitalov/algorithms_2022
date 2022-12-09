da1d194bb713a5f635cec51e93c9c5261c2ee1f1
Kouznetsova <aakuz126@mts.ru>
2022-12-09 18:57:09 +0300

    [fix] lesson 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,9 +13,26 @@
 """
 
 
+from timeit import timeit
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+def func_2(nums):
+    new_arr = [i for i, num in enumerate(nums) if num % 2 == 0]
+    return new_arr
+
+
+nums = range(1, 1000)
+
+print(timeit("func_1(nums)", globals=globals(), number=1000))
+print(timeit("func_2(nums)", globals=globals(), number=1000))
+
+# Переделала функцию с использованием list comprehension
+# Сделала 2 замера - один на 1000 запусков первой функции, один на 1000 запусков второй функции
+# У меня получилось, что вторая функция в два раза быстрее отрабатывает
+
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,9 @@
 """
 
 
+from timeit import timeit, repeat, default_timer
+from random import randint
+
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
         return
@@ -34,4 +37,58 @@ def revers_2(enter_num, revers_num=0):
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
-    return revers_num
+    return int(revers_num)
+
+def revers_4(enter_num):
+    enter_num = str(enter_num).split()
+    revers_num = []
+    for i in range(len(enter_num)):
+        revers_num.append(enter_num[-i-1])
+    return int(''.join(revers_num))
+
+def revers_4(enter_num):
+    enter_num = str(enter_num).split()
+    revers_num = []
+    for i in range(len(enter_num)):
+        revers_num.append(enter_num[-i-1])
+    return int(''.join(revers_num))
+
+setup = '''
+from random import randint
+from __main__ import revers, revers_2, revers_3, revers_4
+enter_num = randint(1000000, 10000000)
+'''
+
+
+functions = [
+    'revers(enter_num)',
+    'revers_2(enter_num)',
+    'revers_3(enter_num)',
+    'revers_4(enter_num)'
+    ]
+
+for func in functions:
+    result = repeat(func, setup, default_timer, 3, 1000)
+    print(func[:-11])
+    print(result)
+    print('Берем минимальное число - ' + str(min(result)))
+
+'''
+Мои результаты:
+
+revers
+[0.0019844199996441603, 0.0019714199988811743, 0.0019768689999182243]
+Берем минимальное число - 0.0019714199988811743
+revers_2
+[0.0013375290000112727, 0.0013235500009614043, 0.0013454100007948]
+Берем минимальное число - 0.0013235500009614043
+revers_3
+[0.0005528599976969417, 0.0005148299969732761, 0.0004843400020035915]
+Берем минимальное число - 0.0004843400020035915
+revers_4
+[0.0010304300012649037, 0.0009301399986725301, 0.0009414599990122952]
+Берем минимальное число - 0.000930139998672530
+
+Получается что от самой эффективной функции к наименее эффективной:
+revers_3 -> rever_4 -> revers_2 -> revers
+'''
