4e6dd240d501f8c48e36456ac6cbd9c4ff732309
MK <ballbes@mail.ru>
2022-12-09 15:09:39 +0300

    lesson_4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -1,50 +1,37 @@
 """
 Задание 2.
-
 Приведен код, который формирует из введенного числа
 обратное по порядку входящих в него цифр.
 Задача решена через рекурсию
-Выполнена попытка оптимизировать решение через мемоизацию.
+Выполнена попытка оптимизировать решение мемоизацией
 Сделаны замеры обеих реализаций.
-
 Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
-
 П.С. задание не такое простое, как кажется
 """
 
 from timeit import timeit
 from random import randint
+from cProfile import run
 
 
 def recursive_reverse(number):
+    recursive_reverse.cnt += 1
     if number == 0:
         return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
-
-
+@@ -25,6 +27,11 @@ def recursive_reverse(number):
 num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
 num_10000 = randint(100000000, 10000000000000)
+recursive_reverse.cnt = 0
+
+print(num_100, num_1000, num_10000)
+recursive_reverse(123456789)
+count_not_opt = recursive_reverse.cnt
 
 print('Не оптимизированная функция recursive_reverse')
 print(
-    timeit(
-        "recursive_reverse(num_100)",
-        setup='from __main__ import recursive_reverse, num_100',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_1000)",
-        setup='from __main__ import recursive_reverse, num_1000',
-        number=10000))
-print(
-    timeit(
-        "recursive_reverse(num_10000)",
-        setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
-
-
-def memoize(f):
+@@ -48,22 +55,28 @@ def memoize(f):
     cache = {}
 
     def decorate(*args):
@@ -54,29 +41,40 @@ def memoize(f):
         else:
             cache[args] = f(*args)
             return cache[args]
+
     return decorate
 
 
 @memoize
 def recursive_reverse_mem(number):
+    recursive_reverse_mem.cnt += 1
     if number == 0:
         return ''
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
+recursive_reverse_mem.cnt = 0
+
+recursive_reverse_mem(123456789)
+count_opt = recursive_reverse_mem.cnt
+
 print('Оптимизированная функция recursive_reverse_mem')
 print(
     timeit(
-        'recursive_reverse_mem(num_100)',
-        setup='from __main__ import recursive_reverse_mem, num_100',
-        number=10000))
-print(
-    timeit(
-        'recursive_reverse_mem(num_1000)',
-        setup='from __main__ import recursive_reverse_mem, num_1000',
-        number=10000))
-print(
-    timeit(
+@@ -80,3 +93,16 @@ def recursive_reverse_mem(number):
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+run('recursive_reverse(num_10000)')
+run('recursive_reverse_mem(num_10000)')
+
+print(f'Количество операций recursive_reverse 123456789 {count_not_opt}')
+print(f'Количество операций recursive_reverse_mem для числа 123456789 {count_opt}')
+
+'''
+как можно видеть из результатов профилирования , при мемоизации в отчет модуля timeit и cProfileпопадает только 
+последнее выполнение фунции и значение берется из кэша, поэтому отображатеся такое быстрое время. Если мы введем 
+счетчики выполения функций для числа 123456789 , то каждая функция будет вызвана по 10 раз, т.е. количество операций 
+одинаково. Из этого делаем , что для данной задачи мемоизация не нужна.
+'''
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,37 +1,39 @@
 """
 Задание 3.
-
 Приведен код, формирующий из введенного числа
 обратное по порядку входящих в него
 цифр и вывести на экран.
-
-Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
+Сделайте профилировку каждого алгоритма через timeit
 Обязательно предложите еще свой вариант решения!
-
-Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
+Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
+
+from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return
-    else:
-        num = enter_num % 10
-        revers_num = (revers_num + num / 10) * 10
-        enter_num //= 10
-        revers(enter_num, revers_num)
-
-
-def revers_2(enter_num, revers_num=0):
-    while enter_num != 0:
-        num = enter_num % 10
-        revers_num = (revers_num + num / 10) * 10
-        enter_num //= 10
-    return revers_num
-
-
-def revers_3(enter_num):
+@@ -35,3 +37,23 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join(n for n in reversed(str(enter_num)))
+
+
+num = int(input('Введите число: '))
+t1 = timeit('revers(num)', globals=globals(), number=1000)
+t2 = timeit('revers_2(num)', globals=globals(), number=1000)
+t3 = timeit('revers_3(num)', globals=globals(), number=1000)
+t4 = timeit('revers_4(num)', globals=globals(), number=1000)
+print(f'Время выполнения функции revers  {t1}')
+print(f'Время выполнения функции revers_2  {t2}')
+print(f'Время выполнения функции revers_3 {t3}')
+print(f'Время выполнения функции revers_4  {t4}')
+
+'''
+Как видлно из аналитики функция revers_4 выполняется быстрее остальных за счет использования итератора, особенно ращзница
+будет заметна при итерировании числа большего порядка.
+'''
