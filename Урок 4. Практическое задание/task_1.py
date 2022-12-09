114c5c17460864ff2a9d0dc95769ad658fd9cc3f
Efrem Pavlov <efrempavlov@yandex.ru>
2022-12-09 18:11:52 +0300

    final

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,9 +13,26 @@
 """
 
 
+from timeit import timeit
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [index for index in range(len(nums)) if nums[index] % 2 == 0]
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(timeit("func_1(a)", globals=globals(), number=100000))
+print(timeit("func_2(a)", globals=globals(), number=100000))
+
+"""
+анализ:
+func_2 работает быстрее, т.к. используется list comprehension
+"""
