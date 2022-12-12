74aa2398a4b3d16a8c704e0f0090250baa4033c1
Sergey Mytsykov <smytsykov@awg.ru>
2022-12-12 19:03:44 +0300

    Задание 1 - 2 Лекция 8 Мыцыков Сергей https://t.me/smytsykov

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -13,6 +13,10 @@
 """
 
 
+class AddTreeWrongSideException(Exception):
+    pass
+
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -22,35 +26,54 @@ class BinaryTree:
         # правый потомок
         self.right_child = None
 
+    # если совсем лениво
+    def insert_child(self, new_node):
+        if self.root < new_node:
+            self.insert_right(new_node)
+        else:
+            self.insert_left(new_node)
+
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            # если нужно добавить с другой стороны
+            if self.root < new_node:
+                raise AddTreeWrongSideException
+            # если у узла нет левого потомка
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+        except AddTreeWrongSideException:
+            print(f'Добавление не в ту сторону, корень не должен быть меньше')
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        # если нужно добавить с другой стороны
+        try:
+            if self.root > new_node:
+                raise AddTreeWrongSideException
+            # если у узла нет правого потомка
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+        except AddTreeWrongSideException:
+            print(f'Добавление не в ту сторону, корень не должен быть больше')
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -69,14 +92,14 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+if __name__ == '__main__':
+    r = BinaryTree(10)
+    print(r.get_root_val())
+    print(r.get_left_child())
+    r.insert_left(1)
+    print(r.get_left_child())
+    r.insert_right(30)
+    print(r.get_right_child())
+    print(r.get_right_child().get_root_val())
+    r.get_right_child().set_root_val(15)
+    print(r.get_right_child().get_root_val())
