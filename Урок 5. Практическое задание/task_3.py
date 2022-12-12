6cbf72bf367a7c250e77de79e64e74888d1bd8ab
MushroomovAA <nynblpka@gmail.com>
2022-12-13 00:34:59 +0300

    Lesson4 Gribov Alexey

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,177 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+from collections import deque
+import timeit
+
+nums = 2000
+my_list = []
+my_deque = deque()
+
+
+def list_append():
+    for i in range(nums):
+        my_list.append(i)
+    return my_list
+
+
+list_append()
+
+
+def deque_append():
+    for i in range(nums):
+        my_deque.append(i)
+    return my_deque
+
+
+deque_append()
+
+
+def list_pop():
+    for i in range(nums):
+        my_list.pop()
+    return my_list
+
+
+list_pop()
+
+
+def deque_pop():
+    for i in range(nums):
+        my_deque.pop()
+    return my_deque
+
+
+deque_pop()
+
+
+def list_extend():
+    for i in range(nums):
+        my_list.extend(range(1, 6))
+    return my_list
+
+
+list_extend()
+
+
+def deque_extend():
+    for i in range(nums):
+        my_deque.extend(range(1, 6))
+    return my_deque
+
+
+deque_extend()
+
+print(list_append.__name__, timeit.timeit('list_append()', globals=globals(), number=1000))
+print(deque_append.__name__, timeit.timeit('deque_append()', globals=globals(), number=1000))
+print(list_pop.__name__, timeit.timeit('list_pop()', globals=globals(), number=1000))
+print(deque_pop.__name__, timeit.timeit('deque_pop()', globals=globals(), number=1000))
+print(list_extend.__name__, timeit.timeit('list_extend()', globals=globals(), number=1000))
+print(deque_extend.__name__, timeit.timeit('deque_extend()', globals=globals(), number=1000))
+
+'''list_append 0.43569786800071597
+deque_append 0.24264599103480577
+list_pop 0.2082758799660951
+deque_pop 0.21727060014382005
+list_extend 1.5429263319820166
+deque_extend 0.8700296960305423'''
+
+my_list2 = []
+my_deque2 = deque()
+
+
+def list_insert():
+    for i in range(nums):
+        my_list2.insert(0, i)
+    return my_list2
+
+
+list_insert()
+
+
+def deque_appendleft():
+    for i in range(nums):
+        my_deque2.appendleft(i)
+    return my_deque2
+
+
+deque_appendleft()
+
+
+def list_popleft():
+    for i in range(nums):
+        my_list2.pop(0)
+    return my_list2
+
+
+list_popleft()
+
+
+def deque_popleft():
+    for i in range(nums):
+        my_deque2.popleft()
+    return my_deque2
+
+
+deque_popleft()
+
+
+def list_extendleft():
+    for i in range(nums):
+        my_list2.insert(0, range(1, 6))
+    return my_deque2
+
+
+list_extendleft()
+
+
+def deque_extendleft():
+    for i in range(nums):
+        my_deque2.extendleft(range(1, 6))
+    return my_deque2
+
+
+deque_extendleft()
+
+print(list_insert.__name__, timeit.timeit('list_insert()', globals=globals(), number=10))
+print(deque_appendleft.__name__, timeit.timeit('deque_appendleft()', globals=globals(), number=10))
+print(list_popleft.__name__, timeit.timeit('list_popleft()', globals=globals(), number=10))
+print(deque_popleft.__name__, timeit.timeit('deque_popleft()', globals=globals(), number=10))
+print(list_extendleft.__name__, timeit.timeit('list_extendleft()', globals=globals(), number=10))
+print(deque_extendleft.__name__, timeit.timeit('deque_extendleft()', globals=globals(), number=10))
+'''
+list_insert 0.18331923009827733
+deque_appendleft 0.0018961362075060606
+list_popleft 0.10272084106691182
+deque_popleft 0.0019028079695999622
+list_extendleft 0.19258438516408205
+deque_extendleft 0.008560584159567952'''
+
+
+def list_take():
+    for i in range(nums):
+        my_list2[i] = i
+    return my_list2
+
+
+list_take()
+
+
+def deque_take():
+    for i in range(nums):
+        my_deque2[i] = i
+    return my_deque2
+
+
+deque_take()
+
+print(list_take.__name__, timeit.timeit('list_take()', globals=globals(), number=10))
+print(deque_take.__name__, timeit.timeit('deque_take()', globals=globals(), number=10))
+
+'''list_take 0.001547698862850666
+deque_take 0.0023133379872888327'''
+'''
+Во всех операциях, кроме получения значения из списка, функции с использованием deque быстрее.
+Это объясняется тем, что  deque двусвязанный список, а для поиска по индексу deque приходится выполнять итерацию,
+поэтому получения элемента из списка с использованием функций с deque занимает больше времени.
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -7,3 +7,59 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях
 """
+from collections import OrderedDict
+from timeit import timeit
+
+my_dict = {}
+my_ordereddict = OrderedDict()
+nums = 200000
+
+
+def dict_append():
+    for i in range(nums):
+        my_dict[i] = i
+    return my_dict
+
+
+dict_append()
+
+
+def ordereddict_append():
+    for i in range(nums):
+        my_ordereddict[i] = i
+    return my_ordereddict
+
+
+ordereddict_append()
+
+
+def dict_pop():
+    for i in range(200000):
+        my_dict.pop(i)
+    return my_dict
+
+
+dict_pop()
+
+
+def ordereddict_pop():
+    for i in range(200000):
+        my_ordereddict.pop(i)
+    return my_ordereddict
+
+
+ordereddict_pop()
+print(dict_append.__name__, timeit('dict_append()', globals=globals(), number=10))
+print(ordereddict_append.__name__, timeit('ordereddict_append()', globals=globals(), number=10))
+print(dict_pop.__name__, timeit('dict_pop()', globals=globals(), number=1))
+print(ordereddict_pop.__name__, timeit('ordereddict_pop()', globals=globals(), number=1))
+
+'''
+В новых версиях Python OrderedDict стал неактуальным, потому что словари в Python стали упорядочеными,
+так же OrderedDict занимает большее время на выполнение операций. 
+У OrderedDict есть два метода которые могут быть полезными:
+1) popitem() возвращает и удаляет пару ключ-значение. Ключ-значение возвращаются в порядке LIFO, если аргумент last=True
+или в порядке FIFO, если last=False.
+2) move_to_end() перемещает существующий ключ в начало или конец упорядоченного словаря в зависимости
+ от аргумента last=True или last=False.
+'''
