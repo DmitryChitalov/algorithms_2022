a69cbae216265651da06466e0c677b5b4971d13a
Andria_S <andreisiradze@gmail.com>
2022-12-12 13:56:29 +0400

    lesson 7 done

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -8,10 +8,64 @@
 которые не меньше медианы,
 в другой – не больше медианы.
 
-Решите задачу тремя способами:
+Решиоте задачу тремя спсобами:
 
 1) с помощью сортировки, которую мы не рассматривали на уроке (Гномья, Шелла,
 Кучей)
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+import random
+from timeit import timeit
+
+
+def shella_sort(array):
+    gap = len(array) // 2
+
+    while gap:
+        for idx in range(gap, len(array)):
+            current_val = array[idx]
+            possition = idx
+
+            while possition >= gap and array[possition - gap] > current_val:
+                array[possition] = array[possition - gap]
+                possition -= gap
+                array[possition] = current_val
+
+        gap //= 2
+
+    return array
+
+
+def find_median(array, m):
+    shella_sort(array)
+    return array[m]
+
+
+if __name__ == '__main__':
+    m = 5  # 10 elems
+    m2 = 50  # 100 elems
+    m3 = 500  # 1000 elems
+    #
+    a = [random.randint(1, 100) for x in range(2 * m + 1)]
+    a2 = [random.randint(1, 100) for x in range(2 * m2 + 1)]
+    a3 = [random.randint(1, 100) for x in range(2 * m3 + 1)]
+
+    print('10')
+    print(
+        timeit(
+            "find_median(a, m)",
+            globals=globals(),
+            number=1000))
+    print('100')
+    print(
+        timeit(
+            "find_median(a2, m2)",
+            globals=globals(),
+            number=1000))
+    print('1000')
+    print(
+        timeit(
+            "find_median(a3, m3)",
+            globals=globals(),
+            number=1000))
--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,43 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+from random import randint
+from statistics import median
+from timeit import timeit
+
+m = 5  # 10 elems
+m2 = 50  # 100 elems
+m3 = 500  # 1000 elems
+#
+a = [randint(1, 100) for x in range(2 * m + 1)]
+a2 = [randint(1, 100) for x in range(2 * m2 + 1)]
+a3 = [randint(1, 100) for x in range(2 * m3 + 1)]
+
+print(median(a))
+print(median(a2))
+print(median(a3))
+
+print('10')
+print(
+    timeit(
+        "median(a[:])",
+        globals=globals(),
+        number=1000))
+print('100')
+print(
+    timeit(
+        "median(a2[:])",
+        globals=globals(),
+        number=1000))
+print('1000')
+print(
+    timeit(
+        "median(a3[:])",
+        globals=globals(),
+        number=1000))
+
+
+"""
+Встроненный способ оказался быстрее всех как и следовало ожидать 
+
+"""
