74d9a85faf89a7275a8429f6ac877cdb700ef1fc
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2022-12-16 13:58:59 +0300

    Update task_1_1.py

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -30,3 +30,101 @@
 
 Это файл для первого скрипта
 """
+from memory_profiler import memory_usage
+
+
+def decor(func):
+    def wrapper():
+        m1 = memory_usage()
+        res = func()
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
+    return wrapper
+
+
+# Исходное решение - задание 1_2 из курса 'Основы языка Python'
+@decor
+def func():
+    # Создать список, состоящий из кубов нечётных чисел от 1 до 1000
+    odd_numb_cube = []
+    for idx in range(0, 1001):
+        if idx % 2:
+            odd_numb_cube.append(idx ** 3)
+    print("Список, состоящий из кубов нечетных чисел от 1 до 1000:\n", odd_numb_cube)
+    # Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7
+    sum_of_numbers = 0
+    for numb in odd_numb_cube:
+        member = numb
+        sum_of_digits = 0
+        while member:
+            sum_of_digits += member % 10
+            member //= 10
+        if not sum_of_digits % 7:
+            sum_of_numbers += numb
+    print("Сумма чисел из списка, сумма цифр которых делится нацело на 7:", sum_of_numbers)
+    # К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого
+    # списка, сумма цифр которых делится нацело на 7.
+    sum_of_numbers = 0
+    for numb in odd_numb_cube:
+        numb += 17
+        member = numb
+        sum_of_digits = 0
+        while member:
+            sum_of_digits += member % 10
+            member //= 10
+        if not sum_of_digits % 7:
+            sum_of_numbers += numb
+    print("Сумма чисел из списка, сумма цифр которых делится нацело на 7, после добавления 17 "
+          "к каждому элементу списка:", sum_of_numbers)
+
+
+# Оптимизированное решение
+@decor
+def func2():
+    # Создать список, состоящий из кубов нечётных чисел от 1 до 1000
+    odd_numb_cube = [x ** 3 for x in filter(lambda x: x % 2, range(0, 1001))]
+    # Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7
+    sum_of_numbers = 0
+    for numb in odd_numb_cube:
+        member = numb
+        sum_of_digits = 0
+        while member:
+            sum_of_digits += member % 10
+            member //= 10
+        del member
+        if not sum_of_digits % 7:
+            sum_of_numbers += numb
+        del sum_of_digits
+    # К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого
+    # списка, сумма цифр которых делится нацело на 7.
+    sum_of_numbers_2 = 0
+    for numb in odd_numb_cube:
+        numb += 17
+        member = numb
+        sum_of_digits = 0
+        while member:
+            sum_of_digits += member % 10
+            member //= 10
+        del member
+        if not sum_of_digits % 7:
+            sum_of_numbers_2 += numb
+        del sum_of_digits
+    return f"Список, состоящий из кубов нечетных чисел от 1 до 1000:\n {odd_numb_cube}\n" \
+           f"Сумма чисел из списка, сумма цифр которых делится нацело на 7: {sum_of_numbers}\n" \
+           f"Сумма чисел из списка, сумма цифр которых делится нацело на 7, после добавления 17 " \
+           f"к каждому элементу списка: {sum_of_numbers_2}"
+
+
+if __name__ == '__main__':
+    print('Исходное решение')
+    res1, mem_dif = func()
+    print(f'Выполнение исходного решения заняло {mem_dif} Mib')  # 0.015625 Mib
+    print('\nОптимизированное решение')
+    res2, mem_dif = func2()
+    print(res2)
+    print(f'Выполнение оптимизированного решения заняло {mem_dif} Mib')  # 0.00390625 Mib
+
+'''С целью оптимизации памяти для создания списка, состоящего из кубов нечётных чисел от 1 до 1000 вместо цикла 
+был использован List Comprehensions. Также для возврата результата были использованы f-строки. Всё это привело
+к оптимизации памяти''
