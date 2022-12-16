91d0571b16250af22e059b03dd892bf014d1b3b8
Efrem Pavlov <contact@efrempavlov.ru>
2022-12-16 19:34:37 +0300

    final

--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,49 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+
+import statistics
+from random import randint
+from timeit import timeit
+
+# замер 10:  0.0006606000024476089
+m = 5
+my_array = [randint(1, 100) for _ in range(2 * m + 1)]
+print('исходный массив:', my_array)
+
+print(
+    timeit(
+        "statistics.median(my_array)",
+        globals=globals(),
+        number=1000))
+
+print(statistics.median(my_array))
+
+# замер 100:  0.004127400003199
+m = 50
+my_array = [randint(1, 100) for _ in range(2 * m + 1)]
+print('исходный массив:', my_array)
+
+print(
+    timeit(
+        "statistics.median(my_array)",
+        globals=globals(),
+        number=1000))
+
+print(statistics.median(my_array))
+
+# замер 1000:  0.08094320000236621
+m = 500
+my_array = [randint(1, 100) for _ in range(2 * m + 1)]
+print('исходный массив:', my_array)
+
+print(
+    timeit(
+        "statistics.median(my_array)",
+        globals=globals(),
+        number=1000))
+
+print(statistics.median(my_array))
+
+# Самый эффективный по времени оказался способ с использованием встроенной функции median(),
+# способ без сортировки на втором месте, способ с сортировкой самый медленный.
