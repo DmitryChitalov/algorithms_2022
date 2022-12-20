395391f0cb7b9cd8c038eea3f4488e6b07d4481e
StanleyIG <oootehts@gmail.com>
2022-12-21 00:37:30 +0300

    HW7

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,63 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+def sort_func(lst):
+    res = lst
+    l_lst = []
+    r_lst = []
+    for i in range(len(res)):
+        for j in range(len(res)):
+            if res[i] > res[j]:
+                l_lst.append(res[j])
+            if res[i] < res[j]:
+                r_lst.append(res[j])
+            if res[i] == res[j] and i > j:
+                l_lst.append(res[j])
+            if res[i] == res[j] and i < j:
+                r_lst.append(res[j])
+        if len(l_lst) == len(r_lst):
+            return res[i]
+        l_lst.clear()
+        r_lst.clear()
+
+
+
+m = 10
+lst = [randint(0, 100) for i in range(2 * m + 1)]
+
+print(
+    timeit(
+        "sort_funct(lst[:])",
+        globals=globals(),
+        number=100))
+
+# ---------------------------------------------------------------------------
+m = 100
+lst = [randint(0, 100) for i in range(2 * m + 1)]
+
+print(
+    timeit(
+        "sort_func(lst[:])",
+        globals=globals(),
+        number=100))
+
+# ---------------------------------------------------------------------------
+m = 1000
+lst = [randint(0, 100) for i in range(2 * m + 1)]
+
+print(
+    timeit(
+        "sort_func(lst[:])",
+        globals=globals(),
+        number=100))
+# ---------------------------------------------------------------------------
+"""
+0.029642699999999994 10 элементов
+3.7271031            100 элементов
+320.855488543        1000 элементов
+"""
+
--- Урок 7. Практическое задание/task_2/task_2_3.py
+++ Урок 7. Практическое задание/task_2/task_2_3.py
@@ -16,3 +16,49 @@
 
 В конце сделайте аналитику какой трех из способов оказался эффективнее
 """
+from random import randint
+from timeit import timeit
+from statistics import median
+
+
+def mediana(lst):
+    return median(lst)
+
+
+
+m = 10
+lst = [randint(0, 100) for i in range(2 * m + 1)]
+print(
+    timeit(
+        "mediana(lst[:])",
+        globals=globals(),
+        number=100))
+
+
+m = 100
+lst = [randint(0, 100) for i in range(2 * m + 1)]
+
+print(
+    timeit(
+        "mediana(lst[:])",
+        globals=globals(),
+        number=100))
+
+
+m = 1000
+lst = [randint(0, 100) for i in range(2 * m + 1)]
+print(
+    timeit(
+        "mediana(lst[:])",
+        globals=globals(),
+        number=100))
+
+
+"""
+0.0004943999999999782 10 элементов
+0.004067300000000024  100 элементов
+0.049219400000000024  1000 элементов
+
+Вывод: самой быстрой оказаласб встроенная функция median() для поиска медианы из модуля
+statistics 
+"""
