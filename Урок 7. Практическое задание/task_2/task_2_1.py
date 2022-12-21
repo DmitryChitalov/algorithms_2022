ae2c19f940510977bade36f98f54ee901552c5d3
MushroomovAA <nynblpka@gmail.com>
2022-12-21 18:47:23 +0300

    Lesson7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -15,3 +15,57 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+
+# m = 3
+# my_list = [randint(-100,100) for i in range(2 * m + 1)]
+# print(my_list)
+# my_list2 = sorted(my_list)
+# print(my_list2)
+# print(my_list2[m])
+
+def dwarf_sort(lst_obj, num):
+
+    i = 1
+    while i < len(lst_obj):
+        if lst_obj[i - 1] <= lst_obj[i]:
+            i += 1
+        else:
+            lst_obj[i - 1], lst_obj[i] = lst_obj[i], lst_obj[i - 1]
+            if i > 1:
+                i -= 1
+
+    return f'Oтсортированный список {lst_obj}' \
+           f' Медиана списка {lst_obj[num]}'
+
+
+m = 7
+my_list = [randint(-100, 100) for i in range(2 * m + 1)]
+print(dwarf_sort(my_list, m))
+
+my_list10 = [randint(-100, 100) for x in range(10)]
+print('Длина массива 10 элементов', timeit(
+        "dwarf_sort(my_list10[:], m)",
+        globals=globals(),
+        number=100))
+
+
+my_list100 = [randint(-100, 100) for _ in range(100)]
+print('Длина массива 100 элементов', timeit(
+        "dwarf_sort(my_list100[:], m)",
+        globals=globals(),
+        number=100))
+
+my_list1000 = [randint(-100, 100) for j in range(1000)]
+print('Длина массива 1000 элементов', timeit(
+        "dwarf_sort(my_list1000[:], m)",
+        globals=globals(),
+        number=100))
+
+'''
+Длина массива 10 элементов 0.0016113229794427752
+Длина массива 100 элементов 0.24784921703394502
+Длина массива 1000 элементов 19.009888597996905
+'''
--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,41 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+from random import randint
+from timeit import timeit
+
+def median(lists, num):
+    for i in range(num):
+        lists.remove(max(lists))
+    return max(lists)
+
+
+m = 5
+my_list10 = [randint(-100, 100) for i in range(2 * m + 1)]
+
+print('Длина массива 10 элементов', timeit(
+        "median(my_list10[:], m)",
+        globals=globals(),
+        number=100))
+
+m2 = 50
+my_list100 = [randint(-100, 100) for _ in range(2 * m2 + 1)]
+
+print('Длина массива 100 элементов', timeit(
+        "median(my_list100[:], m)",
+        globals=globals(),
+        number=100))
+
+m3 = 500
+my_list1000 = [randint(-100, 100) for _ in range(2 * m3 + 1)]
+
+print('Длина массива 1000 элементов', timeit(
+        "median(my_list1000[:], m)",
+        globals=globals(),
+        number=100))
+
+'''
+Длина массива 10 элементов 0.00032534392084926367
+Длина массива 100 элементов 0.001874060952104628
+Длина массива 1000 элементов 0.01595569192431867
+'''
