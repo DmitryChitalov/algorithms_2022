c27aadbec63755301e9b147b18346ac983b05b21
Sanuamba3 <aslaptev.hse@gmail.com>
2022-12-21 13:27:07 +0300

    HW Lesson 6 Alexandr Laptev

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,28 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+from random import randint
+from timeit import timeit
+
+
+def median(m):
+    arr = [randint(0, m) for i in range(2 * m + 1)]
+    count = 0
+    arr_copy = arr[:]
+    while count < m:
+        arr_copy.remove(max(arr_copy))
+        count += 1
+    return arr, max(arr_copy)
+
+
+print(timeit('median(5)', globals=globals(), number=1000))
+print(timeit('median(50)', globals=globals(), number=1000))
+print(timeit('median(500)', globals=globals(), number=1000))
+
+"""
+Результаты:
+0.007597652000185917
+0.11898065700006555
+7.082830861000275
+"""
