10ee57a68433ce5c8587d0226c73bef74e5a903d
MK <ballbes@mail.ru>
2022-12-21 17:02:43 +0300

    lesson7

--- Урок 7. Практическое задание/task_2/task_2_1.py
+++ Урок 7. Практическое задание/task_2/task_2_1.py
@@ -13,4 +13,62 @@
 Кучей)
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
+
+
+Гномья сортировка
 """
+
+from random import randint
+from timeit import timeit
+
+
+def dwarf_sort(lst_obj):
+    i=1
+    while i < len(lst_obj):
+        if lst_obj[i-1] <= lst_obj[i]:
+            i+=1
+        else:
+            lst_obj[i-1], lst_obj[i] = lst_obj[i], lst_obj[i-1]
+            if i > 1:
+                i-= 1
+
+    return lst_obj
+
+m = 5
+orig_list = [randint(1, 9) for _ in range(2*m+1)]
+sort_list = dwarf_sort(orig_list[:])
+print ('Медиана: ' , sort_list, sort_list[m],sep='\n')
+
+
+
+# замеры 10
+
+print(orig_list,dwarf_sort(orig_list[:]),
+    timeit(
+        "dwarf_sort(orig_list[:])",
+        globals=globals(),
+        number=100),sep='\n')
+
+
+orig_list = [randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+
+print(orig_list,dwarf_sort(orig_list[:]),
+    timeit(
+        "dwarf_sort(orig_list[:])",
+        globals=globals(),
+        number=100) ,sep='\n')
+
+
+orig_list = [randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+
+print(orig_list,dwarf_sort(orig_list[:]),
+    timeit(
+        "dwarf_sort(orig_list[:])",
+        globals=globals(),
+        number=100),sep='\n' )
+
+
