459b9bf3719bbdc954e3f1447dfd455c4cf5f6b3
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2022-12-23 18:13:45 +0300

    Update task_2.py

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -12,6 +12,12 @@
 протестируйте на реальных данных - на клиентском коде
 """
 
+class MyErr(Exception):
+
+    def __init__(self, text):
+        self.text = text
+
+
 
 class BinaryTree:
     def __init__(self, root_obj):
@@ -21,6 +27,12 @@ class BinaryTree:
         self.left_child = None
         # правый потомок
         self.right_child = None
+
+    # отобразить дерево в понятном виде
+    def __str__(self):
+        left = self.left_child.root if self.left_child is not None else None
+        right = self.right_child.root if self.right_child is not None else None
+        return f'Узел {self.root} - левый потомок - {left} правый потомок - {right}'
 
     # добавить левого потомка
     def insert_left(self, new_node):
@@ -36,6 +48,17 @@ class BinaryTree:
             # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.left_child = self.left_child
             self.left_child = tree_obj
+        try:
+            if new_node > self.root:
+                raise MyErr('Вставляемое значение не может быть больше корня!')
+            if self.left_child == None:
+                self.left_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+        except MyErr as err:
+            print(err)
 
     # добавить правого потомка
     def insert_right(self, new_node):
@@ -51,21 +74,21 @@ class BinaryTree:
             # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.right_child = self.right_child
             self.right_child = tree_obj
+        try:
+            if new_node < self.root:
+                raise MyErr('Вставляемое значение не может быть меньше корня!')
+            if self.right_child == None:
+                self.right_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+        except MyErr as err:
+            print(err)
 
     # метод доступа к правому потомку
     def get_right_child(self):
-        return self.right_child
-
-    # метод доступа к левому потомку
-    def get_left_child(self):
-        return self.left_child
-
-    # метод установки корня
-    def set_root_val(self, obj):
-        self.root = obj
-
-    # метод доступа к корню
-    def get_root_val(self):
+@@ -69,14 +76,10 @@ def get_root_val(self):
         return self.root
 
 
@@ -80,3 +103,10 @@ print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
 print(r.get_right_child().get_root_val())
+tree = BinaryTree(30)
+tree.insert_right(23)
+tree.insert_right(10)
+tree.insert_left(54)
+tree.insert_right(45)
+tree.insert_left(12)
+print(tree)
