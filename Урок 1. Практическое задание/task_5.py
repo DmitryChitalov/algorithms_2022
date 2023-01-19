3290cae1e9231f5563f3cc60ee4069004920b552
StanleyIG <oootehts@gmail.com>
2023-01-19 14:34:51 +0300

    исправил 1.5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,74 +1,48 @@
-"""
-Задание 5. На закрепление навыков работы со стеком
-
-Реализуйте собственный класс-структуру "стопка тарелок".
-
-Мы можем складывать тарелки в стопку и при превышении некоторого значения
-нужно начать складывать тарелки в новую стопку.
-
-Структура должна предусматривать наличие нескольких стопок.
-Создание новой стопки происходит при достижении предыдущим
-стеком порогового значения.
-
-После реализации структуры, проверьте ее работу на различных сценариях.
-
-Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
---реализуйте по аналогии с примером, рассмотренным на уроке
---создание нового стопки можно реализовать добавлением нового пустого массива
-в массив стопок (lst = [[], [], [], [],....]).
-"""
-
-
 class StackClass:
     def __init__(self):
-        self.elems = []
+        self.elems = [[]]
+
+    def __str__(self):
+        return str(self.elems)
 
     def is_empty(self):
         return self.elems == []
 
-    def push_stack(self, el, n):
-        self.elems.append([])
-        for i in range(len(self.elems)):
-            if len(self.elems[i]) < n:
-                self.elems[i].append(el)
-
-
-
-    def push_in(self):
-        self.elems.append(el)
-
+    def push_in(self, el, n):
+        if len(self.elems[len(self.elems) - 1]) < n:
+            self.elems[len(self.elems) - 1].append(el)
+        else:
+            self.elems.append([])
+            self.elems[len(self.elems) - 1].append(el)
 
     def pop_out(self):
-        return self.elems.pop()
+        result = self.elems[len(self.elems) - 1].pop()
+        if len(self.elems[len(self.elems) - 1]) == 0:
+            self.elems.pop()
+        return result
 
     def get_val(self):
         return self.elems[len(self.elems) - 1]
 
     def stack_size(self):
+        elem_sum = 0
+        for stack in self.elems:
+            elem_sum += len(stack)
+        return elem_sum
+
+    def stack_count(self):
         return len(self.elems)
 
-    def stopka(self):
-        if self.stack_size() > 2:
-            self.elems.append(self.pop_out())
 
 
-
-SC_OBJ = StackClass()
 
 def divide_by_two(dec_number):
     sc_obj = StackClass()
 
     while dec_number > 0:
         res = dec_number % 2
-        sc_obj.push_stack(res, 4)
+        sc_obj.push_in(res, 2)
         dec_number = dec_number // 2
-
-
-    temp_lst = []
-    while not sc_obj.is_empty():
-        temp_lst.append(sc_obj.pop_out())
-    return temp_lst
-
-
+    return sc_obj
 
 print(divide_by_two(233))
